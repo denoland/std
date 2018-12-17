6d307aaec2b621927dd9f94c15a757ba0639bf9b
Ryan Dahl <ry@tinyclouds.org>
2018-12-17 11:40:43 -0500

    Add link

--- http_test.ts
+++ http_test.ts
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Ported from 
+// https://github.com/golang/go/blob/master/src/net/http/responsewrite_test.go
+
 import {
     test,
     assert,
