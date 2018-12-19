0e09af4e1f7aabc8a24da46328df7101d44596d7
zhmushan <zhmushan@qq.com>
2018-12-19 08:14:13 +0800

    migrate path to deno_std

---
+++ path/test/index.ts
@@ -0,0 +1,8 @@
+import './test-path-basename'
+import './test-path-dirname'
+import './test-path-extname'
+import './test-path-isabsolute'
+import './test-path-join'
+import './test-path-relative'
+import './test-path-resolve'
+import './test-path-zero-length-strings'
