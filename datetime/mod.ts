8a5c6b4f6b0cacc23b6a9f25e5d6ab560b17b092
sh7dm <d3dx12.xx@gmail.com>
2019-01-03 17:13:21 +0300

    Minor refactor

--- datetime/mod.ts
+++ datetime/mod.ts
@@ -1,56 +1,52 @@
-export const parseDate = (date, format: string) => {
+export const parseDate = (dateStr: string, format: string): Date => {
   let m, d, y: string;
 
   if (format === "mm-dd-yyyy") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4})$/;
-    [ , m, d, y] = datePattern.exec(date);
+    [, m, d, y] = datePattern.exec(dateStr);
   } else if (format === "dd-mm-yyyy") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4})$/;
-    [ , d, m, y] = datePattern.exec(date);
+    [, d, m, y] = datePattern.exec(dateStr);
   } else if (format === "yyyy-mm-dd") {
     const datePattern = /^(\d{4})-(\d{2})-(\d{2})$/;
-    [ , y, m, d] = datePattern.exec(date);
+    [, y, m, d] = datePattern.exec(dateStr);
   }
 
-  return {
-    year: Number(y),
-    month: Number(m),
-    day: Number(d)
-  }
-}
+  return new Date(Number(y), Number(m) - 1, Number(d));
+};
 
-export const parseDateTime = (datetime, format: string) => {
+export const parseDateTime = (datetimeStr: string, format: string): Date => {
   let m, d, y, ho, mi: string;
 
   if (format === "mm-dd-yyyy hh:mm") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})$/;
-    [ , m, d, y, ho, mi] = datePattern.exec(datetime);
+    [, m, d, y, ho, mi] = datePattern.exec(datetimeStr);
   } else if (format === "dd-mm-yyyy hh:mm") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})$/;
-    [ , d, m, y, ho, mi] = datePattern.exec(datetime);
+    [, d, m, y, ho, mi] = datePattern.exec(datetimeStr);
   } else if (format === "yyyy-mm-dd hh:mm") {
     const datePattern = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})$/;
-    [ , y, m, d, ho, mi] = datePattern.exec(datetime);
+    [, y, m, d, ho, mi] = datePattern.exec(datetimeStr);
   } else if (format === "hh:mm mm-dd-yyyy") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})$/;
-    [ , ho, mi, m, d, y] = datePattern.exec(datetime);
+    [, ho, mi, m, d, y] = datePattern.exec(datetimeStr);
   } else if (format === "hh:mm dd-mm-yyyy") {
     const datePattern = /^(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})$/;
-    [ , ho, mi, d, m, y] = datePattern.exec(datetime);
+    [, ho, mi, d, m, y] = datePattern.exec(datetimeStr);
   } else if (format === "hh:mm yyyy-mm-dd") {
     const datePattern = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})$/;
-    [ , ho, mi, y, m, d] = datePattern.exec(datetime);
+    [, ho, mi, y, m, d] = datePattern.exec(datetimeStr);
   }
 
-  return {
-    year: Number(y),
-    month: Number(m),
-    day: Number(d),
-    hours: Number(ho),
-    minutes: Number(mi)
-  }
-}
+  return new Date(Number(y), Number(m) - 1, Number(d), Number(ho), Number(mi));
+};
 
-export const currentDayOfYear = () => Math.floor(
-  (new Date().getMilliseconds() - new Date(new Date().getFullYear(), 0, 0).getMilliseconds()) / 1000 / 60 / 60 / 24
-);
+export const currentDayOfYear = () =>
+  Math.floor(
+    (new Date().getMilliseconds() -
+      new Date(new Date().getFullYear(), 0, 0).getMilliseconds()) /
+      1000 /
+      60 /
+      60 /
+      24
+  );
