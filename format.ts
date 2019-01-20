1d8a24856cb8ccbc68e10e7d8e998bad06fd2164
木杉 <zhmushan@qq.com>
2019-01-20 20:04:38 +0800

    fix format

--- format.ts
+++ format.ts
@@ -30,7 +30,7 @@ async function main() {
     args: [
       "bash",
       "-c",
-      "prettier --write *.ts */*.ts */**/*.ts *.md */*.md */**/*.md"
+      "prettier --write \"**/*.ts\" \"**/*.md\""
     ]
   });
   const s = await prettier.status();
