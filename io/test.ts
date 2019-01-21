b90365d599d40bce980ad6cda641814bd122b3ed
MaxGraey <maxgraey@gmail.com>
2019-01-22 01:58:31 +0200

    use assert.strictEqual

--- io/test.ts
+++ io/test.ts
@@ -1,4 +1,4 @@
-import { test, assert, assertEqual } from "../testing/mod.ts";
+import { test, assert } from "../testing/mod.ts";
 import { copyBytes } from "./util";
 
 test(function testCopyBytes() {
@@ -8,29 +8,29 @@ test(function testCopyBytes() {
   let src = Uint8Array.of(1, 2);
   let len = copyBytes(dst, src, 0);
   assert(len === 2);
-  assertEqual(dst, Uint8Array.of(1, 2, 0, 0));
+  assert.strictEqual(dst, Uint8Array.of(1, 2, 0, 0));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2);
   len = copyBytes(dst, src, 1);
   assert(len === 2);
-  assertEqual(dst, Uint8Array.of(0, 1, 2, 0));
+  assert.strictEqual(dst, Uint8Array.of(0, 1, 2, 0));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2, 3, 4, 5);
   len = copyBytes(dst, src);
   assert(len === 4);
-  assertEqual(dst, Uint8Array.of(1, 2, 3, 4));
+  assert.strictEqual(dst, Uint8Array.of(1, 2, 3, 4));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2);
   len = copyBytes(dst, src, 100);
   assert(len === 0);
-  assertEqual(dst, Uint8Array.of(0, 0, 0, 0));
+  assert.strictEqual(dst, Uint8Array.of(0, 0, 0, 0));
 
   dst.fill(0);
   src = Uint8Array.of(3, 4);
   len = copyBytes(dst, src, -2);
   assert(len === 2);
-  assertEqual(dst, Uint8Array.of(3, 4, 0, 0));
+  assert.strictEqual(dst, Uint8Array.of(3, 4, 0, 0));
 });
