0518776bcb2d74123aee999b170a50ce72d389aa
MaxGraey <maxgraey@gmail.com>
2019-01-22 02:09:59 +0200

    fallback to assert.equal

--- io/util_test.ts
+++ io/util_test.ts
@@ -8,29 +8,29 @@ test(function testCopyBytes() {
   let src = Uint8Array.of(1, 2);
   let len = copyBytes(dst, src, 0);
   assert(len === 2);
-  assert.strictEqual(dst, Uint8Array.of(1, 2, 0, 0));
+  assert.equal(dst, Uint8Array.of(1, 2, 0, 0));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2);
   len = copyBytes(dst, src, 1);
   assert(len === 2);
-  assert.strictEqual(dst, Uint8Array.of(0, 1, 2, 0));
+  assert.equal(dst, Uint8Array.of(0, 1, 2, 0));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2, 3, 4, 5);
   len = copyBytes(dst, src);
   assert(len === 4);
-  assert.strictEqual(dst, Uint8Array.of(1, 2, 3, 4));
+  assert.equal(dst, Uint8Array.of(1, 2, 3, 4));
 
   dst.fill(0);
   src = Uint8Array.of(1, 2);
   len = copyBytes(dst, src, 100);
   assert(len === 0);
-  assert.strictEqual(dst, Uint8Array.of(0, 0, 0, 0));
+  assert.equal(dst, Uint8Array.of(0, 0, 0, 0));
 
   dst.fill(0);
   src = Uint8Array.of(3, 4);
   len = copyBytes(dst, src, -2);
   assert(len === 2);
-  assert.strictEqual(dst, Uint8Array.of(3, 4, 0, 0));
+  assert.equal(dst, Uint8Array.of(3, 4, 0, 0));
 });
