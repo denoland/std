72ed92b7ad103dd5769e5b29809bff6cce5fd7ae
zhmushan <zhmushan@qq.com>
2019-01-31 17:56:27 +0800

    examples: fix test

--- examples/test.ts
+++ examples/test.ts
@@ -13,7 +13,7 @@ test(function t2() {
 /** A more complicated test that runs a subprocess. */
 test(async function catSmoke() {
   const p = run({
-    args: ["deno", "examples/cat.ts", "README.md"],
+    args: ["deno", "https://deno.land/x/examples/cat.ts", "README.md"],
     stdout: "piped"
   });
   const s = await p.status();
