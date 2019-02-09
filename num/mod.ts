75096fb4c0c70cf5ce93afeaa7a97085deddbfdb
jbreckmckye <jimmy@breck-mckye.com>
2019-02-09 23:36:54 +0000

    Prettify

--- num/mod.ts
+++ num/mod.ts
@@ -1,10 +1,10 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 export interface ClampConfig {
-    min?: number,
-    max?: number
+  min?: number;
+  max?: number;
 }
 
- /**
+/**
  * Clamps a number between a minimum and maximum value
  * @param {number} number - The input number
  * @param {Object} config - A ClampConfig
@@ -12,31 +12,29 @@ export interface ClampConfig {
  * @param {number} [max] - The maximum value. Defaults to positive infinity.
  */
 export function clamp(number: number, config: ClampConfig): number {
-    let {max, min} = config;
+  let { max, min } = config;
 
-    if (max === undefined) {
-        max = Number.POSITIVE_INFINITY;
-    }
+  if (max === undefined) {
+    max = Number.POSITIVE_INFINITY;
+  }
 
-    if (min === undefined) {
-        min = Number.NEGATIVE_INFINITY;
-    }
+  if (min === undefined) {
+    min = Number.NEGATIVE_INFINITY;
+  }
 
-    if (max < min || (max === min && Object.is(max, -0))) {
-        let tmp = max;
-        max = min;
-        min = tmp;
-    }
+  if (max < min || (max === min && Object.is(max, -0))) {
+    let tmp = max;
+    max = min;
+    min = tmp;
+  }
 
-    if (number === 0 && min === 0) {
-        return Object.is(min, -0) ? number : min;
-
-    } else if (number === 0 && max === 0) {
-        return Object.is(max, -0) ? max : number;
-
-    } else {
-        number = number > max ? max : number;
-        number = number < min ? min : number;
-        return number;
-    }
+  if (number === 0 && min === 0) {
+    return Object.is(min, -0) ? number : min;
+  } else if (number === 0 && max === 0) {
+    return Object.is(max, -0) ? max : number;
+  } else {
+    number = number > max ? max : number;
+    number = number < min ? min : number;
+    return number;
+  }
 }
--- num/test.ts
+++ num/test.ts
@@ -3,112 +3,70 @@ import { test, assertEqual, assert } from "../testing/mod.ts";
 import { clamp } from "mod.ts";
 
 test(function clampNumber() {
-    // Numbers between ranges go untouched
+  // Numbers between ranges go untouched
 
-    assertEqual(
-        clamp(50, {min: 25, max: 75}),
-        50
-    );
+  assertEqual(clamp(50, { min: 25, max: 75 }), 50);
 
-    assertEqual(
-        clamp(-50, {min: -75, max: -25}),
-        -50
-    );
+  assertEqual(clamp(-50, { min: -75, max: -25 }), -50);
 
-    assertEqual(
-        Object.is(clamp(0, {min: -0, max: 100}), -0),
-        false
-    );
+  assertEqual(Object.is(clamp(0, { min: -0, max: 100 }), -0), false);
 
-    assertEqual(
-        clamp(Number.MAX_SAFE_INTEGER, {min: 0, max: Infinity}),
-        Number.MAX_SAFE_INTEGER
-    );
+  assertEqual(
+    clamp(Number.MAX_SAFE_INTEGER, { min: 0, max: Infinity }),
+    Number.MAX_SAFE_INTEGER
+  );
 
-    assertEqual(
-        clamp(Number.MIN_SAFE_INTEGER, {min: Number.NEGATIVE_INFINITY, max: 0}),
-        Number.MIN_SAFE_INTEGER
-    );
+  assertEqual(
+    clamp(Number.MIN_SAFE_INTEGER, { min: Number.NEGATIVE_INFINITY, max: 0 }),
+    Number.MIN_SAFE_INTEGER
+  );
 
-    // Numbers below range are bumped upwards
+  // Numbers below range are bumped upwards
 
-    assertEqual(
-        clamp(0, {min: 25, max: 75}),
-        25
-    );
+  assertEqual(clamp(0, { min: 25, max: 75 }), 25);
 
-    assertEqual(
-        clamp(-50, {min: 0, max: 50}),
-        0
-    );
+  assertEqual(clamp(-50, { min: 0, max: 50 }), 0);
 
-    assertEqual(
-        clamp(Number.NEGATIVE_INFINITY, {min: Number.MIN_SAFE_INTEGER, max: 0}),
-        Number.MIN_SAFE_INTEGER
-    );
+  assertEqual(
+    clamp(Number.NEGATIVE_INFINITY, { min: Number.MIN_SAFE_INTEGER, max: 0 }),
+    Number.MIN_SAFE_INTEGER
+  );
 
-    assertEqual(
-        Object.is(clamp(-0, {min: 0, max: 100}), -0),
-        false
-    );
+  assertEqual(Object.is(clamp(-0, { min: 0, max: 100 }), -0), false);
 
-    // Numbers above range are bumped downwards
+  // Numbers above range are bumped downwards
 
-    assertEqual(
-        clamp(100, {min: 25, max: 75}),
-        75
-    );
+  assertEqual(clamp(100, { min: 25, max: 75 }), 75);
 
-    assertEqual(
-        clamp(-10, {min: -75, max: -25}),
-        -25
-    );
+  assertEqual(clamp(-10, { min: -75, max: -25 }), -25);
 
-    assertEqual(
-        Object.is(clamp(+0, {min: -100, max: -0}), -0),
-        true
-    );
+  assertEqual(Object.is(clamp(+0, { min: -100, max: -0 }), -0), true);
 
-    // NaN is left as is
-    assertEqual(
-        clamp(NaN, {min: 0, max: 100}), 
-        NaN
-    );
+  // NaN is left as is
+  assertEqual(clamp(NaN, { min: 0, max: 100 }), NaN);
 
-    // Swaps min & max if wrong way around
+  // Swaps min & max if wrong way around
 
-    assertEqual(
-        clamp(75, {min: 50, max: 0}),
-        50
-    );
+  assertEqual(clamp(75, { min: 50, max: 0 }), 50);
 
-    assertEqual(
-        clamp(75, {min: -50, max: -100}),
-        -50
-    );
+  assertEqual(clamp(75, { min: -50, max: -100 }), -50);
 
-    assertEqual(
-        Object.is(clamp(+0, {min: -0, max: -100}), -0),
-        true
-    );
+  assertEqual(Object.is(clamp(+0, { min: -0, max: -100 }), -0), true);
 
-    // Min defaults to minus infinity
-    
-    assertEqual(
-        clamp(Number.MIN_SAFE_INTEGER, {max: 50}),
-        Number.MIN_SAFE_INTEGER
-    );
+  // Min defaults to minus infinity
 
-    // Max defaults to plus infinity
+  assertEqual(
+    clamp(Number.MIN_SAFE_INTEGER, { max: 50 }),
+    Number.MIN_SAFE_INTEGER
+  );
 
-    assertEqual(
-        clamp(Number.MAX_SAFE_INTEGER, {min: 0}),
-        Number.MAX_SAFE_INTEGER
-    );
+  // Max defaults to plus infinity
 
-    // If min & max equal, returns it
-    assertEqual(
-        clamp(100, {min: 50, max: 50}),
-        50
-    );
-})
+  assertEqual(
+    clamp(Number.MAX_SAFE_INTEGER, { min: 0 }),
+    Number.MAX_SAFE_INTEGER
+  );
+
+  // If min & max equal, returns it
+  assertEqual(clamp(100, { min: 50, max: 50 }), 50);
+});
