5123cce8636a09f1444a4fdc242d324e99eed2a3
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-02-16 12:47:04 +0100

    format

--- examples/subprocess.ts
+++ examples/subprocess.ts
@@ -1,29 +1,34 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
 async function main() {
-    const decoder = new TextDecoder();
-    const filesToCat = Deno.args.slice(1);
-    const args = ["deno", "--allow-read", "https://deno.land/x/examples/cat.ts", ...filesToCat];
+  const decoder = new TextDecoder();
+  const filesToCat = Deno.args.slice(1);
+  const args = [
+    "deno",
+    "--allow-read",
+    "https://deno.land/x/examples/cat.ts",
+    ...filesToCat
+  ];
 
-    const p = Deno.run({
-        args,
-        stdout: "piped",
-        stderr: "piped",
-    });
+  const p = Deno.run({
+    args,
+    stdout: "piped",
+    stderr: "piped"
+  });
 
-    const { code } = await p.status();
+  const { code } = await p.status();
 
-    if (code === 0) {
-        const rawOutput = await Deno.readAll(p.stdout);
-        const output = decoder.decode(rawOutput);
-        console.log(output);
-    } else {
-        const rawErr = await Deno.readAll(p.stderr);
-        const err = decoder.decode(rawErr);
-        console.log(err);
-    }
+  if (code === 0) {
+    const rawOutput = await Deno.readAll(p.stdout);
+    const output = decoder.decode(rawOutput);
+    console.log(output);
+  } else {
+    const rawErr = await Deno.readAll(p.stderr);
+    const err = decoder.decode(rawErr);
+    console.log(err);
+  }
 
-    Deno.exit(code);
+  Deno.exit(code);
 }
 
 main();
--- examples/subprocess_simple.ts
+++ examples/subprocess_simple.ts
@@ -1,12 +1,17 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
 async function main() {
-    const p = Deno.run({
-        args: ["deno", "--allow-read", "https://deno.land/x/examples/cat.ts", "README.md"],
-    });
+  const p = Deno.run({
+    args: [
+      "deno",
+      "--allow-read",
+      "https://deno.land/x/examples/cat.ts",
+      "README.md"
+    ]
+  });
 
-    // start subprocess
-    await p.status();
+  // start subprocess
+  await p.status();
 }
 
 main();
