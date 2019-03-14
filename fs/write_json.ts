ef73f2e3e157e5bbd09f1002b206d5d476b2911b
axetroy <troy450409405@gmail.com>
2019-03-14 10:18:52 +0800

    styles: follow the style guide

--- fs/write_json.ts
+++ fs/write_json.ts
@@ -1,40 +1,29 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 import * as path from "./path/mod.ts";
 
-interface WriteJsonOption {
-  spaces?: number | string;
-  replacer?: (number | string)[] | null | Replacer;
-}
-
 type Replacer = (key: string, value: any) => any;
 
-/**
- * Writes an object to a JSON file.
- * @export
- * @param {string} filePath
- * @param {*} object
- * @param {WriteJsonOption} [options]
- * @returns {Promise<void>}
- */
+export interface WriteJsonOption {
+  spaces?: number | string;
+  replacer?: Array<number | string> | Replacer;
+}
+
+/* Writes an object to a JSON file. */
 export async function writeJson(
   filePath: string,
   object: any,
-  options?: WriteJsonOption
+  options: WriteJsonOption = {}
 ): Promise<void> {
   filePath = path.resolve(filePath);
 
   let contentRaw: string = "";
 
   try {
-    if (options) {
-      contentRaw = JSON.stringify(
-        object,
-        (options.replacer as string[]) || null,
-        options.spaces
-      );
-    } else {
-      contentRaw = JSON.stringify(object);
-    }
+    contentRaw = JSON.stringify(
+      object,
+      options.replacer as string[],
+      options.spaces
+    );
   } catch (err) {
     err.message = `${filePath}: ${err.message}`;
     throw err;
@@ -43,33 +32,22 @@ export async function writeJson(
   await Deno.writeFile(filePath, new TextEncoder().encode(contentRaw));
 }
 
-/**
- * Writes an object to a JSON file.
- * @export
- * @param {string} filePath
- * @param {*} object
- * @param {WriteJsonOption} [options]
- * @returns {void}
- */
+/* Writes an object to a JSON file. */
 export function writeJsonSync(
   filePath: string,
   object: any,
-  options?: WriteJsonOption
+  options: WriteJsonOption = {}
 ): void {
   filePath = path.resolve(filePath);
 
   let contentRaw: string = "";
 
   try {
-    if (options) {
-      contentRaw = JSON.stringify(
-        object,
-        (options.replacer as string[]) || null,
-        options.spaces
-      );
-    } else {
-      contentRaw = JSON.stringify(object);
-    }
+    contentRaw = JSON.stringify(
+      object,
+      options.replacer as string[],
+      options.spaces
+    );
   } catch (err) {
     err.message = `${filePath}: ${err.message}`;
     throw err;
