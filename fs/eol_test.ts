10d939e8d2f76d97d7b5d3cbff2cca3d757648e6
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-03-18 14:43:13 +0100

    test clean

--- fs/eol_test.ts
+++ fs/eol_test.ts
@@ -3,21 +3,20 @@ import { test } from "../testing/mod.ts";
 import { assertEquals } from "../testing/asserts.ts";
 import { format, detect, EOL } from "./eol.ts";
 
+const CRLFinput = "deno\r\nis not\r\nnode";
+const LFinput = "deno\nis not\nnode";
+
 test(function detectCRLF() {
-  const input = "deno\r\nis not\r\nnode";
-  assertEquals(detect(input), EOL.CRLF);
+  assertEquals(detect(CRLFinput), EOL.CRLF);
 });
 test(function detectLF() {
-  const input = "deno\nis not\nnode";
-  assertEquals(detect(input), EOL.LF);
+  assertEquals(detect(LFinput), EOL.LF);
 });
 test(function detectNoNewLine() {
   const input = "deno is not node";
   assertEquals(detect(input), null);
 });
 test(function testFormat() {
-  const CRLFinput = "deno\r\nis not\r\nnode";
-  const LFinput = "deno\nis not\nnode";
   assertEquals(format(CRLFinput, EOL.LF), LFinput);
   assertEquals(format(LFinput, EOL.LF), LFinput);
   assertEquals(format(LFinput, EOL.CRLF), CRLFinput);
