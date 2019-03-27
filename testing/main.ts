14f21c7674d0fe113f79cf699922f6524bafd425
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-03-27 11:53:55 +0100

    add test of RunTest

--- testing/main.ts
+++ testing/main.ts
@@ -1,3 +1,32 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import { runTests } from "./mod.ts";
-runTests();
+import { runTests, test } from "./mod.ts";
+import { assert } from "./asserts.ts";
+
+function genTest(options) {
+  let n = options.parallel ? "parallel" : "serial";
+  test({
+    name: `[RunTests ${n}] run Test 1`,
+    fn() {
+      assert(true);
+    }
+  });
+
+  test({
+    name: `[RunTests ${n}] run Test 2`,
+    fn() {
+      assert(true);
+    }
+  });
+}
+async function testTheTestRunner() {
+  let opt = { parallel: false };
+  genTest(opt);
+  await runTests(opt);
+  opt = { parallel: true };
+  genTest(opt);
+  await runTests(opt);
+}
+
+runTests().then(() => {
+  testTheTestRunner();
+});
