7bbe14cf4c463663ec60a2e8761b335bbb3ee3f1
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-03-28 16:56:42 +0100

    chiefbiiko patch

--- testing/main.ts
+++ testing/main.ts
@@ -1,32 +1,12 @@
+
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import { runTests, test } from "./mod.ts";
-import { assert } from "./asserts.ts";
+import { runTests } from "./mod.ts";
 
-function genTest(options) {
-  let n = options.parallel ? "parallel" : "serial";
-  test({
-    name: `[RunTests ${n}] run Test 1`,
-    fn() {
-      assert(true);
-    }
-  });
-
-  test({
-    name: `[RunTests ${n}] run Test 2`,
-    fn() {
-      assert(true);
-    }
-  });
-}
-async function testTheTestRunner() {
-  let opt = { parallel: false };
-  genTest(opt);
-  await runTests(opt);
-  opt = { parallel: true };
-  genTest(opt);
-  await runTests(opt);
+async function main() {
+  // Testing entire test suite serially
+  await runTests();
+  // Testing parallel execution on a subset that does not depend on exec order
+  await runTests({ parallel: true, only: /^testing/ });
 }
 
-runTests().then(() => {
-  testTheTestRunner();
-});
+main();
