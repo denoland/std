d1a2f6d13e2c892d38654c07a7b83ca119cf1853
axetroy <troy450409405@gmail.com>
2019-04-07 22:07:48 +0800

    update docs

--- fs/remove.ts
+++ fs/remove.ts
@@ -4,6 +4,7 @@
  * Removes the named file or directory.
  * always recursively delete directories.
  * @param path the filepath will be remove
+ * @returns Promise<void>
  */
 export async function remove(path: string): Promise<void> {
   await Deno.remove(path, { recursive: true });
@@ -13,6 +14,7 @@ export async function remove(path: string): Promise<void> {
  * Removes the named file or directory synchronously.
  * always recursively delete directories.
  * @param path the filepath will be remove
+ * @returns void
  */
 export function removeSync(path: string): void {
   Deno.removeSync(path, { recursive: true });
