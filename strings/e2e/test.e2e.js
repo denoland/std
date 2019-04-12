df9365c4d40e5c005ed20ab7d248daf83f4da32c
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-04-12 20:59:39 +0200

    Browser test pad

---
+++ strings/e2e/test.e2e.js
@@ -0,0 +1,23 @@
+const puppeteer = require("puppeteer");
+const assert = require("assert");
+(async () => {
+  // TODO (zekth) Fix chrome Path for all envs
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  });
+  const page = await browser.newPage();
+  const logs = [];
+  page.on("console", msg => logs.push(msg.text()));
+
+  try {
+    await page.goto("http://localhost:4500/strings/e2e/test.html");
+    await page.waitFor(500);
+    assert(logs[0] === "**deno");
+    console.log("OK");
+  } catch (e) {
+    console.log("FAILED");
+  }
+  await browser.close();
+})();
--- strings/pad_test.ts
+++ strings/pad_test.ts
@@ -1,6 +1,57 @@
 import { test } from "../testing/mod.ts";
-import { assertEquals } from "../testing/asserts.ts";
+import { assert, assertEquals } from "../testing/asserts.ts";
 import { pad } from "./pad.ts";
+import { BufReader } from "../io/bufio.ts";
+import { TextProtoReader } from "../textproto/mod.ts";
+const { run } = Deno;
+
+let fileServer;
+
+async function startFileServer(): Promise<void> {
+  fileServer = run({
+    args: [
+      "deno",
+      "--allow-read",
+      "--allow-net",
+      "http/file_server.ts",
+      ".",
+      "--cors"
+    ],
+    stdout: "piped"
+  });
+  // Once fileServer is ready it will write to its stdout.
+  const r = new TextProtoReader(new BufReader(fileServer.stdout));
+  const [s, err] = await r.readLine();
+  assert(err == null);
+  assert(s.includes("server listening"));
+}
+
+function killFileServer(): void {
+  fileServer.close();
+  fileServer.stdout.close();
+}
+
+test({
+  name: "[PAD] Browser test",
+  async fn() {
+    await startFileServer();
+    try {
+      const c = new TextDecoder();
+      run({
+        args: ["tsc", "-m", "ES6", "strings/pad.ts"],
+        stdout: "piped"
+      });
+      const puppeteer = run({
+        args: ["node", "strings/e2e/test.e2e.js"],
+        stdout: "piped"
+      });
+      let out = await puppeteer.output();
+      assertEquals(c.decode(out), "OK\n");
+    } finally {
+      killFileServer();
+    }
+  }
+});
 
 test(function padTest() {
   const expected1 = "**deno";
