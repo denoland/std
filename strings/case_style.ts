49302afd0a1fc3e22dec88feaf9900163955f802
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-04-30 01:06:59 +0200

    error fix add style add format

--- strings/case_style.ts
+++ strings/case_style.ts
@@ -4,14 +4,18 @@ export enum caseStyle {
   pascalCase = "pascalCase",
   camelCase = "camelCase",
   snakeCase = "snakeCase",
-  kebabCase = "kebabCase"
+  kebabCase = "kebabCase",
+  screamingSnakeCase = "screamingSnakeCase",
+  screamingKebabCase = "screamingKebabCase"
 }
 
 const validators = {
   pascalCase: /[A-Z][a-z0-9]+(?:[A-Z][a-z0-9]+)*/,
   camelCase: /[a-z][a-z0-9]+(?:[A-Z][a-z0-9]+)*/,
   snakeCase: /[a-z][a-z0-9]+(?:[_][a-z][a-z0-9]+)*/,
-  kebabCase: /[a-z][a-z0-9]+(?:[-][a-z][a-z0-9]+)*/
+  screamingSnakeCase: /[A-Z][A-Z0-9]+(?:[_][A-Z][A-Z0-9]+)*/,
+  kebabCase: /[a-z][a-z0-9]+(?:[-][a-z][a-z0-9]+)*/,
+  screamingKebabCase: /[A-Z][A-Z0-9]+(?:[-][A-Z][A-Z0-9]+)*/
 };
 
 /**
@@ -21,5 +25,48 @@ const validators = {
  */
 export function validate(str: string, style: caseStyle): boolean {
   const m = validators[style].exec(str);
-  return m[0] === str;
+  return m && m[0] === str;
+}
+
+/**
+ * Format the string into the caseStyle wanted
+ * @param str String to format
+ * @param caseStyle caseStyle to format the string into
+ */
+export function format(str: string, style: caseStyle): string {
+  const acc = [];
+  const reg = /([a-zA-Z0-9À-ž]+)/gm;
+  str = str.trim();
+  const m = str.match(reg);
+  for (let i = 0; i < m.length; i++) {
+    let match = m[i];
+    if (style === caseStyle.camelCase && i === 0) {
+      acc.push(match[0].toLowerCase() + match.slice(1));
+    } else if (
+      style === caseStyle.pascalCase ||
+      style === caseStyle.camelCase
+    ) {
+      acc.push(match[0].toUpperCase() + match.slice(1).toLowerCase());
+    } else if (
+      style === caseStyle.screamingSnakeCase ||
+      style === caseStyle.screamingKebabCase
+    ) {
+      acc.push(match[0].toUpperCase() + match.slice(1).toUpperCase());
+    } else {
+      acc.push(match[0].toLowerCase() + match.slice(1).toLowerCase());
+    }
+  }
+  switch (style) {
+    case caseStyle.pascalCase:
+    case caseStyle.camelCase:
+      return acc.join("");
+    case caseStyle.snakeCase:
+    case caseStyle.screamingSnakeCase:
+      return acc.join("_");
+    case caseStyle.kebabCase:
+    case caseStyle.screamingKebabCase:
+      return acc.join("-");
+    default:
+      throw Error("Unknown case style");
+  }
 }
--- strings/case_style_test.ts
+++ strings/case_style_test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 import { test } from "../testing/mod.ts";
-import { assert } from "../testing/asserts.ts";
-import { validate, caseStyle } from "./case_style.ts";
+import { assert, assertEquals } from "../testing/asserts.ts";
+import { validate, format, caseStyle } from "./case_style.ts";
 
 test({
   name: "[string] Case style validator",
@@ -10,9 +10,50 @@ test({
     assert(validate("jaime_les_fruits_au_sirop", caseStyle.snakeCase));
     assert(validate("jaimeLesFruitsAuSirop", caseStyle.camelCase));
     assert(validate("JaimeLesFruitsAuSirop", caseStyle.pascalCase));
+    assert(validate("DIZ_IZ_DA_GLOBAL_VAL", caseStyle.screamingSnakeCase));
+    assert(validate("SALADE-TOMATE-OIGNONS", caseStyle.screamingKebabCase));
     assert(!validate("OhMyGodASnake", caseStyle.snakeCase));
     assert(!validate("imNotPascal", caseStyle.pascalCase));
     assert(!validate("SmokingIsBad", caseStyle.camelCase));
     assert(!validate("salade_TomateOignons", caseStyle.kebabCase));
+    assert(
+      !validate("jaime-les-fruits-au-sirop", caseStyle.screamingSnakeCase)
+    );
+    assert(
+      !validate("jaime_les_fruits_au_sirop", caseStyle.screamingKebabCase)
+    );
+  }
+});
+
+test({
+  name: "[string] Case style validator",
+  fn(): void {
+    const input = `deja vu i've just been in this place before
+    Higher on the street
+And I know it's my time to go…`;
+    assertEquals(
+      format(input, caseStyle.kebabCase),
+      "deja-vu-i-ve-just-been-in-this-place-before-higher-on-the-street-and-i-know-it-s-my-time-to-go"
+    );
+    assertEquals(
+      format(input, caseStyle.snakeCase),
+      "deja_vu_i_ve_just_been_in_this_place_before_higher_on_the_street_and_i_know_it_s_my_time_to_go"
+    );
+    assertEquals(
+      format(input, caseStyle.camelCase),
+      "dejaVuIVeJustBeenInThisPlaceBeforeHigherOnTheStreetAndIKnowItSMyTimeToGo"
+    );
+    assertEquals(
+      format(input, caseStyle.pascalCase),
+      "DejaVuIVeJustBeenInThisPlaceBeforeHigherOnTheStreetAndIKnowItSMyTimeToGo"
+    );
+    assertEquals(
+      format(input, caseStyle.screamingSnakeCase),
+      "DEJA_VU_I_VE_JUST_BEEN_IN_THIS_PLACE_BEFORE_HIGHER_ON_THE_STREET_AND_I_KNOW_IT_S_MY_TIME_TO_GO"
+    );
+    assertEquals(
+      format(input, caseStyle.screamingKebabCase),
+      "DEJA-VU-I-VE-JUST-BEEN-IN-THIS-PLACE-BEFORE-HIGHER-ON-THE-STREET-AND-I-KNOW-IT-S-MY-TIME-TO-GO"
+    );
   }
 });
