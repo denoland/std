fe9d5b47ccb89c1244c54bf04dfbb941f65ac657
Jun Kato <i@junkato.jp>
2019-05-05 12:39:35 +0900

    [bytes] edit test cases to fail

--- bytes/bytes_test.ts
+++ bytes/bytes_test.ts
@@ -9,7 +9,7 @@ import { assertEquals } from "../testing/asserts.ts";
 
 test(function bytesBytesFindIndex(): void {
   const i = bytesFindIndex(
-    new Uint8Array([1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 3]),
+    new Uint8Array([0, 1, 0, 1, 2, 0, 1, 2, 0, 1, 3]),
     new Uint8Array([0, 1, 2])
   );
   assertEquals(i, 2);
@@ -17,7 +17,7 @@ test(function bytesBytesFindIndex(): void {
 
 test(function bytesBytesFindLastIndex1(): void {
   const i = bytesFindLastIndex(
-    new Uint8Array([0, 1, 2, 0, 1, 2, 0, 1, 3]),
+    new Uint8Array([0, 1, 2, 0, 1, 2, 1, 2]),
     new Uint8Array([0, 1, 2])
   );
   assertEquals(i, 3);
