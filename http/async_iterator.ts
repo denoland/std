0230777fe5a3f1b3d5a6651719ef6bba4f41fc4f
Jun Kato <i@junkato.jp>
2019-05-11 02:19:32 +0900

    Apply prettier

--- http/async_iterator.ts
+++ http/async_iterator.ts
@@ -3,7 +3,9 @@
  *
  * see https://github.com/denoland/deno/issues/2330
  */
-export function toAsyncIterator(r: Deno.Reader): AsyncIterableIterator<Uint8Array> {
+export function toAsyncIterator(
+  r: Deno.Reader
+): AsyncIterableIterator<Uint8Array> {
   const b = new Uint8Array(100);
   let sawEof = false;
 
@@ -14,7 +16,7 @@ export function toAsyncIterator(r: Deno.Reader): AsyncIterableIterator<Uint8Arra
 
     async next(): Promise<IteratorResult<Uint8Array>> {
       if (sawEof) {
-          return { value: null, done: true };
+        return { value: null, done: true };
       }
 
       const result = await r.read(b);
@@ -22,7 +24,7 @@ export function toAsyncIterator(r: Deno.Reader): AsyncIterableIterator<Uint8Arra
 
       return {
         value: b.subarray(0, result.nread),
-        done: false,
+        done: false
       };
     }
   };
