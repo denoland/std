f5436d988180f92eb431fc5cfcae8e0766a04ec4
axetroy <troy450409405@gmail.com>
2019-05-14 00:24:20 +0800

    export each function individually

--- fs/path/mod.ts
+++ fs/path/mod.ts
@@ -1,27 +1,25 @@
 // Copyright the Browserify authors. MIT License.
 // Ported from https://github.com/browserify/path-browserify/
 
-import { win32 as _win32 } from "./win32.ts";
-import { posix as _posix } from "./posix.ts";
+import * as _win32 from "./win32.ts";
+import * as _posix from "./posix.ts";
 
 import { isWindows } from "./constants.ts";
 
-_posix.win32 = _win32.win32 = _win32;
-_posix.posix = _win32.posix = _posix;
+const path = isWindows ? _win32 : _posix;
 
-const module = isWindows ? _win32 : _posix;
 export const win32 = _win32;
 export const posix = _posix;
-export const resolve = module.resolve;
-export const normalize = module.normalize;
-export const isAbsolute = module.isAbsolute;
-export const join = module.join;
-export const relative = module.relative;
-export const toNamespacedPath = module.toNamespacedPath;
-export const dirname = module.dirname;
-export const basename = module.basename;
-export const extname = module.extname;
-export const format = module.format;
-export const parse = module.parse;
-export const sep = module.sep;
-export const delimiter = module.delimiter;
+export const resolve = path.resolve;
+export const normalize = path.normalize;
+export const isAbsolute = path.isAbsolute;
+export const join = path.join;
+export const relative = path.relative;
+export const toNamespacedPath = path.toNamespacedPath;
+export const dirname = path.dirname;
+export const basename = path.basename;
+export const extname = path.extname;
+export const format = path.format;
+export const parse = path.parse;
+export const sep = path.sep;
+export const delimiter = path.delimiter;
