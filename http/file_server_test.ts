7772fc167530e1a1de1ccfb94a8d2246cc1dfb29
Yuqing CHEN <mr.chenyuqing@live.com>
2019-05-22 15:13:24 +0800

    wip: add console to debug in azure. do not merge

--- http/file_server_test.ts
+++ http/file_server_test.ts
@@ -56,7 +56,8 @@ test(async function serveDirectory(): Promise<void> {
     assert(res.headers.has("access-control-allow-headers"));
     const page = await res.text();
     assert(page.includes("azure-pipelines.yml"));
-    assert(/<td class="mode">\([a-zA-Z-]{10}\)<\/td>/.test(page));
+    // assert(/<td class="mode">\([a-zA-Z-]{10}\)<\/td>/.test(page));
+    assert(/<td class="mode">\(unknown mode\)<\/td>/.test(page));
     assert(
       page.includes(
         `<td><a href="/azure-pipelines.yml">azure-pipelines.yml</a></td>`
