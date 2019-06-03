322593d61cb172bfe8c2eef7ec52c78ac7ccf200
Craig Fay <craigfay1@gmail.com>
2019-06-03 08:23:00 -0500

    Add test for HttpHeaders methods

---
+++ http/headers_test.ts
@@ -0,0 +1,48 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+import { ServerRequest, Response } from './server.ts';
+import { getCookies, delCookie, setCookie } from './cookie.ts';
+import { HttpHeaders } from './headers.ts';
+import { assert, assertEquals } from '../testing/asserts.ts';
+import { test } from '../testing/mod.ts';
+import { runTests } from '../testing/mod.ts';
+
+test({
+  name: '[HTTP] HttpHeaders Properties',
+  fn(): void {
+    let headers = new HttpHeaders();
+    assertEquals(headers.has('cookie'), false);
+    assertEquals(headers.get('cookie'), null);
+    
+    headers.set('Cookie', 'hey=kid');
+    assertEquals((headers.get('cookie')), 'hey=kid');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.append('Cookie', 'you=want');
+    assertEquals((headers.get('cookie')), 'hey=kid; you=want');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.append('Cookie', 'a=job');
+    assertEquals((headers.get('cookie')), 'hey=kid; you=want; a=job');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.delete('Cookie');
+    assertEquals(headers.has('cookie'), false);
+    assertEquals(headers.get('cookie'), null);
+
+    headers.set('Cookie', 'i=want');
+    assertEquals((headers.get('cookie')), 'i=want');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.set('Cookie', 'those=pictures');
+    assertEquals((headers.get('cookie')), 'those=pictures');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.append('Cookie', 'of=spiderman');
+    assertEquals((headers.get('cookie')), 'those=pictures; of=spiderman');
+    assertEquals(headers.has('cookie'), true);
+
+    headers.delete('Cookie');
+    assertEquals(headers.has('cookie'), false);
+    assertEquals(headers.get('cookie'), null);
+  }
+})
