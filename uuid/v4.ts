6a371b2f1494261dbbaf57c8e6fa22291ec38e3f
Lucas Caro <lucascaro@gmail.com>
2019-07-01 10:37:45 -0700

    break long line

--- uuid/v4.ts
+++ uuid/v4.ts
@@ -6,7 +6,10 @@
  *  https://gist.github.com/jed/982883
  */
 
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const UUID_RE = new RegExp(
+  '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
+  'i'
+);
 
 export function validate(id: string): boolean {
   return UUID_RE.test(id);
