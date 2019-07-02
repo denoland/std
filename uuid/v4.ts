4453d43aef9e456c3252b91fef66afd3f984a090
Lucas Caro <lucascaro@gmail.com>
2019-07-01 20:15:45 -0700

    remove unnecessary comment

--- uuid/v4.ts
+++ uuid/v4.ts
@@ -1,12 +1,4 @@
-import { NIL_UUID } from "./mod.ts";
-
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-/**
- * Super-small uuid generation.
- *
- * Based on
- *  https://gist.github.com/jed/982883
- */
 
 const UUID_RE = new RegExp(
   "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
@@ -18,9 +10,6 @@ export function validate(id: string): boolean {
 }
 
 export default function generate(): string {
-  // Generate 16 random bytes
-  // adjust 4 msb of 7th byte to 0100
-  // set 2 msb of 9th byte to 10
   return "00000000-0000-4000-8000-000000000000".replace(
     /[0]/g,
     (): string =>
