2ff293db11f96bf9dc9f5b1c8597dce51602db0d
Lucas Caro <lucascaro@gmail.com>
2019-07-01 19:08:12 -0700

    fix formatting

--- uuid/v4.ts
+++ uuid/v4.ts
@@ -9,9 +9,8 @@ import { NIL_UUID } from "./mod.ts";
  */
 
 const UUID_RE = new RegExp(
-  '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
-  'i'
-  
+  "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+  "i"
 );
 
 export function validate(id: string): boolean {
@@ -19,14 +18,10 @@ export function validate(id: string): boolean {
 }
 
 export default function generate(): string {
-  return (
-    "00000000-0000-4000-8000-000000000000"
-      .replace(
-        /[0]/g, 
-        (): string =>
-          // random integer from 0 to 15
-          Math.floor(Math.random() * 16)
-          .toString(16) // as a hex digit.
-      )
+  return "00000000-0000-4000-8000-000000000000".replace(
+    /[0]/g,
+    (): string =>
+      // random integer from 0 to 15
+      Math.floor(Math.random() * 16).toString(16) // as a hex digit.
   );
 }
