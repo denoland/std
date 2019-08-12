f4c8fc3e26a0cf2b9cf0a4473032eeac85fa3f2b
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-08-12 18:46:38 +0200

    add help, glob flag

--- testing/runner.ts
+++ testing/runner.ts
@@ -3,39 +3,67 @@
 import { parse } from "../flags/mod.ts";
 import { glob, walk } from "../fs/mod.ts";
 import { runTests } from "./mod.ts";
-const { args } = Deno;
+const { args, cwd } = Deno;
 
+// TODO: settle on list of default globs
+const DEFAULT_GLOBS = ["**/*_test.ts", "**/*_test.js"];
 
-const { cwd } = Deno;
-const DEFAULT_GLOB = "**/*_test.ts";
+// TODO: upgrade it
+function showHelp() {
+  console.log(`deno test-runner
+  Test runner
+
+USAGE:
+  deno -A https://deno.land/std/testing/runner.ts [OPTIONS]
+
+OPTIONS:
+  -q, --quiet           Don't show output from test cases 
+  -f, --failfast        Stop test suite on first error
+  -g, --glob <PATH...>  List of globs for test files, defaults to: **/*_test.ts,**/*_test.js
+`);
+}
 
 async function main(): Promise<void> {
-  const parsedArgs = parse(args.slice(1));
-
-  console.log(parsedArgs);
-
-  // TODO: find all files matching _test.js/.ts pattern
-  // find all files matching `pattern` glob
-  const iterator = walk(cwd(), {
-    match: [glob(DEFAULT_GLOB)]
+  const parsedArgs = parse(args.slice(1), {
+    boolean: ["quiet", "failfast", "help"],
+    string: ["glob"],
+    alias: {
+      help: ["h"],
+      quiet: ["q"],
+      failfast: ["f"],
+      glob: ["g"]
+    }
   });
 
-  const foundTestFiles = [];
+  if (parsedArgs.help) {
+    return showHelp();
+  }
 
-  for await (const { filename, info } of iterator) {
-    // console.log("found path: ", filename);
+  let fileGlobs;
+  if (parsedArgs.glob) {
+    fileGlobs = (parsedArgs.glob as string).split(",");
+  } else {
+    fileGlobs = DEFAULT_GLOBS;
+  }
+
+  const filesIterator = walk(cwd(), {
+    match: fileGlobs.map(glob)
+  });
+
+  const foundTestFiles: Array<string> = [];
+  for await (const { filename } of filesIterator) {
     foundTestFiles.push(filename);
   }
 
-  console.log("found " + foundTestFiles.length + " matching files");
+  console.log(`Found ${foundTestFiles.length} matching files.`);
 
   for (const filename of foundTestFiles) {
     await import(filename);
   }
 
   await runTests({
-    exitOnFail: !!parsedArgs.exitOnFail,
-    disableLog: !!parsedArgs.silent,
+    exitOnFail: !!parsedArgs.failfast,
+    disableLog: !!parsedArgs.quiet
   });
 }
 
