c4a5727a7a79ad3329a026e016eecc33684b7216
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-08-14 18:50:39 +0200

    upgrade

--- testing/runner.ts
+++ testing/runner.ts
@@ -5,22 +5,28 @@ import { glob, walk } from "../fs/mod.ts";
 import { runTests } from "./mod.ts";
 const { args, cwd } = Deno;
 
-// TODO: settle on list of default globs
-const DEFAULT_GLOBS = ["**/*_test.ts", "**/*_test.js"];
+const DEFAULT_GLOBS = [
+  "**/*_test.ts",
+  "**/*_test.js",
+  "**/test.ts",
+  "**/test.js"
+];
 
-// TODO: upgrade it
 /* eslint-disable max-len */
 function showHelp(): void {
-  console.log(`deno test-runner
-  Test runner
+  console.log(`Deno test runner
 
 USAGE:
-  deno -A https://deno.land/std/testing/runner.ts [OPTIONS]
+  deno -A https://deno.land/std/testing/runner.ts [OPTIONS] [FILES...]
 
 OPTIONS:
-  -q, --quiet           Don't show output from test cases 
-  -f, --failfast        Stop test suite on first error
-  -g, --glob <PATH...>  List of globs for test files, defaults to: **/*_test.ts,**/*_test.js
+  -q, --quiet               Don't show output from test cases 
+  -f, --failfast            Stop test suite on first error
+  -e, --exclude <FILES...>  List of file names to exclude from run. If this options is 
+                            used files to match must be specified after "--". 
+  
+ARGS:
+  [FILES...]  List of file names to run. Defaults to: ${DEFAULT_GLOBS.join(",")} 
 `);
 }
 /* eslint-enable max-len */
@@ -28,12 +34,12 @@ OPTIONS:
 async function main(): Promise<void> {
   const parsedArgs = parse(args.slice(1), {
     boolean: ["quiet", "failfast", "help"],
-    string: ["glob"],
+    string: ["exclude"],
     alias: {
       help: ["h"],
       quiet: ["q"],
       failfast: ["f"],
-      glob: ["g"]
+      exclude: ["e"]
     }
   });
 
@@ -41,15 +47,33 @@ async function main(): Promise<void> {
     return showHelp();
   }
 
-  let fileGlobs;
-  if (parsedArgs.glob) {
-    fileGlobs = (parsedArgs.glob as string).split(",");
+  let includeFiles;
+  let excludeFiles;
+
+  if (parsedArgs._.length) {
+    includeFiles = (parsedArgs._ as string[])
+      .map(
+        (fileGlob: string): string[] => {
+          return fileGlob.split(",");
+        }
+      )
+      .flat();
   } else {
-    fileGlobs = DEFAULT_GLOBS;
+    includeFiles = DEFAULT_GLOBS;
   }
 
+  if (parsedArgs.exclude) {
+    excludeFiles = (parsedArgs.exclude as string).split(",");
+  } else {
+    excludeFiles = [];
+  }
+
+  console.log("includeFiles", includeFiles);
+  console.log("excludeFiles", excludeFiles);
+
   const filesIterator = walk(cwd(), {
-    match: fileGlobs.map(glob)
+    match: includeFiles.map(glob),
+    skip: excludeFiles.map(glob)
   });
 
   const foundTestFiles: string[] = [];
@@ -57,7 +81,13 @@ async function main(): Promise<void> {
     foundTestFiles.push(filename);
   }
 
-  console.log(`Found ${foundTestFiles.length} matching files.`);
+  if (foundTestFiles.length === 0) {
+    console.error("No matching test files found.");
+    Deno.exit(0);
+  }
+
+  console.log(`Found ${foundTestFiles.length} matching test files.`);
+  foundTestFiles.map(e => console.log(e));
 
   for (const filename of foundTestFiles) {
     await import(filename);
