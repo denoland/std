d4c9e5c4b488442010207ea7ffb83926f107acea
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-08-27 19:07:08 +0200

    lint

--- testing/runner_test.ts
+++ testing/runner_test.ts
@@ -8,19 +8,17 @@ import { getMatchingUrls } from "./runner.ts";
  */
 const TEST_ROOT_PATH = Deno.cwd();
 
-test(async function getMatchingUrlsRemote() {
+test(async function getMatchingUrlsRemote(): Promise<void> {
   const matches = [
     "https://deno.land/std/fmt/colors_test.ts",
     "http://deno.land/std/fmt/printf_test.ts"
   ];
 
   const urls = await getMatchingUrls(matches, [], TEST_ROOT_PATH);
-
-  assertEquals(url, matches);
+  assertEquals(urls, matches);
 });
 
-test(async function getMatchingUrlsLocal() {
+test(async function getMatchingUrlsLocal(): Promise<void> {
   const urls = await getMatchingUrls(["fmt/*_test.ts"], [], TEST_ROOT_PATH);
-
   assertEquals(urls.length, 2);
 });
