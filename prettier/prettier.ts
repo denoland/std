7b3ba6bb119bfcb6b38b38427ba1145f746d2d7b
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-09-10 23:32:21 +0200

    add doc

--- prettier/prettier.ts
+++ prettier/prettier.ts
@@ -1,4 +1,8 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+/**
+ * Types are given here because parser files are big
+ * and it's much faster to give TS compiler just type declarations.
+ */
 // @deno-types="./vendor/standalone.d.ts"
 import "./vendor/standalone.js";
 // @deno-types="./vendor/parser_typescript.d.ts"
