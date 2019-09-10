13b24e9224db3dd7bfaa7f601605e839b7783e7d
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-09-10 22:43:22 +0200

    add types to prettier

--- prettier/prettier.ts
+++ prettier/prettier.ts
@@ -1,7 +1,11 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+// @deno-types="./vendor/standalone.d.ts"
 import "./vendor/standalone.js";
+// @deno-types="./vendor/parser_typescript.d.ts"
 import "./vendor/parser_typescript.js";
+// @deno-types="./vendor/parser_babylon.d.ts"
 import "./vendor/parser_babylon.js";
+// @deno-types="./vendor/parser_markdown.d.ts"
 import "./vendor/parser_markdown.js";
 
 // TODO: provide decent type declarions for these
