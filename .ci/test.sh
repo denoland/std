bd970e4106a2628370111d67a0b3b939a64b0ca7
Luca Casonato <lucacasonato@yahoo.com>
2019-09-11 18:48:22 +0200

    fixed path?

---
+++ .ci/test.sh
@@ -0,0 +1,4 @@
+deno run --allow-run --allow-write --allow-read --allow-env ./format.ts --check
+export START_TIME=$(date +%s)
+deno run --allow-run --allow-net --allow-write --allow-read --allow-env --config=tsconfig.test.json ./testing/runner.ts --exclude node_modules
+deno run --allow-run --allow-read .ci/check_source_file_changes.ts $START_TIME
