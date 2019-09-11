f2ea8a6c6ecc42accdbf2ebf09b56499bc90b8db
Luca Casonato <lucacasonato@yahoo.com>
2019-09-11 20:20:38 +0200

    removed echo in test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,4 +1,3 @@
-echo $PATH
 deno$DENO_EXE_SUFFIX run --allow-run --allow-write --allow-read --allow-env ./format.ts --check
 export START_TIME=$(date +%s)
 deno$DENO_EXE_SUFFIX run --allow-run --allow-net --allow-write --allow-read --allow-env --config=tsconfig.test.json ./testing/runner.ts --exclude node_modules
