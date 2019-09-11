e45bfd9ba0b005b7021f2ed664eb9e477d6060a5
Luca Casonato <lucacasonato@yahoo.com>
2019-09-11 19:39:16 +0200

    deno exe suffix

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,4 +1,4 @@
-deno run --allow-run --allow-write --allow-read --allow-env ./format.ts --check
+deno$DENO_EXE_SUFFIX run --allow-run --allow-write --allow-read --allow-env ./format.ts --check
 export START_TIME=$(date +%s)
-deno run --allow-run --allow-net --allow-write --allow-read --allow-env --config=tsconfig.test.json ./testing/runner.ts --exclude node_modules
-deno run --allow-run --allow-read .ci/check_source_file_changes.ts $START_TIME
+deno$DENO_EXE_SUFFIX run --allow-run --allow-net --allow-write --allow-read --allow-env --config=tsconfig.test.json ./testing/runner.ts --exclude node_modules
+deno$DENO_EXE_SUFFIX run --allow-run --allow-read .ci/check_source_file_changes.ts $START_TIME
