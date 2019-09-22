b7ba067ca741d66b4b82dc34e117470229b0b264
Nayeem Rahman <muhammed.9939@gmail.com>
2019-09-23 00:12:47 +0100

    Improve and add more tests

--- testing/runner_test.ts
+++ testing/runner_test.ts
@@ -1,35 +1,77 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 import { test } from "./mod.ts";
-import { assertEquals } from "../testing/asserts.ts";
 import { findTestModules } from "./runner.ts";
-import { join } from "../fs/path/mod.ts";
+import { SEP, isWindows } from "../fs/path/constants.ts";
+import { assertEquals } from "../testing/asserts.ts";
 
-/**
- * IMPORTANT: This file assumes it is run from root of repository.
- */
-const cwd = Deno.cwd();
-const TEST_ROOT_PATH = join(cwd, "fmt");
+const TEST_DATA_URL = new URL("testdata", import.meta.url);
+const TEST_DATA_PATH = TEST_DATA_URL.pathname
+  .slice(isWindows ? 1 : 0)
+  .replace(/\//g, SEP);
+
+test(async function findTestModulesDir1(): Promise<void> {
+  console.log(TEST_DATA_URL.href);
+  console.log(TEST_DATA_PATH);
+  const urls = await findTestModules(["."], [], TEST_DATA_PATH);
+  assertEquals(urls.sort(), [
+    `${TEST_DATA_URL}/bar_test.js`,
+    `${TEST_DATA_URL}/foo_test.ts`,
+    `${TEST_DATA_URL}/subdir/bar_test.js`,
+    `${TEST_DATA_URL}/subdir/foo_test.ts`,
+    `${TEST_DATA_URL}/subdir/test.js`,
+    `${TEST_DATA_URL}/subdir/test.ts`,
+    `${TEST_DATA_URL}/test.js`,
+    `${TEST_DATA_URL}/test.ts`
+  ]);
+});
+
+test(async function findTestModulesDir2(): Promise<void> {
+  const urls = await findTestModules(["subdir"], [], TEST_DATA_PATH);
+  assertEquals(urls.sort(), [
+    `${TEST_DATA_URL}/subdir/bar_test.js`,
+    `${TEST_DATA_URL}/subdir/foo_test.ts`,
+    `${TEST_DATA_URL}/subdir/test.js`,
+    `${TEST_DATA_URL}/subdir/test.ts`
+  ]);
+});
+
+test(async function findTestModulesGlob(): Promise<void> {
+  const urls = await findTestModules(["**/*_test.{js,ts}"], [], TEST_DATA_PATH);
+  assertEquals(urls.sort(), [
+    `${TEST_DATA_URL}/bar_test.js`,
+    `${TEST_DATA_URL}/foo_test.ts`,
+    `${TEST_DATA_URL}/subdir/bar_test.js`,
+    `${TEST_DATA_URL}/subdir/foo_test.ts`
+  ]);
+});
+
+test(async function findTestModulesExcludeDir(): Promise<void> {
+  const urls = await findTestModules(["."], ["subdir"], TEST_DATA_PATH);
+  assertEquals(urls.sort(), [
+    `${TEST_DATA_URL}/bar_test.js`,
+    `${TEST_DATA_URL}/foo_test.ts`,
+    `${TEST_DATA_URL}/test.js`,
+    `${TEST_DATA_URL}/test.ts`
+  ]);
+});
+
+test(async function findTestModulesExcludeGlob(): Promise<void> {
+  const urls = await findTestModules(["."], ["**/foo*"], TEST_DATA_PATH);
+  assertEquals(urls.sort(), [
+    `${TEST_DATA_URL}/bar_test.js`,
+    `${TEST_DATA_URL}/subdir/bar_test.js`,
+    `${TEST_DATA_URL}/subdir/test.js`,
+    `${TEST_DATA_URL}/subdir/test.ts`,
+    `${TEST_DATA_URL}/test.js`,
+    `${TEST_DATA_URL}/test.ts`
+  ]);
+});
 
 test(async function findTestModulesRemote(): Promise<void> {
-  const matches = [
-    "https://deno.land/std/fmt/colors_test.ts",
-    "http://deno.land/std/fmt/printf_test.ts"
+  const urls = [
+    "https://example.com/colors_test.ts",
+    "http://example.com/printf_test.ts"
   ];
-
-  const urls = await findTestModules(matches, [], TEST_ROOT_PATH);
-  assertEquals(urls, matches);
-});
-
-test(async function findTestModulesLocal(): Promise<void> {
-  const urls = await findTestModules(
-    ["*_test.ts"],
-    ["colors*"],
-    TEST_ROOT_PATH
-  );
-  assertEquals(urls.length, 1);
-});
-
-test(async function findTestModulesDirectory(): Promise<void> {
-  const urls = await findTestModules(["."], [], TEST_ROOT_PATH);
-  assertEquals(urls.length, 2);
+  const matches = await findTestModules(urls, []);
+  assertEquals(matches, urls);
 });
