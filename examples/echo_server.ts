b600add5c56bfdf2609171e0b33c098e44159dd0
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-10-07 23:20:41 +0200

    remove async function

--- examples/echo_server.ts
+++ examples/echo_server.ts
@@ -1,13 +1,9 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-const { listen, copy } = Deno;
-
-(async (): Promise<void> => {
-  const hostname = "0.0.0.0";
-  const port = 8080;
-  const listener = listen({ hostname, port });
-  console.log("listening on", addr);
-  while (true) {
-    const conn = await listener.accept();
-    copy(conn, conn);
-  }
-})();
+const hostname = "0.0.0.0";
+const port = 8080;
+const listener = Deno.listen({ hostname, port });
+console.log(`Listening on ${hostname}:${port}`);
+while (true) {
+  const conn = await listener.accept();
+  Deno.copy(conn, conn);
+}
