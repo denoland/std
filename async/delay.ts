64cabebb0218a025a6c49229977dc8cc9ac6a8b9
nurmohammed840 <46383294+nurmohammed840@users.noreply.github.com>
2021-03-03 03:09:09 +0600

    added overloading (async/delay.ts)
    ```ts
    Promise.all([
      delay(1000),
      delay(1500, "Hello, World!"),
      delay(2000, () => "function"),
      delay(2500, Promise.resolve("promise")),
      delay(2000, async () => "async function"),
    ]).then(console.log);
    ```

--- async/delay.ts
+++ async/delay.ts
@@ -1,9 +1,10 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-/* Resolves after the given number of milliseconds. */
-export function delay(ms: number): Promise<void> {
-  return new Promise((res): number =>
-    setTimeout((): void => {
-      res();
-    }, ms)
-  );
+/** Resolves after the given number of milliseconds. */
+export function delay<T>(ms: number): Promise<T>;
+export function delay<T>(ms: number, fn: () => T): Promise<T>;
+export function delay<T>(ms: number, value: T): Promise<T>;
+export function delay<T>(ms: number, arg?: any): Promise<T> {
+  return typeof arg == "function"
+    ? new Promise((res) => setTimeout(() => res(arg()), ms))
+    : new Promise((res) => setTimeout(res, ms, arg));
 }
