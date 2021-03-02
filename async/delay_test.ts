69a951a24ad8893819156c3b74c6dbc8e495750c
nurmohammed840 <46383294+nurmohammed840@users.noreply.github.com>
2021-03-03 03:35:36 +0600

    implement overloaded test. (async/delay.ts)

--- async/delay_test.ts
+++ async/delay_test.ts
@@ -10,3 +10,32 @@ Deno.test("[async] delay", async function (): Promise<void> {
   assert(result === undefined);
   assert(diff >= 100);
 });
+
+Deno.test("[async] delay overloding", async () => {
+  const objects = [
+    {
+      run() {
+        return "Function";
+      },
+      time: 1000,
+      type: "Function",
+    },
+    {
+      async run() {
+        return "Async Function";
+      },
+      time: 500,
+      type: "Async Function",
+    },
+    { run: Promise.resolve("Resolved"), time: 250, type: "Resolved" },
+    { run: "Value", time: 750, type: "Value" },
+  ];
+  for (const value of objects) {
+    const start = new Date();
+    const result = await delay(value.time, value.run);
+    const diff = new Date().getTime() - start.getTime();
+    //@ts-ignore
+    assert(result === value.type);
+    assert(diff >= value.time);
+  }
+});
