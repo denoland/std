3b4679340ae64c7610641098d4f7962fbdf5b5ce
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-03-06 22:34:46 +0100

    feat(encoding/yaml): Add support for JS types and user types

--- encoding/_yaml/type/mod.ts
+++ encoding/_yaml/type/mod.ts
@@ -6,13 +6,16 @@
 export { binary } from "./binary.ts";
 export { bool } from "./bool.ts";
 export { float } from "./float.ts";
+export { func } from "./function.ts";
 export { int } from "./int.ts";
 export { map } from "./map.ts";
 export { merge } from "./merge.ts";
 export { nil } from "./nil.ts";
 export { omap } from "./omap.ts";
 export { pairs } from "./pairs.ts";
+export { regexp } from "./regexp.ts";
 export { seq } from "./seq.ts";
 export { set } from "./set.ts";
 export { str } from "./str.ts";
 export { timestamp } from "./timestamp.ts";
+export { undefinedType } from "./undefined.ts";
