27dcc84ae1fdafa32eb77a9ab2caaa768ccea2f6
Rustam <jamakrus@gmail.com>
2021-03-08 02:19:42 +0300

    fix: encoder/decoder one instance (#790)
    Fixes #788

--- http/_io.ts
+++ http/_io.ts
@@ -5,6 +5,8 @@ import { assert } from "../_util/assert.ts";
 import { Response, ServerRequest } from "./server.ts";
 import { STATUS_TEXT } from "./http_status.ts";
 
+const encoder = new TextEncoder();
+
 export function emptyReader(): Deno.Reader {
   return {
     read(_: Uint8Array): Promise<number | null> {
@@ -173,7 +175,6 @@ export async function writeChunkedBody(
   w: BufWriter,
   r: Deno.Reader,
 ): Promise<void> {
-  const encoder = new TextEncoder();
   for await (const chunk of Deno.iter(r)) {
     if (chunk.byteLength <= 0) continue;
     const start = encoder.encode(`${chunk.byteLength.toString(16)}\r\n`);
@@ -221,7 +222,6 @@ export async function writeTrailers(
   if (undeclared.length > 0) {
     throw new TypeError(`Undeclared trailers: ${Deno.inspect(undeclared)}.`);
   }
-  const encoder = new TextEncoder();
   for (const [key, value] of trailers) {
     await writer.write(encoder.encode(`${key}: ${value}\r\n`));
   }
@@ -238,7 +238,6 @@ export async function writeResponse(
   const statusCode = r.status || 200;
   const statusText = STATUS_TEXT.get(statusCode);
   const writer = BufWriter.create(w);
-  const encoder = new TextEncoder();
   if (!statusText) {
     throw new Deno.errors.InvalidData("Bad status code");
   }
