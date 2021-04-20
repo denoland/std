27fc11e6219b5e823f1fb5180249bb2bc47f5730
getspooky <getspookydev@gmail.com>
2021-04-20 09:22:59 +0000

    fix(http): Import validateCookieName, validateCookiePath and validateCookieValue

--- http/cookie.ts
+++ http/cookie.ts
@@ -2,6 +2,11 @@
 // Structured similarly to Go's cookie.go
 // https://github.com/golang/go/blob/master/src/net/http/cookie.go
 import { assert } from "../_util/assert.ts";
+import {
+  validateCookieName,
+  validateCookiePath,
+  validateCookieValue,
+} from "../_util/rfc_6265.ts";
 import { toIMF } from "../datetime/mod.ts";
 
 export interface Cookie {
@@ -27,8 +32,6 @@ export interface Cookie {
   /** Additional key value pairs with the form "key=value" */
   unparsed?: string[];
 }
-
-const FIELD_CONTENT_REGEXP = /^(?=[\x20-\x7E]*$)[^()@<>,;:\\"\[\]?={}\s]+$/;
 
 function toString(cookie: Cookie): string {
   if (!cookie.name) {
@@ -67,7 +70,7 @@ function toString(cookie: Cookie): string {
     out.push(`SameSite=${cookie.sameSite}`);
   }
   if (cookie.path) {
-    validatePath(cookie.path);
+    validateCookiePath(cookie.path);
     out.push(`Path=${cookie.path}`);
   }
   if (cookie.expires) {
@@ -78,64 +81,6 @@ function toString(cookie: Cookie): string {
     out.push(cookie.unparsed.join("; "));
   }
   return out.join("; ");
-}
-
-/**
- * Validate Cookie Name.
- * @param name Cookie name.
- */
-function validateCookieName(name: string | undefined | null): void {
-  if (name && !FIELD_CONTENT_REGEXP.test(name)) {
-    throw new TypeError(`Invalid cookie name: "${name}".`);
-  }
-}
-
-/**
- * Validate Path Value.
- * @see https://tools.ietf.org/html/rfc6265#section-4.1.2.4
- * @param path Path value.
- */
-function validatePath(path: string | null): void {
-  if (path == null) {
-    return;
-  }
-  for (let i = 0; i < path.length; i++) {
-    const c = path.charAt(i);
-    if (
-      c < String.fromCharCode(0x20) || c > String.fromCharCode(0x7E) || c == ";"
-    ) {
-      throw new Error(
-        path + ": Invalid cookie path char '" + c + "'",
-      );
-    }
-  }
-}
-
-/**
- *Validate Cookie Value.
- * @see https://tools.ietf.org/html/rfc6265#section-4.1
- * @param value Cookie value.
- */
-function validateCookieValue(name: string, value: string | null): void {
-  if (value == null || name == null) return;
-  for (let i = 0; i < value.length; i++) {
-    const c = value.charAt(i);
-    if (
-      c < String.fromCharCode(0x21) || c == String.fromCharCode(0x22) ||
-      c == String.fromCharCode(0x2c) || c == String.fromCharCode(0x3b) ||
-      c == String.fromCharCode(0x5c) || c == String.fromCharCode(0x7f)
-    ) {
-      throw new Error(
-        "RFC2616 cookie '" + name + "' cannot have '" + c + "' as value",
-      );
-    }
-    if (c > String.fromCharCode(0x80)) {
-      throw new Error(
-        "RFC2616 cookie '" + name + "' can only have US-ASCII chars as value" +
-          c.charCodeAt(0).toString(16),
-      );
-    }
-  }
 }
 
 /**
