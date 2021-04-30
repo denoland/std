c3dfe347281ca105e8d3be4c20a7e53a05a948f7
Liam Murphy <liampm32@gmail.com>
2021-04-30 22:54:25 +1000

    feat(node): Add console module
    This just exports the `console` object.
    
    I tried enabling the Node console tests, but all of them either don't work or don't really apply.
    
    Note that Deno's console doesn't have all the same methods as Node's, so this isn't a perfect polyfill. Node has these extra properties:
    - `Console`
    - `context`
    - `profile`
    - `profileEnd`
    - `timeStamp`
    
    I've marked it as partly implemented for now, although I don't know whether those APIs are worth polyfilling.

---
+++ node/console.ts
@@ -0,0 +1,23 @@
+export default console;
+
+export const {
+  assert,
+  clear,
+  count,
+  countReset,
+  debug,
+  dir,
+  dirxml,
+  error,
+  group,
+  groupCollapsed,
+  groupEnd,
+  info,
+  log,
+  table,
+  time,
+  timeEnd,
+  timeLog,
+  trace,
+  warn,
+} = console;
