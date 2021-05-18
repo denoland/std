81f06755450ec3adf9f715c629cd1ebeb2eba0ee
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-18 22:38:53 +0900

    feat(async/mux): take AsyncIterable as source iterator (#923)

--- async/mux_async_iterator.ts
+++ async/mux_async_iterator.ts
@@ -2,7 +2,7 @@
 import { Deferred, deferred } from "./deferred.ts";
 
 interface TaggedYieldedValue<T> {
-  iterator: AsyncIterableIterator<T>;
+  iterator: AsyncIterator<T>;
   value: T;
 }
 
@@ -18,13 +18,13 @@ export class MuxAsyncIterator<T> implements AsyncIterable<T> {
   private throws: any[] = [];
   private signal: Deferred<void> = deferred();
 
-  add(iterator: AsyncIterableIterator<T>): void {
+  add(iterable: AsyncIterable<T>): void {
     ++this.iteratorCount;
-    this.callIteratorNext(iterator);
+    this.callIteratorNext(iterable[Symbol.asyncIterator]());
   }
 
   private async callIteratorNext(
-    iterator: AsyncIterableIterator<T>,
+    iterator: AsyncIterator<T>,
   ) {
     try {
       const { value, done } = await iterator.next();
@@ -63,7 +63,7 @@ export class MuxAsyncIterator<T> implements AsyncIterable<T> {
     }
   }
 
-  [Symbol.asyncIterator](): AsyncIterableIterator<T> {
+  [Symbol.asyncIterator](): AsyncIterator<T> {
     return this.iterate();
   }
 }
