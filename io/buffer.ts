1ef862b5dbe9104be39a2381c204a44dcacca710
Kitson Kelly <me@kitsonkelly.com>
2021-05-27 17:25:58 +1000

    chore: update std for TypeScript 4.3 changes

--- io/buffer.ts
+++ io/buffer.ts
@@ -82,23 +82,22 @@ export class Buffer {
     this.#off = 0;
   }
 
-  #tryGrowByReslice = (n: number) => {
+  #tryGrowByReslice(n: number) {
     const l = this.#buf.byteLength;
     if (n <= this.capacity - l) {
       this.#reslice(l + n);
       return l;
     }
     return -1;
-  };
+  }
 
-  #reslice = (len: number) => {
+  #reslice(len: number) {
     assert(len <= this.#buf.buffer.byteLength);
     this.#buf = new Uint8Array(this.#buf.buffer, 0, len);
-  };
-
-  /** Reads the next `p.length` bytes from the buffer or until the buffer is
+  } /** Reads the next `p.length` bytes from the buffer or until the buffer is
    * drained. Returns the number of bytes read. If the buffer has no data to
    * return, the return is EOF (`null`). */
+
   readSync(p: Uint8Array): number | null {
     if (this.empty()) {
       // Buffer is empty, reset to recover space.
@@ -138,7 +137,7 @@ export class Buffer {
     return Promise.resolve(n);
   }
 
-  #grow = (n: number) => {
+  #grow(n: number) {
     const m = this.length;
     // If buffer is empty, reset to recover space.
     if (m === 0 && this.#off !== 0) {
@@ -168,15 +167,14 @@ export class Buffer {
     this.#off = 0;
     this.#reslice(Math.min(m + n, MAX_SIZE));
     return m;
-  };
-
-  /** Grows the buffer's capacity, if necessary, to guarantee space for
+  } /** Grows the buffer's capacity, if necessary, to guarantee space for
    * another `n` bytes. After `.grow(n)`, at least `n` bytes can be written to
    * the buffer without another allocation. If `n` is negative, `.grow()` will
    * throw. If the buffer can't grow it will throw an error.
    *
    * Based on Go Lang's
    * [Buffer.Grow](https://golang.org/pkg/bytes/#Buffer.Grow). */
+
   grow(n: number): void {
     if (n < 0) {
       throw Error("Buffer.grow: negative count");
--- log/handlers.ts
+++ log/handlers.ts
@@ -102,7 +102,9 @@ export class FileHandler extends WriterHandler {
   protected _mode: LogMode;
   protected _openOptions: Deno.OpenOptions;
   protected _encoder = new TextEncoder();
-  #unloadCallback = () => this.destroy();
+  #unloadCallback() {
+    this.destroy();
+  }
 
   constructor(levelName: LevelName, options: FileHandlerOptions) {
     super(levelName, options);
