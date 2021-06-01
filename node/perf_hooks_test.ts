5c6f274e22ea2a7ff45fa19a7ec87ad6703a1669
Douglas DUTEIL <douglasduteil@gmail.com>
2021-06-01 13:08:36 +0200

    style: run deno --unstable lint

--- node/perf_hooks_test.ts
+++ node/perf_hooks_test.ts
@@ -1,16 +1,16 @@
-import * as perf_hooks from "./perf_hooks.ts";
+import * as perfHooks from "./perf_hooks.ts";
 import { assertEquals } from "../testing/asserts.ts";
 
 Deno.test({
   name: "[perf_hooks] performance",
   fn() {
-    assertEquals(perf_hooks.performance, performance);
+    assertEquals(perfHooks.performance, performance);
   },
 });
 
 Deno.test({
   name: "[perf_hooks] PerformanceEntry",
   fn() {
-    assertEquals(perf_hooks.PerformanceEntry, PerformanceEntry);
+    assertEquals(perfHooks.PerformanceEntry, PerformanceEntry);
   },
 });
