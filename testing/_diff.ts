4fa72646ab4be75a5d29f2b2993daed55ebc00e9
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-06-03 13:29:03 +0200

    feat: improve display

--- testing/_diff.ts
+++ testing/_diff.ts
@@ -274,15 +274,19 @@ export function diffstr(A: string, B: string) {
       return tokens;
     }
   }
-  //Try to autodetect the most suitable string rendering, starting by words-diff
+
+  // Try to autodetect the most suitable string rendering, starting by words-diff
   let wordDiff = true
   let diffResult = diff(tokenize(A, {wordDiff}), tokenize(B, {wordDiff}));
   const common = diffResult.filter(({type, value}) => type === DiffType.common && value.trim()).length
   const edited = diffResult.filter(({type}) => type === DiffType.added || type === DiffType.removed).length
-  //If edited ratio is too high switch to multi-line diff instead
-  if (edited/(common+edited) > 0.9**Math.max(1, A.search(/\n\r?/g), B.search(/\n\r?/g))) {
+
+  // If edited ratio is too high switch to multi-line diff instead
+  // We add a trailing newline to ensure that last tokens are displayed correctly
+  if (edited/(common+edited) > 0.85**Math.max(1, A.match(/\n/g)?.length ?? 0, B.match(/\n/g)?.length ?? 0)) {
     wordDiff = false
-    diffResult = diff(tokenize(A, {wordDiff}), tokenize(B, {wordDiff}));
+    diffResult = diff(tokenize(`${A}\n`, {wordDiff}), tokenize(`${B}\n`, {wordDiff}));
   }
+
   return {diffResult, wordDiff}
 }
