91f960acf265057d979bb18438e315cb5dc391e7
Luca Casonato <hello@lcas.dev>
2021-06-14 14:25:32 +0200

    BREAKING std/uuid: rework v4 and v5 module
    This commit updates README, adds better JSDoc, and updates
    implementations to use new `crypto` features.

--- uuid/v5.ts
+++ uuid/v5.ts
@@ -1,68 +1,56 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-import {
-  bytesToUuid,
-  createBuffer,
-  stringToBytes,
-  uuidToBytes,
-} from "./_common.ts";
-import { Sha1 } from "../hash/sha1.ts";
+import { bytesToUuid, uuidToBytes } from "./_common.ts";
+import { concat } from "../bytes/mod.ts";
 import { assert } from "../_util/assert.ts";
 
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * Validates the UUID v5.
- * @param id UUID value.
+ * Validate that the passed UUID is a RFC4122 v4 UUID.
+ *
+ * ```ts
+ * import { generate as generateV5, validate } from "./v5.ts";
+ *
+ * validate(await generateV5("6ba7b810-9dad-11d1-80b4-00c04fd430c8", new Uint8Array())); // true
+ * validate(crypto.randomUUID()); // false
+ * validate("this-is-not-a-uuid"); // false
+ * ```
  */
 export function validate(id: string): boolean {
   return UUID_RE.test(id);
 }
 
-/** The options used for generating a v5 uuid. */
-export interface V5Options {
-  value: string | number[];
-  namespace: string | number[];
-}
-
 /**
- * Generates a RFC4122 v5 UUID (SHA-1 namespace-based).
- * @param options Can use a namespace and value to create SHA-1 hash.
- * @param buf Can allow the UUID to be written in byte-form starting at the offset.
- * @param offset Index to start writing on the UUID bytes in buffer.
+ * Generate a RFC4122 v5 UUID (SHA-1 namespace).
+ *
+ * ```js
+ * import { generate } from "./v5.ts";
+ *
+ * const NAMESPACE_URL = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
+ *
+ * const uuid = await generate(NAMESPACE_URL, new TextEncoder().encode("python.org"));
+ * uuid === "886313e1-3b8a-5372-9b90-0c9aee199e5d" // true
+ * ```
+ *
+ * @param namespace The namespace to use, encoded as a UUID.
+ * @param data The data to hash to calculate the SHA-1 digest for the UUID.
  */
-export function generate(
-  options: V5Options,
-  buf?: number[],
-  offset?: number,
-): string | number[] {
-  const i = (buf && offset) || 0;
+export async function generate(
+  namespace: string,
+  data: Uint8Array,
+): Promise<string> {
+  // TODO(lucacasonato): validate that `namespace` is a valid UUID.
 
-  let { value, namespace } = options;
-  if (typeof value === "string") {
-    value = stringToBytes(value);
-  }
+  const space = uuidToBytes(namespace);
+  assert(space.length === 16, "namespace must be a valid UUID");
 
-  if (typeof namespace === "string") {
-    namespace = uuidToBytes(namespace);
-  }
-
-  assert(
-    namespace.length === 16,
-    "namespace must be uuid string or an Array of 16 byte values",
-  );
-
-  const content = namespace.concat(value);
-  const bytes = new Sha1().update(createBuffer(content)).digest();
+  const toHash = concat(new Uint8Array(space), data);
+  const buffer = await crypto.subtle.digest("sha-1", toHash);
+  const bytes = new Uint8Array(buffer);
 
   bytes[6] = (bytes[6] & 0x0f) | 0x50;
   bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
-  if (buf !== undefined) {
-    for (let idx = 0; idx < 16; ++idx) {
-      buf[i + idx] = bytes[idx];
-    }
-  }
-
-  return buf ?? bytesToUuid(bytes);
+  return bytesToUuid(bytes);
 }
