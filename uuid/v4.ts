9464b08760eedac884573411a8776e1d0170cc9f
Luca Casonato <hello@lcas.dev>
2021-06-16 16:10:11 +0200

    Apply suggestions from code review
    Co-authored-by: William Perron <hey@wperron.io>

--- uuid/v4.ts
+++ uuid/v4.ts
@@ -4,7 +4,7 @@ const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * Validate that the passed UUID is a RFC4122 v4 UUID.
+ * Validate that the passed UUID is an RFC4122 v4 UUID.
  *
  * ```ts
  * import { validate } from "./v4.ts";
