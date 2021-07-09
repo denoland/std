0192a85f8233dcd5c6597c2a3f1411a624d76757
Luca Casonato <hello@lcas.dev>
2021-07-09 14:27:41 +0200

    BREAKING std/uuid: rework v4 and v5 module (#971)
    Co-authored-by: William Perron <hey@wperron.io>

--- uuid/mod.ts
+++ uuid/mod.ts
@@ -1,6 +1,8 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
 // Based on https://github.com/kelektiv/node-uuid -> https://www.ietf.org/rfc/rfc4122.txt
 // Supporting Support for RFC4122 version 1, 4, and 5 UUIDs
-// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
 import * as v1 from "./v1.ts";
 import * as v4 from "./v4.ts";
 import * as v5 from "./v5.ts";
@@ -8,11 +10,17 @@ import * as v5 from "./v5.ts";
 export const NIL_UUID = "00000000-0000-0000-0000-000000000000";
 
 /**
- * Checks if UUID is nil
- * @param val UUID value
+ * Check if the passed UUID is the nil UUID.
+ *
+ * ```js
+ * import { isNil } from "./mod.ts";
+ *
+ * isNil("00000000-0000-0000-0000-000000000000") // true
+ * isNil(crypto.randomUUID()) // false
+ * ```
  */
-export function isNil(val: string): boolean {
-  return val === NIL_UUID;
+export function isNil(id: string): boolean {
+  return id === NIL_UUID;
 }
 
 export { v1, v4, v5 };
--- uuid/v4.ts
+++ uuid/v4.ts
@@ -1,23 +1,31 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-import { bytesToUuid } from "./_common.ts";
 
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * Validates the UUID v4.
- * @param id UUID value.
+ * Validate that the passed UUID is an RFC4122 v4 UUID.
+ *
+ * ```ts
+ * import { validate } from "./v4.ts";
+ * import { generate as generateV1 } from "./v1.ts";
+ *
+ * validate(crypto.randomUUID()); // true
+ * validate(generateV1() as string); // false
+ * validate("this-is-not-a-uuid"); // false
+ * ```
  */
 export function validate(id: string): boolean {
   return UUID_RE.test(id);
 }
 
-/** Generates a RFC4122 v4 UUID (pseudo-randomly-based) */
+/**
+ * @deprecated v4 UUID generation is deprecated and will be removed in a future
+ * std/uuid release. Use the web standard `globalThis.crypto.randomUUID()`
+ * function instead.
+ *
+ * Generate a RFC4122 v4 UUID (pseudo-random).
+ */
 export function generate(): string {
-  const rnds = crypto.getRandomValues(new Uint8Array(16));
-
-  rnds[6] = (rnds[6] & 0x0f) | 0x40; // Version 4
-  rnds[8] = (rnds[8] & 0x3f) | 0x80; // Variant 10
-
-  return bytesToUuid(rnds);
+  return crypto.randomUUID();
 }
