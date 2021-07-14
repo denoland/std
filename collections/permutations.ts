820a4f71d5dadc781f47959f354eb68201f6dc8a
Leon Strauss <me@lionc.de>
2021-07-14 22:27:04 +0200

    implement collections/permutations

--- collections/permutations.ts
+++ collections/permutations.ts
@@ -1,29 +1,61 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
 /**
- * Builds all possible combinations and orders of elements in the given array and the given size.
- *
- * If size is not given, it is assumed to be the length of the given array.
+ * Builds all possible orders of all elements in the given array
+ * Ignores equality of elements, meaning this will always reutrn the same
+ * number of permutations for a given length of input.
  *
  * Example:
  *
  * ```typescript
- * const numbers = [ 1, 2, 3 ]
- * const windows = permutations(words, 2)
+ * const numbers = [ 1, 2 ]
+ * const windows = permutations(numbers)
  *
  * console.assert(windows === [
  *     [ 1, 2 ],
- *     [ 1, 3 ],
  *     [ 2, 1 ],
- *     [ 2, 3 ],
- *     [ 3, 1 ],
- *     [ 3, 2 ],
  * ])
  * ```
  */
-export function permutations<T>(
-  _array: Array<T>,
-  _size?: number,
-): Array<Array<T>> {
-  throw new Error("unimplemented");
+export function permutations<T>(array: Array<T>): Array<Array<T>> {
+  const ret: Array<Array<T>> = [];
+
+  if (array.length === 0) {
+    return ret;
+  }
+
+  // Heap Algorithm
+  function heapPermutations(k: number, array: Array<T>) {
+    const c = new Array<number>(k).fill(0);
+
+    ret.push([...array]);
+
+    let i = 1;
+
+    while (i < k) {
+      if (c[i] < i) {
+        if (i % 2 === 0) {
+          const swapBuffer = array[0];
+          array[0] = array[i];
+          array[i] = swapBuffer;
+        } else {
+          const swapBuffer = array[c[i]];
+          array[c[i]] = array[i];
+          array[i] = swapBuffer;
+        }
+
+        ret.push([...array]);
+
+        c[i] += 1;
+        i = 1;
+      } else {
+        c[i] = 0;
+        i += 1;
+      }
+    }
+  }
+
+  heapPermutations(array.length, [...array]);
+
+  return ret;
 }
