0fcd92ae42ce2a43817d168944c5809f2ca484b8
Leon Strauss <me@lionc.de>
2021-07-16 06:37:53 +0900

    Add std/collections (#993)
    Co-authored-by: William Perron <hey@wperron.io>

---
+++ collections/distinct.ts
@@ -0,0 +1,19 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * Returns all distinct elements in the given array, preserving order by first occurence
+ *
+ * Example:
+ *
+ * ```typescript
+ * const numbers = [ 3, 2, 5, 2, 5 ]
+ * const distinctNumbers = distinct(numbers)
+ *
+ * console.assert(distinctNumbers === [ 3, 2, 5 ])
+ * ```
+ */
+export function distinct<T>(array: Array<T>): Array<T> {
+  const set = new Set(array);
+
+  return Array.from(set);
+}
---
+++ collections/unzip.ts
@@ -0,0 +1,34 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * Builds two separate arrays from the given array of 2-tuples, with the first returned array holding all first
+ * tuple elements and the second one holding all the second elements
+ *
+ * Example:
+ *
+ * ```typescript
+ * const parents = [
+ *     [ 'Maria', 'Jeff' ],
+ *     [ 'Anna', 'Kim' ],
+ *     [ 'John', 'Leroy' ],
+ * ]
+ * const [ moms, dads ] = unzip(parents)
+ *
+ * console.assert(moms === [ 'Maria', 'Anna', 'John' ])
+ * console.assert(moms === [ 'Jeff', 'Kim', 'Leroy' ])
+ * ```
+ */
+export function unzip<T, U>(pairs: Array<[T, U]>): [Array<T>, Array<U>] {
+  const { length } = pairs;
+  const ret: [Array<T>, Array<U>] = [
+    new Array(length),
+    new Array(length),
+  ];
+
+  pairs.forEach(([first, second], index) => {
+    ret[0][index] = first;
+    ret[1][index] = second;
+  });
+
+  return ret;
+}
