83344e909aedd018f61f0f8a929c33116d8c86da
Yoshiya Hinosawa <stibium121@gmail.com>
2021-07-23 14:33:55 +0900

    fix: use relative specifier in imports in docs

--- datetime/mod.ts
+++ datetime/mod.ts
@@ -160,7 +160,7 @@ export type DifferenceOptions = {
  * example :
  *
  * ```typescript
- * import * as datetime from "https://deno.land/std/datetime/mod.ts";
+ * import * as datetime from "./mod.ts";
  *
  * datetime.difference(new Date("2020/1/1"),new Date("2020/2/2"),{ units : ["days","months"] })
  * ```
