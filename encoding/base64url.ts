68a671f2bb9543cbadd59a544d21011dfcf48b18
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-07-26 18:08:13 +0600

    fix(encoding/base64url): throw TypeError in case of invalid base64url string (#1040)

--- encoding/base64url.ts
+++ encoding/base64url.ts
@@ -17,6 +17,10 @@ export function addPaddingToBase64url(base64url: string): string {
 }
 
 function convertBase64urlToBase64(b64url: string): string {
+  if (!/^[-_A-Z0-9]*?={0,2}$/i.test(b64url)) {
+    // Contains characters not part of base64url spec.
+    throw new TypeError("Failed to decode base64url: invalid character");
+  }
   return addPaddingToBase64url(b64url).replace(/\-/g, "+").replace(/_/g, "/");
 }
 
