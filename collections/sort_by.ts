57225d4cae7421696a0147d5b1ef91fcbe961da7
Leon Strauss <me@lionc.de>
2021-07-28 00:05:52 +0200

    :sparkles: Implement collections/sortBy and tests for it

---
+++ collections/sort_by.ts
@@ -0,0 +1,48 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * Returns all elements in the given collection, sorted by their result using the given selector
+ *
+ * Example:
+ *
+ * ```typescript
+ * const people = [
+ *     { name: 'Anna', age: 34 },
+ *     { name: 'Kim', age: 42 },
+ *     { name: 'John', age: 23 },
+ * ]
+ * const sortedByAge = sortBy(people, it => it.age)
+ *
+ * console.assert(sortedByAge === [
+ *     { name: 'John', age: 23 },
+ *     { name: 'Anna', age: 34 },
+ *     { name: 'Kim', age: 42 },
+ * ])
+ * ```
+ */
+export function sortBy<T>(
+  array: Array<T>,
+  selector:
+      | ((el: T) => number)
+      | ((el: T) => string)
+      | ((el: T) => bigint)
+      | ((el: T) => boolean)
+      | ((el: T) => Date),
+): Array<T> {
+  const ret = Array.from(array)
+
+  return ret.sort((a, b) => {
+    const selectedA = selector(a);
+    const selectedB = selector(b);
+
+    if (selectedA > selectedB) {
+      return 1;
+    }
+
+    if (selectedA < selectedB) {
+      return -1;
+    }
+
+    return 0;
+  });
+}
