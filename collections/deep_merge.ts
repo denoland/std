878a93b1ea6ffaea92a144d50a1dac4d5cb033ee
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-07-28 19:39:37 +0200

    feat(collections): continue deepMerge

--- collections/deep_merge.ts
+++ collections/deep_merge.ts
@@ -1,14 +1,18 @@
 /** Deep merge options */
 interface deepMergeOptions {
   /** Merging strategy for arrays */
-  arrays?:"replace"|"concat",
+  arrays?: "replace" | "concat";
   /** Merging strategy for Maps */
-  maps?:"replace"|"concat",
+  maps?: "replace" | "concat";
   /** Merging strategy for Sets */
-  sets?:"replace"|"concat",
+  sets?: "replace" | "concat";
   /** Whether to include non enumerable properties */
-  includeNonEnumerable?:boolean
+  includeNonEnumerable?: boolean;
 }
+
+//TypeScript does not support 'symbol' as index type currently though it's perfectly valid
+//deno-lint-ignore no-explicit-any
+type propertyKey = any;
 
 /**
  * Merges the two given Records, recursively merging any nested Records with the second collection overriding the first in case of conflict
@@ -16,74 +20,95 @@ interface deepMergeOptions {
  * For arrays, maps and sets, a merging strategy can be specified to either "replace" values, or "concat" them instead.
  * Use "includeNonEnumerable" option to include non enumerable properties too.
  */
-export function deepMerge<T extends Record<keyof any, unknown>>(collection: Partial<T>, other: Partial<T>, options?:deepMergeOptions): T
-export function deepMerge<T extends Record<keyof any, unknown>, U extends Record<keyof unknown, unknown>>(collection: T, other: U, options?:deepMergeOptions): T & U
-export function deepMerge(collection:any, other:any, {arrays = "replace", maps = "replace", sets = "replace", includeNonEnumerable = false} = {}) {
-
+export function deepMerge<
+  T extends Record<PropertyKey, unknown>,
+  U extends Record<PropertyKey, unknown>,
+>(
+  collection: T,
+  other: U,
+  {
+    arrays = "replace",
+    maps = "replace",
+    sets = "replace",
+    includeNonEnumerable = false,
+  }: deepMergeOptions = {},
+): T & U {
   //Extract property and symbols
-  const keys = [...Object.getOwnPropertyNames(other), ...Object.getOwnPropertySymbols(other)].filter(key => includeNonEnumerable || other.propertyIsEnumerable(key))
+  const keys = [
+    ...Object.getOwnPropertyNames(other),
+    ...Object.getOwnPropertySymbols(other),
+  ].filter((key) => includeNonEnumerable || other.propertyIsEnumerable(key));
 
   //Iterate through each key of other object and use correct merging strategy
-  for (const key of keys) {
-    const a = collection[key], b = other[key]
+  for (const key of keys as propertyKey[]) {
+    const a = collection[key], b = other[key];
 
     //Handle arrays
-    if ((Array.isArray(a))&&(Array.isArray(b))) {
-      if (arrays === "concat")
-        collection[key] = a.concat(...b)
-      else
-        collection[key] = b
-      continue
+    if ((Array.isArray(a)) && (Array.isArray(b))) {
+      if (arrays === "concat") {
+        collection[key] = a.concat(...b);
+      } else {
+        collection[key] = b;
+      }
+      continue;
     }
 
     //Handle maps
-    if ((a instanceof Map)&&(b instanceof Map)) {
+    if ((a instanceof Map) && (b instanceof Map)) {
       if (maps === "concat") {
-        for (const [k, v] of b.entries())
-          a.set(k, v)
+        for (const [k, v] of b.entries()) {
+          a.set(k, v);
+        }
+      } else {
+        collection[key] = b;
       }
-      else
-        collection[key] = b
-      continue
+      continue;
     }
 
     //Handle sets
-    if ((a instanceof Set)&&(b instanceof Set)) {
+    if ((a instanceof Set) && (b instanceof Set)) {
       if (sets === "concat") {
-        for (const v of b.values())
-          a.add(v)
+        for (const v of b.values()) {
+          a.add(v);
+        }
+      } else {
+        collection[key] = b;
       }
-      else
-        collection[key] = b
-      continue
+      continue;
     }
 
     //Recursively merge mergeable objects
-    if (isMergeable(a) && isMergeable(b)) {
-      collection[key] = deepMerge(collection[key] ?? {}, b)
-      continue
+    if (isMergeable<T | U>(a) && isMergeable<T | U>(b)) {
+      collection[key] = deepMerge(a ?? {}, b);
+      continue;
     }
 
     //Override value
-    collection[key] = b
+    collection[key] = b;
   }
 
-  return collection
+  return collection as T & U;
 }
 
 /**
  * Test whether a value is mergeable or not
  * Builtins object like, null and user classes are not considered mergeable
  */
-function isMergeable(value:unknown) {
+function isMergeable<T>(value: unknown): value is T {
   //Ignore null
-  if (value === null)
-    return false
+  if (value === null) {
+    return false;
+  }
   //Ignore builtins
-  if ((value instanceof RegExp)||(value instanceof Date))
-    return false
+  if (
+    (value instanceof RegExp) || (value instanceof Date) ||
+    (value instanceof Array)
+  ) {
+    return false;
+  }
   //Ignore classes
-  if ((typeof value === "object")&&("constructor" in value!))
-    return !/^class /.test(value.constructor.toString())
-  return typeof value === "object"
+  if ((typeof value === "object") && ("constructor" in value!)) {
+    return !/^class /.test(value.constructor.toString());
+  }
+  return typeof value === "object";
 }
--- collections/deep_merge_test.ts
+++ collections/deep_merge_test.ts
@@ -1,189 +1,265 @@
 import { assertEquals } from "../testing/asserts.ts";
+import { deepMerge } from "./deep_merge.ts";
 
 Deno.test("deepMerge: simple merge", () => {
-  assertEquals(deepMerge({
-    foo:true
-  }, {
-    bar:true
-  }), {
-    foo:true,
-    bar:true
-  })
-})
+  assertEquals(
+    deepMerge({
+      foo: true,
+    }, {
+      bar: true,
+    }),
+    {
+      foo: true,
+      bar: true,
+    },
+  );
+});
 
 Deno.test("deepMerge: symbol merge", () => {
-  assertEquals(deepMerge({}, {
-    [Symbol.for("deepmerge.test")]:true
-  }), {
-    [Symbol.for("deepmerge.test")]:true
-  })
-})
+  assertEquals(
+    deepMerge({}, {
+      [Symbol.for("deepmerge.test")]: true,
+    }),
+    {
+      [Symbol.for("deepmerge.test")]: true,
+    },
+  );
+});
 
 Deno.test("deepMerge: ignore non enumerable", () => {
-  assertEquals(deepMerge({}, Object.defineProperties({}, {
-    foo:{enumerable:false, value:true},
-    bar:{enumerable:true, value:true}
-  })), {
-    bar:true
-  })
-})
+  assertEquals(
+    deepMerge(
+      {},
+      Object.defineProperties({}, {
+        foo: { enumerable: false, value: true },
+        bar: { enumerable: true, value: true },
+      }),
+    ),
+    {
+      bar: true,
+    },
+  );
+});
 
 Deno.test("deepMerge: include non enumerable", () => {
-  assertEquals(deepMerge({}, Object.defineProperties({}, {
-    foo:{enumerable:false, value:true},
-    bar:{enumerable:true, value:true}
-  }), {includeNonEnumerable:true}), {
-    foo:true,
-    bar:true
-  })
-})
+  assertEquals(
+    deepMerge(
+      {},
+      Object.defineProperties({}, {
+        foo: { enumerable: false, value: true },
+        bar: { enumerable: true, value: true },
+      }),
+      { includeNonEnumerable: true },
+    ),
+    {
+      foo: true,
+      bar: true,
+    },
+  );
+});
 
 Deno.test("deepMerge: nested merge", () => {
-  assertEquals(deepMerge({
-    foo:{
-      bar:true
-    }
-  }, {
-    foo:{
-      baz:true,
-      quux:{}
+  assertEquals(
+    deepMerge({
+      foo: {
+        bar: true,
+      },
+    }, {
+      foo: {
+        baz: true,
+        quux: {},
+      },
+      qux: true,
+    }),
+    {
+      foo: {
+        bar: true,
+        baz: true,
+        quux: {},
+      },
+      qux: true,
     },
-    qux:true
-  }), {
-    foo:{
-      bar:true,
-      baz:true,
-      quux:{}
-    },
-    qux:true
-  })
-})
+  );
+});
 
 Deno.test("deepMerge: override target (non-mergeable source)", () => {
-  assertEquals(deepMerge({
-    foo:{
-      bar:true
-    }
-  }, {
-    foo:true
-  }), {
-    foo:true
-  })
-})
+  assertEquals(
+    deepMerge({
+      foo: {
+        bar: true,
+      },
+    }, {
+      foo: true,
+    }),
+    {
+      foo: true,
+    },
+  );
+});
 
-Deno.test("deepMerge: override target (non-mergeable destination #1)", () => {
-  const CustomClass = class {}
-  assertEquals(deepMerge({
-    foo:new CustomClass()
-  }, {
-    foo:true
-  }), {
-    foo:true
-  })
-})
+Deno.test("deepMerge: override target (non-mergeable destination, object like)", () => {
+  const CustomClass = class {};
+  assertEquals(
+    deepMerge({
+      foo: new CustomClass(),
+    }, {
+      foo: true,
+    }),
+    {
+      foo: true,
+    },
+  );
+});
 
-Deno.test("deepMerge: override target (non-mergeable destination #2)", () => {
-  assertEquals(deepMerge({
-    foo:[]
-  }, {
-    foo:true
-  }), {
-    foo:true
-  })
-})
+Deno.test("deepMerge: override target (non-mergeable destination, array like)", () => {
+  assertEquals(
+    deepMerge({
+      foo: [],
+    }, {
+      foo: true,
+    }),
+    {
+      foo: true,
+    },
+  );
+});
+
+Deno.test("deepMerge: override target (different object like source and destination)", () => {
+  assertEquals(
+    deepMerge({
+      foo: {},
+    }, {
+      foo: [1, 2],
+    }),
+    {
+      foo: [1, 2],
+    },
+  );
+  assertEquals(
+    deepMerge({
+      foo: [],
+    }, {
+      foo: { bar: true },
+    }),
+    {
+      foo: { bar: true },
+    },
+  );
+});
 
 Deno.test("deepMerge: primitive types handling", () => {
-  const CustomClass = class {}
+  const CustomClass = class {};
   const expected = {
-    boolean:true,
-    null:null,
-    undefined:undefined,
-    number:1,
-    bigint:1n,
-    string:"string",
-    symbol:Symbol.for("deepmerge.test"),
-    object:{foo:true},
-    regexp:/regex/,
-    date:new Date(),
+    boolean: true,
+    null: null,
+    undefined: undefined,
+    number: 1,
+    bigint: 1n,
+    string: "string",
+    symbol: Symbol.for("deepmerge.test"),
+    object: { foo: true },
+    regexp: /regex/,
+    date: new Date(),
     function() {},
     async async() {},
-    arrow:() => {},
-    class:new CustomClass(),
-  }
-  assertEquals(deepMerge({
-    boolean:false,
-    null:undefined,
-    undefined:null,
-    number:-1,
-    bigint:-1n,
-    string:"foo",
-    symbol:Symbol(),
-    object:null,
-    regexp:/foo/,
-    date:new Date(0),
-    function:function () {},
-    async: async function() {},
     arrow: () => {},
-    class:null,
-  }, expected), expected)
-})
+    class: new CustomClass(),
+  };
+  assertEquals(
+    deepMerge({
+      boolean: false,
+      null: undefined,
+      undefined: null,
+      number: -1,
+      bigint: -1n,
+      string: "foo",
+      symbol: Symbol(),
+      object: null,
+      regexp: /foo/,
+      date: new Date(0),
+      function: function () {},
+      async: async function () {},
+      arrow: () => {},
+      class: null,
+    }, expected),
+    expected,
+  );
+});
 
 Deno.test("deepMerge: array merge (replace)", () => {
-  assertEquals(deepMerge({
-    foo:[1, 2, 3]
-  }, {
-    foo:[4, 5, 6]
-  }), {
-    foo:[4, 5, 6]
-  })
-})
+  assertEquals(
+    deepMerge({
+      foo: [1, 2, 3],
+    }, {
+      foo: [4, 5, 6],
+    }),
+    {
+      foo: [4, 5, 6],
+    },
+  );
+});
 
 Deno.test("deepMerge: array merge (concat)", () => {
-  assertEquals(deepMerge({
-    foo:[1, 2, 3]
-  }, {
-    foo:[4, 5, 6]
-  }, {arrays:"concat"}), {
-    foo:[1, 2, 3, 4, 5, 6]
-  })
-})
+  assertEquals(
+    deepMerge({
+      foo: [1, 2, 3],
+    }, {
+      foo: [4, 5, 6],
+    }, { arrays: "concat" }),
+    {
+      foo: [1, 2, 3, 4, 5, 6],
+    },
+  );
+});
 
 Deno.test("deepMerge: maps merge (replace)", () => {
-  assertEquals(deepMerge({
-    map:new Map([["foo", true]])
-  }, {
-    map:new Map([["bar", true]])
-  }), {
-    map:new Map([["bar", true]])
-  })
-})
+  assertEquals(
+    deepMerge({
+      map: new Map([["foo", true]]),
+    }, {
+      map: new Map([["bar", true]]),
+    }),
+    {
+      map: new Map([["bar", true]]),
+    },
+  );
+});
 
 Deno.test("deepMerge: maps merge (concat)", () => {
-  assertEquals(deepMerge({
-    map:new Map([["foo", true]])
-  }, {
-    map:new Map([["bar", true]])
-  }, {maps:"concat"}), {
-    map:new Map([["foo", true], ["bar", true]])
-  })
-})
+  assertEquals(
+    deepMerge({
+      map: new Map([["foo", true]]),
+    }, {
+      map: new Map([["bar", true]]),
+    }, { maps: "concat" }),
+    {
+      map: new Map([["foo", true], ["bar", true]]),
+    },
+  );
+});
 
 Deno.test("deepMerge: sets merge (replace)", () => {
-  assertEquals(deepMerge({
-    set:new Set(["foo"])
-  }, {
-    set:new Set(["bar"])
-  }), {
-    set:new Set(["bar"])
-  })
-})
+  assertEquals(
+    deepMerge({
+      set: new Set(["foo"]),
+    }, {
+      set: new Set(["bar"]),
+    }),
+    {
+      set: new Set(["bar"]),
+    },
+  );
+});
 
 Deno.test("deepMerge: sets merge (concat)", () => {
-  assertEquals(deepMerge({
-    set:new Set(["foo"])
-  }, {
-    set:new Set(["bar"])
-  }, {sets:"concat"}), {
-    set:new Set(["foo", "bar"])
-  })
-})
+  assertEquals(
+    deepMerge({
+      set: new Set(["foo"]),
+    }, {
+      set: new Set(["bar"]),
+    }, { sets: "concat" }),
+    {
+      set: new Set(["foo", "bar"]),
+    },
+  );
+});
