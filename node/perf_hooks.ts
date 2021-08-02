9a270e4242119599e8ab1f212f62d8ccfe3d9596
JayaKrishnaNamburu <namburu1995@gmail.com>
2021-08-03 00:14:58 +0530

    Adding shim for perf_hooks in demo

--- node/perf_hooks.ts
+++ node/perf_hooks.ts
@@ -1,5 +1,44 @@
-export const { performance, PerformanceEntry } = globalThis as
-  & typeof globalThis
-  & {
+function unimplemented(functionName: string) {
+  throw new Error(
+    `Node.js performance method ${functionName} is not currently supported by JSPM core in the browser`,
+  );
+}
+
+const { PerformanceObserver, PerformanceEntry, performance: shimPerformance } =
+  globalThis as typeof globalThis & {
     PerformanceEntry: PerformanceEntry;
+    PerformanceObserver: PerformanceObserver;
   };
+const constants = {};
+
+const performance: Partial<Performance> & {
+  eventLoopUtilization: void;
+  nodeTiming: Record<string, string>;
+  timerify: void;
+} = {
+  clearMarks: shimPerformance.clearMarks,
+  eventLoopUtilization: unimplemented("eventLoopUtilization"),
+  mark: shimPerformance.mark,
+  measure: shimPerformance.measure,
+  nodeTiming: {},
+  now: shimPerformance.now,
+  timerify: unimplemented("timerify"),
+  timeOrigin: shimPerformance.timeOrigin,
+};
+
+const monitorEventLoopDelay = unimplemented("monitorEventLoopDelay");
+
+export default {
+  performance,
+  PerformanceObserver,
+  monitorEventLoopDelay,
+  constants,
+};
+
+export {
+  constants,
+  monitorEventLoopDelay,
+  performance,
+  PerformanceEntry,
+  PerformanceObserver,
+};
