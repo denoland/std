01b330534955e04ee215e36040966b0bd721f683
Yoshiya Hinosawa <stibium121@gmail.com>
2021-08-03 18:07:21 +0900

    fix: fix type errors

--- node/perf_hooks.ts
+++ node/perf_hooks.ts
@@ -3,28 +3,36 @@ import { notImplemented } from "./_utils.ts";
 const { PerformanceObserver, PerformanceEntry, performance: shimPerformance } =
   globalThis as typeof globalThis & {
     PerformanceEntry: PerformanceEntry;
-    PerformanceObserver: PerformanceObserver;
+    // deno-lint-ignore no-explicit-any
+    PerformanceObserver: any;
   };
 const constants = {};
 
 const performance: Partial<Performance> & {
-  eventLoopUtilization: void;
+  // deno-lint-ignore no-explicit-any
+  eventLoopUtilization: any;
   nodeTiming: Record<string, string>;
-  timerify: void;
+  // deno-lint-ignore no-explicit-any
+  timerify: any;
+  // deno-lint-ignore no-explicit-any
+  timeOrigin: any;
 } = {
   clearMarks: shimPerformance.clearMarks,
-  eventLoopUtilization: notImplemented("eventLoopUtilization from performance"),
+  eventLoopUtilization: () =>
+    notImplemented("eventLoopUtilization from performance"),
   mark: shimPerformance.mark,
   measure: shimPerformance.measure,
   nodeTiming: {},
   now: shimPerformance.now,
-  timerify: notImplemented("timerify from performance"),
-  timeOrigin: shimPerformance.timeOrigin,
+  timerify: () => notImplemented("timerify from performance"),
+  // deno-lint-ignore no-explicit-any
+  timeOrigin: (shimPerformance as any).timeOrigin,
 };
 
-const monitorEventLoopDelay = notImplemented(
-  "monitorEventLoopDelay from performance",
-);
+const monitorEventLoopDelay = () =>
+  notImplemented(
+    "monitorEventLoopDelay from performance",
+  );
 
 export default {
   performance,
--- node/perf_hooks_test.ts
+++ node/perf_hooks_test.ts
@@ -4,7 +4,9 @@ import { assertEquals } from "../testing/asserts.ts";
 Deno.test({
   name: "[perf_hooks] performance",
   fn() {
-    assertEquals(perfHooks.performance, performance);
+    assertEquals(perfHooks.performance.clearMarks, performance.clearMarks);
+    assertEquals(perfHooks.performance.mark, performance.mark);
+    assertEquals(perfHooks.performance.now, performance.now);
   },
 });
 
