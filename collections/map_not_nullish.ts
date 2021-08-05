a4cddf13a91f8e4f50f289892b85008884e4ff53
Leon Strauss <me@lionc.de>
2021-08-05 19:54:47 +0200

    :sparkles: Implement collections/mapNotNullish and tests for it

---
+++ collections/map_not_nullish.ts
@@ -0,0 +1,36 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * Returns a new array, containing all elements in the given array transformed using the given transformer, except the ones
+ * that were transformed to `null` or `undefined`
+ *
+ * Example:
+ *
+ * ```typescript
+ * const people = [
+ *     { middleName: null },
+ *     { middleName: 'William' },
+ *     { middleName: undefined },
+ *     { middleName: 'Martha' },
+ * ]
+ * const foundMiddleNames = mapNotNullish(people, it => it.middleName)
+ *
+ * console.assert(foundMiddleNames === [ 'William', 'Martha' ])
+ * ```
+ */
+export function mapNotNullish<T, O>(
+  array: Array<T>,
+  transformer: (el: T) => O | undefined | null,
+): Array<O> {
+  const ret = new Array<O>();
+
+  for (const element of array) {
+    const transformedElement = transformer(element);
+
+    if (transformedElement !== undefined && transformedElement !== null) {
+      ret.push(transformedElement);
+    }
+  }
+
+  return ret;
+}
