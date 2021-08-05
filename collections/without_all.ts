9b7451a4d6a3e810291bf33204661140e83c6d65
getspooky <getspookydev@gmail.com>
2021-08-05 11:01:30 +0100

    docs(collections): Returns an array excluding all given values

---
+++ collections/without_all.ts
@@ -0,0 +1,33 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+/** Used as the size to enable large array optimizations. */
+const LARGE_ARRAY_SIZE = 200;
+
+/**
+ * Returns an array excluding all given values.
+ *
+ * Example:
+ *
+ * ```ts
+ * import { withoutAll } from "./without.ts";
+ *
+ * const withoutList = withoutAll([2, 1, 2, 3], [1, 2]);
+ *
+ * console.assert(withoutList === [ 3 ])
+ * ```
+ */
+export function withoutAll<T>(
+  array: Array<T>,
+  values: Array<T>,
+): Array<T> {
+  if (array.length >= LARGE_ARRAY_SIZE) {
+    const set = new Set<T>();
+
+    for (const element of array) {
+      set.add(element);
+    }
+
+    array = Array.from(set);
+  }
+  return array.filter((value) => values.indexOf(value) === -1);
+}
