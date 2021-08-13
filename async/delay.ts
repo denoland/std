dc21dcad5e511a56b15e1552689c945df33f09c0
Justin Chase <justin.m.chase@gmail.com>
2021-08-13 16:38:33 -0500

    Throw on abort

--- async/delay.ts
+++ async/delay.ts
@@ -1,13 +1,16 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 /* Resolves after the given number of milliseconds. */
 export function delay(ms: number, signal?: AbortSignal): Promise<void> {
-  return new Promise((res): void => {
-    const done = () => {
+  return new Promise((resolve, reject): void => {
+    const abort = () => {
       clearTimeout(i);
-      signal?.removeEventListener("abort", done);
-      res();
+      reject(new DOMException("Delay was aborted.", "AbortError"));
+    };
+    const done = () => {
+      signal?.removeEventListener("abort", abort);
+      resolve();
     };
     const i = setTimeout(done, ms);
-    signal?.addEventListener("abort", done, { once: true });
+    signal?.addEventListener("abort", abort, { once: true });
   });
 }
