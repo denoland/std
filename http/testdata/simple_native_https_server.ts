4cd208c8b792b45733f34ac7229591900d1cbd36
cmorten <craig.morten@hotmail.co.uk>
2021-08-14 11:11:05 +0100

    refactor: touch-ups to simple native server examples

--- http/testdata/simple_native_https_server.ts
+++ http/testdata/simple_native_https_server.ts
@@ -1,15 +1,17 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 // This is an example of a https server
 import { listenAndServeTls } from "../native_server.ts";
+import { dirname, fromFileUrl, join } from "../../path/mod.ts";
 
+const moduleDir = dirname(fromFileUrl(import.meta.url));
 const tlsOptions = {
   hostname: "localhost",
-  port: 4503,
-  certFile: "./testdata/tls/localhost.crt",
-  keyFile: "./testdata/tls/localhost.key",
+  port: 4505,
+  certFile: join(moduleDir, "tls/localhost.crt"),
+  keyFile: join(moduleDir, "tls/localhost.key"),
 };
 console.log(
-  `Simple HTTPS server listening on ${tlsOptions.hostname}:${tlsOptions.port}`,
+  `Simple HTTPS server listening on https://${tlsOptions.hostname}:${tlsOptions.port}/`,
 );
 const body = new TextEncoder().encode("Hello HTTPS");
 await listenAndServeTls(tlsOptions, () => new Response(body));
--- http/testdata/simple_native_server.ts
+++ http/testdata/simple_native_server.ts
@@ -2,6 +2,6 @@
 // This is an example of a server that responds with an empty body
 import { listenAndServe } from "../native_server.ts";
 
-const options = { hostname: "0.0.0.0", port: 4502 };
-console.log(`Simple server listening on ${options.hostname}:${options.port}`);
+const options = { hostname: "0.0.0.0", port: 4504 };
+console.log(`Simple server listening on http://${options.hostname}:${options.port}/`);
 await listenAndServe(options, () => new Response());
