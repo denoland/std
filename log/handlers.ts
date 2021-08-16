18e18e5a1d868131ff07c3b4033d478119b338f2
timreichen <tim.reichen@gmail.com>
2021-08-16 12:32:25 +0200

    initial commit

--- log/handlers.ts
+++ log/handlers.ts
@@ -1,98 +1,72 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-import { getLevelByName, LevelName, LogLevels } from "./levels.ts";
+
+import { LogLevel, logLevels } from "./levels.ts";
 import type { LogRecord } from "./logger.ts";
-import { blue, bold, red, yellow } from "../fmt/colors.ts";
-import { exists, existsSync } from "../fs/exists.ts";
+import { blue, red, yellow } from "../fmt/colors.ts";
+import { existsSync } from "../fs/exists.ts";
 import { BufWriterSync } from "../io/bufio.ts";
 
-const DEFAULT_FORMATTER = "{levelName} {msg}";
-type FormatterFunction = (logRecord: LogRecord) => string;
-type LogMode = "a" | "w" | "x";
-
-interface HandlerOptions {
-  formatter?: string | FormatterFunction;
+function defaultFormatter({ logLevel, message, args }: LogRecord) {
+  return `${logLevel.name} ${[message, ...args].join(" ")}`;
 }
 
-export class BaseHandler {
-  level: number;
-  levelName: LevelName;
-  formatter: string | FormatterFunction;
+type FormatterFunction = (logRecord: LogRecord) => string;
+interface HandlerOptions {
+  formatter?: FormatterFunction;
+}
 
-  constructor(levelName: LevelName, options: HandlerOptions = {}) {
-    this.level = getLevelByName(levelName);
-    this.levelName = levelName;
+type HandlerFunctions = {
+  [code: number]: (message: string) => void;
+};
 
-    this.formatter = options.formatter || DEFAULT_FORMATTER;
+export class Handler {
+  readonly logLevel: LogLevel;
+  readonly formatter: FormatterFunction;
+  readonly handlerFunctions: HandlerFunctions = {};
+
+  constructor(
+    logLevel: LogLevel,
+    { formatter = defaultFormatter }: HandlerOptions = {},
+  ) {
+    this.logLevel = logLevel;
+    this.formatter = formatter;
+  }
+
+  addLogLevel(logLevel: LogLevel, fn: (message: string) => void) {
+    this.handlerFunctions[logLevel.code] = fn;
+  }
+  removeLogLevel(logLevel: LogLevel) {
+    delete this.handlerFunctions[logLevel.code];
   }
 
   handle(logRecord: LogRecord): void {
-    if (this.level > logRecord.level) return;
-
-    const msg = this.format(logRecord);
-    return this.log(msg);
-  }
-
-  format(logRecord: LogRecord): string {
-    if (this.formatter instanceof Function) {
-      return this.formatter(logRecord);
+    if (this.logLevel.code > logRecord.logLevel.code) return;
+    const fn = this.handlerFunctions[logRecord.logLevel.code];
+    if (!fn) {
+      throw Error(
+        `logLevel ${logRecord.logLevel.code} ${logRecord.logLevel.name} is not supported.`,
+      );
     }
-
-    return this.formatter.replace(/{(\S+)}/g, (match, p1): string => {
-      const value = logRecord[p1 as keyof LogRecord];
-
-      // do not interpolate missing values
-      if (value == null) {
-        return match;
-      }
-
-      return String(value);
-    });
-  }
-
-  log(_msg: string): void {}
-  async setup() {}
-  async destroy() {}
-}
-
-export class ConsoleHandler extends BaseHandler {
-  format(logRecord: LogRecord): string {
-    let msg = super.format(logRecord);
-
-    switch (logRecord.level) {
-      case LogLevels.INFO:
-        msg = blue(msg);
-        break;
-      case LogLevels.WARNING:
-        msg = yellow(msg);
-        break;
-      case LogLevels.ERROR:
-        msg = red(msg);
-        break;
-      case LogLevels.CRITICAL:
-        msg = bold(red(msg));
-        break;
-      default:
-        break;
-    }
-
-    return msg;
-  }
-
-  log(msg: string): void {
-    console.log(msg);
+    const message = this.formatter(logRecord);
+    fn(message);
   }
 }
 
-export abstract class WriterHandler extends BaseHandler {
+export class ConsoleHandler extends Handler {
+  handlerFunctions = {
+    [logLevels.trace.code]: (message: string) => console.log(message),
+    [logLevels.debug.code]: (message: string) => console.log(message),
+    [logLevels.info.code]: (message: string) => console.log(blue(message)),
+    [logLevels.warn.code]: (message: string) => console.log(yellow(message)),
+    [logLevels.error.code]: (message: string) => console.log(red(message)),
+  };
+}
+
+export abstract class WriterHandler extends Handler {
   protected _writer!: Deno.Writer;
-  #encoder = new TextEncoder();
-
-  abstract log(msg: string): void;
-}
-
-interface FileHandlerOptions extends HandlerOptions {
-  filename: string;
-  mode?: LogMode;
+  abstract open(): void;
+  abstract write(message: string): void;
+  abstract close(): void;
 }
 
 export class FileHandler extends WriterHandler {
@@ -102,12 +76,19 @@ export class FileHandler extends WriterHandler {
   protected _mode: LogMode;
   protected _openOptions: Deno.OpenOptions;
   protected _encoder = new TextEncoder();
-  #unloadCallback() {
-    this.destroy();
-  }
 
-  constructor(levelName: LevelName, options: FileHandlerOptions) {
-    super(levelName, options);
+  #unloadCallback = (): void => this.close();
+
+  handlerFunctions = {
+    [logLevels.trace.code]: (message: string) => this.write(message),
+    [logLevels.debug.code]: (message: string) => this.write(message),
+    [logLevels.info.code]: (message: string) => this.write(message),
+    [logLevels.warn.code]: (message: string) => this.write(message),
+    [logLevels.error.code]: (message: string) => this.write(message),
+  };
+
+  constructor(logLevel: LogLevel, options: FileHandlerOptions) {
+    super(logLevel, options);
     this._filename = options.filename;
     // default to append mode, write only
     this._mode = options.mode ? options.mode : "a";
@@ -120,42 +101,35 @@ export class FileHandler extends WriterHandler {
     };
   }
 
-  async setup() {
-    this._file = await Deno.open(this._filename, this._openOptions);
+  open(): void {
+    this._file = Deno.openSync(this._filename, this._openOptions);
     this._writer = this._file;
     this._buf = new BufWriterSync(this._file);
 
-    addEventListener("unload", this.#unloadCallback.bind(this));
+    addEventListener("unload", this.#unloadCallback);
   }
-
-  handle(logRecord: LogRecord): void {
-    super.handle(logRecord);
-
-    // Immediately flush if log level is higher than ERROR
-    if (logRecord.level > LogLevels.ERROR) {
-      this.flush();
-    }
+  write(message: string): void {
+    if (!this._buf) this.open();
+    this._buf.writeSync(this._encoder.encode(message + "\n"));
   }
-
-  log(msg: string): void {
-    this._buf.writeSync(this._encoder.encode(msg + "\n"));
+  close(): void {
+    this.flush();
+    this._file?.close();
+    this._file = undefined;
+    removeEventListener("unload", this.#unloadCallback);
   }
-
   flush(): void {
     if (this._buf?.buffered() > 0) {
       this._buf.flush();
     }
   }
-
-  destroy() {
-    this.flush();
-    this._file?.close();
-    this._file = undefined;
-    removeEventListener("unload", this.#unloadCallback);
-    return Promise.resolve();
-  }
 }
 
+type LogMode = "a" | "w" | "x";
+interface FileHandlerOptions extends HandlerOptions {
+  filename: string;
+  mode?: LogMode;
+}
 interface RotatingFileHandlerOptions extends FileHandlerOptions {
   maxBytes: number;
   maxBackupCount: number;
@@ -166,56 +140,56 @@ export class RotatingFileHandler extends FileHandler {
   #maxBackupCount: number;
   #currentFileSize = 0;
 
-  constructor(levelName: LevelName, options: RotatingFileHandlerOptions) {
-    super(levelName, options);
+  constructor(logLevel: LogLevel, options: RotatingFileHandlerOptions) {
+    super(logLevel, options);
     this.#maxBytes = options.maxBytes;
     this.#maxBackupCount = options.maxBackupCount;
   }
 
-  async setup() {
+  open(): void {
     if (this.#maxBytes < 1) {
-      this.destroy();
+      this.close();
       throw new Error("maxBytes cannot be less than 1");
     }
     if (this.#maxBackupCount < 1) {
-      this.destroy();
+      this.close();
       throw new Error("maxBackupCount cannot be less than 1");
     }
-    await super.setup();
+    super.open();
 
-    if (this._mode === "w") {
-      // Remove old backups too as it doesn't make sense to start with a clean
-      // log file, but old backups
-      for (let i = 1; i <= this.#maxBackupCount; i++) {
-        if (await exists(this._filename + "." + i)) {
-          await Deno.remove(this._filename + "." + i);
+    switch (this._mode) {
+      case "w":
+        for (let i = 1; i <= this.#maxBackupCount; i++) {
+          if (existsSync(this._filename + "." + i)) {
+            Deno.removeSync(this._filename + "." + i);
+          }
         }
-      }
-    } else if (this._mode === "x") {
-      // Throw if any backups also exist
-      for (let i = 1; i <= this.#maxBackupCount; i++) {
-        if (await exists(this._filename + "." + i)) {
-          this.destroy();
-          throw new Deno.errors.AlreadyExists(
-            "Backup log file " + this._filename + "." + i + " already exists",
-          );
+        break;
+      case "x":
+        for (let i = 1; i <= this.#maxBackupCount; i++) {
+          if (existsSync(this._filename + "." + i)) {
+            this.close();
+            throw new Deno.errors.AlreadyExists(
+              "Backup log file " + this._filename + "." + i + " already exists",
+            );
+          }
         }
-      }
-    } else {
-      this.#currentFileSize = (await Deno.stat(this._filename)).size;
+        break;
+      default:
+        this.#currentFileSize = (Deno.statSync(this._filename)).size;
+        break;
     }
   }
+  write(message: string): void {
+    const messageByteLength = this._encoder.encode(message).byteLength + 1;
 
-  log(msg: string): void {
-    const msgByteLength = this._encoder.encode(msg).byteLength + 1;
-
-    if (this.#currentFileSize + msgByteLength > this.#maxBytes) {
+    if (this.#currentFileSize + messageByteLength > this.#maxBytes) {
       this.rotateLogFiles();
       this.#currentFileSize = 0;
     }
 
-    this._buf.writeSync(this._encoder.encode(msg + "\n"));
-    this.#currentFileSize += msgByteLength;
+    super.write(message);
+    this.#currentFileSize += messageByteLength;
   }
 
   rotateLogFiles(): void {
