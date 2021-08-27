b7ea3cb84f619cafd766bac839d37ed079f84625
cmorten <craig.morten@hotmail.co.uk>
2021-08-27 20:49:29 +0100

    refactor: remove dead import

--- http/bench.ts
+++ http/bench.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-import { _parseAddrFromStr, listenAndServe } from "./server.ts";
+import { listenAndServe } from "./server.ts";
 
 const addr = Deno.args[0] || "127.0.0.1:4500";
 const encoder = new TextEncoder();
