1088fdb2adfec677274c9c6614baf1cb6e8416f0
Cyril Walle <cyril.walle@protonmail.com>
2021-08-31 11:21:20 +0200

    add missing constant

--- flags/mod.ts
+++ flags/mod.ts
@@ -61,6 +61,8 @@ interface NestedMapping {
   [key: string]: NestedMapping | unknown;
 }
 
+const hasOwn = { Object };
+
 function get<T>(obj: Record<string, T>, key: string): T | undefined {
   if (hasOwn(obj, key)) {
     return obj[key];
