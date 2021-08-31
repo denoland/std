332cfb49af1f3bb4717b7db310ea30a1fe0be8b3
Grian <grianguy32@gmail.com>
2021-08-31 12:37:26 +0300

    drop last while

---
+++ collections/drop_last_while.ts
@@ -0,0 +1,28 @@
+/**
+ * Returns all elements in the given collection until the last element that does not match the given predicate
+ *
+ * Example:
+ * ```ts
+ * import { dropLastWhile } from "./drop_last_while.ts";
+ * import { assertEquals } from "../testing/asserts.ts";
+ *
+ * const values = [22, 30, 44];
+ *
+ * const notFourtyFour = dropLastWhile(values, i => i != 44);
+ *
+ * assertEquals(
+ *   notFourtyFour
+ *   [22, 30]
+ * );
+ * ```
+ *
+ */
+export function dropLastWhile<T>(
+  array: readonly T[],
+  predicate: (el: T) => boolean,
+): T[] {
+  let offset = array.length;
+  while (0 < offset && predicate(array[offset - 1])) offset--;
+
+  return array.slice(0, offset);
+}
