2369fd682cfcb7b0e89b9da7747104af44e39c95
grian <54550982+grian32@users.noreply.github.com>
2021-09-01 10:01:36 +0300

    feat(collections): add dropLastWhile (#1193)

--- collections/mod.ts
+++ collections/mod.ts
@@ -31,3 +31,4 @@ export * from "./zip.ts";
 export * from "./take_last_while.ts";
 export * from "./take_while.ts";
 export * from "./first_not_nullish_of.ts";
+export * from "./drop_last_while.ts";
