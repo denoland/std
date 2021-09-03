dc68754b6e4fc85472af3aab688274abe3630bcb
defectivepixel <majidsajadi93@gmail.com>
2021-09-03 17:24:36 +0430

    Update collections/max_with.ts
    Co-authored-by: Leon Strauss <me@lionc.de>

--- collections/max_with.ts
+++ collections/max_with.ts
@@ -1,7 +1,12 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
 /**
- * Returns the first element having the largest value according to the provided comparator or undefined if there are no elements
+ * Returns the first element having the largest value according to the provided
+ * comparator or undefined if there are no elements.
+ *
+ * The comparator is expected to work exactly like one passed to `Array.sort`, which means
+ * that `comparator(a, b)` should return a negative number if `a < b`, a positive number if `a > b`
+ * and `0` if `a == b`.
  *
  * Example:
  *
