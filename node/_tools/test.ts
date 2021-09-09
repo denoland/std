5c925bc5a009885b5a708aaa455d9c0840823327
cmorten <craig.morten@hotmail.co.uk>
2021-09-09 22:38:42 +0100

    revert: don't filter tests to just dns

--- node/_tools/test.ts
+++ node/_tools/test.ts
@@ -18,28 +18,26 @@ const dir = walk(fromFileUrl(new URL(config.suitesFolder, import.meta.url)), {
 const testsFolder = dirname(fromFileUrl(import.meta.url));
 
 for await (const file of dir) {
-  if (file.path.includes("dns")) {
-    Deno.test({
-      name: relative(testsFolder, file.path),
-      fn: async () => {
-        const process = Deno.run({
-          cwd: testsFolder,
-          cmd: [
-            "deno",
-            "run",
-            "-A",
-            "--quiet",
-            "--unstable",
-            "require.ts",
-            file.path,
-          ],
-        });
+  Deno.test({
+    name: relative(testsFolder, file.path),
+    fn: async () => {
+      const process = Deno.run({
+        cwd: testsFolder,
+        cmd: [
+          "deno",
+          "run",
+          "-A",
+          "--quiet",
+          "--unstable",
+          "require.ts",
+          file.path,
+        ],
+      });
 
-        const { code } = await process.status();
-        process.close();
+      const { code } = await process.status();
+      process.close();
 
-        assertEquals(code, 0);
-      },
-    });
-  }
+      assertEquals(code, 0);
+    },
+  });
 }
