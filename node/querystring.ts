51bc3ac231642ca0da5cd2c142cc9ef494c5794a
v1rtl <pilll.pl22@gmail.com>
2021-09-13 00:00:25 +0300

    feat(std/node): add `ParsedUrlQuery` to querystring

--- node/querystring.ts
+++ node/querystring.ts
@@ -25,9 +25,9 @@ export function parse(
   eq = "=",
   { decodeURIComponent = unescape, maxKeys = 1000 }: ParseOptions = {},
 ): { [key: string]: string[] | string } {
-  const entries = str
-    .split(sep)
-    .map((entry) => entry.split(eq).map(decodeURIComponent));
+  const entries = str.split(sep).map((entry) =>
+    entry.split(eq).map(decodeURIComponent)
+  );
   const final: { [key: string]: string[] | string } = {};
 
   let i = 0;
@@ -90,8 +90,7 @@ export function encodeStr(
     }
     if (c < 0xd800 || c >= 0xe000) {
       lastPos = i + 1;
-      out += hexTable[0xe0 | (c >> 12)] +
-        hexTable[0x80 | ((c >> 6) & 0x3f)] +
+      out += hexTable[0xe0 | (c >> 12)] + hexTable[0x80 | ((c >> 6) & 0x3f)] +
         hexTable[0x80 | (c & 0x3f)];
       continue;
     }
@@ -165,3 +164,7 @@ export default {
   unescape,
   escape,
 };
+
+export interface ParsedUrlQuery {
+  [key: string]: string | string[] | undefined;
+}
