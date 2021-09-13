80b6146eacd170afe3aad253c588534f3ff6c64a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-13 22:56:12 +0800

    Delete http directory

--- http/testdata/file_server_as_library.ts
+++
@@ -1,8 +0,0 @@
-import { listenAndServe } from "../server.ts";
-import { serveFile } from "../file_server.ts";
-
-listenAndServe(":8000", (req) => {
-  return serveFile(req, "./testdata/hello.html");
-});
-
-console.log("Server running...");
