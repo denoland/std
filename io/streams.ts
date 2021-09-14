68816960acb5ba8b464e498d087808ea5b659008
Nayeem Rahman <nayeemrmn99@gmail.com>
2021-09-14 06:07:32 +0100

    feat(io/streams): propagate cancel in readableStreamFromIterable() (#1274)

--- io/streams.ts
+++ io/streams.ts
@@ -146,14 +146,34 @@ export function writableStreamFromWriter(
  *      import { readableStreamFromIterable } from "./streams.ts";
  *
  *      const r1 = readableStreamFromIterable(["foo, bar, baz"]);
- *      const r2 = readableStreamFromIterable((async function* () {
+ *      const r2 = readableStreamFromIterable(async function* () {
  *        await new Promise(((r) => setTimeout(r, 1000)));
  *        yield "foo";
  *        await new Promise(((r) => setTimeout(r, 1000)));
  *        yield "bar";
  *        await new Promise(((r) => setTimeout(r, 1000)));
  *        yield "baz";
- *      })());
+ *      }());
+ * ```
+ *
+ * If the produced iterator (`iterable[Symbol.asyncIterator]()` or
+ * `iterable[Symbol.iterator]()`) is a generator, or more specifically is found
+ * to have a `.throw()` method on it, that will be called upon
+ * `readableStream.cancel()`. This is the case for the second input type above:
+ *
+ * ```ts
+ * import { readableStreamFromIterable } from "./streams.ts";
+ *
+ * const r3 = readableStreamFromIterable(async function* () {
+ *   try {
+ *     yield "foo";
+ *   } catch (error) {
+ *     console.log(error); // Error: Cancelled by consumer.
+ *   }
+ * }());
+ * const reader = r3.getReader();
+ * console.log(await reader.read()); // { value: "foo", done: false }
+ * await reader.cancel(new Error("Cancelled by consumer."));
  * ```
  */
 export function readableStreamFromIterable<T>(
@@ -165,11 +185,17 @@ export function readableStreamFromIterable<T>(
   return new ReadableStream({
     async pull(controller) {
       const { value, done } = await iterator.next();
-
       if (done) {
         controller.close();
       } else {
         controller.enqueue(value);
+      }
+    },
+    async cancel(reason) {
+      if (typeof iterator.throw == "function") {
+        try {
+          await iterator.throw(reason);
+        } catch { /* `iterator.throw()` always throws on site. We catch it. */ }
       }
     },
   });
