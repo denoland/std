6598d223531ca7daf4adb3b5b9b2476b0728a2e9
Leon Strauss <me@lionc.de>
2021-09-26 22:11:57 +0200

    :art: Format _util/types.ts

--- _util/types.ts
+++ _util/types.ts
@@ -1,16 +1,17 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
 export type Expand<T> = T extends Record<PropertyKey, unknown>
-  ? T extends infer O
-    ? { [K in keyof O]: O[K] }
-    : never
+  ? T extends infer O ? { [K in keyof O]: O[K] }
+  : never
   : T;
 
-export type SafeOmit<From, Omits> = Omit<From, CommonKeysWithSameType<Omits, From>>
+export type SafeOmit<From, Omits> = Omit<
+  From,
+  CommonKeysWithSameType<Omits, From>
+>;
 
-export type CommonKeys<A, B> = keyof A & keyof B
+export type CommonKeys<A, B> = keyof A & keyof B;
 
 export type CommonKeysWithSameType<T, U> = {
-    [K in CommonKeys<T, U>]: T[K] extends U[K] ? K : never
-}[CommonKeys<T, U>]
-
+  [K in CommonKeys<T, U>]: T[K] extends U[K] ? K : never;
+}[CommonKeys<T, U>];
