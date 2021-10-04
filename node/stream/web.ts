1ff03305739d040fe0311451c42b7c2f42481b3f
v 1 r t l <pilll.PL22@gmail.com>
2021-10-04 05:00:46 +0300

    feat(node/stream/web): export more APIs (#1338)

--- node/stream/web.ts
+++ node/stream/web.ts
@@ -5,13 +5,25 @@ const {
   WritableStream,
   WritableStreamDefaultWriter,
   TransformStream,
+  ReadableByteStreamController,
+  TransformStreamDefaultController,
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  TextEncoderStream,
+  TextDecoderStream,
 } = globalThis;
 
 export {
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  ReadableByteStreamController,
   ReadableStream,
   ReadableStreamDefaultController,
   ReadableStreamDefaultReader,
+  TextDecoderStream,
+  TextEncoderStream,
   TransformStream,
+  TransformStreamDefaultController,
   WritableStream,
   WritableStreamDefaultWriter,
 };
