ab298c8e0da6ab2a0f2d438c4f15d9fa3e0a8073
Yasser A.Idrissi <getspookydev@gmail.com>
2021-10-05 07:59:01 +0100

    test(node/buffer): add tests for `8` , `16` and `32` bit unsigned integers (#1343)

--- node/buffer_test.ts
+++ node/buffer_test.ts
@@ -299,6 +299,48 @@ Deno.test({
 });
 
 Deno.test({
+  name: "Buffer 8 bit unsigned integers",
+  fn() {
+    const buffer = Buffer.from([0xff, 0x2a, 0x2a, 0x2a]);
+    assertEquals(buffer.readUInt8(0), 255);
+    assertEquals(buffer.readUInt8(1), 42);
+    assertEquals(buffer.readUInt8(2), 42);
+    assertEquals(buffer.readUInt8(3), 42);
+  },
+});
+
+Deno.test({
+  name: "Buffer 16 bit unsigned integers",
+  fn() {
+    const buffer = Buffer.from([0x00, 0x2a, 0x42, 0x3f]);
+    assertEquals(buffer.readUInt16BE(0), 0x2a);
+    assertEquals(buffer.readUInt16BE(1), 0x2a42);
+    assertEquals(buffer.readUInt16BE(2), 0x423f);
+    assertEquals(buffer.readUInt16LE(0), 0x2a00);
+    assertEquals(buffer.readUInt16LE(1), 0x422a);
+    assertEquals(buffer.readUInt16LE(2), 0x3f42);
+
+    buffer[0] = 0xfe;
+    buffer[1] = 0xfe;
+    assertEquals(buffer.readUInt16BE(0), 0xfefe);
+    assertEquals(buffer.readUInt16LE(0), 0xfefe);
+  },
+});
+
+Deno.test({
+  name: "Buffer 32 bit unsigned integers",
+  fn() {
+    const buffer = Buffer.from([0x32, 0x65, 0x42, 0x56, 0x23, 0xff]);
+    assertEquals(buffer.readUInt32BE(0), 0x32654256);
+    assertEquals(buffer.readUInt32BE(1), 0x65425623);
+    assertEquals(buffer.readUInt32BE(2), 0x425623ff);
+    assertEquals(buffer.readUInt32LE(0), 0x56426532);
+    assertEquals(buffer.readUInt32LE(1), 0x23564265);
+    assertEquals(buffer.readUInt32LE(2), 0xff235642);
+  },
+});
+
+Deno.test({
   name: "Buffer readUIntBE",
   fn() {
     const buffer = Buffer.from([
