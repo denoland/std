a536f886d7ad798aa6a33e8e7bae4611eae774dc
Aplet123 <aplet@aplet.me>
2021-10-04 20:23:37 -0400

    feat(bytes): switch equals to simd for large byte arrays

--- bytes/mod.ts
+++ bytes/mod.ts
@@ -69,16 +69,36 @@ export function lastIndexOf(
   return -1;
 }
 
-/** Check whether binary arrays are equal to each other.
- * @param a first array to check equality
- * @param b second array to check equality
- */
-export function equals(a: Uint8Array, b: Uint8Array): boolean {
+function equalsNaive(a: Uint8Array, b: Uint8Array): boolean {
   if (a.length !== b.length) return false;
   for (let i = 0; i < b.length; i++) {
     if (a[i] !== b[i]) return false;
   }
   return true;
+}
+
+function equalsSimd(a: Uint8Array, b: Uint8Array): boolean {
+  if (a.length !== b.length) return false;
+  const len = a.length;
+  const compressable = Math.floor(len / 4);
+  const compressedA = new Uint32Array(a.buffer, 0, compressable);
+  const compressedB = new Uint32Array(b.buffer, 0, compressable);
+  for (let i = compressable * 4; i < len; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  for (let i = 0; i < compressedA.length; i++) {
+    if (compressedA[i] !== compressedB[i]) return false;
+  }
+  return true;
+}
+
+/** Check whether binary arrays are equal to each other.
+ * @param a first array to check equality
+ * @param b second array to check equality
+ */
+export function equals(a: Uint8Array, b: Uint8Array): boolean {
+  if (a.length < 1000) return equalsNaive(a, b);
+  return equalsSimd(a, b);
 }
 
 /** Check whether binary array starts with prefix.
