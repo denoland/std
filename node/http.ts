6a9d2fad63c05a5471e6c2fa3ae4d0fd782a942d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-06 11:05:43 -0500

    feature(node/http/STATUS_CODES): Export METHODS

--- node/http.ts
+++ node/http.ts
@@ -1,5 +1,42 @@
 import { Status as STATUS_CODES } from "../http/mod.ts";
 
-export { STATUS_CODES };
+const METHODS = [
+  "ACL",
+  "BIND",
+  "CHECKOUT",
+  "CONNECT",
+  "COPY",
+  "DELETE",
+  "GET",
+  "HEAD",
+  "LINK",
+  "LOCK",
+  "M-SEARCH",
+  "MERGE",
+  "MKACTIVITY",
+  "MKCALENDAR",
+  "MKCOL",
+  "MOVE",
+  "NOTIFY",
+  "OPTIONS",
+  "PATCH",
+  "POST",
+  "PROPFIND",
+  "PROPPATCH",
+  "PURGE",
+  "PUT",
+  "REBIND",
+  "REPORT",
+  "SEARCH",
+  "SOURCE",
+  "SUBSCRIBE",
+  "TRACE",
+  "UNBIND",
+  "UNLINK",
+  "UNLOCK",
+  "UNSUBSCRIBE",
+];
 
-export default { STATUS_CODES };
+export { METHODS, STATUS_CODES };
+
+export default { STATUS_CODES, METHODS };
