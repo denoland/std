451e28fdb6c69c540771624384e7a19c0194b64b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-07 20:41:11 -0500

    feature(node/http): Export STATUS_CODES and METHODS (#1357)

--- node/http.ts
+++ node/http.ts
@@ -1,1 +1,42 @@
-throw new Error('"http" is not yet implemented.');
+import { Status as STATUS_CODES } from "../http/http_status.ts";
+
+const METHODS = [
+  "ACL",
+  "BIND",
+  "CHECKOUT",
+  "CONNECT",
+  "COPY",
+  "DELETE",
+  "GET",
+  "HEAD",
+  "LINK",
+  "LOCK",
+  "M-SEARCH",
+  "MERGE",
+  "MKACTIVITY",
+  "MKCALENDAR",
+  "MKCOL",
+  "MOVE",
+  "NOTIFY",
+  "OPTIONS",
+  "PATCH",
+  "POST",
+  "PROPFIND",
+  "PROPPATCH",
+  "PURGE",
+  "PUT",
+  "REBIND",
+  "REPORT",
+  "SEARCH",
+  "SOURCE",
+  "SUBSCRIBE",
+  "TRACE",
+  "UNBIND",
+  "UNLINK",
+  "UNLOCK",
+  "UNSUBSCRIBE",
+];
+
+export { METHODS, STATUS_CODES };
+
+export default { STATUS_CODES, METHODS };
