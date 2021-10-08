bd543375d59ce51d7a9b9cb6f2936ead4c312b3c
wafuwafu13 <mariobaske@i.softbank.jp>
2021-10-08 18:23:42 +0900

    feat(node/url): Try CI with case of Windows

--- node/_tools/suites/parallel/test-url-pathtofileurl.js
+++ node/_tools/suites/parallel/test-url-pathtofileurl.js
@@ -55,48 +55,48 @@ const url = require('url');
   if (isWindows) {
     // TODO: fix case of Windows
     testCases = [
-      // // Lowercase ascii alpha
-      // { path: 'C:\\foo', expected: 'file:///C:/foo' },
-      // // Uppercase ascii alpha
-      // { path: 'C:\\FOO', expected: 'file:///C:/FOO' },
-      // // dir
-      // { path: 'C:\\dir\\foo', expected: 'file:///C:/dir/foo' },
-      // // trailing separator
-      // { path: 'C:\\dir\\', expected: 'file:///C:/dir/' },
-      // // dot
-      // { path: 'C:\\foo.mjs', expected: 'file:///C:/foo.mjs' },
-      // // space
-      // { path: 'C:\\foo bar', expected: 'file:///C:/foo%20bar' },
-      // // question mark
-      // { path: 'C:\\foo?bar', expected: 'file:///C:/foo%3Fbar' },
-      // // number sign
-      // { path: 'C:\\foo#bar', expected: 'file:///C:/foo%23bar' },
-      // // ampersand
-      // { path: 'C:\\foo&bar', expected: 'file:///C:/foo&bar' },
-      // // equals
-      // { path: 'C:\\foo=bar', expected: 'file:///C:/foo=bar' },
-      // // colon
-      // { path: 'C:\\foo:bar', expected: 'file:///C:/foo:bar' },
-      // // semicolon
-      // { path: 'C:\\foo;bar', expected: 'file:///C:/foo;bar' },
-      // // percent
-      // { path: 'C:\\foo%bar', expected: 'file:///C:/foo%25bar' },
-      // // backslash
-      // { path: 'C:\\foo\\bar', expected: 'file:///C:/foo/bar' },
-      // // backspace
-      // { path: 'C:\\foo\bbar', expected: 'file:///C:/foo%08bar' },
-      // // tab
-      // { path: 'C:\\foo\tbar', expected: 'file:///C:/foo%09bar' },
-      // // newline
-      // { path: 'C:\\foo\nbar', expected: 'file:///C:/foo%0Abar' },
-      // // carriage return
-      // { path: 'C:\\foo\rbar', expected: 'file:///C:/foo%0Dbar' },
-      // // latin1
-      // { path: 'C:\\fóóbàr', expected: 'file:///C:/f%C3%B3%C3%B3b%C3%A0r' },
-      // // Euro sign (BMP code point)
-      // { path: 'C:\\€', expected: 'file:///C:/%E2%82%AC' },
-      // // Rocket emoji (non-BMP code point)
-      // { path: 'C:\\🚀', expected: 'file:///C:/%F0%9F%9A%80' },
+      // Lowercase ascii alpha
+      { path: 'C:\\foo', expected: 'file:///C:/foo' },
+      // Uppercase ascii alpha
+      { path: 'C:\\FOO', expected: 'file:///C:/FOO' },
+      // dir
+      { path: 'C:\\dir\\foo', expected: 'file:///C:/dir/foo' },
+      // trailing separator
+      { path: 'C:\\dir\\', expected: 'file:///C:/dir/' },
+      // dot
+      { path: 'C:\\foo.mjs', expected: 'file:///C:/foo.mjs' },
+      // space
+      { path: 'C:\\foo bar', expected: 'file:///C:/foo%20bar' },
+      // question mark
+      { path: 'C:\\foo?bar', expected: 'file:///C:/foo%3Fbar' },
+      // number sign
+      { path: 'C:\\foo#bar', expected: 'file:///C:/foo%23bar' },
+      // ampersand
+      { path: 'C:\\foo&bar', expected: 'file:///C:/foo&bar' },
+      // equals
+      { path: 'C:\\foo=bar', expected: 'file:///C:/foo=bar' },
+      // colon
+      { path: 'C:\\foo:bar', expected: 'file:///C:/foo:bar' },
+      // semicolon
+      { path: 'C:\\foo;bar', expected: 'file:///C:/foo;bar' },
+      // percent
+      { path: 'C:\\foo%bar', expected: 'file:///C:/foo%25bar' },
+      // backslash
+      { path: 'C:\\foo\\bar', expected: 'file:///C:/foo/bar' },
+      // backspace
+      { path: 'C:\\foo\bbar', expected: 'file:///C:/foo%08bar' },
+      // tab
+      { path: 'C:\\foo\tbar', expected: 'file:///C:/foo%09bar' },
+      // newline
+      { path: 'C:\\foo\nbar', expected: 'file:///C:/foo%0Abar' },
+      // carriage return
+      { path: 'C:\\foo\rbar', expected: 'file:///C:/foo%0Dbar' },
+      // latin1
+      { path: 'C:\\fóóbàr', expected: 'file:///C:/f%C3%B3%C3%B3b%C3%A0r' },
+      // Euro sign (BMP code point)
+      { path: 'C:\\€', expected: 'file:///C:/%E2%82%AC' },
+      // Rocket emoji (non-BMP code point)
+      { path: 'C:\\🚀', expected: 'file:///C:/%F0%9F%9A%80' },
       // UNC path (see https://docs.microsoft.com/en-us/archive/blogs/ie/file-uris-in-windows)
       { path: '\\\\nas\\My Docs\\File.doc', expected: 'file://nas/My%20Docs/File.doc' }
     ];
