68cd7a5d392d2883082465fc8624148006ad9a16
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-10-12 15:28:48 +0200

    feat(node): update built-in modules in node/module.ts (#1378)

--- node/module_all.ts
+++ node/module_all.ts
@@ -1,10 +1,10 @@
 import assert from "./assert.ts";
 import assertStrict from "./assert/strict.ts";
 import buffer from "./buffer.ts";
-import crypto from "./crypto.ts";
+import childProcess from "./child_process.ts";
 import console from "./console.ts";
 import constants from "./constants.ts";
-import childProcess from "./child_process.ts";
+import crypto from "./crypto.ts";
 import dns from "./dns.ts";
 import events from "./events.ts";
 import fs from "./fs.ts";
@@ -14,13 +14,17 @@ import net from "./net.ts";
 import os from "./os.ts";
 import path from "./path.ts";
 import perfHooks from "./perf_hooks.ts";
+import process from "./process.ts";
 import querystring from "./querystring.ts";
 import stream from "./stream.ts";
 import stringDecoder from "./string_decoder.ts";
+import sys from "./sys.ts";
 import timers from "./timers.ts";
+import timersPromises from "./timers/promises.ts";
 import tty from "./tty.ts";
 import url from "./url.ts";
 import util from "./util.ts";
+
 // TODO(kt3k): add these modules when implemented
 // import cluster from "./cluster.ts";
 // import dgram from "./dgram.ts";
@@ -52,10 +56,13 @@ export default {
   os,
   path,
   "perf_hooks": perfHooks,
+  process,
   querystring,
   stream,
   "string_decoder": stringDecoder,
+  sys,
   timers,
+  "timers/promises": timersPromises,
   tty,
   url,
   util,
