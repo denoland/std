ec078c8cb37f0a42e605951df6b0cd75cb2b987f
Yoshiya Hinosawa <stibium121@gmail.com>
2021-10-15 11:06:14 +0900

    chore: fmt

--- node/process.ts
+++ node/process.ts
@@ -343,7 +343,7 @@ class Process extends EventEmitter {
   env = env;
 
   // Typed as any to avoid importing "module" module for types
-  // deno-lint-ignore no-explicit-any 
+  // deno-lint-ignore no-explicit-any
   mainModule: any = undefined;
 
   /** https://nodejs.org/api/process.html#process_process_nexttick_callback_args */
