9c9278c09d3d2706b451a86e4972163e429b8611
wafuwafu13 <mariobaske@i.softbank.jp>
2021-10-15 15:36:33 +0900

    chore(node): Upgrade Node version from 15.5.1 to 16.11.1

--- node/_tools/suites/parallel/test-net-reconnect.js
+++ node/_tools/suites/parallel/test-net-reconnect.js
@@ -27,39 +27,70 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 "use strict";
-const common = require("../common");
+require("../common");
 const assert = require("assert");
+
 const net = require("net");
 
-const iter = 10;
+const N = 50;
+let client_recv_count = 0;
+let client_end_count = 0;
+let disconnect_count = 0;
 
 const server = net.createServer(function (socket) {
-  socket.on("readable", function () {
-    socket.read();
+  console.error("SERVER: got socket connection");
+  socket.resume();
+
+  console.error("SERVER connect, writing");
+  socket.write("hello\r\n");
+
+  socket.on("end", () => {
+    console.error("SERVER socket end, calling end()");
+    socket.end();
   });
 
-  socket.on("end", function () {
-    server.close();
+  socket.on("close", (had_error) => {
+    console.log(`SERVER had_error: ${JSON.stringify(had_error)}`);
+    assert.strictEqual(had_error, false);
   });
 });
 
-server.listen(
-  0,
-  common.mustCall(function () {
-    const client = net.connect(this.address().port);
+server.listen(0, function () {
+  console.log("SERVER listening");
+  const client = net.createConnection(this.address().port);
 
-    client.on(
-      "finish",
-      common.mustCall(() => {
-        assert.strictEqual(client.bufferSize, 0);
-      }),
-    );
+  client.setEncoding("UTF8");
 
-    for (let i = 1; i < iter; i++) {
-      client.write("a");
-      assert.strictEqual(client.bufferSize, i);
-    }
+  client.on("connect", () => {
+    console.error("CLIENT connected", client._writableState);
+  });
 
+  client.on("data", function (chunk) {
+    client_recv_count += 1;
+    console.log(`client_recv_count ${client_recv_count}`);
+    assert.strictEqual(chunk, "hello\r\n");
+    console.error("CLIENT: calling end", client._writableState);
     client.end();
-  }),
-);
+  });
+
+  client.on("end", () => {
+    console.error("CLIENT end");
+    client_end_count++;
+  });
+
+  client.on("close", (had_error) => {
+    console.log("CLIENT disconnect");
+    assert.strictEqual(had_error, false);
+    if (disconnect_count++ < N) {
+      client.connect(server.address().port); // reconnect
+    } else {
+      server.close();
+    }
+  });
+});
+
+process.on("exit", () => {
+  assert.strictEqual(disconnect_count, N + 1);
+  assert.strictEqual(client_recv_count, N + 1);
+  assert.strictEqual(client_end_count, N + 1);
+});
