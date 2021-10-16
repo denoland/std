acd70dced5629ed4e20ac464bdb7d498f3c51d83
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-10-16 17:15:12 +0200

    fix(node): declare 'global' types inline (#1409)

--- node/global.ts
+++ node/global.ts
@@ -1,8 +1,31 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-/// <reference path="./global.d.ts" />
 import processModule from "./process.ts";
 import { Buffer as bufferModule } from "./buffer.ts";
 import timers from "./timers.ts";
+
+type GlobalType = {
+  process: typeof processModule;
+  Buffer: typeof bufferModule;
+  setImmediate: typeof timers.setImmediate;
+  clearImmediate: typeof timers.clearImmediate;
+};
+
+declare global {
+  interface Window {
+    global: GlobalType;
+  }
+
+  interface globalThis {
+    global: GlobalType;
+  }
+
+  var global: GlobalType;
+  var process: typeof processModule;
+  var Buffer: typeof bufferModule;
+  type Buffer = bufferModule;
+  var setImmediate: typeof timers.setImmediate;
+  var clearImmediate: typeof timers.clearImmediate;
+}
 
 Object.defineProperty(globalThis, "global", {
   value: globalThis,
