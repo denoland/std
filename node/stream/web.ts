384802beb61ccb8a0a7a4d8797e0f9b90c7d0cb1
Guy Bedford <guybedford@gmail.com>
2021-10-18 13:23:28 -0700

    fixup: keep simpler declaration form

--- node/stream/web.ts
+++ node/stream/web.ts
@@ -1,16 +1,18 @@
-export const ByteLengthQueuingStrategy = globalThis.ByteLengthQueuingStrategy;
-export const CountQueuingStrategy = globalThis.CountQueuingStrategy;
-export const ReadableByteStreamController = globalThis.ReadableByteStreamController;
-export const ReadableStream = globalThis.ReadableStream;
-export const ReadableStreamDefaultController = globalThis.ReadableStreamDefaultController;
-export const ReadableStreamDefaultReader = globalThis.ReadableStreamDefaultReader;
-export const TextDecoderStream = globalThis.TextDecoderStream;
-export const TextEncoderStream = globalThis.TextEncoderStream;
-export const TransformStream = globalThis.TransformStream;
-export const TransformStreamDefaultController = globalThis.TransformStreamDefaultController;
-export const WritableStream = globalThis.WritableStream;
-export const WritableStreamDefaultController = globalThis.WritableStreamDefaultController;
-export const WritableStreamDefaultWriter = globalThis.WritableStreamDefaultWriter;
+export const {
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  ReadableByteStreamController,
+  ReadableStream,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+  TextDecoderStream,
+  TextEncoderStream,
+  TransformStream,
+  TransformStreamDefaultController,
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter
+} = globalThis;
 
 export default {
   ReadableStream,
