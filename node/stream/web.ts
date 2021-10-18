44b72d270a549b35a5b20fbe0ed4b55b485dca71
Guy Bedford <guybedford@gmail.com>
2021-10-18 13:19:13 -0700

    fix: node:stream/web wrapper formatting, declaration order

--- node/stream/web.ts
+++ node/stream/web.ts
@@ -1,29 +1,29 @@
-const {
-  ReadableStream,
-  ReadableStreamDefaultReader,
-  ReadableStreamDefaultController,
-  WritableStream,
-  WritableStreamDefaultWriter,
-  TransformStream,
-  ReadableByteStreamController,
-  TransformStreamDefaultController,
-  ByteLengthQueuingStrategy,
-  CountQueuingStrategy,
-  TextEncoderStream,
-  TextDecoderStream,
-} = globalThis;
+export const ByteLengthQueuingStrategy = globalThis.ByteLengthQueuingStrategy;
+export const CountQueuingStrategy = globalThis.CountQueuingStrategy;
+export const ReadableByteStreamController = globalThis.ReadableByteStreamController;
+export const ReadableStream = globalThis.ReadableStream;
+export const ReadableStreamDefaultController = globalThis.ReadableStreamDefaultController;
+export const ReadableStreamDefaultReader = globalThis.ReadableStreamDefaultReader;
+export const TextDecoderStream = globalThis.TextDecoderStream;
+export const TextEncoderStream = globalThis.TextEncoderStream;
+export const TransformStream = globalThis.TransformStream;
+export const TransformStreamDefaultController = globalThis.TransformStreamDefaultController;
+export const WritableStream = globalThis.WritableStream;
+export const WritableStreamDefaultController = globalThis.WritableStreamDefaultController;
+export const WritableStreamDefaultWriter = globalThis.WritableStreamDefaultWriter;
 
-export {
-  ByteLengthQueuingStrategy,
-  CountQueuingStrategy,
-  ReadableByteStreamController,
+export default {
   ReadableStream,
-  ReadableStreamDefaultController,
   ReadableStreamDefaultReader,
-  TextDecoderStream,
-  TextEncoderStream,
+  ReadableByteStreamController,
+  ReadableStreamDefaultController,
   TransformStream,
   TransformStreamDefaultController,
   WritableStream,
   WritableStreamDefaultWriter,
+  WritableStreamDefaultController,
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  TextEncoderStream,
+  TextDecoderStream,
 };
