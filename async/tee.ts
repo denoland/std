02ed9ecf91b543a975de31319e91904818422d95
ah-yu <zheyuzhang03@gmail.com>
2021-10-18 18:55:09 +0800

    fix(std/async): fix tee concurent .next() error

--- async/tee.ts
+++ async/tee.ts
@@ -1,5 +1,3 @@
-// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-
 // Utility for representing n-tuple
 type Tuple<T, N extends number> = N extends N
   ? number extends N ? T[] : TupleOf<T, N, []>
@@ -8,99 +6,82 @@ type TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
   ? R
   : TupleOf<T, N, [T, ...R]>;
 
-const noop = () => {};
+interface QueueNode<T> {
+  value: T;
+  next: QueueNode<T> | undefined;
+}
 
-class AsyncIterableClone<T> implements AsyncIterable<T> {
-  currentPromise: Promise<IteratorResult<T>>;
-  resolveCurrent: (x: Promise<IteratorResult<T>>) => void = noop;
-  consumed: Promise<void>;
-  consume: () => void = noop;
+// deno-lint-ignore no-explicit-any
+class Queue<T, TReturn = any> {
+  #source: AsyncIterator<T>;
+  #queue: QueueNode<T>;
+  head: QueueNode<T>;
 
-  constructor() {
-    this.currentPromise = new Promise<IteratorResult<T>>((resolve) => {
-      this.resolveCurrent = resolve;
-    });
-    this.consumed = new Promise<void>((resolve) => {
-      this.consume = resolve;
-    });
+  done: boolean;
+  return: { value: TReturn; isExsit: boolean };
+
+  constructor(iterable: AsyncIterable<T>) {
+    this.#source = iterable[Symbol.asyncIterator]();
+    this.#queue = {
+      value: undefined!,
+      next: undefined,
+    };
+    this.head = this.#queue;
+    this.done = false;
+    this.return = {
+      value: undefined!,
+      isExsit: false,
+    };
   }
 
-  reset() {
-    this.currentPromise = new Promise<IteratorResult<T>>((resolve) => {
-      this.resolveCurrent = resolve;
-    });
-    this.consumed = new Promise<void>((resolve) => {
-      this.consume = resolve;
-    });
-  }
-
-  async next(): Promise<IteratorResult<T>> {
-    const res = await this.currentPromise;
-    this.consume();
-    this.reset();
-    return res;
-  }
-
-  async push(res: Promise<IteratorResult<T>>): Promise<void> {
-    this.resolveCurrent(res);
-    // Wait until current promise is consumed and next item is requested.
-    await this.consumed;
-  }
-
-  [Symbol.asyncIterator](): AsyncIterator<T> {
-    return this;
+  async next(): Promise<void> {
+    const result = await this.#source.next();
+    if (!result.done) {
+      const nextNode: QueueNode<T> = {
+        value: result.value,
+        next: undefined,
+      };
+      this.#queue.next = nextNode;
+      this.#queue = nextNode;
+    } else {
+      this.done = true;
+      if (this.#source.return) {
+        this.return = {
+          value: result.value,
+          isExsit: true,
+        };
+      }
+    }
   }
 }
 
-/**
- * Branches the given async iterable into the n branches.
- *
- * Example:
- *
- * ```ts
- *     import { tee } from "./tee.ts";
- *
- *     const gen = async function* gen() {
- *       yield 1;
- *       yield 2;
- *       yield 3;
- *     }
- *
- *     const [branch1, branch2] = tee(gen());
- *
- *     (async () => {
- *       for await (const n of branch1) {
- *         console.log(n); // => 1, 2, 3
- *       }
- *     })();
- *
- *     (async () => {
- *       for await (const n of branch2) {
- *         console.log(n); // => 1, 2, 3
- *       }
- *     })();
- * ```
- */
 export function tee<T, N extends number = 2>(
-  src: AsyncIterable<T>,
+  iterable: AsyncIterable<T>,
   n: N = 2 as N,
 ): Tuple<AsyncIterable<T>, N> {
-  const clones: Tuple<AsyncIterableClone<T>, N> = Array.from({ length: n }).map(
-    () => new AsyncIterableClone(),
-    // deno-lint-ignore no-explicit-any
-  ) as any;
-  (async () => {
-    const iter = src[Symbol.asyncIterator]();
-    await Promise.resolve();
+  const queue = new Queue<T>(iterable);
+
+  async function* generator(): AsyncGenerator<T> {
+    let buffer = queue.head;
     while (true) {
-      const res = iter.next();
-      await Promise.all(clones.map((c) => c.push(res)));
-      if ((await res).done) {
-        break;
+      if (buffer.next) {
+        buffer = buffer.next;
+        yield buffer.value;
+      } else if (queue.done) {
+        if (queue.return.isExsit) {
+          return queue.return.value;
+        }
+      } else {
+        await queue.next();
       }
     }
-  })().catch((e) => {
-    console.error(e);
-  });
-  return clones;
+  }
+
+  const branches = Array.from({ length: n }).map(
+    () => generator(),
+  ) as Tuple<
+    AsyncIterable<T>,
+    N
+  >;
+  return branches;
 }
