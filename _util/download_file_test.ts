9c7e005e5224fc81692698dbefceba9c67384272
Yoshiya Hinosawa <stibium121@gmail.com>
2021-10-19 14:50:01 +0900

    fix: update test downloadFile test case

--- _util/download_file_test.ts
+++ _util/download_file_test.ts
@@ -3,33 +3,27 @@
 import { join, toFileUrl } from "../path/mod.ts";
 import { delay } from "../async/delay.ts";
 import { assertEquals } from "../testing/asserts.ts";
+import { listenAndServe } from "../http/server.ts";
 
 import { downloadFile } from "./download_file.ts";
 
 Deno.test("[node/_tools/setup] downloadFile", async () => {
   const tmpdir = await Deno.makeTempDir();
   try {
-    const dummyFile = join(tmpdir, "dummy.txt");
-    // Writes 10 * 10,000 chars = 100KB
-    await Deno.writeTextFile(dummyFile, "123456789\n".repeat(10_000));
-    const p = Deno.run({
-      cmd: [
-        Deno.execPath(),
-        "run",
-        "-A",
-        "./http/file_server.ts",
-        tmpdir,
-      ],
-    });
-    await delay(5000);
+    const controller = new AbortController();
+    listenAndServe(":8080", () => {
+      // Responds with 100KB data
+      return new Response("0".repeat(100_000));
+    }, { signal: controller.signal });
+    delay(100);
 
     const downloadedFile = join(tmpdir, "downloaded.txt");
     await downloadFile(
-      "http://localhost:4507/dummy.txt",
+      "http://localhost:8080/dummy.txt",
       toFileUrl(downloadedFile),
     );
     assertEquals((await Deno.readTextFile(downloadedFile)).length, 100_000);
-    p.close();
+    controller.abort();
   } finally {
     await Deno.remove(tmpdir, { recursive: true });
   }
