bbc48fd72813103fef82f776463885e0ae41bfb7
ah-yu <zheyuzhang03@gmail.com>
2021-10-19 18:38:24 +0800

    add doc

--- async/tee.ts
+++ async/tee.ts
@@ -45,6 +45,35 @@ class Queue<T> {
   }
 }
 
+/**
+ * Branches the given async iterable into the n branches.
+ *
+ * Example:
+ *
+ * ```ts
+ *     import { tee } from "./tee.ts";
+ *
+ *     const gen = async function* gen() {
+ *       yield 1;
+ *       yield 2;
+ *       yield 3;
+ *     }
+ *
+ *     const [branch1, branch2] = tee(gen());
+ *
+ *     (async () => {
+ *       for await (const n of branch1) {
+ *         console.log(n); // => 1, 2, 3
+ *       }
+ *     })();
+ *
+ *     (async () => {
+ *       for await (const n of branch2) {
+ *         console.log(n); // => 1, 2, 3
+ *       }
+ *     })();
+ * ```
+ */
 export function tee<T, N extends number = 2>(
   iterable: AsyncIterable<T>,
   n: N = 2 as N,
