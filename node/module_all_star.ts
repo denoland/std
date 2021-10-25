aa87fc85446d927114caa9bc1513025a640ab6af
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-10-25 19:13:47 +0200

    sync

--- node/module_all_star.ts
+++ node/module_all_star.ts
@@ -1,3 +1,4 @@
+// Keep in sync with ./module_all.ts
 import * as assert from "./assert.ts";
 import * as assertStrict from "./assert/strict.ts";
 import * as buffer from "./buffer.ts";
@@ -9,26 +10,28 @@ import * as dns from "./dns.ts";
 import * as events from "./events.ts";
 import * as fs from "./fs.ts";
 import * as fsPromises from "./fs/promises.ts";
+import * as http from "./http.ts";
 import * as net from "./net.ts";
 import * as os from "./os.ts";
 import * as path from "./path.ts";
 import * as perfHooks from "./perf_hooks.ts";
+import * as process from "./process.ts";
 import * as querystring from "./querystring.ts";
 import * as stream from "./stream.ts";
 import * as stringDecoder from "./string_decoder.ts";
+import * as sys from "./sys.ts";
 import * as timers from "./timers.ts";
+import * as timersPromises from "./timers/promises.ts";
 import * as tty from "./tty.ts";
 import * as url from "./url.ts";
 import * as util from "./util.ts";
 // TODO(kt3k): add these modules when implemented
 // import cluster from "./cluster.ts";
 // import dgram from "./dgram.ts";
-// import http from "./http.ts";
 // import http2 from "./http2.ts";
 // import https from "./https.ts";
 // import readline from "./readline.ts";
 // import repl from "./repl.ts";
-// import sys from "./sys.ts";
 // import tls from "./tls.ts";
 // import vm from "./vm.ts";
 // import workerThreads from "./worker_threads.ts";
@@ -47,15 +50,20 @@ export default {
   events,
   fs,
   "fs/promises": fsPromises,
+  http,
   net,
   os,
   path,
   "perf_hooks": perfHooks,
+  process,
   querystring,
   stream,
   "string_decoder": stringDecoder,
+  sys,
   timers,
+  "timers/promises": timersPromises,
   tty,
   url,
   util,
+  zlib: {},
 } as Record<string, unknown>;
