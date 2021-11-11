5d0f0d784bc5dec3417f89dc380b5a00afe97577
Leon Strauss <me@lionc.de>
2021-11-11 14:59:19 +0100

    :memo: Update middleware JSDoc to reflect recent changes

--- http/middleware.ts
+++ http/middleware.ts
@@ -3,11 +3,11 @@ import type { EmptyContext, HttpRequest } from "./request.ts";
 import type { CommonKeys, Expand, SafeOmit } from "../_util/types.ts";
 
 /**
-* Middleware Handler that is expected to call the `next` Middleware at some
-* point, adding its own logic, request context and transformations befor or
-* after it. Can express the request context (see `HttpRequest.context`) it
-* requires to work and any request context extensions it will add to be
-* available to the `next` middleware.
+* Middleware to deal with some part of handling a request. It will be paseed a
+* `next` handler that it can call at any point point, adding its own logic,
+* request context and transformations befor or after it. Can express the
+* request context (see `HttpRequest.context`) it requires to work and any
+* request context it will add to be available to the `next` handler.
 *
 * @typeParam Needs - Request context required by this Middleware, defaults to
 * `EmptyContext` which is the empty object
@@ -22,7 +22,7 @@ import type { CommonKeys, Expand, SafeOmit } from "../_util/types.ts";
 * // Log response time, method and path for requests going through this middleware
 * const log: Middleware = (req, next) => {
 *   const start = performance.now();
-*   const response = await next!(req);
+*   const response = await next(req);
 *   const end = performance.now();
 
 *   console.log(
@@ -42,8 +42,9 @@ export type Middleware<
 ) => Response | Promise<Response>;
 
 /**
- * A `Middleware` that can be chained onto with `.add()`. Use `chain()` to wrap
- * a `Middleware` as a `MiddlewareChain`. */
+ * A `Middleware` that can be chained onto with `.add()`. Use `chain()` to start
+ * a `MiddlewareChain`.
+ */
 export type MiddlewareChain<
   Needs extends EmptyContext,
   Adds extends EmptyContext = EmptyContext,
@@ -53,27 +54,20 @@ export type MiddlewareChain<
     next: Parameters<Middleware<Needs, Adds>>[1],
   ): ReturnType<Middleware<Needs, Adds>>;
 
-  add<AddedNeeds>(
-    handler: Handler<AddedNeeds>,
-  ): Handler<
-    Expand<Needs & SafeOmit<AddedNeeds, Adds>>
-  >;
-
   /**
-   * Chain a given middleware to this middleware, returning a new
-   * `MiddlewareChain`.
+   * Append a given handler to this chain, terminating it, returning a new handler.
    *
    * Example:
    *
    * ```ts
    * const first: Middleware = (req, next) => {
    *   console.log("Hey");
-   *   return await next!(req);
+   *   return await next(req);
    * };
    *
    * const second: Middleware = (req, next) => {
    *   console.log("there!");
-   *   return await next!(req);
+   *   return await next(req);
    * };
    *
    * const helloWorld = (req) => new Response("Hello world");
@@ -81,7 +75,35 @@ export type MiddlewareChain<
    * // This Handler will log "Hey", log "there!" and then respond with "Hello World"
    * const handler = chain(first)
    *   .add(second)
-   *   .add(helloWorld)
+   *   .add(helloWorld);
+   * ```
+   */
+  add<AddedNeeds>(
+    handler: Handler<AddedNeeds>,
+  ): Handler<
+    Expand<Needs & SafeOmit<AddedNeeds, Adds>>
+  >;
+
+  /**
+   * Append a given middleware to this chain, returning a new
+   * `MiddlewareChain`.
+   *
+   * Example:
+   *
+   * ```ts
+   * const first: Middleware = (req, next) => {
+   *   console.log("Hey");
+   *   return await next(req);
+   * };
+   *
+   * const second: Middleware = (req, next) => {
+   *   console.log("there!");
+   *   return await next(req);
+   * };
+   *
+   * // This middleware will log "Hey", log "there!" and pass on to whatever is appended to it
+   * const middleware = chain(first)
+   *   .add(second);
    * ```
    */
   add<AddedNeeds, AddedAdds>(
@@ -92,23 +114,6 @@ export type MiddlewareChain<
   >;
 };
 
-/**
- * Builds a new `Middleware` out of two given ones, chaining them.
- *
- * Example:
- *
- * ```ts
- * const findUser: Middleware<EmptyContext, { user: string }> = (req, next) => {
- *   return await next!(
- *     req.addContext({ user: "Kim" });
- *   );
- * };
- *
- * const hello = (req: HttpRequest<{ user: string }>) => new Response(`Hello ${req.context.user}`);
- *
- * // This Handler will respond with "Hello Kim"
- * const handler = composeMiddleware(findUser, hello)
- * ``` */
 function composeMiddleware<
   FirstNeeds extends EmptyContext,
   FirstAdd extends EmptyContext,
@@ -133,16 +138,19 @@ function composeMiddleware<
     );
 }
 
+/** Wraps the given handler, doing nothing and returning a copy */
 export function chain<Needs extends EmptyContext>(
   handler: Handler<Needs>,
 ): Handler<Needs>;
+
+/** Wraps the given middleware in a `MiddlewareChain` so it can be `.add()`ed onto */
 export function chain<
   Needs extends EmptyContext,
   Adds extends EmptyContext = EmptyContext,
 >(
   middleware: Middleware<Needs, Adds>,
 ): MiddlewareChain<Needs, Adds>;
-/** Wraps the given middleware in a `MiddlewareChain` so it can be `.add()`ed onto */
+
 export function chain<
   Needs extends EmptyContext,
   Adds extends EmptyContext = EmptyContext,
