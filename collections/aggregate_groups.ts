76a246d88d5331295d52c3f9bd84608e1512f1ab
Leon Strauss <me@lionc.de>
2021-11-11 11:45:39 +0100

    :pencil2: Fix typo in assertEquals name

--- collections/aggregate_groups.ts
+++ collections/aggregate_groups.ts
@@ -22,7 +22,7 @@ import { mapEntries } from "./map_entries.ts";
  *     },
  * )
  *
- * assetEquals(descriptions === {
+ * assertEquals(descriptions === {
  *     'Curry': 'Curry is spicy and vegan',
  *     'Omelette': 'Omelette is creamy and vegetarian',
  * })
