5faa4ccdb51cbf3acf33150b1b4701cee0e82e0a
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-11-11 11:52:08 +0100

    add lint ignores

--- node/internal/readline/interface.js
+++ node/internal/readline/interface.js
@@ -19,6 +19,8 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// deno-lint-ignore-file camelcase no-inner-declarations no-this-alias
+
 import { ERR_INVALID_ARG_VALUE } from "../../_errors.ts";
 import {
   validateAbortSignal,
