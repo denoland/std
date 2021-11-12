66f66593cad2f5baaaa06c2bff3b993d19914cc3
getspooky <getspookydev@gmail.com>
2021-11-12 22:54:19 +0100

    feat(node/_fs): Support `fs.read` with an optional options object

--- node/_fs/_fs_read.ts
+++ node/_fs/_fs_read.ts
@@ -3,6 +3,13 @@ import { Buffer } from "../buffer.ts";
 import { assert } from "../../testing/asserts.ts";
 import { ERR_INVALID_ARG_VALUE } from "../_errors.ts";
 
+type readOptions = {
+  buffer: Buffer | Uint8Array;
+  offset: number;
+  length: number;
+  position: number | null;
+};
+
 type BinaryCallback = (
   err: Error | null,
   bytesRead: number | null,
@@ -12,12 +19,50 @@ type Callback = BinaryCallback;
 
 export function read(
   fd: number,
+  options: readOptions,
+  callback: Callback,
+): void;
+export function read(
+  fd: number,
   buffer: Buffer | Uint8Array,
   offset: number,
   length: number,
   position: number | null,
   callback: Callback,
+): void;
+export function read(
+  fd: number,
+  optOrBuffer: Buffer | Uint8Array | readOptions,
+  offsetOrCallback: number | Callback,
+  length?: number,
+  position?: number | null,
+  callback?: Callback,
 ): void {
+  let cb: Callback | undefined;
+  let offset: number = 0,
+    buffer: Buffer | Uint8Array;
+
+  if (typeof offsetOrCallback === "function") {
+    cb = offsetOrCallback;
+  } else {
+    offset = offsetOrCallback;
+    cb = callback;
+  }
+
+  if (optOrBuffer instanceof Buffer) {
+    buffer = optOrBuffer;
+  } else {
+    let opt = optOrBuffer as readOptions;
+    offset = opt.offset;
+    buffer = opt.buffer;
+    position = opt.position;
+    length = opt.length;
+  }
+
+  if (length == null) {
+    length = 0;
+  }
+
   assert(offset >= 0, "offset should be greater or equal to 0");
   assert(
     offset + length <= buffer.byteLength,
@@ -46,11 +91,14 @@ export function read(
     err = error instanceof Error ? error : new Error("[non-error thrown]");
   }
 
-  if (err) {
-    (callback as (err: Error) => void)(err);
-  } else {
-    const data = new Buffer(buffer.buffer, offset, length);
-    callback(null, numberOfBytesRead, data);
+  if (cb) {
+    if (err) {
+      (callback as (err: Error) => void)(err);
+    } else {
+      const data = new Buffer(buffer.buffer, offset, length);
+      cb(null, numberOfBytesRead, data);
+    }
   }
+
   return;
 }
