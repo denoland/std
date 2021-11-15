9a55ccb3fd4e8c59004bf17f04811a6977ee0c48
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-11-15 13:22:27 +0100

    ci: fix lint and format scripts (#1576)

--- _tools/lint.js
+++ _tools/lint.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env -S deno run --allow-read --allow-run
 const p = Deno.run({ cmd: [Deno.execPath(), "lint", "--config", "deno.json"] });
-Deno.exit(await p.status());
+const status = await p.status();
+Deno.exit(status.code);
