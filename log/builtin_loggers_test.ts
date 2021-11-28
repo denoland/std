68192c790b56e1ef1235ae14c5ea325bbdbe3d23
Leon Strauss <me@lionc.de>
2021-11-28 23:51:36 +0100

    :white_check_mark: Add test for console logger

--- log/builtin_loggers_test.ts
+++ log/builtin_loggers_test.ts
@@ -1,13 +1,7 @@
-import {
-    buildConsoleLogger,
-    buildFileLogger,
-    buildMultiLogger,
-    FileLoggerOptions,
-} from "./builtin_loggers.ts";
-import {
-    LogHandler,
-    buildDefaultLogMessage,
-} from "./logging.ts";
+import { partition } from "../collections/partition.ts";
+import { buildFileLogger, FileLoggerOptions } from "./builtin_loggers.ts";
+import { buildDefaultLogMessage } from "./logging.ts";
+import { defaultLogLevels } from "./default_logger.ts";
 import { assertEquals } from "../testing/asserts.ts";
 
 const testLevels = {
@@ -16,62 +10,117 @@ const testLevels = {
   high: 3,
 };
 
-type TestLevels = typeof testLevels;
-type MessageTuple =
-    | [keyof TestLevels, number, unknown]
-    | [keyof TestLevels, number]
+export type TestLevels = typeof testLevels;
+export type MessageTuple =
+  | [keyof TestLevels, number, unknown]
+  | [keyof TestLevels, number];
 
-const logFile = "./__test.log"
-const testLine = "Test line\n"
+const logFile = "./__test.log";
+const testLine = "Test line\n";
 
 function resetLogFile() {
-    Deno.writeTextFileSync(logFile, testLine)
-}
-
-function removeLogFile() {
-    Deno.removeSync(logFile)
+  Deno.writeTextFileSync(logFile, testLine);
 }
 
 function readLogFile() {
-    return Deno.readTextFileSync(logFile)
+  return Deno.readTextFileSync(logFile);
 }
 
-function buildTestFileLogger(options?: FileLoggerOptions<TestLevels, number, unknown>) {
-    return buildFileLogger(
-        testLevels,
-        "middle",
-        logFile,
-        options,
-    )
+function buildTestFileLogger(
+  options?: FileLoggerOptions<TestLevels, number, unknown>,
+) {
+  return buildFileLogger(
+    testLevels,
+    "middle",
+    logFile,
+    options,
+  );
 }
 
-Deno.test("File logger with defautl settings logs to and overwrites the given file", () => {
-    resetLogFile()
-    const logger = buildTestFileLogger()
+Deno.test("File logger with default settings logs to and overwrites the given file", () => {
+  resetLogFile();
+  const logger = buildTestFileLogger();
 
-    const calls: MessageTuple[] = [
-        ["middle", 5],
-        ["low", 1, {}],
-        ["high", 19],
-        ["middle", -3, []],
-        ["low", 32],
-        ["low", 24, "asdf"],
-        ["high", 0, () => {}],
-        ["middle", 13],
-    ]
+  const calls: MessageTuple[] = [
+    ["middle", 5],
+    ["low", 1, {}],
+    ["high", 19],
+    ["middle", -3, []],
+    ["low", 32],
+    ["low", 24, "asdf"],
+    ["high", 0, () => {}],
+    ["middle", 13],
+  ];
 
-    calls.forEach(([level, message, data]) => logger[level](message, data))
-    logger.flush()
+  calls.forEach(([level, message, data]) => logger[level](message, data));
+  logger.flush();
 
-    const logged = calls
-        .filter(([level]) => level !== "low")
-        .map(([level, message, data]) => `${buildDefaultLogMessage(level, message, data)}\n`)
-        .join("")
-
-    assertEquals(
-        readLogFile(),
-        `${testLine}${logged}`,
+  const logged = calls
+    .filter(([level]) => level !== "low")
+    .map(([level, message, data]) =>
+      `${buildDefaultLogMessage(level, message, data)}\n`
     )
+    .join("");
 
-    logger.close()
+  assertEquals(
+    readLogFile(),
+    `${testLine}${logged}`,
+  );
+
+  logger.close();
+});
+
+Deno.test("Console logger logs to the console", async () => {
+  const threshold = "info";
+  const calls = [
+    ["info", 5],
+    ["trace", 1, {}],
+    ["debug", 19],
+    ["info", -3, []],
+    ["warn", 32],
+    ["error", 24, "asdf"],
+    ["info", 0],
+    ["trace", 13],
+  ] as const;
+
+  const process = Deno.run({
+    cmd: [
+      "deno",
+      "run",
+      "log/console_test_process.ts",
+      threshold,
+      ...calls.map((it) => JSON.stringify(it)),
+    ],
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+  const [_, ...rawOuts] = await Promise.all([
+    process.status(),
+    process.output(),
+    process.stderrOutput(),
+  ]);
+  process.close();
+
+  const [output, errOutput] = rawOuts
+    .map((it) => new TextDecoder().decode(it));
+
+  const [stdExpected, errExpected] = partition(
+    calls,
+    ([level]) => defaultLogLevels[level] < defaultLogLevels.error,
+  )
+    .map((calls) =>
+      calls.filter(([level]) =>
+        defaultLogLevels[level] >= defaultLogLevels[threshold]
+      )
+    )
+    .map((calls) =>
+      calls.map(([level, message, data]) =>
+        `${buildDefaultLogMessage(level, message, data)}\n`
+      )
+    )
+    .map((calls) => calls.join(""));
+
+  assertEquals(output, stdExpected);
+  assertEquals(errOutput, errExpected);
 });
