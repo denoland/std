8ee8b886ceb7612e7b98466dc9f74d234bc07122
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-11-29 23:20:19 -0500

    format

---
+++ path/format.ts
@@ -0,0 +1,15 @@
+import type { FormatInputPathObject } from "./_interface.ts";
+import { _format } from "./_util.ts";
+
+/**
+ * Generate a path from `FormatInputPathObject` object.
+ * @param pathObject with path
+ */
+export function format(pathObject: FormatInputPathObject): string {
+  if (pathObject === null || typeof pathObject !== "object") {
+    throw new TypeError(
+      `The "pathObject" argument must be of type Object. Received type ${typeof pathObject}`,
+    );
+  }
+  return _format("\\", pathObject);
+}
