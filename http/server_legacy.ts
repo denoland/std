166a32b24dedef69187794fc2d46db889848be46
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-12-04 02:32:25 +0100

    fmt

--- http/server_legacy.ts
+++ http/server_legacy.ts
@@ -118,7 +118,7 @@ export class ServerRequest {
             .map((e): string => e.trim().toLowerCase());
           assert(
             parts.includes("chunked"),
-            'transfer-encoding must include "chunked" if content-length is not set'
+            'transfer-encoding must include "chunked" if content-length is not set',
           );
           this.#body = chunkedBodyReader(this.headers, this.r);
         } else {
@@ -195,7 +195,7 @@ export class Server implements AsyncIterable<ServerRequest> {
 
   // Yields all HTTP requests on a single TCP connection.
   private async *iterateHttpRequests(
-    conn: Deno.Conn
+    conn: Deno.Conn,
   ): AsyncIterableIterator<ServerRequest> {
     const reader = new BufReader(conn);
     const writer = new BufWriter(conn);
@@ -273,7 +273,7 @@ export class Server implements AsyncIterable<ServerRequest> {
   // same kind and adds it to the request multiplexer so that another TCP
   // connection can be accepted.
   private async *acceptConnAndIterateHttpRequests(
-    mux: MuxAsyncIterator<ServerRequest>
+    mux: MuxAsyncIterator<ServerRequest>,
   ): AsyncIterableIterator<ServerRequest> {
     if (this.#closing) return;
     // Wait for a new connection.
@@ -356,7 +356,7 @@ export function serve(addr: string | HTTPOptions): Server {
  */
 export async function listenAndServe(
   addr: string | HTTPOptions,
-  handler: (req: ServerRequest) => void
+  handler: (req: ServerRequest) => void,
 ) {
   const server = serve(addr);
 
@@ -429,7 +429,7 @@ export function serveTLS(options: HTTPSOptions): Server {
  */
 export async function listenAndServeTLS(
   options: HTTPSOptions,
-  handler: (req: ServerRequest) => void
+  handler: (req: ServerRequest) => void,
 ) {
   const server = serveTLS(options);
 
