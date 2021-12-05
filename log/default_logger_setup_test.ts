f3cde6ceeff33e19a2caa0cc7de97a7b186aa610
Leon Strauss <me@lionc.de>
2021-12-05 03:06:54 +0100

    :white_check_mark: Add tests for logging setups

---
+++ log/default_logger_setup_test.ts
@@ -0,0 +1,62 @@
+import {
+    buildFrameworkLogger,
+  addDefaultLogger,
+  disableDefaultConsoleLogger,
+  log,
+  setDefaultConsoleLoggerThreshold,
+  setThirdPartyDefaultThreshold,
+  setThirdPartyThresholds,
+} from "./default_logger.ts";
+import { assertEquals } from "../testing/asserts.ts";
+import type { DefaultLogger } from "./default_logger.ts";
+
+const calls: unknown[] = [];
+const resetCalls = () => calls.splice(0, calls.length)
+
+const testLogger = new Proxy({}, {
+  get(_, prop) {
+    return (...args: unknown[]) => calls.push([prop, ...args]);
+  },
+});
+
+disableDefaultConsoleLogger();
+addDefaultLogger(testLogger as DefaultLogger);
+addDefaultLogger(testLogger as DefaultLogger);
+
+Deno.test("default logger registrations get called", () => {
+  resetCalls()
+
+  log.warn("Some message");
+  log.info("Another message", { foo: "bar" });
+
+  assertEquals(calls, [
+    ["warn", "Some message", undefined],
+    ["warn", "Some message", undefined],
+    ["info", "Another message", { foo: "bar" }],
+    ["info", "Another message", { foo: "bar" }],
+  ]);
+});
+
+Deno.test("Framework logging", () => {
+  resetCalls()
+
+  const fooLogger = buildFrameworkLogger("foo")
+  const barLogger = buildFrameworkLogger("bar")
+
+  setThirdPartyDefaultThreshold("info")
+  setThirdPartyThresholds({
+      foo: "debug",
+  })
+
+  fooLogger.trace("A")
+  fooLogger.debug("B")
+  barLogger.debug("C")
+  barLogger.info("D")
+
+  assertEquals(calls, [
+      ["debug", "[foo]\tB", undefined],
+      ["debug", "[foo]\tB", undefined],
+      ["info", "[bar]\tD", undefined],
+      ["info", "[bar]\tD", undefined],
+  ])
+});
