78d464a17f55ccf7577564e58648bdbc3fa5d08a
Zheyu Zhang <zheyuzhang03@gmail.com>
2021-12-09 22:15:38 +0800

    feat(toml): align keys by option (#1693)

--- encoding/toml_test.ts
+++ encoding/toml_test.ts
@@ -448,62 +448,62 @@ Deno.test({
       bool: true,
       bool2: false,
     };
-    const expected = `deno    = "is"
-not     = "[node]"
-regex   = "<ic*s*>"
-NANI    = "何?!"
+    const expected = `deno = "is"
+not = "[node]"
+regex = "<ic*s*>"
+NANI = "何?!"
 comment = "Comment inside # the comment"
-int1    = 99
-int2    = 42
-int3    = 0
-int4    = -17
-int5    = 1000
-int6    = 5349221
-int7    = 12345
-flt1    = 1
-flt2    = 3.1415
-flt3    = -0.01
-flt4    = 5e+22
-flt5    = 1000000
-flt6    = -0.02
-flt7    = 6.626e-34
-odt1    = 1979-05-01T07:32:00.000
-odt2    = 1979-05-27T07:32:00.000
-odt3    = 1979-05-27T07:32:00.999
-odt4    = 1979-05-27T07:32:00.000
-ld1     = 1979-05-27T00:00:00.000
-reg     = "/foo[bar]/"
-sf1     = inf
-sf2     = inf
-sf3     = -inf
-sf4     = NaN
-sf5     = NaN
-sf6     = NaN
-data    = [["gamma","delta"],[1,2]]
-hosts   = ["alpha","omega"]
-bool    = true
-bool2   = false
+int1 = 99
+int2 = 42
+int3 = 0
+int4 = -17
+int5 = 1000
+int6 = 5349221
+int7 = 12345
+flt1 = 1
+flt2 = 3.1415
+flt3 = -0.01
+flt4 = 5e+22
+flt5 = 1000000
+flt6 = -0.02
+flt7 = 6.626e-34
+odt1 = 1979-05-01T07:32:00.000
+odt2 = 1979-05-27T07:32:00.000
+odt3 = 1979-05-27T07:32:00.999
+odt4 = 1979-05-27T07:32:00.000
+ld1 = 1979-05-27T00:00:00.000
+reg = "/foo[bar]/"
+sf1 = inf
+sf2 = inf
+sf3 = -inf
+sf4 = NaN
+sf5 = NaN
+sf6 = NaN
+data = [["gamma","delta"],[1,2]]
+hosts = ["alpha","omega"]
+bool = true
+bool2 = false
 
 [foo]
-bar     = "deno"
+bar = "deno"
 
 [this.is]
-nested  = "denonono"
+nested = "denonono"
 
 ["https://deno.land/std"]
-"$"     = "doller"
+"$" = "doller"
 
 ["##".deno."https://deno.land"]
-proto   = "https"
-":80"   = "port"
+proto = "https"
+":80" = "port"
 
 [[arrayObjects]]
-stuff   = "in"
+stuff = "in"
 
 [[arrayObjects]]
 
 [[arrayObjects]]
-the     = "array"
+the = "array"
 `;
     const actual = stringify(src);
     assertEquals(actual, expected);
@@ -526,15 +526,15 @@ Deno.test({
         },
       },
     };
-    const expected = `emptyArray   = []
-mixedArray1  = [1,{b = 2}]
-mixedArray2  = [{b = 2},1]
+    const expected = `emptyArray = []
+mixedArray1 = [1,{b = 2}]
+mixedArray2 = [{b = 2},1]
 nestedArray1 = [[{b = 1}]]
 nestedArray2 = [[[{b = 1}]]]
 nestedArray3 = [[],[{b = 1}]]
 
 [deepNested.a]
-b            = [1,{c = 2,d = [{e = 3},true]}]
+b = [1,{c = 2,d = [{e = 3},true]}]
 `;
     const actual = stringify(src);
     assertEquals(actual, expected);
@@ -554,8 +554,8 @@ Deno.test({
     };
     const expected = `
 "\\"" = "\\""
-"'"  = "'"
-" "  = " "
+"'" = "'"
+" " = " "
 "\\\\" = "\\\\"
 "\\n" = "\\n"
 "\\t" = "\\t"
@@ -683,9 +683,30 @@ Deno.test({
       "helloooooooo": 1,
     };
 
-    const actual = stringify(src);
+    const actual = stringify(src, { keyAlignment: true });
     const expected = `a            = "a = 1"
 helloooooooo = 1
+`;
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "[TOML] stringfy with key alignment",
+  fn(): void {
+    const src = {
+      "a": 1,
+      "aa": 1,
+      "aaa": 1,
+      "aaaa": 1,
+      "aaaaa": 1,
+    };
+    const actual = stringify(src, { keyAlignment: true });
+    const expected = `a     = 1
+aa    = 1
+aaa   = 1
+aaaa  = 1
+aaaaa = 1
 `;
     assertEquals(actual, expected);
   },
