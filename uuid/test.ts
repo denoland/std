334d99876539b212fcb235c430bc5f4abaf1c838
getspooky <getspookydev@gmail.com>
2021-12-17 10:24:42 +0100

    test(uuid): Test validate uuid function

--- uuid/test.ts
+++ uuid/test.ts
@@ -1,10 +1,16 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 import { assert } from "../testing/asserts.ts";
-import { isNil, NIL_UUID } from "./mod.ts";
+import { isNil, NIL_UUID, validate } from "./mod.ts";
 
 Deno.test("[UUID] isNil", () => {
   const nil = NIL_UUID;
   const u = "582cbcff-dad6-4f28-888a-e062ae36bafc";
   assert(isNil(nil));
   assert(!isNil(u));
+});
+
+Deno.test("[UUID] validate", () => {
+  const u = "582cbcff-dad6-4f28-888a-e062ae36bafc";
+  assert(validate(u));
+  assert(!validate("not a UUID"));
 });
