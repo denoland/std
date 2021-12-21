4e01d764eb86caef2e15c42585f323610903802c
Yoshiya Hinosawa <stibium121@gmail.com>
2021-12-22 00:11:50 +0900

    chore(node): add stub modules (#1737)
    Co-authored-by: Ben Noordhuis <info@bnoordhuis.nl>

--- node/https.ts
+++ node/https.ts
@@ -1,1 +1,33 @@
-throw new Error('"https" is not yet implemented.');
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+// Copyright Joyent and Node contributors. All rights reserved. MIT license.
+
+import { notImplemented } from "./_utils.ts";
+
+export class Agent {
+  constructor() {
+    notImplemented();
+  }
+}
+export class Server {
+  constructor() {
+    notImplemented();
+  }
+}
+export function createServer() {
+  notImplemented();
+}
+export function get() {
+  notImplemented();
+}
+export const globalAgent = undefined;
+export function request() {
+  notImplemented();
+}
+export default {
+  Agent,
+  Server,
+  createServer,
+  get,
+  globalAgent,
+  request,
+};
