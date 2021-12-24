d7c9385a5f47e2f9ab3f6b8253386a47f2c0b580
wafuwafu13 <mariobaske@i.softbank.jp>
2021-12-24 18:42:30 +0900

    node(util): Enable test-util-promisify.js

--- node/internal/util.js
+++ node/internal/util.js
@@ -1,6 +1,4 @@
-import { validateString } from "./validators.js";
-import { promisify } from "../_util/_util_promisify.ts";
-export { promisify };
+import { validateFunction, validateString } from "./validators.js";
 
 export function normalizeEncoding(enc) {
   if (enc == null || enc === "utf8" || enc === "utf-8") return "utf8";
@@ -141,9 +139,75 @@ export function deprecate(fn, msg, code) {
   return deprecated;
 }
 
+// In addition to being accessible through util.promisify.custom,
+// this symbol is registered globally and can be accessed in any environment as
+// Symbol.for('nodejs.util.promisify.custom').
+const kCustomPromisifiedSymbol = Symbol.for("nodejs.util.promisify.custom");
+// This is an internal Node symbol used by functions returning multiple
+// arguments, e.g. ['bytesRead', 'buffer'] for fs.read().
+export const kCustomPromisifyArgsSymbol = Symbol.for(
+  "nodejs.util.promisify.customArgs",
+);
+
+export function promisify(
+  original,
+) {
+  validateFunction(original, "original");
+  if (original[kCustomPromisifiedSymbol]) {
+    const fn = original[kCustomPromisifiedSymbol];
+
+    validateFunction(fn, "util.promisify.custom");
+
+    return Object.defineProperty(fn, kCustomPromisifiedSymbol, {
+      value: fn,
+      enumerable: false,
+      writable: false,
+      configurable: true,
+    });
+  }
+
+  // Names to create an object from in case the callback receives multiple
+  // arguments, e.g. ['bytesRead', 'buffer'] for fs.read.
+  const argumentNames = original[kCustomPromisifyArgsSymbol];
+  function fn(...args) {
+    return new Promise((resolve, reject) => {
+      original.call(...args, (err, ...values) => {
+        if (err) {
+          return reject(err);
+        }
+        if (argumentNames !== undefined && values.length > 1) {
+          const obj = {};
+          for (let i = 0; i < argumentNames.length; i++) {
+            obj[argumentNames[i]] = values[i];
+          }
+          resolve(obj);
+        } else {
+          resolve(values[0]);
+        }
+      });
+    });
+  }
+
+  Object.setPrototypeOf(fn, Object.getPrototypeOf(original));
+
+  Object.defineProperty(fn, kCustomPromisifiedSymbol, {
+    value: fn,
+    enumerable: false,
+    writable: false,
+    configurable: true,
+  });
+  return Object.defineProperties(
+    fn,
+    Object.getOwnPropertyDescriptors(original),
+  );
+}
+
+promisify.custom = kCustomPromisifiedSymbol;
+
 export default {
   createDeferredPromise,
   normalizeEncoding,
   once,
   deprecate,
+  promisify,
 };
