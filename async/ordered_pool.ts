775747281aea7e57085410ef6369c0a8a587a12a
lilnasy <69170106+lilnasy@users.noreply.github.com>
2021-12-26 20:43:44 +0530

    format ordered_pool and test

--- async/ordered_pool.ts
+++ async/ordered_pool.ts
@@ -12,50 +12,46 @@
  * @param iteratorFn The async function to call for every item of the array.
  */
 export function orderedPooledMap<T, R>(
-	poolLimit: number,
-	array: Iterable<T> | AsyncIterable<T>,
-	iteratorFn: (data: T) => Promise<R>,
+  poolLimit: number,
+  array: Iterable<T> | AsyncIterable<T>,
+  iteratorFn: (data: T) => Promise<R>,
 ) {
-	// Create the readable stream whose async iterable is returned from this function.
-	const readable = new ReadableStream<[T,R]> ({
-		
-		async start ( controller: ReadableStreamDefaultController<[T,R]> ) {
-			
-			// container for pending applications
-			const executing: Array<Promise<unknown>> = []
-			
-			// container for errors thrown by the iterator function
-			const errors: Array<[T, Error]> = []
-			
-			for await (const item of array) {
-				
-				const exe = iteratorFn(item)
-				
-					// Only write on success.
-					.then( result => controller.enqueue([item, result]) )
-					
-					// If we `controller.enqueue()` a rejected promise,
-					// that will end the iteration. We don't want that yet.
-					// Instead accumulate rejections in an array, and all
-					// rejections among them can be reported together.
-					.catch( reason => errors.push([item, reason]) )
-					
-					// remove self from executing when resolved or rejected 
-					.finally( () => executing.splice(executing.indexOf(exe), 1) )
-				
-				executing.push(exe)
-				
-				if (executing.length >= poolLimit) await Promise.race(executing)
-			}
-			
-			// Wait until all ongoing events have processed, then close the readable stream or throw.
-			await Promise.all(executing)
-			
-			if (errors.length > 0) throw new AggregateError(errors, "Threw while mapping.")
-			
-			controller.close()
-		}
-	})
-	
-	return readable[Symbol.asyncIterator]()
+  // Create the readable stream whose async iterable is returned from this function.
+  const readable = new ReadableStream<[T, R]>({
+    async start(controller: ReadableStreamDefaultController<[T, R]>) {
+      // container for pending applications
+      const executing: Array<Promise<unknown>> = [];
+
+      // container for errors thrown by the iterator function
+      const errors: Array<[T, Error]> = [];
+
+      for await (const item of array) {
+        const exe = iteratorFn(item)
+          // Only write on success.
+          .then((result) => controller.enqueue([item, result]))
+          // If we `controller.enqueue()` a rejected promise,
+          // that will end the iteration. We don't want that yet.
+          // Instead accumulate rejections in an array, and all
+          // rejections among them can be reported together.
+          .catch((reason) => errors.push([item, reason]))
+          // remove self from executing when resolved or rejected
+          .finally(() => executing.splice(executing.indexOf(exe), 1));
+
+        executing.push(exe);
+
+        if (executing.length >= poolLimit) await Promise.race(executing);
+      }
+
+      // Wait until all ongoing events have processed, then close the readable stream or throw.
+      await Promise.all(executing);
+
+      if (errors.length > 0) {
+        throw new AggregateError(errors, "Threw while mapping.");
+      }
+
+      controller.close();
+    },
+  });
+
+  return readable[Symbol.asyncIterator]();
 }
--- async/ordered_pool_test.ts
+++ async/ordered_pool_test.ts
@@ -1,77 +1,75 @@
-import { delay } from "./delay.ts"
-import { orderedPooledMap } from "./ordered_pool.ts"
+import { delay } from "./delay.ts";
+import { orderedPooledMap } from "./ordered_pool.ts";
 import {
-	assert,
-	assertEquals,
-	assertRejects,
-	assertStringIncludes
-} from "../testing/asserts.ts"
+  assert,
+  assertEquals,
+  assertRejects,
+  assertStringIncludes,
+} from "../testing/asserts.ts";
 
-Deno.test("[async] orderedPooledMap", async () => {	
+Deno.test("[async] orderedPooledMap", async () => {
+  const start = performance.now();
 
-	const start = performance.now()
+  const poolLimit = 2;
+  const array = [1, 2, 3];
+  const msPerMapping = 1000;
+  const iteratorFn = (x: number) => delay(msPerMapping).then(() => x ** 2);
 
-	const poolLimit = 2
-	const array = [1, 2, 3]
-	const msPerMapping = 1000
-	const iteratorFn = (x: number) => delay(msPerMapping).then( () => x ** 2 )
+  const results = orderedPooledMap(poolLimit, array, iteratorFn);
 
-	const results = orderedPooledMap( poolLimit, array, iteratorFn )
+  for await (const [input, result] of results) console.info(input, result);
 
-	for await (const [input, result] of results) console.info(input, result)
+  const diff = performance.now() - start;
 
-	const diff = performance.now() - start
-
-	assert(
-		diff >= 2000,
-		`It took shorter (${diff}ms) than expected (at least 2000ms) to map ${array.length} items, taking ${msPerMapping}ms each, with a pool limit of ${poolLimit}`
-	)
-	assert(
-		diff < 3000,
-		`It took longer (${diff}ms) than expected (at most 3000ms) to map ${array.length} items, taking ${msPerMapping}ms each, with a pool limit of ${poolLimit}`
-	)
-})
+  assert(
+    diff >= 2000,
+    `It took shorter (${diff}ms) than expected (at least 2000ms) to map ${array.length} items, taking ${msPerMapping}ms each, with a pool limit of ${poolLimit}`,
+  );
+  assert(
+    diff < 3000,
+    `It took longer (${diff}ms) than expected (at most 3000ms) to map ${array.length} items, taking ${msPerMapping}ms each, with a pool limit of ${poolLimit}`,
+  );
+});
 
 Deno.test("[async] orderedPooledMap order", async () => {
+  // [1...50]
+  const array = Array(50).fill(1).map((i, v) => i + v);
 
-	// [1...50]
-	const array = Array(50).fill(1).map( (i, v) => i + v )
+  const iteratorFn = (x: number) =>
+    delay(Math.random() * 1000).then(() => x ** 2);
 
-	const iteratorFn = (x: number) => delay( Math.random() * 1000 ).then( () => x ** 2 )
+  const results = orderedPooledMap(10, array, iteratorFn);
 
-	const results = orderedPooledMap( 10, array, iteratorFn )
-
-	for await (const [input, result] of results)
-		assert(
-			input ** 2 == result,
-			`input: ${input}\n result: ${result}`
-		)
-
-})
-
+  for await (const [input, result] of results) {
+    assert(
+      input ** 2 == result,
+      `input: ${input}\n result: ${result}`,
+    );
+  }
+});
 
 Deno.test("[async] orderedPooledMap errors", async () => {
-		
-	async function mapNumber(n: number): Promise<number> {
-		if (n <= 2) throw new Error(`Bad number: ${n}`)
-		await delay(100)
-		return n
-	}
+  async function mapNumber(n: number): Promise<number> {
+    if (n <= 2) throw new Error(`Bad number: ${n}`);
+    await delay(100);
+    return n;
+  }
 
-	const mappedNumbers: number[] = []
+  const mappedNumbers: number[] = [];
 
-	await assertRejects(
-		async () => {
-			for await (const [ _, m ] of orderedPooledMap(3, [1, 2, 3, 4], mapNumber))
-				mappedNumbers.push(m)
-		},
-		(error: Error) => {
-			assert(error instanceof AggregateError)
-			assertEquals(error.errors.length, 2)
-			assertStringIncludes(error.errors[0][1].stack, "Error: Bad number: 1")
-			assertStringIncludes(error.errors[1][1].stack, "Error: Bad number: 2")
-		}
-	)
+  await assertRejects(
+    async () => {
+      for await (const [_, m] of orderedPooledMap(3, [1, 2, 3, 4], mapNumber)) {
+        mappedNumbers.push(m);
+      }
+    },
+    (error: Error) => {
+      assert(error instanceof AggregateError);
+      assertEquals(error.errors.length, 2);
+      assertStringIncludes(error.errors[0][1].stack, "Error: Bad number: 1");
+      assertStringIncludes(error.errors[1][1].stack, "Error: Bad number: 2");
+    },
+  );
 
-	assertEquals(mappedNumbers, [3, 4])
-})
+  assertEquals(mappedNumbers, [3, 4]);
+});
