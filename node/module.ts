8ffa6ffa85d73f706c7ae2e8e9e56d599e172544
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-01-07 16:38:42 +0100

    fix(node): set prototype for Module

--- node/module.ts
+++ node/module.ts
@@ -752,25 +752,27 @@ function createNativeModule(id: string, exports: any): Module {
 for (const key of Object.keys(nodeMods)) {
   nativeModulePolyfill.set(key, createNativeModule(key, nodeMods[key]));
 }
+const m = {
+  _cache: Module._cache,
+  _extensions: Module._extensions,
+  _findPath: Module._findPath,
+  _initPaths: Module._initPaths,
+  _load: Module._load,
+  _nodeModulePaths: Module._nodeModulePaths,
+  _pathCache: Module._pathCache,
+  _preloadModules: Module._preloadModules,
+  _resolveFilename: Module._resolveFilename,
+  _resolveLookupPaths: Module._resolveLookupPaths,
+  builtinModules: Module.builtinModules,
+  createRequire: Module.createRequire,
+  globalPaths: Module.globalPaths,
+  Module,
+  wrap: Module.wrap,
+};
+Object.setPrototypeOf(m, Module);
 nativeModulePolyfill.set(
   "module",
-  createNativeModule("module", {
-    _cache: Module._cache,
-    _extensions: Module._extensions,
-    _findPath: Module._findPath,
-    _initPaths: Module._initPaths,
-    _load: Module._load,
-    _nodeModulePaths: Module._nodeModulePaths,
-    _pathCache: Module._pathCache,
-    _preloadModules: Module._preloadModules,
-    _resolveFilename: Module._resolveFilename,
-    _resolveLookupPaths: Module._resolveLookupPaths,
-    builtinModules: Module.builtinModules,
-    createRequire: Module.createRequire,
-    globalPaths: Module.globalPaths,
-    Module,
-    wrap: Module.wrap,
-  }),
+  createNativeModule("module", m),
 );
 
 function loadNativeModule(
