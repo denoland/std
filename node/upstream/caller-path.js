696363c79f2bc722b9d9c485a03c7708cdb0f5f4
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-01-08 17:00:24 +0100

    fmt

--- node/upstream/caller-path.js
+++ node/upstream/caller-path.js
@@ -1,10 +1,10 @@
 import { createRequire } from "../module.ts";
 
 const require = createRequire(import.meta.url);
-const re = /^file:///;
+const re = /^file:/; //;
 
 export default () => {
-    const callerCallsite = require('caller-callsite');
-    const fileUrl = callerCallsite().getFileName();
-    return fileUrl.replace(re, "");
+  const callerCallsite = require("caller-callsite");
+  const fileUrl = callerCallsite().getFileName();
+  return fileUrl.replace(re, "");
 };
