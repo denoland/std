22802ffec10534c71c5a3108be210ab75d35a35a
Jeremy Banks <_@jeremy.ca>
2022-01-09 09:49:02 -0500

    crypto(build): update Rust and Rust dependencies (#1805)

--- _wasm_crypto/_build.ts
+++ _wasm_crypto/_build.ts
@@ -33,8 +33,6 @@ if (
       "cargo",
       "build",
       "--release",
-      "--target",
-      "wasm32-unknown-unknown",
     ],
     env: {
       // eliminate some potential sources of non-determinism
@@ -117,8 +115,9 @@ import wasmBytes from "./crypto.wasm.js";
 
 ${
   generatedScript.replace(
-    /^const file =.*?;\nconst wasmFile =.*?;\nconst wasmModule =.*?;\n/sm,
-    `const wasmModule = new WebAssembly.Module(wasmBytes);`,
+    /^const wasm_url =.*?;\nlet wasmCode =.*?;\n.*?const wasmInstance =.*?;\n/sm,
+    `const wasmModule = new WebAssembly.Module(wasmBytes);\n` +
+      `const wasmInstance = new WebAssembly.Instance(wasmModule, imports);`,
   )
 }
 
