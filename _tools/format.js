a470485e7313cf3d515fa039f13ab15a6facdf85
Yoshiya Hinosawa <stibium121@gmail.com>
2022-01-18 20:22:11 +0900

    chore: deprecate _tools scripts

--- _tools/format.js
+++ _tools/format.js
@@ -1,6 +1,7 @@
-#!/usr/bin/env -S deno run --allow-read --allow-run
-const p = Deno.run({
-  cmd: [Deno.execPath(), "fmt", "--config", "deno.json", ...Deno.args],
-});
-const status = await p.status();
-Deno.exit(status.code);
+#!/usr/bin/env -S deno run --no-check
+import * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+if (semver.lt(Deno.version.deno, "1.18.0")) {
+  console.log("This script is deprecated. Upgrade to the latest Deno and run `deno fmt` command directly");
+} else {
+  console.log("This script is deprecated. Run `deno fmt` command directly");
+}
--- _tools/lint.js
+++ _tools/lint.js
@@ -1,4 +1,7 @@
-#!/usr/bin/env -S deno run --allow-read --allow-run
-const p = Deno.run({ cmd: [Deno.execPath(), "lint", "--config", "deno.json"] });
-const status = await p.status();
-Deno.exit(status.code);
+#!/usr/bin/env -S deno run --no-check
+import * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+if (semver.lt(Deno.version.deno, "1.18.0")) {
+  console.log("This script is deprecated. Upgrade to the latest Deno and run `deno lint` command directly");
+} else {
+  console.log("This script is deprecated. Run `deno lint` command directly");
+}
