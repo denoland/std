3b43d075ea6977e10bb1d9bd58fbeacc721a114e
Ryan Dahl <ry@tinyclouds.org>
2022-02-03 17:46:48 -0500

    0.125.0
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>

--- http/_mock_conn.ts
+++ http/_mock_conn.ts
@@ -24,7 +24,10 @@ export function mockConn(base: Partial<Deno.Conn> = {}): Deno.Conn {
       return Promise.resolve(-1);
     },
     close: (): void => {},
+    // TODO(ry) Remove the following ts-ignore.
+    // @ts-ignore This was added to workaround incompatibilities between Deno versions.
     setNoDelay: (_nodelay?: boolean): void => {},
+    // @ts-ignore This was added to workaround incompatibilities between Deno versions.
     setKeepAlive: (_keepalive?: boolean): void => {},
     ...base,
   };
