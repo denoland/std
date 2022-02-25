a95861e857c0ae2a3630366570ad18f66b2707b9
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-25 18:25:37 +0900

    fix(dotenv): avoid top-level-await in load.ts

--- dotenv/load_test.ts
+++ dotenv/load_test.ts
@@ -29,3 +29,28 @@ Deno.test({
     p.close();
   },
 });
+
+Deno.test({
+  name: "load when multiple files",
+  async fn() {
+    const p = Deno.run({
+      cmd: [
+        Deno.execPath(),
+        "run",
+        "--allow-read",
+        "--allow-env",
+        path.join(testdataDir, "./app_load_parent.ts"),
+      ],
+      cwd: testdataDir,
+      stdout: "piped",
+    });
+
+    const decoder = new TextDecoder();
+    const rawOutput = await p.output();
+    assertEquals(
+      decoder.decode(rawOutput).trim(),
+      "hello world",
+    );
+    p.close();
+  },
+});
