ccddd2d17ed1f9fc382658072e7b460d83ff5e22
timreichen <tim.reichen@gmail.com>
2022-02-26 13:32:56 +0100

    renove loadSync, add defaults

--- dotenv/load.ts
+++ dotenv/load.ts
@@ -6,58 +6,42 @@ export interface LoadOptions {
   defaultsPath?: string | URL;
 }
 
-export function loadSync(
-  denoEnv: DenoEnv,
-  {
-    envPath = ".env",
-    examplePath,
-    defaultsPath,
-  }: LoadOptions = {},
-) {
-  let exampleSource;
-  let defaultsSource;
-  if (examplePath) exampleSource = Deno.readTextFileSync(examplePath);
-  if (defaultsPath) defaultsSource = Deno.readTextFileSync(defaultsPath);
-  const source = Deno.readTextFileSync(envPath);
-  return configure(denoEnv, source, exampleSource, defaultsSource);
-}
-
 export async function load(
-  denoEnv: DenoEnv,
+  denoEnv: DenoEnv = Deno.env,
   {
     envPath = ".env",
-    examplePath,
-    defaultsPath,
+    examplePath = ".env.example",
+    defaultsPath = ".env.defaults",
   }: LoadOptions = {},
-) {
-  let exampleSource;
-  let defaultsSource;
-  if (examplePath) exampleSource = await Deno.readTextFile(examplePath);
-  if (defaultsPath) defaultsSource = await Deno.readTextFile(defaultsPath);
-  const source = await Deno.readTextFile(envPath);
-  return configure(denoEnv, source, exampleSource, defaultsSource);
-}
-
-function configure(
-  denoEnv: DenoEnv,
-  envSource: string,
-  exampleSource?: string,
-  defaultsSource?: string,
 ) {
   let example;
-  let defaultEnv = {};
-  if (exampleSource) example = parse(exampleSource);
-  if (defaultsSource) defaultEnv = parse(defaultsSource).env;
+  let defaultsEnv = {};
+  try {
+    const exampleSource = await Deno.readTextFile(examplePath);
+    example = parse(exampleSource);
+  } catch (error) {
+    if (!(error instanceof Deno.errors.NotFound)) {
+      throw error;
+    }
+  }
+  try {
+    const defaultsSource = await Deno.readTextFile(defaultsPath);
+    defaultsEnv = parse(defaultsSource).env;
+  } catch (error) {
+    if (!(error instanceof Deno.errors.NotFound)) {
+      throw error;
+    }
+  }
+  const envSource = await Deno.readTextFile(envPath);
+
   const object = parse(envSource, { example });
   // initialEnv is passed at the end of assign to prevent overwrites
   const initialEnv = denoEnv.toObject();
-  const combinedObject: Record<string, string> = {
-    ...defaultEnv,
+  const env: Record<string, string> = {
+    ...defaultsEnv,
     ...object.env,
     ...initialEnv,
   };
-  Object.entries(combinedObject).forEach(([key, value]) =>
-    denoEnv.set(key, value)
-  );
+  Object.entries(env).forEach(([key, value]) => denoEnv.set(key, value));
   return denoEnv;
 }
--- dotenv/load_test.ts
+++ dotenv/load_test.ts
@@ -1,4 +1,4 @@
-import { load, loadSync } from "./load.ts";
+import { load } from "./load.ts";
 import { assertEquals } from "../testing/asserts.ts";
 import * as path from "../path/mod.ts";
 
@@ -14,17 +14,6 @@ Deno.test("load", async () => {
     envPath: path.join(testdataDir, ".env.test"),
     examplePath: path.join(testdataDir, ".env.example.test"),
     defaultsPath: path.join(testdataDir, ".env.defaults.test"),
-  });
-  assertEquals(Deno.env.get("GREETING"), "Hello World");
-  assertEquals(Deno.env.get("DEFAULT1"), "Some Default");
-  clearDenoEnv();
-});
-
-Deno.test("loadSync", async () => {
-  await loadSync(Deno.env, {
-    envPath: "./dotenv/testdata/.env.test",
-    examplePath: "./dotenv/testdata/.env.example.test",
-    defaultsPath: "./dotenv/testdata/.env.defaults.test",
   });
   assertEquals(Deno.env.get("GREETING"), "Hello World");
   assertEquals(Deno.env.get("DEFAULT1"), "Some Default");
