5f06cd7654adc6ec12894647a19cbfed39a46904
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-28 20:08:09 -0600

    add modules from _util, encoding

--- encoding/hex.ts
+++ encoding/hex.ts
@@ -4,6 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+// This module is browser compatible.
 
 const hexTable = new TextEncoder().encode("0123456789abcdef");
 
