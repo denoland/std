58204e6f5617fe7a80dfdbe98e6d9f1cadef1d1e
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-28 22:25:50 -0600

    refactor: mark modules as browser compatible (#1972)

--- collections/mod.ts
+++ collections/mod.ts
@@ -1,4 +1,5 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+// This module is browser compatible.
 
 export * from "./aggregate_groups.ts";
 export * from "./associate_by.ts";
