6f524038c0813b93f70a09066029659a5a816e69
crowlkats <crowlkats@toaxl.com>
2022-03-07 17:22:17 +0100

    add comments

--- streams/buffer.ts
+++ streams/buffer.ts
@@ -167,6 +167,20 @@ export class Buffer {
   }
 }
 
+/** A TransformStream that will only read & enqueue `size` amount of bytes.
+ * This operation is chunk based and not BYOB based,
+ * and as such will read more than needed.
+ *
+ * if options.error is set, then instead of terminating the stream,
+ * an error will be thrown.
+ *
+ * ```ts
+ * import { LimitedBytesTransformStream } from "./buffer.ts";
+ * const res = await fetch("https://example.com");
+ * const parts = res.body!
+ *   .pipeThrough(new LimitedBytesTransformStream(512 * 1024));
+ * ```
+ */
 export class LimitedBytesTransformStream
   extends TransformStream<Uint8Array, Uint8Array> {
   #read = 0;
@@ -188,6 +202,17 @@ export class LimitedBytesTransformStream
   }
 }
 
+/** A TransformStream that will only read & enqueue `size` amount of chunks.
+ *
+ * if options.error is set, then instead of terminating the stream,
+ * an error will be thrown.
+ *
+ * ```ts
+ * import { LimitedTransformStream } from "./buffer.ts";
+ * const res = await fetch("https://example.com");
+ * const parts = res.body!.pipeThrough(new LimitedTransformStream(50));
+ * ```
+ */
 export class LimitedTransformStream<T> extends TransformStream<T, T> {
   #read = 0;
   constructor(size: number, options: { error?: boolean } = {}) {
