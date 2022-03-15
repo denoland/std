af06e97faea9f06b00d07e42c82786f8210c0d74
Yoshiya Hinosawa <stibium121@gmail.com>
2022-03-15 19:00:26 +0900

    feat(node): shim get-caller-file

--- node/upstream_modules.ts
+++ node/upstream_modules.ts
@@ -9,6 +9,30 @@ module.exports = () => {
 };
 `;
 
+const getCallerFile = `
+const re = /^file:/;
+
+function getCallerFile(position = 2) {
+  if (position >= Error.stackTraceLimit) {
+    throw new TypeError('getCallerFile(position) requires position be less then Error.stackTraceLimit but position was: \`' + position + '\` and Error.stackTraceLimit was: \`' + Error.stackTraceLimit + '\`');
+  }
+
+  const oldPrepareStackTrace = Error.prepareStackTrace;
+  Error.prepareStackTrace = (_, stack)  => stack;
+  const stack = new Error().stack;
+  Error.prepareStackTrace = oldPrepareStackTrace;
+
+
+  if (stack !== null && typeof stack === 'object') {
+    // stack[0] holds this file
+    // stack[1] holds where this function was called
+    // stack[2] holds the file we're interested in
+    return stack[position] ? (stack[position] as any).getFileName().replace(re, "") : undefined;
+  }
+};
+`;
+
 export default {
   "caller-path": callerPath,
+  "get-caller-file": getCallerFile,
 } as Record<string, string>;
