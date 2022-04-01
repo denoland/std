ff2131993f151840845672c49120de6ab170a982
David Sherret <dsherret@gmail.com>
2022-04-01 13:19:33 -0400

    Starting to add version bumping workflow.

---
+++ _tools/release/01_bump_version.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-run=git --allow-net --no-check
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+import { getReleasesMdFile, loadRepo, VersionFile } from "./repo.ts";
+
+const versionFile = new VersionFile();
+const currentVersion = versionFile.version;
+const newVersion = versionFile.version.inc(getVersionIncrementKind());
+
+// update version.ts
+console.log(`Bumping version from ${currentVersion} to ${newVersion}...`);
+versionFile.updateVersion(newVersion);
+
+// update Releases.md
+const releasesMdFile = getReleasesMdFile();
+const versionGitLog = await getGitLogFromLastVersion();
+releasesMdFile.updateWithGitLog({
+  gitLog: versionGitLog,
+  version: newVersion.toString(),
+});
+
+function getVersionIncrementKind() {
+  if (Deno.args.some((a) => a === "--patch")) {
+    return "patch";
+  } else if (Deno.args.some((a) => a === "--minor")) {
+    return "minor";
+  } else if (Deno.args.some((a) => a === "--major")) {
+    return "major";
+  } else {
+    throw new Error("Please provide a --patch, --minor, or --major flag.");
+  }
+}
+
+async function getGitLogFromLastVersion() {
+  const repo = await loadRepo();
+
+  // fetch the upstream tags and history
+  await repo.gitFetchTags("upstream");
+  await repo.gitFetchHistory("upstream");
+
+  // get the git log from the current commit to the last version
+  return await repo.getGitLogFromTags(
+    "upstream",
+    currentVersion.toString(),
+    undefined,
+  );
+}
---
+++ _tools/release/deps.ts
@@ -0,0 +1,4 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+export * from "../../../deno_automation/mod.ts";
+export * from "../../../deno_automation/github_actions.ts";
