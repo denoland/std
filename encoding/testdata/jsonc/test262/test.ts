3ca82139cd367bc197a515f90c5f34999ed764a3
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-05-07 02:59:17 +0900

    remove file for ignored test case

--- encoding/testdata/jsonc/test262/test.ts
+++ encoding/testdata/jsonc/test262/test.ts
@@ -3,33 +3,6 @@
 import { walk } from "../../../../fs/mod.ts";
 import { fromFileUrl } from "../../../../path/mod.ts";
 
-// Ignore these files as they are irrelevant to the jsonc parser.
-const ignoreFile = new Set([
-  "builtin.js",
-  "length.js",
-  "not-a-constructor.js",
-  "prop-desc.js",
-  "revived-proxy-revoked.js",
-  "revived-proxy.js",
-  "reviver-array-define-prop-err.js",
-  "reviver-array-delete-err.js",
-  "reviver-array-get-prop-from-prototype.js",
-  "reviver-array-length-coerce-err.js",
-  "reviver-array-length-get-err.js",
-  "reviver-array-non-configurable-prop-create.js",
-  "reviver-array-non-configurable-prop-delete.js",
-  "reviver-call-err.js",
-  "reviver-call-order.js",
-  "reviver-get-name-err.js",
-  "reviver-object-define-prop-err.js",
-  "reviver-object-delete-err.js",
-  "reviver-object-get-prop-from-prototype.js",
-  "reviver-object-non-configurable-prop-create.js",
-  "reviver-object-non-configurable-prop-delete.js",
-  "reviver-object-own-keys-err.js",
-  "reviver-wrapper.js",
-]);
-
 // helper used for testing
 const sta = await Deno.readTextFile(new URL("./sta.js", import.meta.url));
 const assert = await Deno.readTextFile(new URL("./assert.js", import.meta.url));
@@ -41,9 +14,6 @@ for await (
   const dirEntry of walk(fromFileUrl(new URL("./JSON/", import.meta.url)))
 ) {
   if (!dirEntry.isFile) {
-    continue;
-  }
-  if (ignoreFile.has(dirEntry.name)) {
     continue;
   }
   // Register a test case for each file.
