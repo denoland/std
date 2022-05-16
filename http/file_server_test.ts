f6a037a32091def0416fe84361233b2a963671d6
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-16 19:44:09 +0800

    Update file_server_test.ts

--- http/file_server_test.ts
+++ http/file_server_test.ts
@@ -1005,9 +1005,38 @@ Deno.test(
         const res = await fetch(
           testurl,
           {
-            headers: {
-              "Accept-Encoding": "gzip, deflate, br",
-            },
+            headers: [
+              [
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+              ],
+              ["Accept-Encoding", "gzip, deflate, br"],
+              [
+                "Accept-Language",
+                "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+              ],
+              ["Cache-Control", "no-cache"],
+              ["Connection", "keep-alive"],
+              ["DNT", "1"],
+              ["Host", "localhost:4507"],
+              ["Pragma", "no-cache"],
+              ["Referer", "http://localhost:4507/testdata/"],
+              ["Sec-Fetch-Dest", "document"],
+              ["Sec-Fetch-Mode", "navigate"],
+              ["Sec-Fetch-Site", "same-origin"],
+              ["Sec-Fetch-User", "?1"],
+              ["Upgrade-Insecure-Requests", "1"],
+              [
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
+              ],
+              [
+                "sec-ch-ua",
+                '" Not A;Brand";v="99", "Chromium";v="101", "Microsoft Edge";v="101"',
+              ],
+              ["sec-ch-ua-mobile", "?0"],
+              ["sec-ch-ua-platform", '"Windows"'],
+            ],
           },
         );
         assertEquals(res.status, 200);
