215139c170cbcc0cb93fb9c463f63504cf7475b6
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2022-05-19 07:05:49 +0200

    fix(async/pooledMap): return ordered result (#2239)
    closes #2236

--- async/pool_test.ts
+++ async/pool_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 import { delay } from "./delay.ts";
-import { pooledMap } from "./pool.ts";
+import { ERROR_WHILE_MAPPING_MESSAGE, pooledMap } from "./pool.ts";
 import {
   assert,
   assertEquals,
@@ -23,7 +23,7 @@ Deno.test("[async] pooledMap", async function () {
   assert(diff < 3000);
 });
 
-Deno.test("[async] pooledMap errors", async function () {
+Deno.test("[async] pooledMap errors", async () => {
   async function mapNumber(n: number): Promise<number> {
     if (n <= 2) {
       throw new Error(`Bad number: ${n}`);
@@ -38,9 +38,31 @@ Deno.test("[async] pooledMap errors", async function () {
     }
   }, (error: Error) => {
     assert(error instanceof AggregateError);
+    assert(error.message === ERROR_WHILE_MAPPING_MESSAGE);
     assertEquals(error.errors.length, 2);
     assertStringIncludes(error.errors[0].stack, "Error: Bad number: 1");
     assertStringIncludes(error.errors[1].stack, "Error: Bad number: 2");
   });
   assertEquals(mappedNumbers, [3]);
+});
+
+Deno.test("pooledMap returns ordered items", async () => {
+  function getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+  const results = pooledMap(
+    2,
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    (i) =>
+      new Promise((r) => setTimeout(() => r(i), getRandomInt(5, 20) * 100)),
+  );
+
+  const returned = [];
+  for await (const value of results) {
+    returned.push(value);
+  }
+  assertEquals(returned, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
