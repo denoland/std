f2ab2374f8f8d25b824dbc78cc2e555b5a07e7a4
crowlkats <crowlkats@toaxl.com>
2022-05-24 23:45:19 +0200

    lint

--- examples/chat/server_test.ts
+++ examples/chat/server_test.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 import { assert, assertEquals } from "../../testing/asserts.ts";
-import { delay } from "../../async/delay.ts";
 import { dirname, fromFileUrl, resolve } from "../../path/mod.ts";
 import { TextLineStream } from "../../streams/delimiter.ts";
 
@@ -53,8 +52,7 @@ Deno.test({
   sanitizeResources: false, // TODO(@crowlKats): re-enable once https://github.com/denoland/deno/pull/14686 lands
   async fn() {
     const server = await startServer();
-    let ws: WebSocket;
-    ws = new WebSocket("ws://127.0.0.1:8080/ws");
+    const ws = new WebSocket("ws://127.0.0.1:8080/ws");
     await new Promise<void>((resolve) => {
       ws.onmessage = (message) => {
         assertEquals(message.data, "Connected: [1]");
