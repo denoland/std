9848f386eb388b3e3cec0774e7a046208e5078eb
MierenManz <christiaanmens@gmail.com>
2022-05-24 16:22:14 +0200

    fix: change case to camelCase

--- encoding/varint.ts
+++ encoding/varint.ts
@@ -1,1 +1,6 @@
-export * from "../_wasm_varint/varint.mjs";
+export {
+  decode_u32 as decodeU32,
+  decode_u64 as decodeU64,
+  encode_u32 as encodeU32,
+  encode_u64 as encodeU64,
+} from "../_wasm_varint/varint.mjs";
