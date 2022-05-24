0cc2bf6f077fdd34118eeaf0a46c2c382e52b9dc
MierenManz <christiaanmens@gmail.com>
2022-05-24 18:02:16 +0200

    add docs

--- encoding/varint.ts
+++ encoding/varint.ts
@@ -1,6 +1,53 @@
-export {
-  decode_u32 as decodeU32,
-  decode_u64 as decodeU64,
-  encode_u32 as encodeU32,
-  encode_u64 as encodeU64,
-} from "../_wasm_varint/varint.mjs";
+import * as wasm from "../_wasm_varint/varint.mjs";
+
+/**
+ * ```ts
+ * import { encodeU32 } from "./varint.ts";
+ *
+ * const encodedValue = encodeU32(42);
+ * // Do something with the encoded value
+ * ```
+ */
+export function encodeU32(val: number): Uint8Array {
+  return wasm.encode_u32(val);
+}
+
+/**
+ * ```ts
+ * import { encodeU64 } from "./varint.ts";
+ *
+ * const encodedValue = encodeU64(42n);
+ * // Do something with the encoded value
+ * ```
+ */
+export function encodeU64(val: bigint): Uint8Array {
+  return wasm.encode_u64(val);
+}
+
+/**
+ * ```ts
+ * import { decodeU32 } from "./varint.ts";
+ * const bytes = Uint8Array.from([221, 199, 1]);
+ * const decodedValue = decodeU32(bytes);
+ *
+ * // Do something with the decoded value
+ * console.log(decodedValue === 25565);
+ * ```
+ */
+export function decodeU32(val: Uint8Array): number {
+  return wasm.decode_u32(val);
+}
+
+/**
+ * ```ts
+ * import { decodeU32 } from "./varint.ts";
+ * const bytes = Uint8Array.from([221, 199, 1]);
+ * const decodedValue = decodeU32(bytes);
+ *
+ * // Do something with the decoded value
+ * console.log(decodedValue === 25565);
+ * ```
+ */
+export function decodeU64(val: Uint8Array): BigInt {
+  return wasm.decode_u64(val);
+}
