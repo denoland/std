f72972314faca010b232ed91bfde68cf03616b6e
cmorten <craig.morten@hotmail.co.uk>
2022-05-26 10:05:59 +0100

    fix: dynamic import not playing nice

--- node/cluster.ts
+++ node/cluster.ts
@@ -2,9 +2,9 @@
 // Copyright Joyent, Inc. and Node.js contributors. All rights reserved. MIT license.
 
 import process from "./process.ts";
+import childCluster from "./internal/cluster/child.ts";
+import primaryCluster from "./internal/cluster/primary.ts";
 
-const childOrPrimary = "NODE_UNIQUE_ID" in process.env ? "child" : "primary";
+const cluster = "NODE_UNIQUE_ID" in process.env ? childCluster : primaryCluster;
 
-const exports = await import(`./internal/cluster/${childOrPrimary}.ts`);
-
-export default exports;
+export default cluster;
