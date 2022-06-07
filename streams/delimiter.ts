664875786b55c0e60c68f46287833a519d179e25
undefined <sjx233@qq.com>
2022-06-08 01:14:45 +0800

    do not use regex

--- streams/delimiter.ts
+++ streams/delimiter.ts
@@ -92,7 +92,7 @@ interface TextLineStreamOptions {
  * ```
  */
 export class TextLineStream extends TransformStream<string, string> {
-  readonly #delimiter: RegExp;
+  readonly #allowCR: boolean;
   #buf = "";
 
   constructor(options?: TextLineStreamOptions) {
@@ -100,15 +100,42 @@ export class TextLineStream extends TransformStream<string, string> {
       transform: (chunk, controller) => this.#handle(chunk, controller),
       flush: (controller) => this.#handle("\r\n", controller),
     });
-    this.#delimiter = options?.allowCR ? /\r?\n|\r(?!$)/g : /\r?\n/g;
+    this.#allowCR = options?.allowCR ?? false;
   }
 
   #handle(chunk: string, controller: TransformStreamDefaultController<string>) {
-    const lines = (this.#buf + chunk).split(this.#delimiter);
-    this.#buf = lines.pop()!;
-    for (const line of lines) {
-      controller.enqueue(line);
+    chunk = this.#buf + chunk;
+
+    for (;;) {
+      const lfIndex = chunk.indexOf("\n");
+
+      if (this.#allowCR) {
+        const crIndex = chunk.indexOf("\r");
+
+        if (
+          crIndex !== -1 && crIndex !== (chunk.length - 1) &&
+          (lfIndex === -1 || (lfIndex - 1) > crIndex)
+        ) {
+          controller.enqueue(chunk.slice(0, crIndex));
+          chunk = chunk.slice(crIndex + 1);
+          continue;
+        }
+      }
+
+      if (lfIndex !== -1) {
+        let crOrLfIndex = lfIndex;
+        if (chunk[lfIndex - 1] === "\r") {
+          crOrLfIndex--;
+        }
+        controller.enqueue(chunk.slice(0, crOrLfIndex));
+        chunk = chunk.slice(lfIndex + 1);
+        continue;
+      }
+
+      break;
     }
+
+    this.#buf = chunk;
   }
 }
 
