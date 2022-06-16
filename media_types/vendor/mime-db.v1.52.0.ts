0e35a8da737fb573f18476ed9720b333224b7f36
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-06-16 19:42:04 +0900

    fix lint

--- media_types/vendor/mime-db.v1.52.0.ts
+++ media_types/vendor/mime-db.v1.52.0.ts
@@ -1,3 +1,6 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 export default {
   "application/1d-interleaved-parityfec": {
     "source": "iana",
