417b10fcf98bf844ea9f05119856e4947623c609
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-19 15:21:50 +0800

    Create diagnostics_channel.ts

---
+++ node/diagnostics_channel.ts
@@ -0,0 +1,13 @@
+
+export function channel() {
+    throw new Error("not implemented");
+}
+export function hasSubscribers() {
+    throw new Error("not implemented");
+}
+export function Channel() {
+    throw new Error("not implemented");
+}
+
+export default { channel, Channel, hasSubscribers };
+
