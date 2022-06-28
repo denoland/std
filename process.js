6f8f3e7742e126d28144e22d420232a40b8e19ec
cjihrig <cjihrig@gmail.com>
2022-06-28 16:23:25 -0400

    add tests
    test-process-beforeexit.js is failing on line 77

--- process.js
+++
@@ -1,9 +0,0 @@
-process.on("beforeExit", (code) => {
-  console.log("Process beforeExit event with code: ", code);
-});
-
-process.on("exit", (code) => {
-  console.log("Process exit event with code: ", code);
-});
-
-console.log("This message is displayed first.");
