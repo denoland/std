07e21eb38073a79bc0b31a3c979fb245fcd6e5f4
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-07-21 22:01:42 +0200

    Revert "revert changes to tests"
    This reverts commit c9cfc0a94a00904dd8183ff982973e1e8cf7411a.

--- node/child_process_test.ts
+++ node/child_process_test.ts
@@ -104,7 +104,11 @@ Deno.test({
       assert(childProcess.stdout, "stdout should be defined");
       let data: Buffer;
       childProcess.stdout.on("data", (chunk) => {
-        data = chunk;
+        if (!data) {
+          data = chunk;
+        } else {
+          data = Buffer.concat([data, chunk]);
+        }
       });
       const buffer = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
       childProcess.stdin.write(buffer);
