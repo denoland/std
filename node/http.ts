4a4ea22d4f56020dfc67df4cca4815c1cc431fe2
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-07-22 13:01:06 +0200

    revert changes

--- node/http.ts
+++ node/http.ts
@@ -15,7 +15,6 @@ import {
 import { OutgoingMessage } from "./_http_outgoing.ts";
 import { Agent } from "./_http_agent.mjs";
 import { urlToHttpOptions } from "./internal/url.ts";
-// import Duplex from "./internal/streams/duplex.mjs";
 import { constants, TCP } from "./internal_binding/tcp_wrap.ts";
 
 const METHODS = [
