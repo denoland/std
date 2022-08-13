f693ad60d4243ccf84038441234b7aac9381e4e6
JongChan Choi <disjukr@naver.com>
2022-08-13 22:44:50 +0900

    feat(http): support cert, key options in serveTls (#2508)

--- http/server_test.ts
+++ http/server_test.ts
@@ -1447,3 +1447,27 @@ Deno.test("serveTls - onListen callback is called with ephemeral port", () => {
     signal: abortController.signal,
   });
 });
+
+Deno.test("serveTls - cert, key can be injected directly from memory rather than file system.", () => {
+  const abortController = new AbortController();
+  return serveTls((_) => new Response("hello"), {
+    port: 0,
+    cert: Deno.readTextFileSync(join(testdataDir, "tls/localhost.crt")),
+    key: Deno.readTextFileSync(join(testdataDir, "tls/localhost.key")),
+    async onListen({ hostname, port }) {
+      assertEquals(hostname, "0.0.0.0");
+      assertNotEquals(port, 0);
+      const caCert = await Deno.readTextFile(
+        join(testdataDir, "tls/RootCA.pem"),
+      );
+      const client = Deno.createHttpClient({ caCerts: [caCert] });
+      const responseText = await (
+        await fetch(`https://localhost:${port}/`, { client })
+      ).text();
+      client.close();
+      assertEquals(responseText, "hello");
+      abortController.abort();
+    },
+    signal: abortController.signal,
+  });
+});
