d7fa0016c360ac72b1a1bf1ddf4a1ea4cf4dceaa
Yoshiya Hinosawa <stibium121@gmail.com>
2022-08-23 19:06:52 +0900

    ci: fix browser type checking (#2549)

--- encoding/yaml.ts
+++ encoding/yaml.ts
@@ -7,8 +7,6 @@
  * Ported from
  * [js-yaml v3.13.1](https://github.com/nodeca/js-yaml/commit/665aadda42349dcae869f12040d9b10ef18d12da)
  *
- * This module is browser compatible.
- *
  * @module
  */
 
