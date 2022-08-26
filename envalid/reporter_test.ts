62dac8da7797802241cee9e9a10333c8f45620c4
Roj <ez@roj.im>
2022-08-26 10:08:02 +0300

    chore: improve reporter tests

--- envalid/reporter_test.ts
+++ envalid/reporter_test.ts
@@ -9,27 +9,9 @@ import {
   envalidErrorFormatter as mainEnvalidErrorFormatter,
 } from "./mod.ts";
 import { defaultReporter, envalidErrorFormatter } from "./reporter.ts";
-import { EnvError, EnvMissingError } from "./errors.ts";
-
-class Tracker {
-  // deno-lint-ignore no-explicit-any
-  calls = new Array<any>();
-
-  // deno-lint-ignore no-explicit-any
-  func(...args: any[]) {
-    this.calls.push(args);
-  }
-
-  reset() {
-    this.calls = [];
-  }
-}
+import { EnvMissingError } from "./errors.ts";
 
 Deno.test("default reporter", async (t) => {
-  const exitTracker = new Tracker();
-
-  Object.assign(globalThis.Deno, { exit: exitTracker.func.bind(exitTracker) });
-
   await t.step(
     "default reporter should be exported from the top-level module",
     () => {
@@ -37,100 +19,115 @@ Deno.test("default reporter", async (t) => {
     },
   );
 
-  await t.step("simple usage for reporting a missing variable error", () => {
-    exitTracker.reset();
-
-    const tracker = new Tracker();
-    defaultReporter(
-      {
-        errors: { FOO: new EnvMissingError() },
-        env: {},
-      },
-      { logger: tracker.func.bind(tracker) },
-    );
-    assertEquals(tracker.calls.length, 2);
-
-    const output1 = tracker.calls[0]?.[0];
-    assertMatch(output1, /Missing\S+ environment variables:/);
-    assertMatch(output1, /FOO\S+/);
-    assertMatch(output1, /\(required\)/);
-    assertNotMatch(output1, /Invalid\S+ environment variables:/);
-
-    const output2 = tracker.calls[1]?.[0];
-    assertMatch(output2, /Exiting with error code 1/);
-
-    assertEquals(exitTracker.calls.length, 1);
-    assertEquals(exitTracker.calls[0]?.[0], 1);
-  });
-
-  await t.step("simple usage for reporting an invalid variable error", () => {
-    exitTracker.reset();
-
-    const tracker = new Tracker();
-    defaultReporter(
-      {
-        errors: { FOO: new EnvError() },
-        env: { FOO: 123 },
-      },
-      { logger: tracker.func.bind(tracker) },
-    );
-    assertEquals(tracker.calls.length, 2);
-
-    const output1 = tracker.calls[0]?.[0];
-    assertMatch(output1, /Invalid\S+ environment variables:/);
-    assertMatch(output1, /FOO\S+/);
-    assertMatch(output1, /\(invalid format\)/);
-    assertNotMatch(output1, /Missing\S+ environment variables:/);
-
-    const output2 = tracker.calls[1]?.[0];
-    assertMatch(output2, /Exiting with error code 1/);
-
-    assertEquals(exitTracker.calls.length, 1);
-    assertEquals(exitTracker.calls[0]?.[0], 1);
-  });
-
   await t.step(
-    "reporting an invalid variable error with a custom error message",
-    () => {
-      exitTracker.reset();
+    "simple usage for reporting a missing variable error",
+    async () => {
+      const { code, stderr } = await Deno.spawn(Deno.execPath(), {
+        args: [
+          "eval",
+          `
+          import { defaultReporter } from "./envalid/reporter.ts";
+          import { EnvMissingError } from "./envalid/errors.ts";
+          defaultReporter(
+            {
+              errors: { FOO: new EnvMissingError() },
+              env: {},
+            },
+          );
+        `,
+        ],
+      });
 
-      const tracker = new Tracker();
-      defaultReporter(
-        {
-          errors: { FOO: new EnvError("custom msg") },
-          env: { FOO: 123 },
-        },
-        { logger: tracker.func.bind(tracker) },
-      );
-      assertEquals(tracker.calls.length, 2);
+      const output = new TextDecoder().decode(stderr);
 
-      const output1 = tracker.calls[0]?.[0];
-      assertMatch(output1, /Invalid\S+ environment variables:/);
-      assertMatch(output1, /FOO\S+/);
-      assertMatch(output1, /custom msg/);
-
-      const output2 = tracker.calls[1]?.[0];
-      assertMatch(output2, /Exiting with error code 1/);
-
-      assertEquals(exitTracker.calls.length, 1);
-      assertEquals(exitTracker.calls[0]?.[0], 1);
+      assertMatch(output, /Missing\S+ environment variables:/);
+      assertMatch(output, /FOO\S+/);
+      assertMatch(output, /\(required\)/);
+      assertNotMatch(output, /Invalid\S+ environment variables:/);
+      assertMatch(output, /Exiting with error code 1/);
+      assertEquals(code, 1);
     },
   );
 
-  await t.step("does nothing when there are no errors", () => {
-    exitTracker.reset();
+  await t.step(
+    "simple usage for reporting an invalid variable error",
+    async () => {
+      const { code, stderr } = await Deno.spawn(Deno.execPath(), {
+        args: [
+          "eval",
+          `
+            import { defaultReporter } from "./envalid/reporter.ts";
+            import { EnvError } from "./envalid/errors.ts";
+            defaultReporter(
+              {
+                errors: { FOO: new EnvError() },
+                env: { FOO: 123 },
+              },
+            );
+          `,
+        ],
+      });
 
-    const tracker = new Tracker();
-    defaultReporter(
-      {
-        errors: {},
-        env: { FOO: "great success" },
-      },
-      { logger: tracker.func.bind(this) },
-    );
+      const output = new TextDecoder().decode(stderr);
 
-    assertEquals(tracker.calls.length, 0);
-    assertEquals(exitTracker.calls.length, 0);
+      assertMatch(output, /Invalid\S+ environment variables:/);
+      assertMatch(output, /FOO\S+/);
+      assertMatch(output, /\(invalid format\)/);
+      assertNotMatch(output, /Missing\S+ environment variables:/);
+      assertMatch(output, /Exiting with error code 1/);
+      assertEquals(code, 1);
+    },
+  );
+
+  await t.step(
+    "reporting an invalid variable error with a custom error message",
+    async () => {
+      const { code, stderr } = await Deno.spawn(Deno.execPath(), {
+        args: [
+          "eval",
+          `
+            import { defaultReporter } from "./envalid/reporter.ts";
+            import { EnvError } from "./envalid/errors.ts";
+            defaultReporter(
+              {
+                errors: { FOO: new EnvError("custom msg") },
+                env: { FOO: 123 },
+              },
+            );
+          `,
+        ],
+      });
+
+      const output = new TextDecoder().decode(stderr);
+
+      assertMatch(output, /Invalid\S+ environment variables:/);
+      assertMatch(output, /FOO\S+/);
+      assertMatch(output, /custom msg/);
+      assertMatch(output, /Exiting with error code 1/);
+      assertEquals(code, 1);
+    },
+  );
+
+  await t.step("does nothing when there are no errors", async () => {
+    const { code, stderr } = await Deno.spawn(Deno.execPath(), {
+      args: [
+        "eval",
+        `
+          import { defaultReporter } from "./envalid/reporter.ts";
+          defaultReporter(
+            {
+              errors: {},
+              env: { FOO: "great success" },
+            },
+          );
+        `,
+      ],
+    });
+
+    const output = new TextDecoder().decode(stderr);
+
+    assertEquals(output, "");
+    assertEquals(code, 0);
   });
 });
 
@@ -143,15 +140,15 @@ Deno.test("envalidErrorFormatter", async (t) => {
   );
 
   await t.step("simple usage for formatting a single error", () => {
-    const tracker = new Tracker();
-    assertEquals(tracker.calls.length, 0);
+    const messages = new Array<string>();
     envalidErrorFormatter(
       { FOO: new EnvMissingError() },
-      tracker.func.bind(tracker),
+      (msg:string)=>messages.push(msg)
     );
-    assertEquals(tracker.calls.length, 1);
 
-    const output = tracker.calls[0]?.[0];
+    const output = messages[0];
+
+    assertEquals(messages.length, 1);
     assertMatch(output, /Missing\S+ environment variables:/);
     assertMatch(output, /FOO\S+/);
     assertMatch(output, /\(required\)/);
