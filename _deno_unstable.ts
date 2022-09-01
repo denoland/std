0c1a09dcabcbb482c40fc0e816852d196af67fcc
Asher Gomez <ashersaupingomez@gmail.com>
2022-09-01 13:28:02 +1000

    remove: custom fns

--- _deno_unstable.ts
+++ _deno_unstable.ts
@@ -7,193 +7,27 @@ export type UnixListenOptions = Deno.UnixListenOptions;
 export type DatagramConn = Deno.DatagramConn;
 export type ServeHandler = Deno.ServeHandler;
 
-export function serve(
-  ...args: Parameters<typeof Deno.serve>
-): ReturnType<typeof Deno.serve> {
-  if (typeof Deno.serve == "function") {
-    return Deno.serve(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function upgradeHttpRaw(
-  ...args: Parameters<typeof Deno.upgradeHttpRaw>
-): ReturnType<typeof Deno.upgradeHttpRaw> {
-  if (typeof Deno.upgradeHttpRaw == "function") {
-    return Deno.upgradeHttpRaw(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function addSignalListener(
-  ...args: Parameters<typeof Deno.addSignalListener>
-): ReturnType<typeof Deno.addSignalListener> {
-  if (typeof Deno.addSignalListener == "function") {
-    return Deno.addSignalListener(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function createHttpClient(
-  ...args: Parameters<typeof Deno.createHttpClient>
-): ReturnType<typeof Deno.createHttpClient> {
-  if (typeof Deno.createHttpClient == "function") {
-    return Deno.createHttpClient(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function consoleSize(
-  ...args: Parameters<typeof Deno.consoleSize>
-): ReturnType<typeof Deno.consoleSize> {
-  if (typeof Deno.consoleSize == "function") {
-    return Deno.consoleSize(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function futime(
-  ...args: Parameters<typeof Deno.futime>
-): ReturnType<typeof Deno.futime> {
-  if (typeof Deno.futime == "function") {
-    return Deno.futime(...args);
-  } else {
-    return Promise.reject(new TypeError("Requires --unstable"));
-  }
-}
-
-export function futimeSync(
-  ...args: Parameters<typeof Deno.futimeSync>
-): ReturnType<typeof Deno.futimeSync> {
-  if (typeof Deno.futimeSync == "function") {
-    return Deno.futimeSync(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function getUid(
-  ...args: Parameters<typeof Deno.getUid>
-): ReturnType<typeof Deno.getUid> {
-  if (typeof Deno.getUid == "function") {
-    return Deno.getUid(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function hostname(
-  ...args: Parameters<typeof Deno.hostname>
-): ReturnType<typeof Deno.hostname> {
-  if (typeof Deno.hostname == "function") {
-    return Deno.hostname(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function loadavg(
-  ...args: Parameters<typeof Deno.loadavg>
-): ReturnType<typeof Deno.loadavg> {
-  if (typeof Deno.loadavg == "function") {
-    return Deno.loadavg(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function osRelease(
-  ...args: Parameters<typeof Deno.osRelease>
-): ReturnType<typeof Deno.osRelease> {
-  if (typeof Deno.osRelease == "function") {
-    return Deno.osRelease(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function removeSignalListener(
-  ...args: Parameters<typeof Deno.removeSignalListener>
-): ReturnType<typeof Deno.removeSignalListener> {
-  if (typeof Deno.removeSignalListener == "function") {
-    return Deno.removeSignalListener(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function setRaw(
-  ...args: Parameters<typeof Deno.setRaw>
-): ReturnType<typeof Deno.setRaw> {
-  if (typeof Deno.setRaw == "function") {
-    return Deno.setRaw(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function systemMemoryInfo(
-  ...args: Parameters<typeof Deno.systemMemoryInfo>
-): ReturnType<typeof Deno.systemMemoryInfo> {
-  if (typeof Deno.systemMemoryInfo == "function") {
-    return Deno.systemMemoryInfo(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function utime(
-  ...args: Parameters<typeof Deno.utime>
-): ReturnType<typeof Deno.utime> {
-  if (typeof Deno.utime == "function") {
-    return Deno.utime(...args);
-  } else {
-    return Promise.reject(new TypeError("Requires --unstable"));
-  }
-}
-
-export function utimeSync(
-  ...args: Parameters<typeof Deno.utimeSync>
-): ReturnType<typeof Deno.utimeSync> {
-  if (typeof Deno.utimeSync == "function") {
-    return Deno.utimeSync(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function networkInterfaces(
-  ...args: Parameters<typeof Deno.networkInterfaces>
-): ReturnType<typeof Deno.networkInterfaces> {
-  if (typeof Deno.networkInterfaces == "function") {
-    return Deno.networkInterfaces(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export async function connect(
-  options: UnixConnectOptions,
-): Promise<Deno.UnixConn> {
-  return await Deno.connect(options);
-}
-
-export function listen(
-  options: UnixListenOptions & { transport: "unix" },
-): ReturnType<typeof Deno.listen> {
-  return Deno.listen(options);
-}
-
-export function listenDatagram(
-  options: Deno.ListenOptions & { transport: "udp" },
-): ReturnType<typeof Deno.listenDatagram> {
-  return Deno.listenDatagram(options);
-}
+export const serve = Deno.serve;
+export const upgradeHttpRaw = Deno.upgradeHttpRaw;
+export const addSignalListener = Deno.addSignalListener;
+export const createHttpClient = Deno.createHttpClient;
+export const consoleSize = Deno.consoleSize;
+export const futime = Deno.futime;
+export const futimeSync = Deno.futimeSync;
+export const getUid = Deno.getUid;
+export const hostname = Deno.hostname;
+export const loadavg = Deno.loadavg;
+export const osRelease = Deno.osRelease;
+export const removeSignalListener = Deno.removeSignalListener;
+export const setRaw = Deno.setRaw;
+export const systemMemoryInfo = Deno.systemMemoryInfo;
+export const utime = Deno.utime;
+export const utimeSync = Deno.utimeSync;
+export const networkInterfaces = Deno.networkInterfaces;
+export const unrefTimer = Deno.unrefTimer;
+export const connect = Deno.connect;
+export const listen = Deno.listen;
+export const listenDatagram = Deno.listenDatagram;
 
 export function ListenerRef(
   listener: Deno.Listener,
@@ -212,16 +46,6 @@ export function ListenerUnref(
 ): ReturnType<Deno.Listener["unref"]> {
   if (typeof listener.unref == "function") {
     return listener.unref(...args);
-  } else {
-    throw new TypeError("Requires --unstable");
-  }
-}
-
-export function unrefTimer(
-  ...args: Parameters<typeof Deno.unrefTimer>
-): ReturnType<typeof Deno.unrefTimer> {
-  if (typeof Deno.unrefTimer == "function") {
-    return Deno.unrefTimer(...args);
   } else {
     throw new TypeError("Requires --unstable");
   }
