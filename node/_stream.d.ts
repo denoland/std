a36b32787510a0de371bc1d38c3c79c09e9e3b86
wangb <wangbinyq@gmail.com>
2022-09-07 19:17:40 +0800

    docs(node): add space between words (#2624)

--- node/_stream.d.ts
+++ node/_stream.d.ts
@@ -1023,7 +1023,7 @@ export class Transform extends Duplex {
 }
 /**
  * The `stream.PassThrough` class is a trivial implementation of a `Transform` stream that simply passes the input bytes across to the output. Its purpose is
- * primarily for examples and testing, but there are some use cases where`stream.PassThrough` is useful as a building block for novel sorts of streams.
+ * primarily for examples and testing, but there are some use cases where `stream.PassThrough` is useful as a building block for novel sorts of streams.
  */
 export class PassThrough extends Transform {}
 /**
