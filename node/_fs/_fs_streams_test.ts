67266461986bf0938bb03c18a5a777e1ab010b49
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2022-09-09 14:34:39 +0900

    remove duplicate test cases

--- node/_fs/_fs_streams_test.ts
+++ node/_fs/_fs_streams_test.ts
@@ -7,44 +7,11 @@ const moduleDir = path.dirname(path.fromFileUrl(import.meta.url));
 const testData = path.resolve(moduleDir, "testdata", "hello.txt");
 
 Deno.test({
-  name: "[node/fs.ReadStream] Read a chunk of data using 'ReadStream()'",
-  fn() {
-    const readable = ReadStream(testData);
-
-    let data: Uint8Array;
-    readable.on("data", (chunk: Uint8Array) => {
-      data = chunk;
-    });
-
-    readable.on("close", () => {
-      assertEquals(new TextDecoder().decode(data as Uint8Array), "hello world");
-    });
-  },
-});
-
-Deno.test({
   name: "[node/fs.ReadStream] Read a chunk of data using 'new ReadStream()'",
   fn() {
     // deno-lint-ignore ban-ts-comment
     // @ts-ignore
     const readable = new ReadStream(testData);
-
-    let data: Uint8Array;
-    readable.on("data", (chunk: Uint8Array) => {
-      data = chunk;
-    });
-
-    readable.on("close", () => {
-      assertEquals(new TextDecoder().decode(data as Uint8Array), "hello world");
-    });
-  },
-});
-
-Deno.test({
-  name:
-    "[node/fs.createReadStream] Read a chunk of data using 'creatReadStream()'",
-  fn() {
-    const readable = createReadStream(testData);
 
     let data: Uint8Array;
     readable.on("data", (chunk: Uint8Array) => {
@@ -77,72 +44,6 @@ Deno.test({
 });
 
 Deno.test({
-  name: "[node/fs.createReadStream] Read a chunk of data with options",
-  fn() {
-    const readable = createReadStream(testData, { encoding: "utf8" });
-
-    let data: string;
-    readable.on("data", (chunk: string) => {
-      data = chunk;
-    });
-
-    readable.on("close", () => {
-      assertEquals(data, "hello world");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createReadStream] Call parent function",
-  fn() {
-    const readable = createReadStream(testData);
-    readable.setEncoding("utf8");
-
-    let data: string;
-    readable.on("data", (chunk: string) => {
-      data = chunk;
-    });
-
-    readable.on("close", () => {
-      assertEquals(data, "hello world");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createReadStream] Access parent property",
-  fn() {
-    const readable = createReadStream(testData);
-
-    readable.on("data", () => null);
-
-    readable.on("close", () => {
-      assertEquals(readable.readableEnded, true);
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createReadStream] Read data",
-  fn() {
-    const readable = createReadStream(testData);
-
-    const data: (Uint8Array | null)[] = [];
-    readable.on("readable", function () {
-      data.push(readable.read());
-    });
-
-    readable.on("close", () => {
-      assertEquals(
-        new TextDecoder().decode(data[0] as Uint8Array),
-        "hello world",
-      );
-      assertEquals(data[1], null);
-    });
-  },
-});
-
-Deno.test({
   name: "[node/fs.createReadStream] Read given amount of data",
   fn() {
     const readable = createReadStream(testData);
@@ -157,24 +58,6 @@ Deno.test({
       assertEquals(new TextDecoder().decode(data[0] as Uint8Array), "hello");
       assertEquals(new TextDecoder().decode(data[1] as Uint8Array), " world");
       assertEquals(data[2], null);
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createReadStream] Destroy the stream",
-  fn() {
-    const readable = createReadStream(testData);
-
-    const data: (Uint8Array | null)[] = [];
-    readable.on("readable", function () {
-      data.push(readable.read(5));
-      readable.destroy();
-    });
-
-    readable.on("close", () => {
-      assertEquals(new TextDecoder().decode(data[0] as Uint8Array), "hello");
-      assertEquals(data.length, 1);
     });
   },
 });
--- node/internal/fs/streams_test.ts
+++ node/internal/fs/streams_test.ts
@@ -3,24 +3,6 @@ import { createWriteStream, WriteStream } from "./streams.ts";
 import { assertEquals } from "../../../testing/asserts.ts";
 
 Deno.test({
-  name: "[node/fs.WriteStream] Write data using 'WriteStream()'",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = WriteStream(tempFile);
-
-    writable.write("hello world");
-    writable.end("\n");
-
-    writable.on("close", () => {
-      const data = Deno.readFileSync(tempFile);
-      Deno.removeSync(tempFile);
-      assertEquals(new TextDecoder("utf-8").decode(data), "hello world\n");
-    });
-  },
-});
-
-Deno.test({
   name: "[node/fs.WriteStream] Write data using 'new WriteStream()'",
   sanitizeOps: false,
   fn() {
@@ -28,24 +10,6 @@ Deno.test({
     // deno-lint-ignore ban-ts-comment
     // @ts-ignore
     const writable = new WriteStream(tempFile);
-
-    writable.write("hello world");
-    writable.end("\n");
-
-    writable.on("close", () => {
-      const data = Deno.readFileSync(tempFile);
-      Deno.removeSync(tempFile);
-      assertEquals(new TextDecoder("utf-8").decode(data), "hello world\n");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createWriteStream] Write data using 'createWriteStream()'",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = createWriteStream(tempFile);
 
     writable.write("hello world");
     writable.end("\n");
@@ -75,77 +39,6 @@ Deno.test({
       const data = Deno.readFileSync(tempFile);
       Deno.removeSync(tempFile);
       assertEquals(new TextDecoder("utf-8").decode(data), "hello world\n");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createWriteStream] Write data with options",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = createWriteStream(tempFile, { encoding: "utf16le" });
-
-    writable.write("hello world");
-    writable.end("\n");
-
-    writable.on("close", () => {
-      const data = Deno.readFileSync(tempFile);
-      Deno.removeSync(tempFile);
-      assertEquals(new TextDecoder("utf-16le").decode(data), "hello world\n");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createWriteStream] Call parent function",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = createWriteStream(tempFile);
-    writable.setDefaultEncoding("utf16le");
-
-    writable.write("hello world");
-    writable.end("\n");
-
-    writable.on("close", () => {
-      const data = Deno.readFileSync(tempFile);
-      Deno.removeSync(tempFile);
-      assertEquals(new TextDecoder("utf-16le").decode(data), "hello world\n");
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createWriteStream] Access parent property",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = createWriteStream(tempFile);
-
-    writable.end("");
-
-    writable.on("close", () => {
-      Deno.removeSync(tempFile);
-      assertEquals(writable.writableEnded, true);
-    });
-  },
-});
-
-Deno.test({
-  name: "[node/fs.createWriteStream] Destroy the stream",
-  sanitizeOps: false,
-  fn() {
-    const tempFile: string = Deno.makeTempFileSync();
-    const writable = createWriteStream(tempFile);
-
-    writable.write("hello world");
-    writable.destroy();
-
-    writable.on("close", () => {
-      const data = Deno.readFileSync(tempFile);
-      Deno.removeSync(tempFile);
-      assertEquals(new TextDecoder("utf-8").decode(data), "");
     });
   },
 });
