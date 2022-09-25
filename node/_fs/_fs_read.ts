833593f98beaba7593782b426311d34420cbfa58
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-25 17:55:45 +0900

    fix(node/fs): Intentionally create TypeError

--- node/_fs/_fs_read.ts
+++ node/_fs/_fs_read.ts
@@ -82,8 +82,19 @@ export function read(
     position = null;
   } else {
     const opt = optOrBufferOrCb as readOptions;
+    if (!(opt.buffer instanceof Buffer) && !(opt.buffer instanceof Uint8Array)) {
+      if (opt.buffer === null) {
+        // @ts-ignore: Intentionally create TypeError for passing test-fs-read.js#L87
+        length = opt.buffer.byteLength;
+      }
+      throw new ERR_INVALID_ARG_TYPE("buffer", [
+        "Buffer",
+        "TypedArray",
+        "DataView",
+      ], optOrBufferOrCb);
+    } 
     offset = opt.offset ?? 0;
-    buffer = opt.buffer;
+    buffer = opt.buffer ?? Buffer.alloc(16384);
     length = opt.length ?? buffer.byteLength;
     position = opt.position ?? null;
   }
