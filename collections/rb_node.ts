d933c679cb23d8244d143635c3a2ab8af3b18fb2
Asher Gomez <ashersaupingomez@gmail.com>
2022-09-29 22:16:58 +1000

    initial commit

--- collections/rb_node.ts
+++ collections/rb_node.ts
@@ -2,12 +2,5 @@
 /** This module is browser compatible. */
 
 import { Direction } from "./bs_node.ts";
-import { RedBlackNode } from "./red_black_node.ts";
-
-/** @deprecated (will be removed after 0.157.0) use Direction instead */
-export type { Direction as direction };
 
 export type { Direction };
-
-/** @deprecated (will be removed after 0.157.0) use RedBlackNode instead */
-export { RedBlackNode as RBNode };
