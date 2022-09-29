8207c25d7a65abb3cd54cc54ec228af2325049f0
Asher Gomez <ashersaupingomez@gmail.com>
2022-09-29 22:26:50 +1000

    fixes and tweaks

--- collections/bs_node.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-/** This module is browser compatible. */
-
-import { Direction } from "./binary_search_node.ts";
-
-export type { Direction };
--- collections/bs_tree.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-/** This module is browser compatible. */
-
-export * from "./_comparators.ts";
