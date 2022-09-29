931c18635c6a535ec65bf3f2de3dea505184197c
Asher Gomez <ashersaupingomez@gmail.com>
2022-09-29 22:54:49 +1000

    fixes

--- http/testdata/simple_server.ts
+++ http/testdata/simple_server.ts
@@ -1,9 +1,5 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 // This is an example of a server that responds with an empty body
-import { listenAndServe } from "../server.ts";
+import { serve } from "../server.ts";
 
-const addr = "0.0.0.0:4504";
-
-console.log(`Simple server listening on http://localhost:4504`);
-
-await listenAndServe(addr, () => new Response());
+await serve(() => new Response(), { port: 4504 });
