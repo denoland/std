8e34d809032e10f6356a187525e9368bdd167242
wafuwafu13 <mariobaske@i.softbank.jp>
2022-10-18 22:32:39 +0900

    fix(node/fs): use getOptions

--- node/_fs/_fs_opendir.ts
+++ node/_fs/_fs_opendir.ts
@@ -2,9 +2,16 @@
 
 import Dir from "./_fs_dir.ts";
 import { Buffer } from "../buffer.ts";
-import { assertEncoding, getValidatedPath } from "../internal/fs/utils.mjs";
+import {
+  assertEncoding,
+  getOptions,
+  getValidatedPath,
+} from "../internal/fs/utils.mjs";
 import { denoErrorToNodeError } from "../internal/errors.ts";
-import { validateFunction, validateUint32 } from "../internal/validators.mjs";
+import {
+  validateFunction,
+  validateInteger,
+} from "../internal/validators.mjs";
 import { promisify } from "../internal/util.mjs";
 
 /** These options aren't funcitonally used right now, as `Dir` doesn't yet support them.
@@ -20,18 +27,6 @@ function _validateFunction(callback: unknown): asserts callback is Callback {
   validateFunction(callback, "callback");
 }
 
-/**
- * Required as `assertIntegerRange` from "../_utils.ts" throws `Error` instead of `RangeError`.
- * Node throws `RangeError` when this assertion fails.
- */
-function checkBufferSize(value: number, name: string) {
-  if (!Number.isInteger(value) || value < 1 || value > 4294967295) {
-    throw new RangeError(
-      `The value of "${name}" is out of range. It must be >= 1 && <= 4294967295. Received ${value}`,
-    );
-  }
-}
-
 /** @link https://nodejs.org/api/fs.html#fsopendirsyncpath-options */
 export function opendir(
   path: string | Buffer | URL,
@@ -43,15 +38,16 @@ export function opendir(
 
   path = getValidatedPath(path).toString();
 
-  options = typeof options === "object" ? options : {};
-  const encoding = options?.encoding ?? "utf8";
-  const bufferSize = options?.bufferSize ?? 32;
+  const { bufferSize, encoding } = getOptions(options, {
+    encoding: "utf8",
+    bufferSize: 32,
+  });
+
+  validateInteger(bufferSize, "options.bufferSize", 1, 4294967295);
 
   let err, dir;
   try {
     assertEncoding(encoding);
-
-    checkBufferSize(bufferSize, "options.bufferSize");
 
     /** Throws if path is invalid */
     Deno.readDirSync(path);
@@ -79,21 +75,15 @@ export function opendirSync(
 ): Dir {
   path = getValidatedPath(path).toString();
 
-  let encoding: string;
-  let bufferSize: number;
-  if (options) {
-    ({ encoding = "utf8", bufferSize = 32 } = options);
-  } else {
-    encoding = "utf8";
-    bufferSize = 32;
-  }
+  const { bufferSize, encoding } = getOptions(options, {
+    encoding: "utf8",
+    bufferSize: 32,
+  });
 
-  validateUint32(bufferSize, "bufferSize", true);
+  validateInteger(bufferSize, "options.bufferSize", 1, 4294967295);
 
   try {
     assertEncoding(encoding);
-
-    checkBufferSize(bufferSize, "options.bufferSize");
 
     /** Throws if path is invalid */
     Deno.readDirSync(path);
