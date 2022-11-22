bbdfac4bc18982e930555c9c1e3586e2e299d473
Nicolas Guerrero <stephenguerrero43@gmail.com>
2022-11-21 20:31:37 -0500

    fix: Typechecking

--- node/dgram.ts
+++ node/dgram.ts
@@ -455,12 +455,12 @@ export class Socket extends EventEmitter {
         return;
       }
 
-      let flags = 0;
+      let flags: number | undefined = 0;
 
       if (state.reuseAddr) {
         flags |= UV_UDP_REUSEADDR;
       }
-      if (state.ipv6Only) {
+      if (state.ipv6Only && UV_UDP_IPV6ONLY) {
         flags |= UV_UDP_IPV6ONLY;
       }
 
