3afe98b70323a54356973e5a21e57152e4362460
Asher Gomez <ashersaupingomez@gmail.com>
2022-11-24 18:00:22 +1100

    refactor: cleanup check doc imports

--- _tools/check_doc_imports.ts
+++ _tools/check_doc_imports.ts
@@ -85,38 +85,19 @@ function checkCodeBlocks(
 ): void {
   for (const codeBlockMatch of content.matchAll(RX_CODE_BLOCK)) {
     const [, language, codeBlock] = codeBlockMatch;
-    const shortFilePath = filePath.substring(ROOT_LENGTH + 1);
     const codeBlockLineNumber =
       content.slice(0, codeBlockMatch.index).split("\n").length;
 
-    switch (language.toLowerCase()) {
-      case "ts":
-      case "js":
-      case "typescript":
-      case "javascript":
-      case "":
-        if (language === "") {
-          console.log(
-            yellow("Warn") + " codeblock with no language specified at " +
-              blue(shortFilePath) + yellow(":" + codeBlockLineNumber),
-          );
-        }
-
-        checkImportStatements(
-          codeBlock,
-          filePath,
-          lineNumber + codeBlockLineNumber,
-        );
-        break;
-
-      default:
-        console.log(
-          yellow("Warn") + " skipping code block with language " +
-            blue(language) + " at " +
-            blue(shortFilePath) +
-            yellow(":" + codeBlockLineNumber),
-        );
-        break;
+    if (
+      ["ts", "js", "typescript", "javascript"].includes(
+        language.toLocaleLowerCase(),
+      )
+    ) {
+      checkImportStatements(
+        codeBlock,
+        filePath,
+        lineNumber + codeBlockLineNumber,
+      );
     }
   }
 }
