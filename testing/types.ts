079c826f6c09856f5828de604987d2de8bac5d76
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-11-27 14:07:26 -0800

    ignore purposely failing example

--- testing/types.ts
+++ testing/types.ts
@@ -5,7 +5,7 @@
  * Asserts at compile time that the provided type argument's type resolves to the expected boolean literal type.
  * @param expectTrue - True if the passed in type argument resolved to true.
  * @example
- * ```typescript
+ * ```typescript, ignore
  * import { assertType, IsExact, IsNullable } from "https://deno.land/std@$STD_VERSION/testing/types.ts";
  *
  * const result = "some result" as string | number;
