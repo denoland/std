524eaac532da10e4e671adfbbdf29404a4cef7dd
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-11-27 13:40:59 -0800

    fix merge conflict

--- async/retry.ts
+++ async/retry.ts
@@ -28,6 +28,24 @@ const defaultRetryOptions = {
 
 /**
  * Creates a retry promise which resolves to the value of the input using exponential backoff.
+ * If the input promise throws, it will be retried `maxAttempts` number of times.
+ * It will retry the input every certain amount of milliseconds, starting at `minTimeout` and multiplying by the `multiplier` until it reaches the `maxTimeout`
+ *
+ * @example
+ * ```typescript
+ * import { retry } from "https://deno.land/std@$STD_VERSION/async/mod.ts";
+ * const req = async () => {
+ *  // some function that throws sometimes
+ * };
+ *
+ * // Below resolves to the first non-error result of `req`
+ * const retryPromise = await retry(req, {
+ *  multiplier: 2,
+ *  maxTimeout: 60000,
+ *  maxAttempts: 5,
+ *  minTimeout: 100,
+ * });
+```
  */
 export async function retry<T>(
   fn: (() => Promise<T>) | (() => T),
