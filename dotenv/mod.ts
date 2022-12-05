05d7dda8b26be24af1b79fb0faa626379e628e7f
Asher Gomez <ashersaupingomez@gmail.com>
2022-12-05 18:05:16 +1100

    refactor(dotenv): remove duplicated functionality (#2978)

--- dotenv/mod.ts
+++ dotenv/mod.ts
@@ -90,7 +90,8 @@
  * @module
  */
 
-import { difference, removeEmptyValues } from "./util.ts";
+import { filterValues } from "../collections/filter_values.ts";
+import { withoutAll } from "../collections/without_all.ts";
 
 /**
  * @deprecated (will be removed after 0.170.0). Use `Record<string, string>` instead
@@ -458,11 +459,11 @@ function assertSafe(
   // Not all the variables have to be defined in .env, they can be supplied externally
   const confWithEnv = Object.assign({}, currentEnv, conf);
 
-  const missing = difference(
+  const missing = withoutAll(
     Object.keys(confExample),
     // If allowEmptyValues is false, filter out empty values from configuration
     Object.keys(
-      allowEmptyValues ? confWithEnv : removeEmptyValues(confWithEnv),
+      allowEmptyValues ? confWithEnv : filterValues(confWithEnv, Boolean),
     ),
   );
 
