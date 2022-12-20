74d4170c945c4333a14814b9a3f9842b766f12aa
timreichen <tim.reichen@gmail.com>
2022-12-20 09:46:53 +0100

    add re-exports file

---
+++ encoding/yaml.ts
@@ -0,0 +1,4 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+/** @deprecated (will be removed after 0.170.0) use the extended API under std/encoding/yaml/ instead */
+export * from "./yaml/mod.ts";
