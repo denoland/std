aba3ce06f67d108d5e7606c98712bf3864e8da98
timreichen <tim.reichen@gmail.com>
2022-12-20 09:57:40 +0100

    add re-exports file

---
+++ encoding/toml.ts
@@ -0,0 +1,4 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+/** @deprecated (will be removed after 0.170.0) use the extended API under std/encoding/toml/ instead */
+export * from "./toml/mod.ts";
