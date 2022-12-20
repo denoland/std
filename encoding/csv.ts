58d4ff0b0b299aa86f22b97f9d5ca8115f935ed8
timreichen <tim.reichen@gmail.com>
2022-12-20 09:48:38 +0100

    fmt headers

--- encoding/csv.ts
+++ encoding/csv.ts
@@ -1,2 +1,4 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
 /** @deprecated (will be removed after 0.170.0) use the extended API under std/encoding/csv/ instead */
 export * from "./csv/mod.ts";
