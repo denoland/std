9175db5fd3ea56da818ef51be4003e7baef8afd7
Benjamin Fischer <c4spar@gmx.de>
2022-12-22 15:40:13 +0100

    add more tests

--- flags/test.ts
+++ flags/test.ts
@@ -1590,6 +1590,41 @@ Deno.test("typesOfCollectArgsWithSingleArgs", function () {
   >(true);
 });
 
+Deno.test("typesOfCollectArgsWithEmptyTypeArray", function () {
+  const argv = parse([], {
+    boolean: [],
+    collect: ["foo"],
+  });
+  assertType<
+    IsExact<
+      typeof argv,
+      & { [x: string]: unknown }
+      & {
+        foo: Array<unknown>;
+        _: Array<string | number>;
+      }
+    >
+  >(true);
+});
+
+Deno.test("typesOfCollectArgsWithUnknownArgs", function () {
+  const argv = parse([], {
+    boolean: ["bar"],
+    collect: ["foo"],
+  });
+  assertType<
+    IsExact<
+      typeof argv,
+      & { [x: string]: unknown }
+      & {
+        bar: boolean;
+        foo: Array<unknown>;
+        _: Array<string | number>;
+      }
+    >
+  >(true);
+});
+
 Deno.test("typesOfCollectArgsWithKnownAndUnknownArgs", function () {
   const argv = parse([], {
     boolean: ["foo"],
