e21e29120c74ec176813b69cb6d91e5243dc22c1
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-02-23 14:51:40 +0000

    fix(fs/ensure_symlink): lstat relative symlink properly

--- fs/ensure_symlink.ts
+++ fs/ensure_symlink.ts
@@ -4,6 +4,16 @@ import { ensureDir, ensureDirSync } from "./ensure_dir.ts";
 import { getFileInfoType, toPathString } from "./_util.ts";
 import { isWindows } from "../_util/os.ts";
 
+export function resolveSymlinkSrc(src: string, dest: string): string;
+export function resolveSymlinkSrc(src: URL, dest: URL): URL;
+export function resolveSymlinkSrc(src: string | URL, dest: string | URL) {
+  if (typeof src == "string" && typeof dest == "string") {
+    return path.resolve(path.dirname(dest), src);
+  } else {
+    return new URL(src, dest)
+  }
+}
+
 /**
  * Ensures that the link exists.
  * If the directory structure does not exist, it is created.
@@ -11,8 +21,11 @@ import { isWindows } from "../_util/os.ts";
  * @param src the source file path
  * @param dest the destination link path
  */
-export async function ensureSymlink(src: string | URL, dest: string | URL) {
-  const srcStatInfo = await Deno.lstat(src);
+export function ensureSymlink(src: string, dest: string);
+export function ensureSymlink(src: URL, dest: URL);
+export function ensureSymlink(src: any, dest: any) {
+  const srcReal = resolveSymlinkSrc(src, dest);
+  const srcStatInfo = await Deno.lstat(srcReal);
   const srcFilePathType = getFileInfoType(srcStatInfo);
 
   await ensureDir(path.dirname(toPathString(dest)));
@@ -39,8 +52,11 @@ export async function ensureSymlink(src: string | URL, dest: string | URL) {
  * @param src the source file path
  * @param dest the destination link path
  */
-export function ensureSymlinkSync(src: string | URL, dest: string | URL) {
-  const srcStatInfo = Deno.lstatSync(src);
+export function ensureSymlinkSync(src: string, dest: string);
+export function ensureSymlinkSync(src: URL, dest: URL);
+export function ensureSymlinkSync(src: any, dest: any) {
+  const srcReal = resolveSymlinkSrc(src, dest);
+  const srcStatInfo = Deno.lstatSync(srcReal);
   const srcFilePathType = getFileInfoType(srcStatInfo);
 
   ensureDirSync(path.dirname(toPathString(dest)));
