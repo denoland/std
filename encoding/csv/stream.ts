b0bf7020651c0c35604a7ed8545948566e572079
Asher Gomez <ashersaupingomez@gmail.com>
2023-03-11 14:13:14 +1100

    update deprecation version

--- encoding/csv/stream.ts
+++ encoding/csv/stream.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 export {
-  /** @deprecated (will be removed after 0.181.0) Import from `csv/stream.ts` instead. */
+  /** @deprecated (will be removed after 0.182.0) Import from `csv/stream.ts` instead. */
   CsvStream,
-  /** @deprecated (will be removed after 0.181.0) Import from `csv/stream.ts` instead. */
+  /** @deprecated (will be removed after 0.182.0) Import from `csv/stream.ts` instead. */
   type CsvStreamOptions,
 } from "../../csv/stream.ts";
