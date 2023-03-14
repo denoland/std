85ef58062281c0b78a2601841422eeab08258661
Asher Gomez <ashersaupingomez@gmail.com>
2023-03-14 18:40:02 +1100

    BREAKING(yaml): move to top-level folder (#3251)

--- encoding/yaml.ts
+++ encoding/yaml.ts
@@ -2,6 +2,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 /**
+ * @deprecated (will be removed after 0.182.0) Import from `std/yaml` instead.
+ *
  * {@linkcode parse} and {@linkcode stringify} for handling
  * [YAML](https://yaml.org/) encoded data.
  *
@@ -23,7 +25,7 @@
  * import {
  *   parse,
  *   stringify,
- * } from "https://deno.land/std@$STD_VERSION/encoding/yaml.ts";
+ * } from "https://deno.land/std@$STD_VERSION/yaml/mod.ts";
  *
  * const data = parse(`
  * foo: bar
@@ -46,17 +48,47 @@
  * @module
  */
 
-export type { ParseOptions } from "./_yaml/parse.ts";
-export { parse, parseAll } from "./_yaml/parse.ts";
-export type { DumpOptions as StringifyOptions } from "./_yaml/stringify.ts";
-export { stringify } from "./_yaml/stringify.ts";
-export type { SchemaDefinition } from "./_yaml/schema.ts";
-export { Type } from "./_yaml/type.ts";
-export type { KindType, RepresentFn, StyleVariant } from "./_yaml/type.ts";
 export {
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/parse.ts` instead. */
+  parse,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/parse.ts` instead. */
+  parseAll,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/parse.ts` instead. */
+  type ParseOptions,
+} from "../yaml/parse.ts";
+export {
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/stringify.ts` instead. */
+  type DumpOptions as StringifyOptions,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/stringify.ts` instead. */
+  stringify,
+} from "../yaml/stringify.ts";
+export type {
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema.ts` instead. */
+  SchemaDefinition,
+} from "../yaml/schema.ts";
+export type {
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/type.ts` instead. */
+  KindType,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/type.ts` instead. */
+  RepresentFn,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/type.ts` instead. */
+  StyleVariant,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/type.ts` instead. */
+  Type,
+} from "../yaml/type.ts";
+export {
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema/mod.ts` instead. */
   CORE_SCHEMA,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema/mod.ts` instead. */
+
   DEFAULT_SCHEMA,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema/mod.ts` instead. */
+
   EXTENDED_SCHEMA,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema/mod.ts` instead. */
+
   FAILSAFE_SCHEMA,
+  /** @deprecated (will be removed after 0.182.0) Import from `std/yaml/schema/mod.ts` instead. */
+
   JSON_SCHEMA,
-} from "./_yaml/schema/mod.ts";
+} from "../yaml/schema/mod.ts";
