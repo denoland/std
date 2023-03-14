cc89e596597ddf0df6abbc3ac26c05c50aa445a8
justinmchase <justin.m.chase@gmail.com>
2023-03-14 12:02:59 -0500

    equality tests

--- semver/equality_test.ts
+++ semver/equality_test.ts
@@ -1,41 +1,32 @@
 // Copyright Isaac Z. Schlueter and Contributors. All rights reserved. ISC license.
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assert } from "../testing/asserts.ts";
+import { assertEquals } from "../testing/asserts.ts";
 import * as semver from "./mod.ts";
 
-Deno.test("equality", function () {
+Deno.test("equality", async (t) => {
   // [version1, version2]
   // version1 should be equivalent to version2
-  const versions: [string, string][] = [
-    ["1.2.3-beta+build", "1.2.3-beta+otherbuild"],
-    ["1.2.3+build", "1.2.3+otherbuild"],
-    ["  v1.2.3+build", "1.2.3+otherbuild"],
+  const versions: [string, string, boolean][] = [
+    ["1.2.3", "1.2.3", true],
+    ["1.2.3-pr.1", "1.2.3-pr.1", true],
+    ["1.2.3+abc123", "1.2.3+abc123", true],
+    ["1.2.3-alpha", "1.2.3-beta", false],
+    ["1.2.3-beta+build", "1.2.3-beta+otherbuild", false],
+    ["1.2.3+build", "1.2.3+otherbuild", false],
   ];
 
-  versions.forEach(function (v) {
-    const v0 = v[0];
-    const v1 = v[1];
-
-    assert(semver.eq(v0, v1), "eq('" + v0 + "', '" + v1 + "')");
-    assert(!semver.neq(v0, v1), "!neq('" + v0 + "', '" + v1 + "')");
-    assert(semver.cmp(v0, "==", v1), "cmp(" + v0 + "==" + v1 + ")");
-    assert(!semver.cmp(v0, "!=", v1), "!cmp(" + v0 + "!=" + v1 + ")");
-    assert(!semver.cmp(v0, "===", v1), "!cmp(" + v0 + "===" + v1 + ")");
-
-    assert(semver.cmp(v0, "!==", v1), "cmp(" + v0 + "!==" + v1 + ")");
-
-    assert(
-      !semver.cmp(
-        new semver.SemVer(v0),
-        "!==",
-        new semver.SemVer(v1),
-      ),
-      "cmp(" + v0 + "!==" + v1 + ") object",
-    );
-
-    assert(!semver.gt(v0, v1), "!gt('" + v0 + "', '" + v1 + "')");
-    assert(semver.gte(v0, v1), "gte('" + v0 + "', '" + v1 + "')");
-    assert(!semver.lt(v0, v1), "!lt('" + v0 + "', '" + v1 + "')");
-    assert(semver.lte(v0, v1), "lte('" + v0 + "', '" + v1 + "')");
-  });
+  for (const [v0, v1, expected] of versions) {
+    const s0 = semver.parse(v0);
+    const s1 = semver.parse(v1);
+    await t.step(`${v0} <=> ${v1}`, () => {
+      assertEquals(semver.eq(s0, s1), expected, `eq(${v0},${v1})`);
+      assertEquals(!semver.neq(s0, s1), expected, `neq(${v0},${v1})`);
+      assertEquals(semver.cmp(s0, "", s1), expected, `${v0} ${v1}`);
+      assertEquals(semver.cmp(s0, "=", s1), expected, `${v0} = ${v1}`);
+      assertEquals(semver.cmp(s0, "==", s1), expected, `${v0} == ${v1}`);
+      assertEquals(semver.cmp(s0, "===", s1), expected, `${v0} === ${v1}`);
+      assertEquals(!semver.cmp(s0, "!=", s1), expected, `!(${v0} != ${v1})`);
+      assertEquals(!semver.cmp(s0, "!==", s1), expected, `${v0} !== ${v1}`);
+    });
+  }
 });
