758630faa64f8ace4ea4a2067aa69a623a87f3cc
Asher Gomez <ashersaupingomez@gmail.com>
2023-03-15 08:02:40 +1100

    use STRIPE_SECRET_KEY

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(Deno.env.get("STRIPE_API_KEY")!, {
+export const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY")!, {
   apiVersion: "2022-11-15",
   // Use the Fetch API instead of Node's HTTP client.
   httpClient: Stripe.createFetchHttpClient(),
