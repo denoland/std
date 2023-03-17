c772e03158c9c6675c2077b59e0784e5ac671a82
Yoshiya Hinosawa <stibium121@gmail.com>
2023-03-17 19:49:05 +0900

    chore(encoding/yaml): restore deprecated exports

---
+++ encoding/_yaml/loader/loader.ts
@@ -0,0 +1,8 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+export {
+  /** @deprecated (will be removed after 0.182.0) use `parse` from std/yaml/mod.ts instead */
+  load,
+  /** @deprecated (will be removed after 0.182.0) use `parseAll` from std/yaml/mod.ts instead */
+  loadAll,
+} from "../../../yaml/_loader/loader.ts";
