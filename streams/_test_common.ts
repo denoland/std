3fefa90c919e708afa79bf77f0e5da12104305a4
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-03-22 09:14:38 -0700

    feat: disposition on TextDelimiterStream

--- streams/_test_common.ts
+++ streams/_test_common.ts
@@ -1,5 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
+import { assert, assertEquals } from "../testing/asserts.ts";
+import { readableStreamFromIterable } from "./readable_stream_from_iterable.ts";
+
 // N controls how many iterations of certain checks are performed.
 const N = 100;
 
@@ -9,4 +12,27 @@ export function init(): Uint8Array {
     testBytes[i] = "a".charCodeAt(0) + (i % 26);
   }
   return testBytes;
+}
+
+/**
+ * Verify that a transform stream produces the expected output data.
+ * @param transform The transform stream to test
+ * @param inputs Source input data
+ * @param outputs Expected output data
+ */
+export async function testTransformStream<T, U>(
+  transform: TransformStream<T, U>,
+  inputs: Iterable<T> | AsyncIterable<T>,
+  outputs: Iterable<U> | AsyncIterable<U>,
+) {
+  const reader = readableStreamFromIterable(inputs)
+    .pipeThrough(transform)
+    .getReader();
+  for await (const output of outputs) {
+    const { value, done } = await reader.read();
+    assertEquals(value, output);
+    assertEquals(done, false);
+  }
+  const f = await reader.read();
+  assert(f.done, `stream not done, value was: ${f.value}`);
 }
