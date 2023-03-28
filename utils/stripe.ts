6fb271ea0e3eff2dcc534892cd0efa4035e4c733
thorwebdev <thor@supabase.io>
2023-03-28 12:55:59 +0800

    chore: some supabase and stripe suggestions.

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -5,3 +5,28 @@ export const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY")!, {
   // Use the Fetch API instead of Node's HTTP client.
   httpClient: Stripe.createFetchHttpClient(),
 });
+
+const isClient = typeof window === "object";
+
+export function formatAmountForDisplay(
+  amount: number,
+  currency: string,
+): string {
+  const numberFormat = new Intl.NumberFormat(
+    isClient ? navigator.language : "en-US",
+    {
+      style: "currency",
+      currency: currency,
+      currencyDisplay: "symbol",
+    },
+  );
+  const parts = numberFormat.formatToParts(amount);
+  let zeroDecimalCurrency = true;
+  for (const part of parts) {
+    if (part.type === "decimal") {
+      zeroDecimalCurrency = false;
+    }
+  }
+  const amountToFormat = zeroDecimalCurrency ? amount : amount / 100;
+  return numberFormat.format(amountToFormat);
+}
