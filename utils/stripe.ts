dda02b62bf1301f7d79d13092e33b3a5ec5dd134
Asher Gomez <ashersaupingomez@gmail.com>
2023-03-30 06:32:28 +1100

    fix: dummary Stripe API key

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,10 +1,23 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY")!, {
-  apiVersion: "2022-11-15",
-  // Use the Fetch API instead of Node's HTTP client.
-  httpClient: Stripe.createFetchHttpClient(),
-});
+/** This constant allows preview deployments to successfully start up, making everything outside of the dashboard viewable. */
+const DUMMY_API_KEY =
+  "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+if (!Deno.env.get("STRIPE_SECRET_KEY")) {
+  console.warn(
+    "`STRIPE_SECRET_KEY` environment variable is not defined. Dummy Stripe API key is currently in use. Stripe functionality is now limited.",
+  );
+}
+
+export const stripe = new Stripe(
+  Deno.env.get("STRIPE_SECRET_KEY") ?? DUMMY_API_KEY,
+  {
+    apiVersion: "2022-11-15",
+    // Use the Fetch API instead of Node's HTTP client.
+    httpClient: Stripe.createFetchHttpClient(),
+  },
+);
 
 export function formatAmountForDisplay(
   amount: number,
