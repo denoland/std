8f506e6831444f53740da99d6e9c8cd2bc3eef60
Asher Gomez <ashersaupingomez@gmail.com>
2023-03-31 08:19:06 +1100

    minor fixes and cleanups

--- routes/dashboard/todos.tsx
+++ routes/dashboard/todos.tsx
@@ -1,13 +1,14 @@
 import type { Handlers, PageProps } from "$fresh/server.ts";
-import { getTodos, type Todo } from "@/utils/todos.ts";
+import { getTodos } from "@/utils/todos.ts";
 import Head from "@/components/Head.tsx";
 import TodoList from "@/islands/TodoList.tsx";
 import Notice from "@/components/Notice.tsx";
 import { DashboardState } from "./_middleware.ts";
 import Dashboard from "@/components/Dashboard.tsx";
+import { Database } from "@/utils/supabase_types.ts";
 
 interface TodosPageProps extends DashboardState {
-  todos: Todo[];
+  todos: Database["public"]["Tables"]["todos"]["Insert"][];
 }
 
 export const handler: Handlers<TodosPageProps, DashboardState> = {
@@ -40,7 +41,7 @@ export default function TodosPage(props: PageProps<TodosPageProps>) {
           />
         )}
         <TodoList
-          isSubscribed={props.data.customer.is_subscribed}
+          isSubscribed={props.data.customer.is_subscribed!}
           todos={props.data.todos}
         />
       </Dashboard>
--- utils/todos.ts
+++ utils/todos.ts
@@ -1,13 +1,9 @@
-import type { SupabaseClient } from "./supabase.ts";
-
-export interface Todo {
-  id: string;
-  name: string;
-}
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "./supabase_types.ts";
 
 const TABLE_NAME = "todos";
 
-export async function getTodos(client: SupabaseClient) {
+export async function getTodos(client: SupabaseClient<Database>) {
   const { data } = await client
     .from(TABLE_NAME)
     .select("id, name")
@@ -15,14 +11,20 @@ export async function getTodos(client: SupabaseClient) {
   return data!;
 }
 
-export async function createTodo(client: SupabaseClient, todo: Todo) {
+export async function createTodo(
+  client: SupabaseClient<Database>,
+  todo: Database["public"]["Tables"]["todos"]["Insert"],
+) {
   await client
     .from(TABLE_NAME)
     .insert(todo)
     .throwOnError();
 }
 
-export async function deleteTodo(client: SupabaseClient, id: Todo["id"]) {
+export async function deleteTodo(
+  client: SupabaseClient<Database>,
+  id: Database["public"]["Tables"]["todos"]["Insert"]["id"],
+) {
   await client
     .from(TABLE_NAME)
     .delete()
