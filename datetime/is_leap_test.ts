396c025c44e872f7862fd4ade215b1eec2dfa3f2
Ryan Clements <ryanclementshax@gmail.com>
2023-03-30 11:28:42 -0400

    feat(isUTCLeap): Add function to determine a leap year in UTC to avoid timezone issues - Add isUTCLeap that is the same as isLeap but uses getUTCFullYear to avoid timezone issues - Add tests for isUTCLeap - Fix tests for isLeap that only failed on machines running in GMT-<anything>

--- datetime/is_leap_test.ts
+++ datetime/is_leap_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { assert } from "../testing/asserts.ts";
-import { isLeap } from "./is_leap.ts";
+import { isLeap, isUTCLeap } from "./is_leap.ts";
 
 Deno.test({
   name: "[std/datetime] isLeap",
@@ -9,9 +9,35 @@ Deno.test({
     assert(isLeap(2000));
     assert(!isLeap(2003));
     assert(!isLeap(2007));
+    assert(!isLeap(new Date("1970-01-02")));
+    assert(isLeap(new Date("1972-01-02")));
+    assert(isLeap(new Date("2000-01-02")));
+    assert(!isLeap(new Date("2100-01-02")));
+  },
+});
+
+Deno.test({
+  name: "[std/datetime] isUTCLeap",
+  fn() {
+    assert(isUTCLeap(1992));
+    assert(isUTCLeap(2000));
+    assert(!isUTCLeap(2003));
+    assert(!isUTCLeap(2007));
+
+    // Date assumes the string given is UTC by default
     assert(!isLeap(new Date("1970-01-01")));
-    assert(isLeap(new Date("1972-01-01")));
-    assert(isLeap(new Date("2000-01-01")));
-    assert(!isLeap(new Date("2100-01-01")));
+    assert(isUTCLeap(new Date("1972-01-01")));
+    assert(isUTCLeap(new Date("2000-01-01")));
+    assert(!isUTCLeap(new Date("2100-01-01")));
+
+    // Bookends of a leap year
+    assert(isUTCLeap(new Date("January 1, 2000 00:00:00 GMT+00:00")));
+    assert(isUTCLeap(new Date("December 31, 2000 23:59:59 GMT+00:00")));
+
+    // Edge cases of a UTC leap year from different time zones
+    assert(!isUTCLeap(new Date("January 1, 2000 00:00:00 GMT+01:00")));
+    assert(!isUTCLeap(new Date("December 31, 2000 23:59:59 GMT-01:00")));
+    assert(isUTCLeap(new Date("January 1, 2001 00:00:00 GMT+01:00")));
+    assert(isUTCLeap(new Date("December 31, 1999 23:59:59 GMT-01:00")));
   },
 });
