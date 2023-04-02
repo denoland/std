3b626f55886f6c960c8a1414348fd9afcafb5d81
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-02 11:22:05 +1000

    test naming tweaks

--- utils/posts_test.ts
+++ utils/posts_test.ts
@@ -2,18 +2,18 @@ import { getPost, getPosts } from "./posts.ts";
 
 import { assert, assertEquals } from "std/testing/asserts.ts";
 
-Deno.test("get Post", async () => {
+Deno.test("[blog] getPost()", async () => {
   const post = await getPost("first-post");
   assert(post);
   assertEquals(post.title, "This is my first blog post!");
 });
 
-Deno.test("get Post non existent", async () => {
+Deno.test("[blog ] getPost() for non-existent post", async () => {
   const post = await getPost("third-post");
   assertEquals(post, null);
 });
 
-Deno.test("get Posts from data directory", async () => {
+Deno.test("[blog] getPosts() from data directory", async () => {
   const posts = await getPosts();
   assert(posts);
   assertEquals(posts.length, 2);
