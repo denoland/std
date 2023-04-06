26ca8d01061244c1a7fe7a2db2bcb2fd3d1a48bf
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-06 19:28:52 +1000

    fmt lol the irony

--- utils/supabase_types.ts
+++ utils/supabase_types.ts
@@ -4,57 +4,57 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       customers: {
         Row: {
-          is_subscribed: boolean
-          stripe_customer_id: string
-          user_id: string
-        }
+          is_subscribed: boolean;
+          stripe_customer_id: string;
+          user_id: string;
+        };
         Insert: {
-          is_subscribed?: boolean
-          stripe_customer_id: string
-          user_id?: string
-        }
+          is_subscribed?: boolean;
+          stripe_customer_id: string;
+          user_id?: string;
+        };
         Update: {
-          is_subscribed?: boolean
-          stripe_customer_id?: string
-          user_id?: string
-        }
-      }
+          is_subscribed?: boolean;
+          stripe_customer_id?: string;
+          user_id?: string;
+        };
+      };
       todos: {
         Row: {
-          id: string
-          name: string | null
-          user_id: string
-        }
+          id: string;
+          name: string | null;
+          user_id: string;
+        };
         Insert: {
-          id: string
-          name?: string | null
-          user_id?: string
-        }
+          id: string;
+          name?: string | null;
+          user_id?: string;
+        };
         Update: {
-          id?: string
-          name?: string | null
-          user_id?: string
-        }
-      }
-    }
+          id?: string;
+          name?: string | null;
+          user_id?: string;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
