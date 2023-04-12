a023ed37a00c8b694645257564d1029cae0f11c6
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-12 10:44:15 +1000

    refactor: replace components with style strings

--- islands/TodoList.tsx
+++ islands/TodoList.tsx
@@ -1,13 +1,15 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import Button from "@/components/Button.tsx";
 import { type Signal, useSignal } from "@preact/signals";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import type { Todo } from "@/utils/todos.ts";
-import { FREE_PLAN_TODOS_LIMIT } from "@/constants.ts";
+import {
+  BASE_BUTTON_STYLES,
+  BASE_INPUT_STYLES,
+  FREE_PLAN_TODOS_LIMIT,
+} from "@/constants.ts";
 import IconTrash from "tabler-icons/trash.tsx";
 import { assert } from "std/testing/asserts.ts";
 import { useRef } from "preact/hooks";
-import Input from "@/components/Input.tsx";
 
 async function requestCreateTodo(todo: Todo) {
   const response = await fetch("/dashboard/api/todo", {
@@ -85,19 +87,19 @@ export default function TodoList(props: TodoListProps) {
           newTodoRef.current!.form!.reset();
         }}
       >
-        <Input
+        <input
           ref={newTodoRef}
           disabled={!isMoreTodos}
-          class="flex-1"
+          class={`${BASE_INPUT_STYLES} flex-1`}
           required
         />
-        <Button
+        <button
           disabled={!isMoreTodos}
           type="submit"
-          class="px-4"
+          class={`${BASE_BUTTON_STYLES} px-4`}
         >
           +
-        </Button>
+        </button>
       </form>
     </div>
   );
