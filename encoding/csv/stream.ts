35eb16e2065910023b9df289e049421f083add1a
David <ducthangn71007@gmail.com>
2023-04-13 21:10:24 +0700

    chore: remove deprecated encoding apis

--- encoding/csv/stream.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-// This module is browser compatible.
-
-export {
-  /** @deprecated (will be removed after 0.182.0) Import from `csv/stream.ts` instead. */
-  CsvStream,
-  /** @deprecated (will be removed after 0.182.0) Import from `csv/stream.ts` instead. */
-  type CsvStreamOptions,
-} from "../../csv/stream.ts";
