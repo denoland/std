e6c530f80c2e6c89434b33d9903ac710e9936e46
sorakor0830 <khawsora@gmail.com>
2023-04-14 15:45:01 +0800

    bump fresh & twind ver

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
+import twindPlugin from "twind_fresh_plugin/twind.ts";
 import twindConfig from "./twind.config.ts";
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
