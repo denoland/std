bf6e887fda169d8433768d764de0783faf6daf4d
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-20 17:57:51 +1000

    refactor: disable oauth for preview deployment

--- components/OAuthLoginButton.tsx
+++ components/OAuthLoginButton.tsx
@@ -13,7 +13,9 @@ export default function OAuthLoginButton(props: OAuthLoginButtonProps) {
       <input type="hidden" value={props.provider} name="provider" />
       <button
         type="submit"
-        class="px-4 py-2 w-full bg-white text-black text-lg rounded-lg border-2 border-black"
+        class="px-4 py-2 w-full bg-white text-black text-lg rounded-lg border-2 border-black disabled:(opacity-50 cursor-not-allowed)"
+        /** @todo disabled for preview deployment */
+        disabled
       >
         {props.children}
       </button>
