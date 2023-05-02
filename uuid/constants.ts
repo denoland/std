cab4035c90fbea796f6be5d52f983606c99cb089
David <ducthangn71007@gmail.com>
2023-05-02 15:48:47 +0700

    format jsdoc

--- uuid/constants.ts
+++ uuid/constants.ts
@@ -1,7 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-/** Name string is a fully-qualified domain name.
+/**
+ * Name string is a fully-qualified domain name.
  *
  * @example
  * ```ts
@@ -11,7 +12,8 @@
  * ```
  */
 export const NAMESPACE_DNS = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
-/** Name string is a URL.
+/**
+ * Name string is a URL.
  *
  * @example
  * ```ts
@@ -21,7 +23,8 @@ export const NAMESPACE_DNS = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
  * ```
  */
 export const NAMESPACE_URL = "6ba7b811-9dad-11d1-80b4-00c04fd430c8";
-/** Name string is an ISO OID.
+/**
+ * Name string is an ISO OID.
  *
  * @example
  * ```ts
@@ -31,7 +34,8 @@ export const NAMESPACE_URL = "6ba7b811-9dad-11d1-80b4-00c04fd430c8";
  * ```
  */
 export const NAMESPACE_OID = "6ba7b812-9dad-11d1-80b4-00c04fd430c8";
-/** Name string is an X.500 DN (in DER or a text output format).
+/**
+ * Name string is an X.500 DN (in DER or a text output format).
  *
  * @example
  * ```ts
