00b4c095224643e857e9c31cfd8d165f69897e13
Yoshiya Hinosawa <stibium121@gmail.com>
2023-05-03 17:14:19 +0900

    make examples pass type checks

--- semver/semver.ts
+++ semver/semver.ts
@@ -37,7 +37,13 @@ export const MIN: SemVer = {
 /**
  * A sentinel value used to denote an invalid SemVer object
  * which may be the result of impossible ranges or comparator operations.
- * @example `eq(parseRange("<0.0.0"), INVALID)`
+ * @example
+ * ```ts
+ * import { eq } from "https://deno.land/std@$STD_VERSION/semver/eq.ts";
+ * import { parse } from "https://deno.land/std@$STD_VERSION/semver/parse.ts";
+ * import { INVALID } from "https://deno.land/std@$STD_VERSION/semver/semver.ts"
+ * eq(parse("1.2.3"), INVALID);
+ * ```
  */
 export const INVALID: SemVer = {
   major: Number.NEGATIVE_INFINITY,
@@ -50,7 +56,13 @@ export const INVALID: SemVer = {
 /**
  * ANY is a sentinel value used by some range calculations. It is not a valid
  * SemVer object and should not be used directly.
- * @example `eq(parseRange("*.*.*"), ANY)`
+ * @example
+ * ```ts
+ * import { eq } from "https://deno.land/std@$STD_VERSION/semver/eq.ts";
+ * import { parse } from "https://deno.land/std@$STD_VERSION/semver/parse.ts";
+ * import { ANY } from "https://deno.land/std@$STD_VERSION/semver/semver.ts"
+ * eq(parse("1.2.3"), ANY); // false
+ * ```
  */
 export const ANY: SemVer = {
   major: Number.NaN,
