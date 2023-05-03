81e5a25f73e6c312a21a18b40890cc5d9b3ce14f
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-03 16:02:05 +1000

    feat: export resetKv()

--- tools/reset_kv.ts
+++ tools/reset_kv.ts
@@ -1,17 +1,22 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { kv } from "@/utils/db.ts";
 
-if (
-  !confirm(
-    "This script deletes all data from the Deno KV database. Are you sure you'd like to continue?",
-  )
-) {
-  close();
+export async function resetKv() {
+  const iter = kv.list({ prefix: [] });
+  const promises = [];
+  for await (const res of iter) promises.push(kv.delete(res.key));
+  await Promise.all(promises);
+
+  await kv.close();
 }
 
-const iter = kv.list({ prefix: [] });
-const promises = [];
-for await (const res of iter) promises.push(kv.delete(res.key));
-await Promise.all(promises);
-
-await kv.close();
+if (import.meta.main) {
+  if (
+    !confirm(
+      "This script deletes all data from the Deno KV database. Are you sure you'd like to continue?",
+    )
+  ) {
+    close();
+  }
+  await resetKv();
+}
