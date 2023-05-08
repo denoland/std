895f44e7bc8efccef270a21021f79db26d53c4d0
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-09 07:38:08 +1000

    further cleanups

--- routes/api/vote.ts
+++ routes/api/vote.ts
@@ -3,10 +3,9 @@ import type { HandlerContext, Handlers, PageProps } from "$fresh/server.ts";
 import type { State } from "@/routes/_middleware.ts";
 import { createVote, deleteVote } from "@/utils/db.ts";
 
-async function responseFn(
+async function sharedHandler(
   req: Request,
-  // deno-lint-ignore no-explicit-any
-  ctx: HandlerContext<PageProps<any>, State>,
+  ctx: HandlerContext<PageProps<undefined>, State>,
 ) {
   if (!ctx.state.session) {
     return new Response(null, { status: 401 });
@@ -19,19 +18,16 @@ async function responseFn(
   }
 
   const userId = ctx.state.session.user.id;
-  const data = {
-    userId,
-    itemId,
-  };
+  const vote = { userId, itemId };
   let status;
   switch (req.method) {
     case "DELETE":
       status = 204;
-      await deleteVote(data);
+      await deleteVote(vote);
       break;
     case "POST":
       status = 201;
-      await createVote(data);
+      await createVote(vote);
       break;
     default:
       return new Response(null, { status: 400 });
@@ -41,6 +37,6 @@ async function responseFn(
 }
 
 export const handler: Handlers<PageProps, State> = {
-  POST: responseFn,
-  DELETE: responseFn,
+  POST: sharedHandler,
+  DELETE: sharedHandler,
 };
