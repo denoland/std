50b56bbeaa358c9696f0866dd1928e73f2014345
sorakor0830 <khawsora@gmail.com>
2023-05-08 13:09:26 +0800

    centralize verification of vote data

--- routes/api/vote.ts
+++ routes/api/vote.ts
@@ -1,49 +1,49 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { Handlers, PageProps } from "$fresh/server.ts";
+import type { HandlerContext, Handlers, PageProps } from "$fresh/server.ts";
 import type { State } from "@/routes/_middleware.ts";
 import { createVote, deleteVote } from "@/utils/db.ts";
 
 export const handler: Handlers<PageProps, State> = {
   async POST(req, ctx) {
-    if (!ctx.state.session) {
-      return new Response(null, { status: 400 });
-    }
-
-    const params = new URL(req.url).searchParams;
-    const itemId = params.get("item_id");
-
-    if (!itemId) {
-      return new Response(null, { status: 400 });
-    }
-
-    const userId = ctx.state.session.user.id;
-
-    await createVote({
-      userId,
-      itemId,
-    });
-
-    return new Response(null, { status: 201 });
+    return await (responseFn(req, ctx));
   },
 
   async DELETE(req, ctx) {
-    if (!ctx.state.session) {
-      return new Response(null, { status: 400 });
-    }
-    const params = new URL(req.url).searchParams;
-    const itemId = params.get("item_id");
-
-    if (!itemId) {
-      return new Response(null, { status: 400 });
-    }
-
-    const userId = ctx.state.session.user.id;
-
-    await deleteVote({
-      userId,
-      itemId,
-    });
-
-    return new Response(null, { status: 204 });
+    return await (responseFn(req, ctx));
   },
+};
+
+const responseFn = async (
+  req: Request,
+  // deno-lint-ignore no-explicit-any
+  ctx: HandlerContext<PageProps<any>, State>,
+) => {
+  if (!ctx.state.session) {
+    return new Response(null, { status: 400 });
+  }
+
+  const params = new URL(req.url).searchParams;
+  const itemId = params.get("item_id");
+
+  if (!itemId) {
+    return new Response(null, { status: 400 });
+  }
+
+  const userId = ctx.state.session.user.id;
+  const data = {
+    userId,
+    itemId,
+  };
+  switch (req.method) {
+    case "DELETE":
+      await deleteVote(data);
+      break;
+    case "POST":
+      await createVote(data);
+      break;
+    default:
+      return new Response(null, { status: 400 });
+  }
+
+  return new Response(null, { status: 201 });
 };
