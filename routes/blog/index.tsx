908b005a79ff7264354fa104a970f1ca412c78f0
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-08 14:02:40 +1000

    refactor: `<PostCard />`

--- routes/blog/index.tsx
+++ routes/blog/index.tsx
@@ -1,7 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { Handlers } from "$fresh/server.ts";
 import { PageProps } from "$fresh/server.ts";
-import PostCard from "@/components/PostCard.tsx";
 import { getPosts, Post } from "@/utils/posts.ts";
 import { SITE_NAME } from "@/utils/constants.ts";
 import Head from "@/components/Head.tsx";
@@ -20,6 +19,30 @@ export const handler: Handlers<BlogPageData, State> = {
   },
 };
 
+function PostCard(props: Post) {
+  return (
+    <div class="py-8 border(t gray-200)">
+      <a class="sm:col-span-2" href={`/blog/${props.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">
+          {props.title}
+        </h3>
+        {props.publishedAt.toString() !== "Invalid Date" && (
+          <time class="text-gray-500">
+            {new Date(props.publishedAt).toLocaleDateString("en-us", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </time>
+        )}
+        <div class="mt-4 text-gray-900">
+          {props.summary}
+        </div>
+      </a>
+    </div>
+  );
+}
+
 export default function BlogPage(props: PageProps<BlogPageData>) {
   return (
     <>
@@ -32,7 +55,7 @@ export default function BlogPage(props: PageProps<BlogPageData>) {
         <main class={`${SITE_WIDTH_STYLES} px-8 pt-16 flex-1`}>
           <h1 class="text-5xl font-bold">Blog</h1>
           <div class="mt-8">
-            {props.data.posts.map((post) => <PostCard post={post} />)}
+            {props.data.posts.map((post) => <PostCard {...post} />)}
           </div>
         </main>
       </Layout>
