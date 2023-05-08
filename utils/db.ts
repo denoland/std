eb23d07ec25de16a1dc08922df38730d0b2e8aa6
sorakor0830 <khawsora@gmail.com>
2023-05-08 13:49:50 +0800

    remove unused `voteId` type

--- utils/db.ts
+++ utils/db.ts
@@ -103,7 +103,6 @@ export async function getItemCommentsCount(itemId: string) {
 interface InitVote {
   userId: string;
   itemId: string;
-  voteId?: string;
 }
 
 export interface Vote extends InitVote {
