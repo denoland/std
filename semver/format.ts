4d95a5fcf0112bc3a45ebdb4f5aee1535443b819
Yoshiya Hinosawa <stibium121@gmail.com>
2023-05-17 15:49:36 +0900

    Update semver/format.ts
    Co-authored-by: Max Duval <hi@mxdvl.com>

--- semver/format.ts
+++ semver/format.ts
@@ -60,7 +60,7 @@ export function comparatorFormat(comparator: SemVerComparator) {
 }
 
 /**
- * Formats teh range into a string
+ * Formats the range into a string
  * @example >=0.0.0 || <1.0.0
  * @param range The range to format
  * @returns A string representation of the range
