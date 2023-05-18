48b98af153be0e32c79ee161328ba2e573b576f8
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-18 14:27:19 +1000

    chore: add internal reset KV trigger

--- main.ts
+++ main.ts
@@ -11,4 +11,13 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
+/**
+ * @todo Remove at v1. This is a quick way to reset Deno KV, as database changes are likely to occur and require reset.
+ */
+import { resetKv } from "./tools/reset_kv.ts";
+
+if (Deno.env.get("RESET_DENO_KV") === "1") {
+  await resetKv();
+}
+
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
