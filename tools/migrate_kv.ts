6e75b104d66945faa5082427c6f28e17ffd2c439
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-01 09:35:34 +1000

    fix: migrate KV for #245 (#248)
    * fix: migrate KV for #245
    
    * fix

--- tools/migrate_kv.ts
+++ tools/migrate_kv.ts
@@ -1,21 +1,11 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { type Item, kv } from "@/utils/db.ts";
 
-function incrementMonth(oldDate: string) {
-  const oldDateParts = oldDate.split("-").map((part) => parseInt(part));
-  oldDateParts[1] = oldDateParts[1] + 1;
-  const newDate = new Date(oldDateParts.join("-"));
-  return newDate.toISOString().split("T")[0];
-}
-
 export async function migrateKv() {
-  const iter = kv.list<Item>({ prefix: ["visits"] });
+  const iter = kv.list<Item>({ prefix: ["items_by_time"] });
   const promises = [];
   for await (const res of iter) {
-    promises.push(kv.delete(res.key));
-    const oldDate = res.key.at(-1);
-    const newDate = incrementMonth(oldDate as string);
-    promises.push(kv.set([res.key[0], newDate], res.value));
+    promises.push(kv.set(["items", res.value.id], res.value));
   }
   await Promise.all(promises);
 }
