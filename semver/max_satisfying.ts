f11e9b9142873d53d5f881f283e245cd2d5fa307
Yoshiya Hinosawa <stibium121@gmail.com>
2023-06-05 19:57:45 +0900

    make maxSatisfying, minSatisfying, intersects backward compatible

--- semver/max_satisfying.ts
+++ semver/max_satisfying.ts
@@ -2,6 +2,8 @@
 import type { SemVer, SemVerRange } from "./types.ts";
 import { sort } from "./sort.ts";
 import { inRange } from "./in_range.ts";
+import { parseRange } from "./parse_range.ts";
+import { parse } from "./parse.ts";
 
 /**
  * Returns the highest version in the list that satisfies the range, or `undefined`
@@ -13,8 +15,22 @@ import { inRange } from "./in_range.ts";
 export function maxSatisfying(
   versions: SemVer[],
   range: SemVerRange,
+): SemVer | undefined;
+/**
+ * @deprecated (will be removed after 0.191.0) Use `maxSatisfying(versions: SemVer[], range: SemVerRange)` instead.
+ */
+export function maxSatisfying<T extends string | SemVer>(
+  versions: readonly T[],
+  range: string | SemVerRange,
+  options?: { includePrerelease: boolean },
+): T | undefined;
+export function maxSatisfying(
+  versions: readonly SemVer[],
+  range: string | SemVerRange,
+  options?: { includePrerelease: boolean },
 ): SemVer | undefined {
-  const satisfying = versions.filter((v) => inRange(v, range));
+  const r = typeof range === "string" ? parseRange(range) : range;
+  const satisfying = versions.filter((v) => inRange(typeof v === "string" ? parse(v, options) : v, r));
   const sorted = sort(satisfying);
   return sorted.pop();
 }
