40a48f05b43e6fb5791fd1f940b024fcc86b697d
gustrb <gustavoreisbauer@gmail.com>
2023-06-09 20:30:37 -0300

    chore(fmt/case): Fixing spacings and inconsistent linting

--- fmt/case.ts
+++ fmt/case.ts
@@ -1,68 +1,68 @@
 /**
  * Converts a str into PascalCase
- * 
+ *
  * @example
  * ```ts
  * import {
  *  toPascalCase,
  * } from "https://deno.land/std@$STD_VERSION/fmt/case.ts";
- * 
+ *
  * assertEquals(toPascalCase("deno is awesome"), "DenoIsAwesome");
  * ```
- * 
+ *
  * @param str The string that is going to be converted into PascalCase
  * @returns The string as PascalCase
  */
 export function toPascalCase(str: string): string {
-    if (typeof str !== "string") {
-        throw new TypeError("str must be a string");
-    }
+  if (typeof str !== "string") {
+    throw new TypeError("str must be a string");
+  }
 
-    if (str === "") {
-        return "";
-    }
+  if (str === "") {
+    return "";
+  }
 
-    return str.split(" ").map(capitalizeWord).join("");
+  return str.split(" ").map(capitalizeWord).join("");
 }
 
 /**
  * Converts a str into camelCase
- * 
+ *
  * @example
  * ```ts
  * import {
  *  toCamelCase,
  * } from "https://deno.land/std@$STD_VERSION/fmt/case.ts";
- * 
+ *
  * assertEquals(toCamelCase("deno is awesome"), "denoIsAwesome");
  * ```
- * 
+ *
  * @param str The string that is going to be converted into camelCase
  * @returns The string as camelCase
  */
 export function toCamelCase(str: string): string {
-    if (typeof str !== "string") {
-        throw new TypeError("str must be a string");
-    }
+  if (typeof str !== "string") {
+    throw new TypeError("str must be a string");
+  }
 
-    if (str === "") {
-        return "";
-    }
+  if (str === "") {
+    return "";
+  }
 
-    const [firstWord, ...restOfWords] = str.split(" ");
-    return [firstWord, restOfWords.map(capitalizeWord)].flat(1).join("");
+  const [firstWord, ...restOfWords] = str.split(" ");
+  return [firstWord, restOfWords.map(capitalizeWord)].flat(1).join("");
 }
 
 function capitalizeWord(word: string): string {
-    const firstChar = word[0];
-    const firstCharAsUpperCase = firstChar.toLocaleUpperCase();
-    const restOfTheString = word.slice(1).toLocaleLowerCase();
-    return firstCharAsUpperCase + restOfTheString;
+  const firstChar = word[0];
+  const firstCharAsUpperCase = firstChar.toLocaleUpperCase();
+  const restOfTheString = word.slice(1).toLocaleLowerCase();
+  return firstCharAsUpperCase + restOfTheString;
 }
 
 /**
  * Converts a str into snake_case
- * 
+ *
  * @example
  * ```ts
  * import {
@@ -70,25 +70,25 @@ function capitalizeWord(word: string): string {
  * } from "https://deno.land/std@$STD_VERSION/fmt/case.ts";
  * assertEquals(toSnakeCase("deno is awesome"), "deno_is_awesome");
  * ```
- * 
+ *
  * @param str is the string that is going to be converted into snake_case
  * @returns The string as snake_case
  */
 export function toSnakeCase(str: string): string {
-    if (typeof str !== "string") {
-        throw new TypeError("str must be a string");
-    }
+  if (typeof str !== "string") {
+    throw new TypeError("str must be a string");
+  }
 
-    if (str === "") {
-        return "";
-    }
-    
-    return str.split(" ").join("_").toLocaleLowerCase();
+  if (str === "") {
+    return "";
+  }
+
+  return str.split(" ").join("_").toLocaleLowerCase();
 }
 
 /**
  * Converts a str into SCREAMING_SNAKE_CASE
- * 
+ *
  * @example
  * ```ts
  * import {
@@ -96,17 +96,17 @@ export function toSnakeCase(str: string): string {
  * } from "https://deno.land/std@$STD_VERSION/fmt/case.ts";
  * assertEquals(toScreamingSnakeCase("deno is awesome"), "DENO_IS_AWESOME");
  * ```
- * 
+ *
  * @param str is the string that is going to be converted into SCREAMING_SNAKE_CASE
  * @returns The string as SCREAMING_SNAKE_CASE
  */
 export function toScreamingSnakeCase(str: string): string {
-    return toSnakeCase(str).toLocaleUpperCase();
+  return toSnakeCase(str).toLocaleUpperCase();
 }
 
 /**
  * Converts a str into kebab-case
- * 
+ *
  * @example
  * ```ts
  * import {
@@ -114,18 +114,18 @@ export function toScreamingSnakeCase(str: string): string {
  * } from "https://deno.land/std@$STD_VERSION/fmt/case.ts";
  * assertEquals(toKebabCase("deno is awesome"), "deno-is-awesome");
  * ```
- * 
+ *
  * @param str is the string that is going to be converted into kebab-case
  * @returns The string as kebab-case
  */
 export function toKebabCase(str: string): string {
-    if (typeof str !== "string") {
-        throw new TypeError("str must be a string");
-    }
+  if (typeof str !== "string") {
+    throw new TypeError("str must be a string");
+  }
 
-    if (str === "") {
-        return "";
-    }
-    
-    return str.split(" ").join("-").toLocaleLowerCase();
+  if (str === "") {
+    return "";
+  }
+
+  return str.split(" ").join("-").toLocaleLowerCase();
 }
--- fmt/case_test.ts
+++ fmt/case_test.ts
@@ -1,142 +1,162 @@
-import * as casing from './case.ts';
+import * as casing from "./case.ts";
 import { assertEquals, assertThrows } from "../testing/asserts.ts";
 
 Deno.test("toPascalCase: Throws in invalid inputs", () => {
-    assertThrows(() => casing.toPascalCase(undefined as any));
-    assertThrows(() => casing.toPascalCase(NaN as any));
-    assertThrows(() => casing.toPascalCase(Infinity as any));
-    assertThrows(() => casing.toPascalCase(10 as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toPascalCase(undefined as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toPascalCase(NaN as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toPascalCase(Infinity as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toPascalCase(10 as any));
 });
 
 Deno.test("toPascalCase: An empty string is already pascal case", () => {
-    assertEquals(casing.toPascalCase(''), '');
+  assertEquals(casing.toPascalCase(""), "");
 });
 
 Deno.test("toPascalCase: Convert a single word to pascal case", () => {
-    const SINGLE_WORD = "shruberry";
-    const EXPECTED = "Shruberry";
-    assertEquals(casing.toPascalCase(SINGLE_WORD), EXPECTED);
+  const SINGLE_WORD = "shruberry";
+  const EXPECTED = "Shruberry";
+  assertEquals(casing.toPascalCase(SINGLE_WORD), EXPECTED);
 });
 
 Deno.test("toPascalCase: Convert a sentence into pascal case", () => {
-    assertEquals(
-        casing.toPascalCase("she turned me into a newt"), 
-        "SheTurnedMeIntoANewt"
-    );
-    assertEquals(
-        casing.toPascalCase("tis But a Scratch"), 
-        "TisButAScratch"
-    );
+  assertEquals(
+    casing.toPascalCase("she turned me into a newt"),
+    "SheTurnedMeIntoANewt",
+  );
+  assertEquals(
+    casing.toPascalCase("tis But a Scratch"),
+    "TisButAScratch",
+  );
 });
 
 Deno.test("toSnakeCase: Throws in invalid inputs", () => {
-    assertThrows(() => casing.toSnakeCase(undefined as any));
-    assertThrows(() => casing.toSnakeCase(NaN as any));
-    assertThrows(() => casing.toSnakeCase(Infinity as any));
-    assertThrows(() => casing.toSnakeCase(10 as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toSnakeCase(undefined as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toSnakeCase(NaN as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toSnakeCase(Infinity as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toSnakeCase(10 as any));
 });
 
 Deno.test("toSnakeCase: An empty string is already snake case", () => {
-    assertEquals(casing.toSnakeCase(''), '');
+  assertEquals(casing.toSnakeCase(""), "");
 });
 
 Deno.test("toSnakeCase: Convert a single word to snake case", () => {
-    const SINGLE_WORD = "shruberry";
-    const EXPECTED = "shruberry";
-    assertEquals(casing.toSnakeCase(SINGLE_WORD), EXPECTED);
+  const SINGLE_WORD = "shruberry";
+  const EXPECTED = "shruberry";
+  assertEquals(casing.toSnakeCase(SINGLE_WORD), EXPECTED);
 });
 
 Deno.test("toSnakeCase: Convert a sentence into snake case", () => {
-    assertEquals(
-        casing.toSnakeCase("she turned me into a newt"), 
-        "she_turned_me_into_a_newt"
-    );
-    assertEquals(
-        casing.toSnakeCase("tis But a Scratch"), 
-        "tis_but_a_scratch"
-    );
+  assertEquals(
+    casing.toSnakeCase("she turned me into a newt"),
+    "she_turned_me_into_a_newt",
+  );
+  assertEquals(
+    casing.toSnakeCase("tis But a Scratch"),
+    "tis_but_a_scratch",
+  );
 });
 
 Deno.test("toScreamingSnakeCase: Throws in invalid inputs", () => {
-    assertThrows(() => casing.toScreamingSnakeCase(undefined as any));
-    assertThrows(() => casing.toScreamingSnakeCase(NaN as any));
-    assertThrows(() => casing.toScreamingSnakeCase(Infinity as any));
-    assertThrows(() => casing.toScreamingSnakeCase(10 as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toScreamingSnakeCase(undefined as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toScreamingSnakeCase(NaN as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toScreamingSnakeCase(Infinity as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toScreamingSnakeCase(10 as any));
 });
 
 Deno.test("toScreamingSnakeCase: An empty string is already screaming snake case", () => {
-    assertEquals(casing.toScreamingSnakeCase(''), '');
+  assertEquals(casing.toScreamingSnakeCase(""), "");
 });
 
 Deno.test("toScreamingSnakeCase: Convert a single word to screaming snake case", () => {
-    const SINGLE_WORD = "shruberry";
-    const EXPECTED = "SHRUBERRY";
-    assertEquals(casing.toScreamingSnakeCase(SINGLE_WORD), EXPECTED);
+  const SINGLE_WORD = "shruberry";
+  const EXPECTED = "SHRUBERRY";
+  assertEquals(casing.toScreamingSnakeCase(SINGLE_WORD), EXPECTED);
 });
 
 Deno.test("toScreamingSnakeCase: Convert a sentence into screaming snake case", () => {
-    assertEquals(
-        casing.toScreamingSnakeCase("she turned me into a newt"), 
-        "SHE_TURNED_ME_INTO_A_NEWT"
-    );
-    assertEquals(
-        casing.toScreamingSnakeCase("tis But a Scratch"), 
-        "TIS_BUT_A_SCRATCH"
-    );
+  assertEquals(
+    casing.toScreamingSnakeCase("she turned me into a newt"),
+    "SHE_TURNED_ME_INTO_A_NEWT",
+  );
+  assertEquals(
+    casing.toScreamingSnakeCase("tis But a Scratch"),
+    "TIS_BUT_A_SCRATCH",
+  );
 });
 
 Deno.test("toKebabCase: Throws in invalid inputs", () => {
-    assertThrows(() => casing.toKebabCase(undefined as any));
-    assertThrows(() => casing.toKebabCase(NaN as any));
-    assertThrows(() => casing.toKebabCase(Infinity as any));
-    assertThrows(() => casing.toKebabCase(10 as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toKebabCase(undefined as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toKebabCase(NaN as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toKebabCase(Infinity as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toKebabCase(10 as any));
 });
 
 Deno.test("toKebabCase: An empty string is already snake case", () => {
-    assertEquals(casing.toKebabCase(''), '');
+  assertEquals(casing.toKebabCase(""), "");
 });
 
 Deno.test("toKebabCase: Convert a single word to snake case", () => {
-    const SINGLE_WORD = "shruberry";
-    const EXPECTED = "shruberry";
-    assertEquals(casing.toKebabCase(SINGLE_WORD), EXPECTED);
+  const SINGLE_WORD = "shruberry";
+  const EXPECTED = "shruberry";
+  assertEquals(casing.toKebabCase(SINGLE_WORD), EXPECTED);
 });
 
 Deno.test("toKebabCase: Convert a sentence into snake case", () => {
-    assertEquals(
-        casing.toKebabCase("she turned me into a newt"), 
-        "she-turned-me-into-a-newt"
-    );
-    assertEquals(
-        casing.toKebabCase("tis But a Scratch"), 
-        "tis-but-a-scratch"
-    );
+  assertEquals(
+    casing.toKebabCase("she turned me into a newt"),
+    "she-turned-me-into-a-newt",
+  );
+  assertEquals(
+    casing.toKebabCase("tis But a Scratch"),
+    "tis-but-a-scratch",
+  );
 });
 
 Deno.test("toCamelCase: Throws in invalid inputs", () => {
-    assertThrows(() => casing.toCamelCase(undefined as any));
-    assertThrows(() => casing.toCamelCase(NaN as any));
-    assertThrows(() => casing.toCamelCase(Infinity as any));
-    assertThrows(() => casing.toCamelCase(10 as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toCamelCase(undefined as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toCamelCase(NaN as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toCamelCase(Infinity as any));
+  // deno-lint-ignore no-explicit-any
+  assertThrows(() => casing.toCamelCase(10 as any));
 });
 
 Deno.test("toCamelCase: An empty string is already camel case", () => {
-    assertEquals(casing.toCamelCase(''), '');
+  assertEquals(casing.toCamelCase(""), "");
 });
 
 Deno.test("toCamelCase: Convert a single word to camel case", () => {
-    const SINGLE_WORD = "shruberry";
-    const EXPECTED = "shruberry";
-    assertEquals(casing.toCamelCase(SINGLE_WORD), EXPECTED);
+  const SINGLE_WORD = "shruberry";
+  const EXPECTED = "shruberry";
+  assertEquals(casing.toCamelCase(SINGLE_WORD), EXPECTED);
 });
 
 Deno.test("toCamelCase: Convert a sentence into camel case", () => {
-    assertEquals(
-        casing.toCamelCase("she turned me into a newt"), 
-        "sheTurnedMeIntoANewt"
-    );
-    assertEquals(
-        casing.toCamelCase("tis But a Scratch"), 
-        "tisButAScratch"
-    );
+  assertEquals(
+    casing.toCamelCase("she turned me into a newt"),
+    "sheTurnedMeIntoANewt",
+  );
+  assertEquals(
+    casing.toCamelCase("tis But a Scratch"),
+    "tisButAScratch",
+  );
 });
