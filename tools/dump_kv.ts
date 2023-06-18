5e19532590420ccfa2b3ff58e9e9ec233dbeecdc
Javier Ríos <josemdecarglass@gmail.com>
2023-06-18 02:47:43 +0200

    fix: bigint serialization error (#263)
    * fix: bigint serialization error
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- tools/dump_kv.ts
+++ tools/dump_kv.ts
@@ -3,13 +3,17 @@
 // Usage: deno run -A --unstable tools/dump_kv.ts
 import { kv } from "@/utils/db.ts";
 
+// https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-521460510
+function replacer(_key: unknown, value: unknown) {
+  return typeof value === "bigint" ? value.toString() : value; // return everything else unchanged
+}
 export async function dumpKv() {
   const iter = kv.list({ prefix: [] });
   const items = [];
   for await (const res of iter) {
     items.push({ [res.key.toString()]: res.value });
   }
-  console.log(`${JSON.stringify(items, null, 2)}`);
+  console.log(`${JSON.stringify(items, replacer, 2)}`);
 }
 
 if (import.meta.main) {
