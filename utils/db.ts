7543b4c625ed25036a1e201028a4ff4204569cf6
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-22 19:56:22 +1000

    chore: set `KV_PATH` env var (#285)

--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,15 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { DAY, WEEK } from "std/datetime/constants.ts";
 
-export const kv = await Deno.openKv();
+const KV_PATH_KEY = "KV_PATH";
+let path = undefined;
+if (
+  (await Deno.permissions.query({ name: "env", variable: KV_PATH_KEY }))
+    .state === "granted"
+) {
+  path = Deno.env.get(KV_PATH_KEY);
+}
+export const kv = await Deno.openKv(path);
 
 // Helpers
 async function getValue<T>(
