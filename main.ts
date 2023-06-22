e8282b717138aaf019897c42f0ec630bf3b27b92
Sjors Smits <smits.sjors@gmail.com>
2023-06-22 23:38:07 +0200

    Add reference to the unstable types (#279)
    As Deno KV is still unstable, its types are not included in `deno.ns`. This means that when opening the project in VS Code, it does not recognise functions such as `Deno.openKv()`. Thus, by referencing `deno.unstable` we also include these types.
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
