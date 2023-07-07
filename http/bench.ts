579d7de2776394dcb6c3a3687a641fd7ce1218ae
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-07-07 09:01:37 -0700

    chore: removing remaining instances of serve

--- http/bench.ts
+++ http/bench.ts
@@ -1,9 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { serve } from "./server.ts";
 
 const DEFAULT_PORT = 4500;
 const addr = Deno.args[0] || `127.0.0.1:${DEFAULT_PORT}`;
 const encoder = new TextEncoder();
 const body = encoder.encode("Hello World");
 const [hostname, p] = addr.split(":");
-serve(() => new Response(body), { port: (+p || DEFAULT_PORT), hostname });
+Deno.serve({ port: (+p || DEFAULT_PORT), hostname }, () => new Response(body));
--- http/testdata/simple_server.ts
+++ http/testdata/simple_server.ts
@@ -1,5 +1,4 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 // This is an example of a server that responds with an empty body
-import { serve } from "../server.ts";
 
-await serve(() => new Response(), { port: 4504 });
+Deno.serve({ port: 4505 }, () => new Response());
