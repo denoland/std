88d5c1c27ea1963654c48fb196454fac37e23307
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-07-08 06:34:35 -0700

    chore: remove unused files

--- http/bench.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-
-const DEFAULT_PORT = 4500;
-const addr = Deno.args[0] || `127.0.0.1:${DEFAULT_PORT}`;
-const encoder = new TextEncoder();
-const body = encoder.encode("Hello World");
-const [hostname, p] = addr.split(":");
-Deno.serve({ port: (+p || DEFAULT_PORT), hostname }, () => new Response(body));
--- http/testdata/simple_server.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-// This is an example of a server that responds with an empty body
-
-Deno.serve({ port: 4505 }, () => new Response());
