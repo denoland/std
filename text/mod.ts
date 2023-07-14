1f2fa491e8b3ea13aea860bbdd424fee18873d0a
Jeff Hykin <jeff.hykin@gmail.com>
2023-07-14 13:10:51 -0500

    fmt

--- text/mod.ts
+++ text/mod.ts
@@ -6,25 +6,35 @@
  * @returns {number} The Levenshtein distance between the two strings.
  */
 export function levenshteinDistance(str1: string, str2: string): number {
-    if (str1.length > str2.length) {
-        ;[str1, str2] = [str2, str1]
-    }
+  if (str1.length > str2.length) {
+    [str1, str2] = [str2, str1];
+  }
 
-    let distances: number[] = Array.from({ length: str1.length + 1 }, (_, i) => +i)
-    for (let str2Index = 0; str2Index < str2.length; str2Index++) {
-        const tempDistances: number[] = [str2Index + 1]
-        for (let str1Index = 0; str1Index < str1.length; str1Index++) {
-            let char1 = str1[str1Index]
-            let char2 = str2[str2Index]
-            if (char1 === char2) {
-                tempDistances.push(distances[str1Index])
-            } else {
-                tempDistances.push(1 + Math.min(distances[str1Index], distances[str1Index + 1], tempDistances[tempDistances.length - 1]))
-            }
-        }
-        distances = tempDistances
+  let distances: number[] = Array.from(
+    { length: str1.length + 1 },
+    (_, i) => +i,
+  );
+  for (let str2Index = 0; str2Index < str2.length; str2Index++) {
+    const tempDistances: number[] = [str2Index + 1];
+    for (let str1Index = 0; str1Index < str1.length; str1Index++) {
+      let char1 = str1[str1Index];
+      let char2 = str2[str2Index];
+      if (char1 === char2) {
+        tempDistances.push(distances[str1Index]);
+      } else {
+        tempDistances.push(
+          1 +
+            Math.min(
+              distances[str1Index],
+              distances[str1Index + 1],
+              tempDistances[tempDistances.length - 1],
+            ),
+        );
+      }
     }
-    return distances[distances.length - 1]
+    distances = tempDistances;
+  }
+  return distances[distances.length - 1];
 }
 
 /**
@@ -60,35 +70,45 @@ export function levenshteinDistance(str1: string, str2: string): number {
  * @returns {string[]} An array of possible correct spellings for the given word.
  */
 export function didYouMean(
-    guessWord: string,
-    possibleWords: string[],
-    {
-        caseSensitive = false,
-        autoThrow = false,
-        suggestionLimit = Infinity,
-    }: {
-        caseSensitive?: boolean
-        suggestionLimit?: number
-        autoThrow?: boolean
-    }
+  guessWord: string,
+  possibleWords: string[],
+  {
+    caseSensitive = false,
+    autoThrow = false,
+    suggestionLimit = Infinity,
+  }: {
+    caseSensitive?: boolean;
+    suggestionLimit?: number;
+    autoThrow?: boolean;
+  },
 ): string[] {
-    if (!caseSensitive) {
-        possibleWords = possibleWords.map((each) => each.toLowerCase())
-        guessWord = guessWord.toLowerCase()
+  if (!caseSensitive) {
+    possibleWords = possibleWords.map((each) => each.toLowerCase());
+    guessWord = guessWord.toLowerCase();
+  }
+  if (!possibleWords.includes(guessWord) && autoThrow) {
+    let suggestions: string[] = didYouMean({
+      guessWord,
+      possibleWords,
+      caseSensitive,
+      suggestionLimit,
+    });
+    if (suggestionLimit == 1 && suggestions.length > 0) {
+      throw new Error(
+        `For ${JSON.stringify(guessWord)}, did you mean ${
+          JSON.stringify(suggestions[0])
+        }?`,
+      );
+    } else {
+      throw new Error(
+        `For ${JSON.stringify(guessWord)}, did you mean one of ${
+          JSON.stringify(suggestions)
+        }?`,
+      );
     }
-    if (!possibleWords.includes(guessWord) && autoThrow) {
-        let suggestions: string[] = didYouMean({
-            guessWord,
-            possibleWords,
-            caseSensitive,
-            suggestionLimit,
-        })
-        if (suggestionLimit == 1 && suggestions.length > 0) {
-            throw new Error(`For ${JSON.stringify(guessWord)}, did you mean ${JSON.stringify(suggestions[0])}?`)
-        } else {
-            throw new Error(`For ${JSON.stringify(guessWord)}, did you mean one of ${JSON.stringify(suggestions)}?`)
-        }
-    }
-    // this distance metric could be swapped/improved in the future
-    return [...possibleWords].sort((a, b) => levenshteinDistance(guessWord, a) - levenshteinDistance(guessWord, b)).slice(0, suggestionLimit)
+  }
+  // this distance metric could be swapped/improved in the future
+  return [...possibleWords].sort((a, b) =>
+    levenshteinDistance(guessWord, a) - levenshteinDistance(guessWord, b)
+  ).slice(0, suggestionLimit);
 }
