82c63fc1f65820edcbfc7d9d1a7ceae7789a4069
Jesse Jackson <jsejcksn@users.noreply.github.com>
2023-07-14 00:03:05 -0500

    fix(async/delay): reject with existing AbortSignal reason (#3479)

--- async/delay.ts
+++ async/delay.ts
@@ -38,13 +38,11 @@ export interface DelayOptions {
  */
 export function delay(ms: number, options: DelayOptions = {}): Promise<void> {
   const { signal, persistent } = options;
-  if (signal?.aborted) {
-    return Promise.reject(new DOMException("Delay was aborted.", "AbortError"));
-  }
+  if (signal?.aborted) return Promise.reject(signal.reason);
   return new Promise((resolve, reject) => {
     const abort = () => {
       clearTimeout(i);
-      reject(new DOMException("Delay was aborted.", "AbortError"));
+      reject(signal?.reason);
     };
     const done = () => {
       signal?.removeEventListener("abort", abort);
