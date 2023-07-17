ea6380fcc7db4657780343266d3fc505a0ffee55
Jeff Hykin <jeff.hykin@gmail.com>
2023-07-17 09:45:38 -0500

    extract out levenshteinDistance

---
+++ text/levenshtein_distance_test.ts
@@ -0,0 +1,17 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+import { assertEquals } from "../testing/asserts.ts";
+import { levenshteinDistance } from "./levenshtein_distance.ts";
+
+Deno.test("levenshteinDistanceAbove0", function () {
+  assertEquals(
+    levenshteinDistance("aa", "bb"),
+    2,
+  )
+});
+
+Deno.test("levenshteinDistance0", function () {
+  assertEquals(
+    levenshteinDistance("aa", "aa"),
+    0,
+  )
+});
