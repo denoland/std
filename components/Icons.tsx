4985a53b5d5b9925b3bc1d6696a671209596d5d5
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-25 08:23:00 +1000

    refactor: redesign submit page (#395)
    Hash: I feel like this is missing something, but I don't know what. Any
    ideas, or do you think it's good?
    Andy: I included minimal copy, which looks sufficient to me.
    
    Before:
    <img width="1335" alt="image"
    src="https://github.com/denoland/saaskit/assets/3132889/19bee038-839a-4f39-9f64-d750682422e8">
    
    After:
    <img width="1326" alt="image"
    src="https://github.com/denoland/saaskit/assets/3132889/c126f4c3-b069-47dd-973c-cee01bd84fa4">
    
    ---------
    
    Co-authored-by: hashrock <hashrock@users.noreply.github.com>

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -95,6 +95,47 @@ export function Check(props: JSX.SVGAttributes<SVGSVGElement>) {
   );
 }
 
+export function CheckCircle(props: JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+      <polyline points="22 4 12 14.01 9 11.01"></polyline>
+    </svg>
+  );
+}
+
+export function XCircle(props: JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="15" y1="9" x2="9" y2="15"></line>
+      <line x1="9" y1="9" x2="15" y2="15"></line>
+    </svg>
+  );
+}
+
 export function Rss(props: JSX.SVGAttributes<SVGSVGElement>) {
   return (
     <svg
--- routes/submit.tsx
+++ routes/submit.tsx
@@ -11,6 +11,7 @@ import {
 import { redirect } from "@/utils/redirect.ts";
 import { redirectToLogin } from "@/utils/redirect.ts";
 import Head from "@/components/Head.tsx";
+import { CheckCircle, XCircle } from "@/components/Icons.tsx";
 
 export const handler: Handlers<State, State> = {
   GET(req, ctx) {
@@ -59,31 +60,80 @@ export default function SubmitPage(props: PageProps) {
   return (
     <>
       <Head title="Submit" href={props.url.href} />
-      <main class="flex-1 flex flex-col justify-center max-w-sm mx-auto w-full space-y-8">
-        <h1 class="text-center text-2xl font-bold">
-          Share your project
-        </h1>
-        <form class="space-y-2" method="post">
-          <input
-            class={`${INPUT_STYLES} w-full`}
-            type="text"
-            name="title"
-            required
-            placeholder="Title"
-          />
-          <input
-            class={`${INPUT_STYLES} w-full`}
-            type="url"
-            name="url"
-            required
-            placeholder="URL"
-          />
-          <div class="w-full rounded-lg bg-gradient-to-tr from-secondary to-primary p-px">
-            <button class="w-full text-white text-left rounded-[7px] transition duration-300 px-4 py-2 block hover:(bg-white text-black dark:(bg-gray-900 !text-white))">
-              Submit
-            </button>
+      <main class="flex-1 flex flex-col justify-center mx-auto w-full space-y-16 p-4 max-w-6xl">
+        <div class="text-center">
+          <h1 class="text-center text-3xl font-bold">
+            Share your project
+          </h1>
+          <p class="text-gray-500">
+            Let the community know about your Deno-related blog post, video or
+            module!
+          </p>
+        </div>
+        <div class="flex flex-col md:flex-row gap-8 md:gap-16 md:items-center">
+          <div class="flex-1 space-y-6">
+            <p>
+              <XCircle class="inline-block mr-2" />
+              <strong>Don't</strong> post duplicate content
+            </p>
+            <p>
+              <XCircle class="inline-block mr-2" />
+              <strong>Don't</strong> share dummy or test posts
+            </p>
+            <div>
+              <CheckCircle class="inline-block mr-2" />
+              <strong>Do</strong> include a description with your title.
+
+              <div class="text-sm text-gray-500">
+                E.g. “Deno Hunt: the best place to share your Deno project”
+              </div>
+            </div>
+            <p>
+            </p>
           </div>
-        </form>
+          <form
+            class="flex-1 flex flex-col justify-center"
+            method="post"
+          >
+            <div class="mt-4">
+              <label
+                htmlFor="submit_title"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Title
+              </label>
+              <input
+                id="submit_title"
+                class={`${INPUT_STYLES} w-full mt-2`}
+                type="text"
+                name="title"
+                required
+                placeholder="Deno Hunt: the best place to share your Deno project"
+              />
+            </div>
+
+            <div class="mt-4">
+              <label
+                htmlFor="submit_title"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                URL
+              </label>
+              <input
+                class={`${INPUT_STYLES} w-full mt-2`}
+                type="url"
+                name="url"
+                required
+                placeholder="https://my-awesome-project.com"
+              />
+            </div>
+            <div class="w-full rounded-lg bg-gradient-to-tr from-secondary to-primary p-px mt-8">
+              <button class="w-full text-white text-center rounded-[7px] transition duration-300 px-4 py-2 block hover:(bg-white text-black dark:(bg-gray-900 !text-white))">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </main>
     </>
   );
