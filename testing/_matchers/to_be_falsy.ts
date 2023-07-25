79413e77482abb45c37f61810a831c4848495790
Sorikairo <sorikairox@gmail.com>
2023-07-26 01:00:36 +0900

    feat: toBeClose, toBeFalsy, toBeTruthy

---
+++ testing/_matchers/to_be_falsy.ts
@@ -0,0 +1,23 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+import { MatcherContext, MatchResult } from "../_types.ts";
+import { AssertionError } from '../../assert/assertion_error';
+
+/* Similar to assertEqual(!!value) */
+export function toBeTruthy(
+  context: MatcherContext
+): MatchResult {
+  const isFalsy = !(context.value);
+  if (context.isNot) {
+    if (isFalsy) {
+      throw new AssertionError(
+        `Expected ${context.value} to NOT be falsy`,
+      );
+    }
+  }
+  if (!isFalsy) {
+    throw new AssertionError(
+      `Expected ${context.value} to be falsy`,
+    );
+  }
+}
