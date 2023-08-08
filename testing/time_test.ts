4e12dee2ee757299e696fd343c58bc610a2327a5
Milly <milly.ca@gmail.com>
2023-08-09 08:59:54 +0900

    fix(testing/time): fix restoreFor to be atomic

--- testing/time_test.ts
+++ testing/time_test.ts
@@ -304,6 +304,70 @@ Deno.test("FakeTime restoreFor restores real time temporarily", async () => {
   }
 });
 
+Deno.test("FakeTime restoreFor restores real time and re-overridden atomically", async () => {
+  const time: FakeTime = new FakeTime();
+  const fakeSetTimeout = setTimeout;
+  const actualSetTimeouts: (typeof setTimeout)[] = [];
+
+  try {
+    const asyncFn = async () => {
+      actualSetTimeouts.push(setTimeout);
+      await Promise.resolve();
+      actualSetTimeouts.push(setTimeout);
+      await Promise.resolve();
+      actualSetTimeouts.push(setTimeout);
+    };
+    const promise = asyncFn();
+    await new Promise((resolve) => {
+      FakeTime.restoreFor(() => setTimeout(resolve, 0));
+    });
+    await promise;
+    assertEquals(actualSetTimeouts, [
+      fakeSetTimeout,
+      fakeSetTimeout,
+      fakeSetTimeout,
+    ]);
+  } finally {
+    time.restore();
+  }
+});
+
+Deno.test("FakeTime restoreFor returns promise that resolved to result of callback", async () => {
+  const time: FakeTime = new FakeTime();
+
+  try {
+    const resultSync = await FakeTime.restoreFor(() => "a");
+    assertEquals(resultSync, "a");
+    const resultAsync = await FakeTime.restoreFor(() => Promise.resolve("b"));
+    assertEquals(resultAsync, "b");
+  } finally {
+    time.restore();
+  }
+});
+
+Deno.test("FakeTime restoreFor returns promise that rejected to error in callback", async () => {
+  const time: FakeTime = new FakeTime();
+
+  try {
+    await assertRejects(
+      () => FakeTime.restoreFor(() => {
+        throw new Error("Error in sync callback");
+      }),
+      Error,
+      "Error in sync callback",
+    );
+    await assertRejects(
+      () => FakeTime.restoreFor(() => {
+        return Promise.reject(new Error("Error in async callback"));
+      }),
+      Error,
+      "Error in async callback",
+    );
+  } finally {
+    time.restore();
+  }
+});
+
 Deno.test("delay uses real time", async () => {
   const time: FakeTime = new FakeTime();
   const start: number = Date.now();
