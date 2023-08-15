981f59d9cd7fa69718426b483d05aaaac67b457a
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-08-15 16:39:32 -0700

    fix(log): rotating file handler sync setup and destroy

--- log/mod.ts
+++ log/mod.ts
@@ -64,7 +64,7 @@
  * import * as log from "https://deno.land/std@$STD_VERSION/log/mod.ts";
  * import { sum } from "<the-awesome-module>/mod.ts";
  *
- * await log.setup({
+ * log.setup({
  *   handlers: {
  *     console: new log.handlers.ConsoleHandler("DEBUG"),
  *   },
@@ -108,7 +108,7 @@
  * log.critical("500 Internal server error");
  *
  * // custom configuration with 2 loggers (the default and `tasks` loggers).
- * await log.setup({
+ * log.setup({
  *   handlers: {
  *     console: new log.handlers.ConsoleHandler("DEBUG"),
  *
@@ -157,7 +157,7 @@
  * ```ts
  * import * as log from "https://deno.land/std@$STD_VERSION/log/mod.ts";
  *
- * await log.setup({
+ * log.setup({
  *   handlers: {
  *     stringFmt: new log.handlers.ConsoleHandler("DEBUG", {
  *       formatter: "[{levelName}] {msg}",
@@ -225,7 +225,7 @@
  * ```ts
  * import * as log from "https://deno.land/std@$STD_VERSION/log/mod.ts";
  *
- * await log.setup({
+ * log.setup({
  *   handlers: {
  *     console: new log.handlers.ConsoleHandler("DEBUG"),
  *   },
