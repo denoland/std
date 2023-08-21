fd5722bbb6192db4497481a1e9b765961652b020
Mizuki <esrseiker@gmail.com>
2023-08-21 13:26:58 +0900

    fix(log): fix serializing BigInt value in object (#3550)

--- log/logger_test.ts
+++ log/logger_test.ts
@@ -241,15 +241,33 @@ Deno.test(
       payload: "data",
       other: 123,
     });
+    const data19: { payload: string; other: bigint } = logger.error({
+      payload: "data",
+      other: 123n,
+    });
+    assertEquals(data19, {
+      payload: "data",
+      other: 123n,
+    });
+    const data20: { payload: string; other: bigint } = logger.error(
+      { payload: "data", other: 123n },
+      1,
+    );
+    assertEquals(data20, {
+      payload: "data",
+      other: 123n,
+    });
     assertEquals(handler.messages[16], 'ERROR {"payload":"data","other":123}');
     assertEquals(handler.messages[17], 'ERROR {"payload":"data","other":123}');
+    assertEquals(handler.messages[18], 'ERROR {"payload":"data","other":123}');
+    assertEquals(handler.messages[19], 'ERROR {"payload":"data","other":123}');
 
     // error
     const error = new RangeError("Uh-oh!");
-    const data19: RangeError = logger.error(error);
-    assertEquals(data19, error);
-    const messages19 = handler.messages[18].split("\n");
-    assertEquals(messages19[0], `ERROR ${error.name}: ${error.message}`);
-    assertMatch(messages19[1], /^\s+at file:.*\d+:\d+$/);
+    const data21: RangeError = logger.error(error);
+    assertEquals(data21, error);
+    const messages21 = handler.messages[20].split("\n");
+    assertEquals(messages21[0], `ERROR ${error.name}: ${error.message}`);
+    assertMatch(messages21[1], /^\s+at file:.*\d+:\d+$/);
   },
 );
