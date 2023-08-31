0d63e640fcae9c86dd10a4ec438b262a0e9df3eb
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-31 19:44:22 +0900

    BREAKING(semver): remove deprecated APIs (#3591)

--- semver/eq.ts
+++ semver/eq.ts
@@ -4,20 +4,6 @@ import type { SemVer } from "./types.ts";
 
 /** Returns `true` if they're logically equivalent, even if they're not the exact
  * same version object. */
-export function eq(s0: SemVer, s1: SemVer): boolean;
-/** @deprecated (will be removed after 0.200.0) Use `eq(s0: SemVer, s1: SemVer)` instead.
- *
- * Returns `true` if they're logically equivalent, even if they're not the exact
- * same version object. */
-export function eq(
-  s0: string | SemVer,
-  s1: string | SemVer,
-  options?: { includePrerelease: boolean },
-): boolean;
-export function eq(
-  s0: string | SemVer,
-  s1: string | SemVer,
-  options?: { includePrerelease: boolean },
-): boolean {
-  return compare(s0, s1, options) === 0;
+export function eq(s0: SemVer, s1: SemVer): boolean {
+  return compare(s0, s1) === 0;
 }
