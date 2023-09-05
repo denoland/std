03f5d8ea45bd82a99b7cd297e803ece791ed3a52
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-06 07:35:24 +1000

    refactor: simplify `db:dump` task (#511)
    Related to #471

--- tasks/db_dump.ts
+++ tasks/db_dump.ts
@@ -1,22 +1,14 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-// Description: Prints kv to stdout
-// Usage: deno run -A --unstable tools/dump_kv.ts
 import { kv } from "@/utils/db.ts";
 
 // https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-521460510
 function replacer(_key: unknown, value: unknown) {
-  return typeof value === "bigint" ? value.toString() : value; // return everything else unchanged
-}
-export async function dumpKv() {
-  const iter = kv.list({ prefix: [] });
-  const items = [];
-  for await (const res of iter) {
-    items.push({ [res.key.toString()]: res.value });
-  }
-  console.log(`${JSON.stringify(items, replacer, 2)}`);
+  return typeof value === "bigint" ? value.toString() : value;
 }
 
-if (import.meta.main) {
-  await dumpKv();
-  await kv.close();
-}
+const iter = kv.list({ prefix: [] });
+const items = [];
+for await (const res of iter) items.push({ [res.key.toString()]: res.value });
+console.log(`${JSON.stringify(items, replacer, 2)}`);
+
+kv.close();
