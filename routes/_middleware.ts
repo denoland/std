c452c57cc8a87886165ebfc795390000ba7cf2d2
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-12 11:37:35 +1000

    refactor: remove `redirectToNewOrigin` middleware (#572)
    Other marketing pages now reference the new home of Deno Hunt,
    https://hunt.deno.land/.

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,20 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { redirect } from "@/utils/http.ts";
-import { Status } from "std/http/http_status.ts";
 import { setSessionState } from "@/middleware/session.ts";
 
-async function redirectToNewOrigin(
-  req: Request,
-  ctx: MiddlewareHandlerContext,
-) {
-  const { hostname } = new URL(req.url);
-  return hostname === "saaskit.deno.dev"
-    ? redirect("https://hunt.deno.land", Status.Found)
-    : await ctx.next();
-}
-
 export const handler = [
-  redirectToNewOrigin,
   setSessionState,
 ];
