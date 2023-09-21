a461ce1d84677fde30d1aac53530a7e6b793ac68
Tiger Oakes <contact@tigeroakes.com>
2023-09-21 03:18:57 -0700

    fix(collections): accept readonly arrays in aggregateGroups, reduceGroups, zip (#3662)

--- collections/zip.ts
+++ collections/zip.ts
@@ -5,6 +5,7 @@
  * Builds N-tuples of elements from the given N arrays with matching indices,
  * stopping when the smallest array's end is reached.
  *
+ * @template T the type of the tuples produced by this function.
  * @example
  * ```ts
  * import { zip } from "https://deno.land/std@$STD_VERSION/collections/zip.ts";
@@ -29,7 +30,7 @@
 import { minOf } from "./min_of.ts";
 
 export function zip<T extends unknown[]>(
-  ...arrays: { [K in keyof T]: T[K][] }
+  ...arrays: { [K in keyof T]: ReadonlyArray<T[K]> }
 ): T[] {
   const minLength = minOf(arrays, (it) => it.length) ?? 0;
 
