5e7d99dd83baa493ee63a97abb4c11f1da4c5e81
Yoshiya Hinosawa <stibium121@gmail.com>
2023-10-11 16:21:06 +0900

    BREAKING(front_matter): deprecate language-specific `test` functions (#3654)

--- front_matter/json.ts
+++ front_matter/json.ts
@@ -5,6 +5,7 @@ import { test as _test } from "./test.ts";
 
 export { Format } from "./_formats.ts";
 
+/* @deprecated (will be removed after 0.210.0) Import test from test.ts and use `test(str, ["json"])`. */
 export function test(str: string): boolean {
   return _test(str, ["json"]);
 }
