d3814b208458526f6ebcb0c5c4b1b90c02ad4f8d
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-10-12 12:08:16 -0700

    chore: restore docs for method.ts

--- http/method.ts
+++ http/method.ts
@@ -13,5 +13,23 @@
 
 /**
  * @deprecated (will be removed after 0.210.0) Import from `std/http/unstable_method.ts` instead.
+ *
+ * A constant array of common HTTP methods.
+ *
+ * This list is compatible with Node.js `http` module.
  */
-export * from "./unstable_method.ts";
+export { HTTP_METHODS } from "./unstable_method.ts";
+
+/**
+ * @deprecated (will be removed after 0.210.0) Import from `std/http/unstable_method.ts` instead.
+ *
+ * A type representing string literals of each of the common HTTP method.
+ */
+export type { HttpMethod } from "./unstable_method.ts";
+
+/**
+ * @deprecated (will be removed after 0.210.0) Import from `std/http/unstable_method.ts` instead.
+ *
+ * A type guard that determines if a value is a valid HTTP method.
+ */
+export { isHttpMethod } from "./unstable_method.ts";
