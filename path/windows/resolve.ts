dc339f16aa61f1f1de4a1abe3469f1c29c453bc8
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-24 05:53:56 +0000

    fix: try not modifiying windows resolve

--- path/windows/resolve.ts
+++ path/windows/resolve.ts
@@ -32,14 +32,7 @@ export function resolve(...pathSegments: string[]): string {
       ) {
         throw new TypeError("Resolved a relative path without a CWD.");
       }
-      try {
-        path = Deno.cwd();
-      } catch (error) {
-        if (!(error instanceof Deno.errors.PermissionDenied)) {
-          throw error;
-        }
-        path = undefined as unknown as string;
-      }
+      path = Deno.cwd();
 
       // Verify that a cwd was found and that it actually points
       // to our drive. If not, default to the drive's root.
