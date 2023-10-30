7b836af9cb1d35752ff24d039e6cf0cbe2609d89
Asher Gomez <ashersaupingomez@gmail.com>
2023-10-31 09:08:25 +1100

    feat: `ServerSentEventMessageStream()`

---
+++ http/server_sent_event_message_stream.ts
@@ -0,0 +1,82 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#fields}
+ */
+export interface ServerSentEventMessage {
+  /** Ignored by the client. */
+  comment?: string;
+  /** A string identifying the type of event described. */
+  event?: string;
+  /** The data field for the message. Split by new lines. */
+  data?: string;
+  /** The event ID to set the {@linkcode EventSource} object's last event ID value. */
+  id?: string | number;
+  /** The reconnection time. */
+  retry?: number;
+}
+
+/**
+ * Converts a server-sent message object into a string for the client.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format}
+ *
+ * @example
+ * ```ts
+ * import { stringify } from "https://deno.land/std@$STD_VERSION/http/server_sent_event_message_stream.ts";
+ *
+ * stringify({
+ *   comment: "crunchy on the outside",
+ *   event: "soft on the inside",
+ *   data: "milk chocolate chips\npeanut-butter chips",
+ *   id: "123",
+ *   retry: 456,
+ * });
+ * ```
+ */
+export function stringify(message: ServerSentEventMessage) {
+  const lines = [];
+  if (message.comment) lines.push(`:${message.comment}`);
+  if (message.event) lines.push(`event:${message.event}`);
+  if (message.data) {
+    message.data.split(/\r?\n/).forEach((line) => lines.push(`data:${line}`));
+  }
+  if (message.id) lines.push(`id:${message.id}`);
+  if (message.retry) lines.push(`retry:${message.retry}`);
+  return lines.join("\n") + "\n\n";
+}
+
+/**
+ * Transforms server-sent message objects into strings for the client.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events}
+ *
+ * @example
+ * ```ts
+ * import {
+ *   type ServerSentEventMessage,
+ *   ServerSentEventMessageStream,
+ * } from "https://deno.land/std@$STD_VERSION/http/server_sent_event_message_stream.ts";
+ *
+ * const stream = ReadableStream.from<ServerSentEventMessage>([
+ *   { data: "hello there" }
+ * ]).pipeThrough(new ServerSentEventMessageStream())
+ *   .pipeThrough(new TextEncoderStream());
+ * new Response(stream, {
+ *   headers: {
+ *     "content-type": "text/event-stream",
+ *     "cache-control": "no-cache",
+ *   },
+ * });
+ * ```
+ */
+export class ServerSentEventMessageStream
+  extends TransformStream<ServerSentEventMessage, string> {
+  constructor() {
+    super({
+      transform: (message, controller) => {
+        controller.enqueue(stringify(message));
+      },
+    });
+  }
+}
---
+++ http/server_sent_event_message_stream_test.ts
@@ -0,0 +1,37 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+import { assertEquals } from "../assert/assert_equals.ts";
+import {
+  type ServerSentEventMessage,
+  ServerSentEventMessageStream,
+  stringify,
+} from "./server_sent_event_message_stream.ts";
+
+Deno.test("stringify() stringifies a server-sent event object", () => {
+  assertEquals(
+    stringify({
+      comment: "a",
+      event: "b",
+      data: "c\nd",
+      id: "123",
+      retry: 456,
+    }),
+    `:a\nevent:b\ndata:c\ndata:d\nid:123\nretry:456\n\n`,
+  );
+});
+
+Deno.test("ServerSentEventMessageStream() enqueues a stringified server-sent event message object", async () => {
+  const stream = ReadableStream.from<ServerSentEventMessage>([{
+    comment: "a",
+    event: "b",
+    data: "c\nd",
+    id: "123",
+    retry: 456,
+  }]).pipeThrough(new ServerSentEventMessageStream());
+  let chunks = "";
+  for await (const chunk of stream) chunks += chunk;
+
+  assertEquals(
+    chunks,
+    `:a\nevent:b\ndata:c\ndata:d\nid:123\nretry:456\n\n`,
+  );
+});
