e9301c4dec30f50e51c574d7b2f59f6875d0c309
Jeff Hykin <jeff.hykin@gmail.com>
2023-10-31 11:06:29 -0500

    remove didYouMean, expose other tools

--- text/mod.ts
+++ text/mod.ts
@@ -1,4 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 export { levenshteinDistance } from "./levenshtein_distance.ts";
-export { didYouMean, DidYouMeanError } from "./did_you_mean.ts";
+export { closestString } from "./closest_string.ts";
+export { compareSimilarity } from "./compare_similarity.ts";
+export { wordSimilaritySort } from "./word_similarity_sort.ts";
