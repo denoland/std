fdb93985160904e090d0c88c693215a494736d93
Jeff Hykin <jeff.hykin@gmail.com>
2023-10-31 11:11:03 -0500

    fix duplication

--- text/word_similarity_sort.ts
+++ text/word_similarity_sort.ts
@@ -16,18 +16,6 @@ import { compareSimilarity } from "./compare_similarity.ts";
  * wordSimilaritySort("hep", possibleWords, { caseSensitive: true });
  * ```
  * 
- * ```ts
- * import { wordSimilaritySort } from "https://deno.land/std@$STD_VERSION/text/word_similarity_sort.ts";
- *
- * const possibleWords = ["length", "size", "blah", "help"];
- *
- * // case-insensitive by default
- * const suggestions = wordSimilaritySort("hep", possibleWords).join(", ");
- *
- * // force case sensitive
- * wordSimilaritySort("hep", possibleWords, { caseSensitive: true });
- * ```
- *
  * @param givenWord - The string to measure distance against
  * @param possibleWords - The string-array that will be sorted
  * @param options.caseSensitive - Flag indicating whether the distance should include case. Default is false.
