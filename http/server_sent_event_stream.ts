5ffa9e8312dcda2c6fc51f17f7e8d47d56d8d8a1
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-03 15:07:37 +1100

    refactor: more thorough testing and checks

--- http/server_sent_event_stream.ts
+++ http/server_sent_event_stream.ts
@@ -1,6 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
+const NEWLINE_REGEXP = /\r\n|\r|\n/;
+
 /**
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#fields}
  */
@@ -17,6 +19,12 @@ export interface ServerSentEventMessage {
   retry?: number;
 }
 
+function assertHasNoNewline(value: string, varName: string) {
+  if (value.match(NEWLINE_REGEXP) !== null) {
+    throw new RangeError(`${varName} cannot contain a newline`);
+  }
+}
+
 /**
  * Converts a server-sent message object into a string for the client.
  *
@@ -24,14 +32,23 @@ export interface ServerSentEventMessage {
  */
 function stringify(message: ServerSentEventMessage) {
   const lines = [];
-  if (message.comment) lines.push(`:${message.comment}`);
-  if (message.event) lines.push(`event:${message.event}`);
+  if (message.comment) {
+    assertHasNoNewline(message.comment, "`message.comment`");
+    lines.push(`:${message.comment}`);
+  }
+  if (message.event) {
+    assertHasNoNewline(message.event, "`message.event`");
+    lines.push(`event:${message.event}`);
+  }
   if (message.data) {
-    message.data.split(/\r\n|\r|\n/).forEach((line) =>
+    message.data.split(NEWLINE_REGEXP).forEach((line) =>
       lines.push(`data:${line}`)
     );
   }
-  if (message.id) lines.push(`id:${message.id}`);
+  if (message.id) {
+    assertHasNoNewline(message.id.toString(), "`message.id`");
+    lines.push(`id:${message.id}`);
+  }
   if (message.retry) lines.push(`retry:${message.retry}`);
   return lines.join("\n") + "\n\n";
 }
--- http/server_sent_event_stream_test.ts
+++ http/server_sent_event_stream_test.ts
@@ -1,23 +1,117 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assertEquals } from "../assert/assert_equals.ts";
+import { assertEquals, assertRejects } from "../assert/mod.ts";
 import {
   type ServerSentEventMessage,
   ServerSentEventStream,
 } from "./server_sent_event_stream.ts";
 
+function createStream(
+  messages: ServerSentEventMessage[],
+): ReadableStream<string> {
+  return ReadableStream
+    .from<ServerSentEventMessage>(messages)
+    .pipeThrough(new ServerSentEventStream());
+}
+
 Deno.test("ServerSentEventStream() enqueues a stringified server-sent event message object", async () => {
-  const stream = ReadableStream.from<ServerSentEventMessage>([{
-    comment: "a",
-    event: "b",
-    data: "c\nd\re\r\nf",
-    id: "123",
-    retry: 456,
-  }]).pipeThrough(new ServerSentEventStream());
-  let chunks = "";
-  for await (const chunk of stream) chunks += chunk;
+  const stream = createStream([
+    {
+      comment: "a",
+      event: "b",
+      data: "c\nd\re\r\nf",
+      id: "123",
+      retry: 456,
+    },
+    {
+      comment: "a",
+    },
+    {
+      event: "b",
+    },
+    {
+      data: "c\nd\re\r\nf",
+    },
+    {
+      id: "123",
+    },
+    {
+      id: 123,
+    },
+    {
+      retry: 456,
+    },
+  ]);
+  const clientMessages = await Array.fromAsync(stream);
 
   assertEquals(
-    chunks,
-    `:a\nevent:b\ndata:c\ndata:d\ndata:e\ndata:f\nid:123\nretry:456\n\n`,
+    clientMessages,
+    [
+      ":a\nevent:b\ndata:c\ndata:d\ndata:e\ndata:f\nid:123\nretry:456\n\n",
+      ":a\n\n",
+      "event:b\n\n",
+      "data:c\ndata:d\ndata:e\ndata:f\n\n",
+      "id:123\n\n",
+      "id:123\n\n",
+      "retry:456\n\n",
+    ],
+  );
+});
+
+Deno.test("ServerSentEventStream() throws if single-line fields contain a newline", async () => {
+  // Comment
+  await assertRejects(
+    async () => await createStream([{ comment: "a\n" }]).getReader().read(),
+    RangeError,
+    "`message.comment` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ comment: "a\r" }]).getReader().read(),
+    RangeError,
+    "`message.comment` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ comment: "a\n\r" }]).getReader().read(),
+    RangeError,
+    "`message.comment` cannot contain a newline",
+  );
+
+  // Event
+  await assertRejects(
+    async () => await createStream([{ event: "a\n" }]).getReader().read(),
+    RangeError,
+    "`message.event` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ event: "a\r" }]).getReader().read(),
+    RangeError,
+    "`message.event` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ event: "a\n\r" }]).getReader().read(),
+    RangeError,
+    "`message.event` cannot contain a newline",
+  );
+
+  // ID
+  await assertRejects(
+    async () => await createStream([{ id: "a\n" }]).getReader().read(),
+    RangeError,
+    "`message.id` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ id: "a\r" }]).getReader().read(),
+    RangeError,
+    "`message.id` cannot contain a newline",
+  );
+
+  await assertRejects(
+    async () => await createStream([{ id: "a\n\r" }]).getReader().read(),
+    RangeError,
+    "`message.id` cannot contain a newline",
   );
 });
