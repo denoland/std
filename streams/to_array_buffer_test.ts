bd3eae8c2e50a0bb215cd3359113ecedff1055fd
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-10 14:40:31 +1100

    chore: use `ReadableStream.from()` where possible

--- streams/to_array_buffer_test.ts
+++ streams/to_array_buffer_test.ts
@@ -4,15 +4,12 @@ import { assertEquals } from "../assert/assert_equals.ts";
 import { toArrayBuffer } from "./to_array_buffer.ts";
 
 Deno.test("[streams] toArrayBuffer", async () => {
-  const stream = new ReadableStream<Uint8Array>({
-    start(controller) {
-      controller.enqueue(Uint8Array.of(1, 2, 3, 4, 5));
-      controller.enqueue(Uint8Array.of(6, 7));
-      controller.enqueue(Uint8Array.of(8, 9));
-      controller.close();
-    },
-  });
+  const stream = ReadableStream.from([
+    new Uint8Array([1, 2, 3, 4, 5]),
+    new Uint8Array([6, 7]),
+    new Uint8Array([8, 9]),
+  ]);
 
   const buf = await toArrayBuffer(stream);
-  assertEquals(buf, Uint8Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9).buffer);
+  assertEquals(buf, new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]).buffer);
 });
