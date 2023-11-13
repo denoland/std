eee291e4eb5e36e5a1b3d5cc2eee47c6fa3e895e
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-14 10:42:59 +1100

    fix

--- fs/eol.ts
+++ fs/eol.ts
@@ -1,5 +1,4 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-// This module is browser compatible.
 
 /** End-of-line character for POSIX platforms like Linux and macOS. */
 export const LF = "\n" as const;
