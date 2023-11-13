9c6537f14661cb73875a6251769fa9587de4a49f
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-13 16:28:05 +1100

    cleanup

--- bytes/concat.ts
+++ bytes/concat.ts
@@ -30,21 +30,6 @@ export function concat(buf: Uint8Array[]): Uint8Array;
  * ```
  */
 export function concat(...buf: Uint8Array[]): Uint8Array;
-/**
- * @deprecated (will be removed in 0.209.0) Pass in an array instead of a
- * spread of arguments.
- *
- * Concatenate an array of {@linkcode Uint8Array}s.
- *
- * @example
- * ```ts
- * import { concat } from "https://deno.land/std@$STD_VERSION/bytes/concat.ts";
- *
- * const a = new Uint8Array([0, 1, 2]);
- * const b = new Uint8Array([3, 4, 5]);
- * concat(a, b); // Uint8Array(6) [ 0, 1, 2, 3, 4, 5 ]
- * ```
- */
 export function concat(...buf: (Uint8Array | Uint8Array[])[]): Uint8Array {
   /**
    * @todo(iuioiua): Revert to the old implementation upon removal of the
