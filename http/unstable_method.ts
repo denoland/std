207f5cad67150e2bf0b8a169e33f7130b4ebb9c3
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-22 08:50:14 +1100

    BREAKING: deprecate `std/http/method`

--- http/unstable_method.ts
+++ http/unstable_method.ts
@@ -2,6 +2,8 @@
 // This module is browser compatible.
 
 /**
+ * @deprecated (will be removed in 0.210.0)
+ *
  * Contains the constant {@linkcode HTTP_METHODS} and the type
  * {@linkcode HttpMethod} and the type guard {@linkcode isHttpMethod} for
  * working with HTTP methods with type safety.
@@ -9,7 +11,10 @@
  * @module
  */
 
-/** A constant array of common HTTP methods.
+/**
+ * @deprecated (will be removed in 0.210.0)
+ *
+ * A constant array of common HTTP methods.
  *
  * This list is compatible with Node.js `http` module.
  */
@@ -50,10 +55,18 @@ export const HTTP_METHODS = [
   "UNSUBSCRIBE",
 ] as const;
 
-/** A type representing string literals of each of the common HTTP method. */
+/**
+ * @deprecated (will be removed in 0.210.0)
+ *
+ * A type representing string literals of each of the common HTTP method.
+ */
 export type HttpMethod = typeof HTTP_METHODS[number];
 
-/** A type guard that determines if a value is a valid HTTP method. */
+/**
+ * @deprecated (will be removed in 0.210.0)
+ *
+ * A type guard that determines if a value is a valid HTTP method.
+ */
 export function isHttpMethod(value: unknown): value is HttpMethod {
   return HTTP_METHODS.includes(value as HttpMethod);
 }
