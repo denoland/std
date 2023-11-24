f99378e2f55ffb6f1212d80fb4213641f7658bd7
realpha <0xrealpha@proton.me>
2023-11-24 11:11:46 +0100

    chore(log): retain bimap for const object and add deprecation notices

--- log/levels.ts
+++ log/levels.ts
@@ -12,16 +12,30 @@ export const LogLevels = {
   WARNING: 30,
   ERROR: 40,
   CRITICAL: 50,
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  0: "NOTSET",
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  10: "DEBUG",
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  20: "INFO",
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  30: "WARNING",
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  40: "ERROR",
+  /* @deprecated (will be removed after 0.211.0) Use {@linkcode getLevelName} instead */
+  50: "CRITICAL",
 } as const;
 
 /** Union of valid log levels */
 export type LogLevel = typeof LogLevels[LevelName];
 
 /** Union of valid log level names */
-export type LevelName = keyof typeof LogLevels;
+export type LevelName = Exclude<keyof typeof LogLevels, number>;
 
 /** Permitted log level names */
-export const LogLevelNames = Object.keys(LogLevels) as LevelName[];
+export const LogLevelNames = Object.keys(LogLevels).filter((key) =>
+  isNaN(Number(key))
+) as LevelName[];
 
 const byLevel: Record<LogLevel, LevelName> = {
   [LogLevels.NOTSET]: "NOTSET",
@@ -35,18 +49,26 @@ const byLevel: Record<LogLevel, LevelName> = {
 /**
  * Returns the numeric log level associated with the passed,
  * stringy log level name.
+ *
+ * @param {LevelName} name
+ * @returns {number} - Deprecated (will return {@linkcode LogLevel} after 0.211.0)
  */
-export function getLevelByName(name: LevelName): LogLevel {
+export function getLevelByName(name: LevelName): number {
   const level = LogLevels[name];
   if (level !== undefined) {
     return level;
   }
-  throw new Error(`no log level found for "${name}"`);
+  throw new Error(`no log level found for name: ${name}`);
 }
 
-/** Returns the stringy log level name provided the numeric log level */
-export function getLevelName(level: LogLevel): LevelName {
-  const levelName = byLevel[level];
+/**
+ * Returns the stringy log level name provided the numeric log level
+ *
+ * @param {number} level - Deprecated (will accept {@linkcode LogLevel} after 0.211.0)
+ * @returns {LevelName}
+ */
+export function getLevelName(level: number): LevelName {
+  const levelName = byLevel[level as LogLevel];
   if (levelName) {
     return levelName;
   }
