6648b53031633db5f26113cc2e1ba7ae0cf4e8f3
Asher Gomez <ashersaupingomez@gmail.com>
2023-11-24 19:15:58 +1100

    BREAKIING(fs): remove `EOL` enum, add OS-dependent `EOL` (#3850)

--- fs/eol_test.ts
+++ fs/eol_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { assertEquals } from "../assert/mod.ts";
-import { detect, EOL, format } from "./eol.ts";
+import { CRLF, detect, format, LF } from "./eol.ts";
 
 const CRLFinput = "deno\r\nis not\r\nnode";
 const Mixedinput = "deno\nis not\r\nnode";
@@ -11,14 +11,14 @@ const NoNLinput = "deno is not node";
 Deno.test({
   name: "detect() detects CRLF as the end-of-line character",
   fn() {
-    assertEquals(detect(CRLFinput), EOL.CRLF);
+    assertEquals(detect(CRLFinput), CRLF);
   },
 });
 
 Deno.test({
   name: "detect() detects LF as the end-of-line character",
   fn() {
-    assertEquals(detect(LFinput), EOL.LF);
+    assertEquals(detect(LFinput), LF);
   },
 });
 
@@ -33,23 +33,23 @@ Deno.test({
   name:
     "detect() detects the most common end-of-line character in a mixed string",
   fn() {
-    assertEquals(detect(Mixedinput), EOL.CRLF);
-    assertEquals(detect(Mixedinput2), EOL.CRLF);
+    assertEquals(detect(Mixedinput), CRLF);
+    assertEquals(detect(Mixedinput2), CRLF);
   },
 });
 
 Deno.test({
   name: "format() converts the end-of-line character to the specified one",
   fn() {
-    assertEquals(format(CRLFinput, EOL.LF), LFinput);
-    assertEquals(format(LFinput, EOL.LF), LFinput);
-    assertEquals(format(LFinput, EOL.CRLF), CRLFinput);
-    assertEquals(format(CRLFinput, EOL.CRLF), CRLFinput);
-    assertEquals(format(CRLFinput, EOL.CRLF), CRLFinput);
-    assertEquals(format(NoNLinput, EOL.CRLF), NoNLinput);
-    assertEquals(format(Mixedinput, EOL.CRLF), CRLFinput);
-    assertEquals(format(Mixedinput, EOL.LF), LFinput);
-    assertEquals(format(Mixedinput2, EOL.CRLF), CRLFinput);
-    assertEquals(format(Mixedinput2, EOL.LF), LFinput);
+    assertEquals(format(CRLFinput, LF), LFinput);
+    assertEquals(format(LFinput, LF), LFinput);
+    assertEquals(format(LFinput, CRLF), CRLFinput);
+    assertEquals(format(CRLFinput, CRLF), CRLFinput);
+    assertEquals(format(CRLFinput, CRLF), CRLFinput);
+    assertEquals(format(NoNLinput, CRLF), NoNLinput);
+    assertEquals(format(Mixedinput, CRLF), CRLFinput);
+    assertEquals(format(Mixedinput, LF), LFinput);
+    assertEquals(format(Mixedinput2, CRLF), CRLFinput);
+    assertEquals(format(Mixedinput2, LF), LFinput);
   },
 });
