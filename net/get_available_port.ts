b44abccfef4db9d904f49f8fe3adf04b2ec50dbb
Asher Gomez <ashersaupingomez@gmail.com>
2023-12-04 09:51:48 +1100

    tweaks

--- net/get_available_port.ts
+++ net/get_available_port.ts
@@ -1,9 +1,18 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-/** Returns an available port */
+/**
+ * Returns an available network port.
+ *
+ * @example
+ * ```ts
+ * import { getAvailablePort } from "https://deno.land/std@$STD_VERSION/net/get_available_port.ts";
+ *
+ * const port = getAvailablePort();
+ * Deno.serve({ port }, () => new Response("Hello, world!"));
+ * ```
+ */
 export function getAvailablePort(): number {
-  const tmp = Deno.listen({ port: 0 });
-  const { port } = tmp.addr as Deno.NetAddr;
-  tmp.close();
-  return port;
+  const listener = Deno.listen({ port: 0 });
+  listener.close();
+  return (listener.addr as Deno.NetAddr).port;
 }
