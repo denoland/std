896b61ecb4a548b65d771fb668f7109497ce0f71
Yoshiya Hinosawa <stibium121@gmail.com>
2023-12-03 20:54:35 +0900

    fix lint errors

--- net/mod.ts
+++ net/mod.ts
@@ -1,1 +1,3 @@
-export * from "./port.ts"
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+export * from "./port.ts";
--- net/port.ts
+++ net/port.ts
@@ -1,7 +1,9 @@
-/** @returns an available port */
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+/** Returns an available port */
 export function getAvailablePort(): number {
-  const tmp = Deno.listen({ port: 0 })
-  const { port } = tmp.addr as Deno.NetAddr
-  tmp.close()
-  return port
+  const tmp = Deno.listen({ port: 0 });
+  const { port } = tmp.addr as Deno.NetAddr;
+  tmp.close();
+  return port;
 }
