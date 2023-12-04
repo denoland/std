311e1033e5763e057e565217de0f4405f4ced6d4
Asher Gomez <ashersaupingomez@gmail.com>
2023-12-04 22:18:17 +1100

    Revert "Revert "refactor: use `Array.fromAsync()`"" (#645)

--- utils/posts.ts
+++ utils/posts.ts
@@ -68,13 +68,11 @@ export async function getPost(slug: string): Promise<Post | null> {
  * ```
  */
 export async function getPosts(): Promise<Post[]> {
-  const files = Deno.readDir("./posts");
-  const promises = [];
-  for await (const file of files) {
-    const slug = file.name.replace(".md", "");
-    promises.push(getPost(slug));
-  }
-  const posts = await Promise.all(promises) as Post[];
-  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
-  return posts;
+  const posts = await Array.fromAsync(
+    Deno.readDir("./posts"),
+    async (file) => await getPost(file.name.replace(".md", "")),
+  ) as Post[];
+  return posts.toSorted((a, b) =>
+    b.publishedAt.getTime() - a.publishedAt.getTime()
+  );
 }
