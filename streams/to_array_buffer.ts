49b3dee344864e6562e31ffc10edbcc956b59ac8
Asher Gomez <ashersaupingomez@gmail.com>
2023-12-12 15:02:10 +1100

    refactor(streams): use `Response.arrayBuffer()` in `toArrayBuffer()`

--- streams/to_array_buffer.ts
+++ streams/to_array_buffer.ts
@@ -1,11 +1,9 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import { concat } from "../bytes/concat.ts";
-
 /**
  * Converts a {@linkcode ReadableStream} of {@linkcode Uint8Array}s to an
- * {@linkcode ArrayBuffer}. Works the same as{@linkcode Response.arrayBuffer}.
+ * {@linkcode ArrayBuffer}. Works the same as {@linkcode Response.arrayBuffer}.
  *
  * @example
  * ```ts
@@ -19,20 +17,7 @@ import { concat } from "../bytes/concat.ts";
  * ```
  */
 export async function toArrayBuffer(
-  readableStream: ReadableStream<Uint8Array>,
+  stream: ReadableStream<Uint8Array>,
 ): Promise<ArrayBuffer> {
-  const reader = readableStream.getReader();
-  const chunks: Uint8Array[] = [];
-
-  while (true) {
-    const { done, value } = await reader.read();
-
-    if (done) {
-      break;
-    }
-
-    chunks.push(value);
-  }
-
-  return concat(chunks).buffer;
+  return await new Response(stream).arrayBuffer();
 }
