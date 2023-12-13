b81ded950bc55028e7cd6620c9e0479e43d5dd07
Tim <tim.reichen@gmail.com>
2023-12-13 10:18:25 +0100

    add export function canParse(version: string): boolean;

--- semver/can_parse.ts
+++ semver/can_parse.ts
@@ -6,6 +6,7 @@ import { parse } from "./parse.ts";
  * @deprecated (will be removed in 0.211.0) use string as an argument
  */
 export function canParse(version: SemVer): boolean;
+export function canParse(version: string): boolean;
 export function canParse(version: string | SemVer) {
   try {
     parse(version as SemVer);
