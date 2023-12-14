0486fbe1c76d439a64c75d0280f6cdac37a8e048
Tim Reichen <timreichen@users.noreply.github.com>
2023-12-14 22:15:00 +0100

    Update semver/comparator_format.ts
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- semver/comparator_format.ts
+++ semver/comparator_format.ts
@@ -7,7 +7,7 @@ import { stringifyComparator } from "./stringify_comparator.ts";
  * @example >=0.0.0
  * @param comparator
  * @returns A string representation of the comparator
- * @deprecated (will be removed in 0.212.0) use {@linkcode stringifyComparator} instead.
+ * @deprecated (will be removed in 0.212.0) Use {@linkcode stringifyComparator} instead.
  */
 export function comparatorFormat(comparator: SemVerComparator) {
   return stringifyComparator(comparator);
