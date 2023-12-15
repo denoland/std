96649cf4a258a30cf97df850dc975f2c1dd33e7a
Simon Holloway <simon@syhol.io>
2023-12-15 21:08:19 +0000

    refactor(log): test outputted json strings

--- log/formatters_test.ts
+++ log/formatters_test.ts
@@ -1,9 +1,10 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { assertEquals } from "../assert/mod.ts";
+import { FakeTime } from "../testing/time.ts";
 import { jsonFormatter } from "./formatters.ts";
 import { LogRecord } from "./logger.ts";
 
-const makeRecord = (msg: string, args: unknown[] = []) =>
+const log = (msg: string, args: unknown[] = []) =>
   new LogRecord({
     msg,
     args,
@@ -12,34 +13,28 @@ const makeRecord = (msg: string, args: unknown[] = []) =>
   });
 
 Deno.test("jsonFormatter with just a message", function () {
-  const result = JSON.parse(jsonFormatter(makeRecord("User service exploded")));
+  using _time = new FakeTime(1);
 
-  assertEquals(result.level, "INFO");
-  assertEquals(typeof result.datetime, "number");
-  assertEquals(result.message, "User service exploded");
-  assertEquals(Object.keys(result), ["level", "datetime", "message"]);
+  assertEquals(
+    jsonFormatter(log("msg")),
+    `{"level":"INFO","datetime":1,"message":"msg"}`,
+  );
 });
 
 Deno.test("jsonFormatter with one argument", function () {
-  const result = JSON.parse(
-    jsonFormatter(makeRecord("User service exploded", [{ user: "Dave" }])),
-  );
+  using _time = new FakeTime(1);
 
-  assertEquals(result.level, "INFO");
-  assertEquals(typeof result.datetime, "number");
-  assertEquals(result.message, "User service exploded");
-  assertEquals(result.args, { user: "Dave" });
-  assertEquals(Object.keys(result), ["level", "datetime", "message", "args"]);
+  assertEquals(
+    jsonFormatter(log("msg", [{ user: "Dave" }])),
+    `{"level":"INFO","datetime":1,"message":"msg","args":{"user":"Dave"}}`,
+  );
 });
 
 Deno.test("jsonFormatter with many arguments", function () {
-  const result = JSON.parse(
-    jsonFormatter(makeRecord("User service exploded", [1, true, 3, [], {}])),
-  );
+  using _time = new FakeTime(1);
 
-  assertEquals(result.level, "INFO");
-  assertEquals(typeof result.datetime, "number");
-  assertEquals(result.message, "User service exploded");
-  assertEquals(result.args, [1, true, 3, [], {}]);
-  assertEquals(Object.keys(result), ["level", "datetime", "message", "args"]);
+  assertEquals(
+    jsonFormatter(log("msg", [1, true, null, [], {}])),
+    `{"level":"INFO","datetime":1,"message":"msg","args":[1,true,null,[],{}]}`,
+  );
 });
