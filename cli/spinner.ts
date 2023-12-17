dff1a8a59a3c35689cae0f3d84188efcd887b06b
darkdarcool <66882633+darkdarcool@users.noreply.github.com>
2023-12-17 17:51:45 -0500

    feat(cli/spinner.ts): Added iuioiua's suggestions, and got rid of the createSpinner function

--- cli/spinner.ts
+++ cli/spinner.ts
@@ -5,66 +5,78 @@ const encoder = new TextEncoder();
 const LINE_CLEAR = encoder.encode("\r\u001b[K"); // From cli/prompt_secret.ts
 const COLOR_RESET = "\u001b[0m";
 const DEFAULT_SPEED = 75;
+const DEFAULT_SPINNER = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
-/** The options for the spinner */
+/** Options for {@linkcode Spinner}. */
 export interface SpinnerOptions {
-  /** The spinner to be animated */
-  spinner: string[];
-  /** The message to display next to the spinner */
+  /**
+   * The sequence of characters to be iterated through for animation.
+   *
+   * @default {["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]}
+   */
+  spinner?: string[];
+  /** The message to display next to the spinner. */
   message?: string;
-  /** The speed of the spinner. Defaults to 75 */
+  /** The speed of the spinner.
+   *
+   * @default {75}
+   */
   speed?: number;
-  /** The color of the spinner. Defaults to the default terminal color */
+  /** The color of the spinner. Defaults to the default terminal color. */
   color?: string;
-  /** Whether to cleanup the output if the process is terminated. Defaults to true */
+  /**
+   * Whether to cleanup the output if the process is terminated.
+   *
+   * @default {true}
+   */
   cleanup?: boolean;
-}
-
-/** The default spinners */
-export const spinners = {
-  lines: ['-', '\\', '|', '/'],
-  binary: ['0000', '1000', '1100', '1110', '1111', '0111', '0011', '0001'],
-  dots: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'],
-  dots2: ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'],
-  blink: ['◜', '◠', '◝', '◞', '◡', '◟'],
 }
 
 /**
  * A spinner that can be used to indicate that something is loading.
  */
 export class Spinner {
-  #message: string | undefined;
-  #speed: number;
-  #spinner: string[];
-  #color: string | undefined;
-  #cleanup: boolean;
-  
+  #options: SpinnerOptions;
+
   #intervalId: number | undefined;
+  #active = false;
 
   /**
    * Creates a new spinner.
-   * @param message - The message to display next to the spinner.
-   * @param speed - The speed of the spinner.
-   * @param spinner - The spinner to use.
-   * @param color - The color of the spinner.
-   * @param cleanup - Whether to cleanup the output if the process is terminated.
+   * @param options - The options for the spinner.
+   *
+   * @example
+   * ```ts
+   * const spinner = new Spinner({ message: "Loading..." });
+   * spinner.start();
+   * ```
    */
-  constructor(message: string | undefined, speed: number, spinner: string[], color: string | undefined, cleanup: boolean) {
-    this.#message = message;
-    this.#speed = speed;
-    this.#spinner = spinner;
-    this.#color = color;
-    this.#cleanup = cleanup;
+  constructor(options?: SpinnerOptions) {
+    this.#options = options ?? {
+      message: undefined,
+      speed: DEFAULT_SPEED,
+      spinner: DEFAULT_SPINNER,
+      color: undefined,
+      cleanup: true,
+    };
   }
 
   /**
    * Starts the spinner.
    */
   start() {
+    // Check if the spinner is already active.
+    if (this.#active) return;
+    this.#active = true;
+
     let i = 0;
-    const message = this.#message ?? '';
-    
-    const color = this.#color ?? '';
+
+    const message = this.#options.message ?? "";
+    const spinner = this.#options.spinner ?? DEFAULT_SPINNER;
+    const speed = this.#options.speed ?? DEFAULT_SPEED;
+    const cleanup = this.#options.cleanup ?? true;
+
+    const color = this.#options.color ?? "";
 
     if (Deno.stdout.writable.locked) return;
 
@@ -73,91 +85,31 @@ export class Spinner {
       Deno.stdout.writeSync(LINE_CLEAR);
       let frame;
       // If color was specified, add it to the frame. Otherwise, just use the spinner and message.
-      if (this.#color) frame = encoder.encode(color + this.#spinner[i] + COLOR_RESET + ' ' + message);
-      else frame = encoder.encode(this.#spinner[i] + ' ' + message);
-      Deno.stdout.writeSync(frame);
-      i = (i + 1) % this.#spinner.length;
-    }
+      if (this.#options.color) {
+        frame = encoder.encode(color + spinner + COLOR_RESET + " " + message);
+      } else frame = encoder.encode(spinner[i] + " " + message);
 
-    this.#intervalId = setInterval(updateFrame, this.#speed);
+      Deno.stdout.writeSync(frame);
+      i = (i + 1) % spinner.length;
+    };
+
+    this.#intervalId = setInterval(updateFrame, speed);
 
     // Cleanup if the process is terminated.
-    if (this.#cleanup) Deno.addSignalListener("SIGINT" , this.cleanup.bind(this));
+    if (cleanup) {
+      Deno.addSignalListener("SIGINT", this.stop.bind(this));
+    }
   }
 
   /**
    * Stops the spinner.
    */
   stop() {
-    if (this.#intervalId) {
+    if (this.#intervalId && this.#active) {
       clearInterval(this.#intervalId);
       Deno.stdout.writeSync(LINE_CLEAR); // Clear the current line
+      Deno.removeSignalListener("SIGINT", this.stop.bind(this));
+      this.#active = false;
     }
   }
-  
-  /**
-   * Stops the spinner and cleans up the output.
-   */
-  cleanup() {
-    this.stop();
-    Deno.stdout.writeSync(encoder.encode("\n"));
-  }
-}
-
-/**
- * Creates a spinner with the given options.
- * 
- * Note: The spinner will _not_ start automatically.
- * @param options - The options for the spinner. 
- * @returns The created spinner.
- * 
- * @example 
- * ```ts
- * const mySpinner = createSpinner({
- *   spinner: spinners.lineSpinner,
- *   message: 'Loading...',
- * });
- * 
- * mySpinner.start();
- * ```
- * 
- * You can also add custom spinners:
- * ```ts
- * const mySpinner = createSpinner({
- *  spinner: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
- *  message: 'Loading...',
- *  speed: 20
- * });
- * 
- * mySpinner.start();
- * ```
- */
-export function createSpinner({ message, speed = DEFAULT_SPEED, spinner, color, cleanup = true }: SpinnerOptions): Spinner {
-  return new Spinner(message, speed, spinner, color, cleanup);
-}
-
-/**
- * Creates a spinner with the given options and starts it.
- * 
- * @param promise The async callback function
- * @param spinner The spinner to use
- * @returns The result of the promise
- * 
- * @example
- * ```ts
- * const myPromise = () => new Promise((resolve) => setTimeout(resolve, 5000));
- * 
- * const spinner = createSpinner({
- *  spinner: spinners.dots,
- *  message: 'Loading...',
- * });
- * 
- * spinnerPromise(myPromise, spinner).then(() => console.log('Done!'));
- * ```
- */
-export async function spinnerPromise<T>(promise: () => Promise<T>, spinner: Spinner): Promise<T> {
-  spinner.start();
-  const result = await promise();
-  spinner.stop();
-  return result;
 }
