8afc35c49cef9268ba5f75a5ef8a2dae3da739b8
Tim <tim.reichen@gmail.com>
2023-12-17 22:11:59 +0100

    remove redundant import

--- semver/is_semver_comparator.ts
+++ semver/is_semver_comparator.ts
@@ -1,5 +1,4 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import type { SemVerComparator } from "./types.ts";
 import { isComparator } from "./is_comparator.ts";
 
 /**
