c155fc9a0a87591d2a3e47459c6c625aca3ac37e
Tim <tim.reichen@gmail.com>
2023-12-18 18:56:00 +0100

    initial commit

--- text/levenshtein_distance_test.ts
+++ text/levenshtein_distance_test.ts
@@ -2,14 +2,14 @@
 import { assertEquals } from "../assert/mod.ts";
 import { levenshteinDistance } from "./mod.ts";
 
-Deno.test("levenshteinDistanceAbove - basic cases", function () {
+Deno.test("levenshteinDistance() checks basic cases", function () {
   assertEquals(
     levenshteinDistance("aa", "bb"),
     2,
   );
 });
 
-Deno.test("levenshteinDistance - same strings", function () {
+Deno.test("levenshteinDistance() checks same strings", function () {
   assertEquals(
     levenshteinDistance("aa", "aa"),
     0,
--- text/word_similarity_sort_test.ts
+++ text/word_similarity_sort_test.ts
@@ -2,7 +2,7 @@
 import { assertEquals } from "../assert/mod.ts";
 import { wordSimilaritySort } from "./word_similarity_sort.ts";
 
-Deno.test("basicWordSimilaritySort", function () {
+Deno.test("wordSimilaritySort() checks basic example", function () {
   const possibleWords: string[] = ["length", "size", "blah", "help"];
   const badArg = "hep";
   assertEquals(
@@ -11,7 +11,7 @@ Deno.test("basicWordSimilaritySort", function () {
   );
 });
 
-Deno.test("emptyStringSimilaritySort", function () {
+Deno.test("wordSimilaritySort() checks empty string", function () {
   const possibleWords: string[] = ["length", "size", "blah", "help", ""];
   const badArg = "";
 
@@ -21,7 +21,7 @@ Deno.test("emptyStringSimilaritySort", function () {
   );
 });
 
-Deno.test("emptyArraySimilaritySort", function () {
+Deno.test("wordSimilaritySort() checks empty array", function () {
   const possibleWords: string[] = [];
   const badArg = "";
 
