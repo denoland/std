4d0e74463dba66ad73518b95f0a6a74e111801f0
babiabeo <ducthangn71007@gmail.com>
2023-12-20 20:50:30 +0700

    chore(uuid): improve test coverage and format test names

--- uuid/v1_test.ts
+++ uuid/v1_test.ts
@@ -1,8 +1,9 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assert, assertEquals } from "../assert/mod.ts";
+import { assert, assertEquals, assertThrows } from "../assert/mod.ts";
 import { generate, validate } from "./v1.ts";
+import { uuidToBytes } from "./_common.ts";
 
-Deno.test("[UUID] is_valid_uuid_v1", () => {
+Deno.test("v1.validate() checks if a string is a valid v1 UUID", () => {
   const u = generate();
   const t = "63655efa-7ee6-11ea-bc55-0242ac130003";
   const n = "63655efa-7ee6-11eg-bc55-0242ac130003";
@@ -12,20 +13,27 @@ Deno.test("[UUID] is_valid_uuid_v1", () => {
   assert(!validate(n), `${n} should not be valid`);
 });
 
-Deno.test("[UUID] test_uuid_v1", () => {
-  const u = generate();
-  assertEquals(typeof u, "string", "returns a string");
-  assert(u !== "", "return string is not empty");
+Deno.test("v1.generate() generates a non-empty string", () => {
+  const u1 = generate();
+  const u2 = generate({
+    msecs: new Date("2011-11-01").getTime(),
+    nsecs: 10000,
+  });
+
+  assertEquals(typeof u1, "string", "returns a string");
+  assert(u1 !== "", "return string is not empty");
+  assertEquals(typeof u2, "string", "returns a string");
+  assert(u2 !== "", "return string is not empty");
 });
 
-Deno.test("[UUID] test_uuid_v1_format", () => {
+Deno.test("v1.generate() generates UUIDs in version 1 format", () => {
   for (let i = 0; i < 10000; i++) {
     const u = generate() as string;
     assert(validate(u), `${u} is not a valid uuid v1`);
   }
 });
 
-Deno.test("[UUID] test_uuid_v1_static", () => {
+Deno.test("v1.generate() can generate a static v1 UUID", () => {
   const v1options = {
     node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
     clockseq: 0x1234,
@@ -34,4 +42,29 @@ Deno.test("[UUID] test_uuid_v1_static", () => {
   };
   const u = generate(v1options);
   assertEquals(u, "710b962e-041c-11e1-9234-0123456789ab");
+});
+
+Deno.test("v1.generate() can fill the UUID into a buffer", () => {
+  const buf: number[] = [];
+  const v1options = {
+    node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
+    clockseq: 0x1234,
+    msecs: new Date("2011-11-01").getTime(),
+    nsecs: 5678,
+  };
+  const uuid = generate(v1options, buf, 0);
+  const expect = uuidToBytes("710b962e-041c-11e1-9234-0123456789ab");
+
+  assertEquals(buf, expect);
+  assertEquals(buf, uuid);
+});
+
+Deno.test("v1.generate() throws when create more than 10M uuids/sec", () => {
+  assertThrows(
+    () => {
+      generate({ nsecs: 10001 });
+    },
+    Error,
+    "Can't create more than 10M uuids/sec",
+  );
 });
--- uuid/v3_test.ts
+++ uuid/v3_test.ts
@@ -4,28 +4,28 @@ import { generate, validate } from "./v3.ts";
 
 const NAMESPACE = "1b671a64-40d5-491e-99b0-da01ff1f3341";
 
-Deno.test("[UUID] test_uuid_v3", async () => {
+Deno.test("v3.generate() generates a non-empty string", async () => {
   const u = await generate(NAMESPACE, new Uint8Array());
   assertEquals(typeof u, "string", "returns a string");
   assert(u !== "", "return string is not empty");
 });
 
-Deno.test("[UUID] test_uuid_v3_format", async () => {
+Deno.test("v3.generate() generates UUIDs in version 3 format", async () => {
   for (let i = 0; i < 10000; i++) {
     const u = await generate(
       NAMESPACE,
       new TextEncoder().encode(i.toString()),
     ) as string;
-    assert(validate(u), `${u} is not a valid uuid v5`);
+    assert(validate(u), `${u} is not a valid uuid v3`);
   }
 });
 
-Deno.test("[UUID] test_uuid_v3_option", async () => {
+Deno.test("v3.generate() generates a name-based UUID using MD5 hash", async () => {
   const u = await generate(NAMESPACE, new TextEncoder().encode("Hello, World"));
   assertEquals(u, "71d7129f-e809-30ed-a2c6-ea9032b43c0d");
 });
 
-Deno.test("[UUID] is_valid_uuid_v3", async () => {
+Deno.test("v3.validate() checks if a string is a valid v3 UUID", async () => {
   const u = await generate(
     "1b671a64-40d5-491e-99b0-da01ff1f3341",
     new TextEncoder().encode("Hello, World"),
