a22ab27cca3b420c44aa2a7166479b88fc2b2292
Asher Gomez <ashersaupingomez@gmail.com>
2023-12-22 07:25:47 +1100

    refactor: remove remaining deprecated `std/front_matter` APIs

--- front_matter/any.ts
+++ front_matter/any.ts
@@ -4,15 +4,8 @@ import { createExtractor, type Extractor, Parser } from "./create_extractor.ts";
 import { parse as parseYAML } from "../yaml/parse.ts";
 import { parse as parseTOML } from "../toml/parse.ts";
 
-export {
-  /** @deprecated (will be removed after 0.210.0) Import from {@link https://deno.land/std/front_matter/test.ts} instead. */
-  test,
-} from "./test.ts";
-
 export const extract: Extractor = createExtractor({
   yaml: parseYAML as Parser,
   toml: parseTOML as Parser,
   json: JSON.parse as Parser,
 });
-/** @deprecated (will be removed after 0.210.0) Import {@linkcode extract} as a named import instead. */
-export default extract;
--- front_matter/yaml.ts
+++ front_matter/yaml.ts
@@ -1,16 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 import { createExtractor, type Extractor, Parser } from "./create_extractor.ts";
-import { test as _test } from "./test.ts";
 import { parse } from "../yaml/parse.ts";
-
-/** @deprecated (will be removed after 0.210.0) Import {@link https://deno.land/std/front_matter/yaml.ts} and use `test(str, ["yaml"])` instead. */
-export function test(str: string): boolean {
-  return _test(str, ["yaml"]);
-}
 
 export const extract: Extractor = createExtractor({
   ["yaml"]: parse as Parser,
 });
-/** @deprecated (will be removed after 0.210.0) Import {@linkcode extract} as a named import instead. */
-export default extract;
