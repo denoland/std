81bcbc1ca15c900d3c20616d1a554075751013de
Tim Reichen <timreichen@users.noreply.github.com>
2023-12-28 11:00:18 +0100

    feat(semver): optional SemVer build and prerelease properties (#4024)
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>

--- semver/types.ts
+++ semver/types.ts
@@ -65,8 +65,8 @@ export interface SemVer {
   major: number;
   minor: number;
   patch: number;
-  prerelease: (string | number)[];
-  build: string[];
+  prerelease?: (string | number)[];
+  build?: string[];
 }
 
 type SemVerRangeAnd = SemVerComparator[];
