d383899804a6d8c3c40ea45433e8138db147db36
Tim <tim.reichen@gmail.com>
2024-01-02 13:54:20 +0100

    initial commit

--- semver/increment.ts
+++ semver/increment.ts
@@ -1,46 +1,39 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+import { parseBuild } from "./_shared.ts";
 import type { ReleaseType, SemVer } from "./types.ts";
 
-function pre(
+function bumpPrereleaseNumber(
   prerelease: ReadonlyArray<string | number> = [],
-  identifier: string | undefined,
 ) {
-  let values = [...prerelease];
+  const values = [...prerelease];
 
-  // In reality this will either be 0, 1 or 2 entries.
-  let i: number = values.length;
-  while (--i >= 0) {
-    if (typeof values[i] === "number") {
-      // deno-fmt-ignore
-      (values[i] as number)++;
-      i = -2;
+  let index = values.length;
+  while (index >= 0) {
+    const value = values[index];
+    if (typeof value === "number") {
+      values[index] = value + 1;
+      break;
     }
+    index -= 1;
   }
+  // if no number was bumped
+  if (index === -1) values.push(0);
 
-  if (i === -1) {
-    // didn't increment anything
-    values.push(0);
-  }
-
-  if (identifier) {
-    // 1.2.0-beta.1 bumps to 1.2.0-beta.2,
-    // 1.2.0-beta.foobar or 1.2.0-beta bumps to 1.2.0-beta.0
-    if (values[0] === identifier) {
-      if (isNaN(values[1] as number)) {
-        values = [identifier, 0];
-      }
-    } else {
-      values = [identifier, 0];
-    }
-  }
   return values;
 }
 
-function parseBuild(
-  build: string[] = [],
-  metadata: string | undefined,
+function bumpPrerelease(
+  prerelease: ReadonlyArray<string | number> = [],
+  identifier: string | undefined,
 ) {
-  return metadata === undefined ? build : metadata.split(".").filter((m) => m);
+  let values = bumpPrereleaseNumber(prerelease);
+  if (!identifier) return values;
+  // 1.2.0-beta.1 bumps to 1.2.0-beta.2,
+  // 1.2.0-beta.foobar or 1.2.0-beta bumps to 1.2.0-beta.0
+  if (values[0] !== identifier || isNaN(values[1] as number)) {
+    values = [identifier, 0];
+  }
+  return values;
 }
 
 /**
@@ -72,154 +65,111 @@ function parseBuild(
 export function increment(
   version: SemVer,
   release: ReleaseType,
-  prerelease?: string,
-  build?: string,
+  prerelease = "",
+  buildmetadata?: string,
 ): SemVer {
-  let result: SemVer;
+  const build = buildmetadata !== undefined
+    ? parseBuild(buildmetadata)
+    : version.build;
+
   switch (release) {
     case "premajor":
-      result = {
+      return {
         major: version.major + 1,
         minor: 0,
         patch: 0,
-        prerelease: pre(version.prerelease, prerelease),
-        build: parseBuild(version.build, build),
+        prerelease: bumpPrerelease(version.prerelease, prerelease),
+        build,
       };
-      break;
     case "preminor":
-      result = {
+      return {
         major: version.major,
         minor: version.minor + 1,
         patch: 0,
-        prerelease: pre(version.prerelease, prerelease),
-        build: parseBuild(version.build, build),
+        prerelease: bumpPrerelease(version.prerelease, prerelease),
+        build,
       };
-      break;
     case "prepatch":
-      result = {
+      return {
         major: version.major,
         minor: version.minor,
         patch: version.patch + 1,
-        prerelease: pre(version.prerelease, prerelease),
-        build: parseBuild(version.build, build),
+        prerelease: bumpPrerelease(version.prerelease, prerelease),
+        build,
       };
-      break;
-    // If the input is a non-prerelease version, this acts the same as
-    // prepatch.
-    case "prerelease":
-      if ((version.prerelease ?? []).length === 0) {
-        result = {
-          major: version.major,
-          minor: version.minor,
-          patch: version.patch + 1,
-          prerelease: pre(version.prerelease, prerelease),
-          build: parseBuild(version.build, build),
-        };
-        break;
-      } else {
-        result = {
-          major: version.major,
-          minor: version.minor,
-          patch: version.patch,
-          prerelease: pre(version.prerelease, prerelease),
-          build: parseBuild(version.build, build),
-        };
-        break;
-      }
-    case "major":
-      // If this is a pre-major version, bump up to the same major version.
-      // Otherwise increment major.
+    case "prerelease": {
+      // If the input is a non-prerelease version, this acts the same as prepatch.
+      const isPrerelease = (version.prerelease ?? []).length === 0;
+      const patch = isPrerelease ? version.patch + 1 : version.patch;
+      return {
+        major: version.major,
+        minor: version.minor,
+        patch,
+        prerelease: bumpPrerelease(version.prerelease, prerelease),
+        build,
+      };
+    }
+    case "major": {
+      // If this is a pre-major version, bump up to the same major version. Otherwise increment major.
       // 1.0.0-5 bumps to 1.0.0
       // 1.1.0 bumps to 2.0.0
-      if (
-        version.minor !== 0 ||
-        version.patch !== 0 ||
-        (version.prerelease ?? []).length === 0
-      ) {
-        result = {
-          major: version.major + 1,
-          minor: 0,
-          patch: 0,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      } else {
-        result = {
-          major: version.major,
-          minor: 0,
-          patch: 0,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      }
-    case "minor":
-      // If this is a pre-minor version, bump up to the same minor version.
-      // Otherwise increment minor.
+      const isPrerelease = (version.prerelease ?? []).length === 0;
+      const major = isPrerelease || version.minor !== 0 || version.patch !== 0
+        ? version.major + 1
+        : version.major;
+      return {
+        major,
+        minor: 0,
+        patch: 0,
+        prerelease: [],
+        build,
+      };
+    }
+    case "minor": {
+      // If this is a pre-minor version, bump up to the same minor version. Otherwise increment minor.
       // 1.2.0-5 bumps to 1.2.0
       // 1.2.1 bumps to 1.3.0
-      if (
-        version.patch !== 0 ||
-        (version.prerelease ?? []).length === 0
-      ) {
-        result = {
-          major: version.major,
-          minor: version.minor + 1,
-          patch: 0,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      } else {
-        result = {
-          major: version.major,
-          minor: version.minor,
-          patch: 0,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      }
-    case "patch":
+      const isPrerelease = (version.prerelease ?? []).length === 0;
+      const minor = isPrerelease || version.patch !== 0
+        ? version.minor + 1
+        : version.minor;
+      return {
+        major: version.major,
+        minor,
+        patch: 0,
+        prerelease: [],
+        build,
+      };
+    }
+    case "patch": {
       // If this is not a pre-release version, it will increment the patch.
       // If it is a pre-release it will bump up to the same patch version.
       // 1.2.0-5 patches to 1.2.0
       // 1.2.0 patches to 1.2.1
-      if ((version.prerelease ?? []).length === 0) {
-        result = {
-          major: version.major,
-          minor: version.minor,
-          patch: version.patch + 1,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      } else {
-        result = {
-          major: version.major,
-          minor: version.minor,
-          patch: version.patch,
-          prerelease: [],
-          build: parseBuild(version.build, build),
-        };
-        break;
-      }
-    // 1.0.0 "pre" would become 1.0.0-0
-    // 1.0.0-0 would become 1.0.0-1
-    // 1.0.0-beta.0 would be come 1.0.0-beta.1
-    // switching the pre identifier resets the number to 0
-    case "pre":
-      result = {
+      const isPrerelease = (version.prerelease ?? []).length === 0;
+      const patch = isPrerelease ? version.patch + 1 : version.patch;
+      return {
+        major: version.major,
+        minor: version.minor,
+        patch,
+        prerelease: [],
+        build,
+      };
+    }
+    case "pre": {
+      // 1.0.0 "pre" would become 1.0.0-0
+      // 1.0.0-0 would become 1.0.0-1
+      // 1.0.0-beta.0 would be come 1.0.0-beta.1
+      // switching the pre identifier resets the number to 0
+      return {
         major: version.major,
         minor: version.minor,
         patch: version.patch,
-        prerelease: pre(version.prerelease, prerelease),
-        build: parseBuild(version.build, build),
+        prerelease: bumpPrerelease(version.prerelease, prerelease),
+        build,
       };
-      break;
+    }
     default:
       throw new Error(`invalid increment argument: ${release}`);
   }
-  return result;
 }
