209cb2bfb97967cb287d010f06877937a61be782
Tim <tim.reichen@gmail.com>
2024-01-03 13:32:48 +0100

    initial commit

--- semver/is_semver.ts
+++ semver/is_semver.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { ANY, INVALID } from "./constants.ts";
 import type { SemVer } from "./types.ts";
-import { isValidNumber, isValidString } from "./_shared.ts";
+import { checkIdentifierValidity, checkNumberValidity } from "./_shared.ts";
 
 /**
  * Checks to see if value is a valid SemVer object. It does a check
@@ -25,22 +25,28 @@ export function isSemVer(value: unknown): value is SemVer {
   if (value === INVALID) return true;
   if (value === ANY) return true;
 
-  const { major, minor, patch, build = [], prerelease = [] } = value as Record<
-    string,
-    unknown
-  >;
-  const result = typeof major === "number" && isValidNumber(major) &&
-    typeof minor === "number" && isValidNumber(minor) &&
-    typeof patch === "number" && isValidNumber(patch) &&
+  const {
+    major,
+    minor,
+    patch,
+    build = [],
+    prerelease = [],
+  } = value as Record<string, unknown>;
+  return (
+    typeof major === "number" && checkNumberValidity(major) &&
+    typeof minor === "number" && checkNumberValidity(minor) &&
+    typeof patch === "number" && checkNumberValidity(patch) &&
     Array.isArray(prerelease) &&
+    prerelease
+      .every((v) => typeof v === "string" || typeof v === "number") &&
+    prerelease
+      .filter((v) => typeof v === "string")
+      .every((v) => checkIdentifierValidity(v)) &&
+    prerelease
+      .filter((v) => typeof v === "number")
+      .every((v) => checkNumberValidity(v)) &&
     Array.isArray(build) &&
-    prerelease.every((v) => typeof v === "string" || typeof v === "number") &&
-    prerelease.filter((v) => typeof v === "string").every((v) =>
-      isValidString(v)
-    ) &&
-    prerelease.filter((v) => typeof v === "number").every((v) =>
-      isValidNumber(v)
-    ) &&
-    build.every((v) => typeof v === "string" && isValidString(v));
-  return result;
+    build
+      .every((v) => typeof v === "string" && checkIdentifierValidity(v))
+  );
 }
