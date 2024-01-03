3abb9712e62740f1c01f2814c27b8e7c151dff2b
Tim <tim.reichen@gmail.com>
2024-01-03 15:49:28 +0100

    initial commit

--- cli/spinner.ts
+++ cli/spinner.ts
@@ -63,7 +63,7 @@ export interface SpinnerOptions {
  */
 export class Spinner {
   #spinner: string[];
-  #message: string;
+  message: string;
   #interval: number;
   #color?: Color;
   #intervalId: number | undefined;
@@ -81,7 +81,7 @@ export class Spinner {
    */
   constructor(options?: SpinnerOptions) {
     this.#spinner = options?.spinner ?? DEFAULT_SPINNER;
-    this.#message = options?.message ?? "";
+    this.message = options?.message ?? "";
     this.#interval = options?.interval ?? DEFAULT_INTERVAL;
     this.#color = options?.color ? COLORS[options.color] : undefined;
   }
@@ -107,7 +107,7 @@ export class Spinner {
     const updateFrame = () => {
       Deno.stdout.writeSync(LINE_CLEAR);
       const frame = encoder.encode(
-        color + this.#spinner[i] + COLOR_RESET + " " + this.#message,
+        color + this.#spinner[i] + COLOR_RESET + " " + this.message,
       );
       Deno.stdout.writeSync(frame);
       i = (i + 1) % this.#spinner.length;
