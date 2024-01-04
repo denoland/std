f1128e1d600fbbf6b83a893f5e20a6ebf5d75a0f
Tim <tim.reichen@gmail.com>
2024-01-04 17:52:39 +0100

    update deprecation structure

--- semver/gt.ts
+++ semver/gt.ts
@@ -1,9 +1,13 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 import { greater } from "./greater.ts";
+import { SemVer } from "./types.ts";
 
 /**
- * Greater than comparison
  * @deprecated (will be removed after 0.213.0) Use {@linkcode greater} instead.
+ *
+ * Greater than comparison
  */
-export const gt = greater;
+export function gt(s0: SemVer, s1: SemVer): boolean {
+  return greater(s0, s1);
+}
--- semver/lte.ts
+++ semver/lte.ts
@@ -1,9 +1,13 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 import { lessOrEqual } from "./less_or_equal.ts";
+import { SemVer } from "./types.ts";
 
 /**
- * Less than or equal to comparison
  * @deprecated (will be removed after 0.213.0) Use {@linkcode lessOrEqual} instead.
+ *
+ * Less than or equal to comparison
  */
-export const lte = lessOrEqual;
+export function lte(s0: SemVer, s1: SemVer): boolean {
+  return lessOrEqual(s0, s1);
+}
