fb520c73ec0cb9fc7da4e300948bfec87281be19
Tim <tim.reichen@gmail.com>
2024-01-04 14:40:51 +0100

    update

--- semver/min_satisfying.ts
+++ semver/min_satisfying.ts
@@ -1,8 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-import type { SemVer, SemVerComparator, SemVerRange } from "./types.ts";
+import type { SemVer, SemVerRange } from "./types.ts";
 import { testRange } from "./test_range.ts";
-import { lt } from "https://deno.land/std@$STD_VERSION/semver/lt.ts";
-import { testComparator } from "https://deno.land/std@$STD_VERSION/semver/test_comparator.ts";
+import { lt } from "./lt.ts";
 
 /**
  * Returns the lowest version in the list that satisfies the range, or `undefined` if
