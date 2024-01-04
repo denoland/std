d159ddb2dc97b85c2ed861c9ce9e0eebd2481760
Tim Reichen <timreichen@users.noreply.github.com>
2024-01-04 02:19:12 +0100

    eq.ts aktualisieren
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- semver/eq.ts
+++ semver/eq.ts
@@ -3,6 +3,7 @@ import { equals } from "./equals.ts";
 
 /**
  * Returns `true` if they're logically equivalent, even if they're not the exact same version object.
+ *
  * @deprecated (will be removed after 0.213.0) Use {@linkcode equals} instead.
  */
 export const eq = equals;
