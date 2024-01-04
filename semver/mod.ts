22eeeb2f9117af541d698e7aeb1116d8123ca855
Asher Gomez <ashersaupingomez@gmail.com>
2024-01-05 10:29:51 +1100

    BREAKING(semver): remove `rcompare()`

--- semver/mod.ts
+++ semver/mod.ts
@@ -305,7 +305,6 @@ export * from "./range_format.ts";
 export * from "./range_intersects.ts";
 export * from "./range_max.ts";
 export * from "./range_min.ts";
-export * from "./rcompare.ts";
 export * from "./rsort.ts";
 export * from "./sort.ts";
 export * from "./types.ts";
