3dded73b261d2372d09024f9408730a19c468241
Asher Gomez <ashersaupingomez@gmail.com>
2024-01-05 13:30:55 +1100

    tweak

--- path/posix/to_namespaced_path.ts
+++ path/posix/to_namespaced_path.ts
@@ -5,7 +5,8 @@
  * Resolves path to a namespace path
  * @param path to resolve to namespace
  *
- * @deprecated (will be removed in 0.214.0)
+ * @deprecated (will be removed in 0.214.0) Use the path value itself as this
+ * is a no-op function.
  */
 export function toNamespacedPath(path: string): string {
   // Non-op on posix systems
