5aca0284699725d32ed0bb872c755fe21d159587
Tim Reichen <timreichen@users.noreply.github.com>
2024-01-08 12:12:25 +0100

    Update semver/comparator_format.ts
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>

--- semver/comparator_format.ts
+++ semver/comparator_format.ts
@@ -8,7 +8,7 @@ import { format } from "./format.ts";
  * @param comparator
  * @returns A string representation of the comparator
  *
- * @deprecated (will be removed in 0.213.0) Use {@linkcode formatRange} instead.
+ * @deprecated (will be removed in 0.213.0) Use {@linkcode SemVerRange} instead of {@linkcode Comparator} and {@linkcode formatRange} for formatting it.
  */
 export function comparatorFormat(comparator: Comparator): string {
   const { semver, operator } = comparator;
