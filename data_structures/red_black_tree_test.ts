f2324e55f43082f86e7672ca7e8cf6a9328a3ecc
Simon Holloway <simon@syhol.io>
2024-01-10 20:18:34 +0000

    refactor(data_structures): prepare for `noUncheckedIndexedAccess` (#4146)
    * refactor(data_structures): prepare for `noUncheckedIndexedAccess`
    
    * tweak
    
    ---------
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- data_structures/red_black_tree_test.ts
+++ data_structures/red_black_tree_test.ts
@@ -5,10 +5,10 @@ import { ascend, descend } from "./comparators.ts";
 import { Container, MyMath } from "./_test_utils.ts";
 
 Deno.test("RedBlackTree works as expected with default ascend comparator", () => {
-  const trees: RedBlackTree<number>[] = [
+  const trees = [
     new RedBlackTree(),
     new RedBlackTree(),
-  ];
+  ] as const;
   const values: number[] = [-10, 9, -1, 100, 1, 0, -100, 10, -9];
 
   const expectedMin: number[][] = [
@@ -19,49 +19,49 @@ Deno.test("RedBlackTree works as expected with default ascend comparator", () =>
     [-10, 9, 9, 100, 100, 100, 100, 100, 100],
     [-9, 10, 10, 10, 10, 100, 100, 100, 100],
   ];
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, 0);
-    assertEquals(trees[i].isEmpty(), true);
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].find(values[j]), null);
-      assertEquals(trees[i].insert(values[j]), true);
-      assertEquals(trees[i].find(values[j]), values[j]);
-      assertEquals(trees[i].size, j + 1);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].min(), expectedMin[i][j]);
-      assertEquals(trees[i].max(), expectedMax[i][j]);
+  for (const [i, tree] of trees.entries()) {
+    assertEquals(tree.size, 0);
+    assertEquals(tree.isEmpty(), true);
+    for (const [j, value] of values.entries()) {
+      assertEquals(tree.find(value), null);
+      assertEquals(tree.insert(value), true);
+      assertEquals(tree.find(value), value);
+      assertEquals(tree.size, j + 1);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.min(), expectedMin?.[i]?.[j]);
+      assertEquals(tree.max(), expectedMax?.[i]?.[j]);
     }
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].insert(values[j]), false);
-      assertEquals(trees[i].size, values.length);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].min(), -100);
-      assertEquals(trees[i].max(), 100);
+    for (const value of values) {
+      assertEquals(tree.insert(value), false);
+      assertEquals(tree.size, values.length);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.min(), -100);
+      assertEquals(tree.max(), 100);
     }
     values.reverse();
   }
 
-  for (let i = 0; i < 2; i++) {
+  for (const tree of trees) {
     assertEquals(
-      [...trees[i].lnrValues()],
+      [...tree.lnrValues()],
       [-100, -10, -9, -1, 0, 1, 9, 10, 100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
 
     assertEquals(
-      [...trees[i]],
+      [...tree],
       [-100, -10, -9, -1, 0, 1, 9, 10, 100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
 
     assertEquals(
-      [...trees[i].rnlValues()],
+      [...tree.rnlValues()],
       [100, 10, 9, 1, 0, -1, -9, -10, -100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -72,9 +72,9 @@ Deno.test("RedBlackTree works as expected with default ascend comparator", () =>
     [...trees[1].nlrValues()],
     [-9, -100, -10, 1, 0, -1, 10, 9, 100],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -85,9 +85,9 @@ Deno.test("RedBlackTree works as expected with default ascend comparator", () =>
     [...trees[1].lrnValues()],
     [-10, -100, -1, 0, 9, 100, 10, 1, -9],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -98,37 +98,37 @@ Deno.test("RedBlackTree works as expected with default ascend comparator", () =>
     [...trees[1].lvlValues()],
     [-9, -100, 1, -10, 0, 10, -1, 9, 100],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
-  for (let i = 0; i < 2; i++) {
+  for (const tree of trees) {
     const expected: number[] = [-100, -10, -9, -1, 0, 1, 9, 10, 100];
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].size, values.length - j);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].find(values[j]), values[j]);
+    for (const [j, value] of values.entries()) {
+      assertEquals(tree.size, values.length - j);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.find(value), value);
 
-      assertEquals(trees[i].remove(values[j]), true);
-      expected.splice(expected.indexOf(values[j]), 1);
-      assertEquals([...trees[i]], expected);
-      assertEquals(trees[i].find(values[j]), null);
+      assertEquals(tree.remove(value), true);
+      expected.splice(expected.indexOf(value), 1);
+      assertEquals([...tree], expected);
+      assertEquals(tree.find(value), null);
 
-      assertEquals(trees[i].remove(values[j]), false);
-      assertEquals([...trees[i]], expected);
-      assertEquals(trees[i].find(values[j]), null);
+      assertEquals(tree.remove(value), false);
+      assertEquals([...tree], expected);
+      assertEquals(tree.find(value), null);
     }
-    assertEquals(trees[i].size, 0);
-    assertEquals(trees[i].isEmpty(), true);
+    assertEquals(tree.size, 0);
+    assertEquals(tree.isEmpty(), true);
   }
 });
 
 Deno.test("RedBlackTree works as exepcted with descend comparator", () => {
-  const trees: RedBlackTree<number>[] = [
+  const trees = [
     new RedBlackTree(descend),
     new RedBlackTree(descend),
-  ];
+  ] as const;
   const values: number[] = [-10, 9, -1, 100, 1, 0, -100, 10, -9];
 
   const expectedMin: number[][] = [
@@ -139,49 +139,49 @@ Deno.test("RedBlackTree works as exepcted with descend comparator", () => {
     [-10, -10, -10, -10, -10, -10, -100, -100, -100],
     [-9, -9, -100, -100, -100, -100, -100, -100, -100],
   ];
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, 0);
-    assertEquals(trees[i].isEmpty(), true);
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].find(values[j]), null);
-      assertEquals(trees[i].insert(values[j]), true);
-      assertEquals(trees[i].find(values[j]), values[j]);
-      assertEquals(trees[i].size, j + 1);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].min(), expectedMin[i][j]);
-      assertEquals(trees[i].max(), expectedMax[i][j]);
+  for (const [i, tree] of trees.entries()) {
+    assertEquals(tree.size, 0);
+    assertEquals(tree.isEmpty(), true);
+    for (const [j, value] of values.entries()) {
+      assertEquals(tree.find(value), null);
+      assertEquals(tree.insert(value), true);
+      assertEquals(tree.find(value), value);
+      assertEquals(tree.size, j + 1);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.min(), expectedMin?.[i]?.[j]);
+      assertEquals(tree.max(), expectedMax?.[i]?.[j]);
     }
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].insert(values[j]), false);
-      assertEquals(trees[i].size, values.length);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].min(), 100);
-      assertEquals(trees[i].max(), -100);
+    for (const value of values) {
+      assertEquals(tree.insert(value), false);
+      assertEquals(tree.size, values.length);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.min(), 100);
+      assertEquals(tree.max(), -100);
     }
     values.reverse();
   }
 
-  for (let i = 0; i < 2; i++) {
+  for (const tree of trees) {
     assertEquals(
-      [...trees[i].lnrValues()],
+      [...tree.lnrValues()],
       [100, 10, 9, 1, 0, -1, -9, -10, -100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
 
     assertEquals(
-      [...trees[i]],
+      [...tree],
       [100, 10, 9, 1, 0, -1, -9, -10, -100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
 
     assertEquals(
-      [...trees[i].rnlValues()],
+      [...tree.rnlValues()],
       [-100, -10, -9, -1, 0, 1, 9, 10, 100],
     );
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -192,9 +192,9 @@ Deno.test("RedBlackTree works as exepcted with descend comparator", () => {
     [...trees[1].nlrValues()],
     [-9, 1, 10, 100, 9, 0, -1, -100, -10],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -205,9 +205,9 @@ Deno.test("RedBlackTree works as exepcted with descend comparator", () => {
     [...trees[1].lrnValues()],
     [100, 9, 10, -1, 0, 1, -10, -100, -9],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
   assertEquals(
@@ -218,29 +218,29 @@ Deno.test("RedBlackTree works as exepcted with descend comparator", () => {
     [...trees[1].lvlValues()],
     [-9, 1, -100, 10, 0, -10, 100, 9, -1],
   );
-  for (let i = 0; i < 2; i++) {
-    assertEquals(trees[i].size, values.length);
-    assertEquals(trees[i].isEmpty(), false);
+  for (const tree of trees) {
+    assertEquals(tree.size, values.length);
+    assertEquals(tree.isEmpty(), false);
   }
 
-  for (let i = 0; i < 2; i++) {
+  for (const tree of trees) {
     const expected: number[] = [100, 10, 9, 1, 0, -1, -9, -10, -100];
-    for (let j = 0; j < values.length; j++) {
-      assertEquals(trees[i].size, values.length - j);
-      assertEquals(trees[i].isEmpty(), false);
-      assertEquals(trees[i].find(values[j]), values[j]);
+    for (const [j, value] of values.entries()) {
+      assertEquals(tree.size, values.length - j);
+      assertEquals(tree.isEmpty(), false);
+      assertEquals(tree.find(value), value);
 
-      assertEquals(trees[i].remove(values[j]), true);
-      expected.splice(expected.indexOf(values[j]), 1);
-      assertEquals([...trees[i]], expected);
-      assertEquals(trees[i].find(values[j]), null);
+      assertEquals(tree.remove(value), true);
+      expected.splice(expected.indexOf(value), 1);
+      assertEquals([...tree], expected);
+      assertEquals(tree.find(value), null);
 
-      assertEquals(trees[i].remove(values[j]), false);
-      assertEquals([...trees[i]], expected);
-      assertEquals(trees[i].find(values[j]), null);
+      assertEquals(tree.remove(value), false);
+      assertEquals([...tree], expected);
+      assertEquals(tree.find(value), null);
     }
-    assertEquals(trees[i].size, 0);
-    assertEquals(trees[i].isEmpty(), true);
+    assertEquals(tree.size, 0);
+    assertEquals(tree.isEmpty(), true);
   }
 });
 
@@ -251,25 +251,25 @@ Deno.test("RedBlackTree works with object items", () => {
   ) => ascend(a.id, b.id));
   const ids: number[] = [-10, 9, -1, 100, 1, 0, -100, 10, -9];
 
-  for (let i = 0; i < ids.length; i++) {
-    const newContainer: Container = { id: ids[i], values: [] };
+  for (const [i, id] of ids.entries()) {
+    const newContainer: Container = { id, values: [] };
     assertEquals(tree.find(newContainer), null);
     assertEquals(tree.insert(newContainer), true);
     newContainer.values.push(i - 1, i, i + 1);
-    assertStrictEquals(tree.find({ id: ids[i], values: [] }), newContainer);
+    assertStrictEquals(tree.find({ id, values: [] }), newContainer);
     assertEquals(tree.size, i + 1);
     assertEquals(tree.isEmpty(), false);
   }
-  for (let i = 0; i < ids.length; i++) {
-    const newContainer: Container = { id: ids[i], values: [] };
+  for (const [i, id] of ids.entries()) {
+    const newContainer: Container = { id, values: [] };
     assertEquals(
-      tree.find({ id: ids[i] } as Container),
-      { id: ids[i], values: [i - 1, i, i + 1] },
+      tree.find({ id } as Container),
+      { id, values: [i - 1, i, i + 1] },
     );
     assertEquals(tree.insert(newContainer), false);
     assertEquals(
-      tree.find({ id: ids[i], values: [] }),
-      { id: ids[i], values: [i - 1, i, i + 1] },
+      tree.find({ id, values: [] }),
+      { id, values: [i - 1, i, i + 1] },
     );
     assertEquals(tree.size, ids.length);
     assertEquals(tree.isEmpty(), false);
@@ -283,22 +283,22 @@ Deno.test("RedBlackTree works with object items", () => {
   assertEquals(tree.isEmpty(), false);
 
   const expected: number[] = [-100, -10, -9, -1, 0, 1, 9, 10, 100];
-  for (let i = 0; i < ids.length; i++) {
+  for (const [i, id] of ids.entries()) {
     assertEquals(tree.size, ids.length - i);
     assertEquals(tree.isEmpty(), false);
     assertEquals(
-      tree.find({ id: ids[i], values: [] }),
-      { id: ids[i], values: [i - 1, i, i + 1] },
+      tree.find({ id, values: [] }),
+      { id, values: [i - 1, i, i + 1] },
     );
 
-    assertEquals(tree.remove({ id: ids[i], values: [] }), true);
-    expected.splice(expected.indexOf(ids[i]), 1);
+    assertEquals(tree.remove({ id, values: [] }), true);
+    expected.splice(expected.indexOf(id), 1);
     assertEquals([...tree].map((container) => container.id), expected);
-    assertEquals(tree.find({ id: ids[i], values: [] }), null);
+    assertEquals(tree.find({ id, values: [] }), null);
 
-    assertEquals(tree.remove({ id: ids[i], values: [] }), false);
+    assertEquals(tree.remove({ id, values: [] }), false);
     assertEquals([...tree].map((container) => container.id), expected);
-    assertEquals(tree.find({ id: ids[i], values: [] }), null);
+    assertEquals(tree.find({ id, values: [] }), null);
   }
   assertEquals(tree.size, 0);
   assertEquals(tree.isEmpty(), true);
