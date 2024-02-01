dbf1883f5a81dcf668c3c1cd66fd19aa7043c5e5
Tim <tim.reichen@gmail.com>
2024-02-01 23:22:37 +0100

    remove replacement

--- semver/gtr.ts
+++ semver/gtr.ts
@@ -1,12 +1,22 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import type { Range, SemVer, SemVerRange } from "./types.ts";
 import { rangeMax } from "./range_max.ts";
-import { gt } from "./gt.ts";
+import { greaterThan } from "./greater_than.ts";
+import { warnOnDeprecatedApi } from "../internal/warn_on_deprecated_api.ts";
 
-/** Checks to see if the version is greater than all possible versions of the range. */
+/**
+ * Checks to see if the version is greater than all possible versions of the range.
+ * @deprecated (will be removed after 0.215.0) Use `greaterThan(version, rangeMax(range))` instead.
+ */
 export function gtr(
   version: SemVer,
   range: SemVerRange | Range,
 ): boolean {
-  return gt(version, rangeMax(range));
+  warnOnDeprecatedApi({
+    apiName: "gtr",
+    stack: new Error().stack!,
+    removalVersion: "0.215.0",
+    suggestion: "Use 'greaterThan(version, rangeMax(range))' instead.",
+  });
+  return greaterThan(version, rangeMax(range));
 }
--- semver/ltr.ts
+++ semver/ltr.ts
@@ -1,12 +1,22 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import type { Range, SemVer, SemVerRange } from "./types.ts";
-import { lt } from "./lt.ts";
+import { lessThan } from "./less_than.ts";
 import { rangeMin } from "./range_min.ts";
+import { warnOnDeprecatedApi } from "../internal/warn_on_deprecated_api.ts";
 
-/** Less than range comparison */
+/**
+ *  Less than range comparison
+ * @deprecated (will be removed after 0.215.0) Use `lessThan(version, rangeMin(range))` instead.
+ */
 export function ltr(
   version: SemVer,
   range: SemVerRange | Range,
 ): boolean {
-  return lt(version, rangeMin(range));
+  warnOnDeprecatedApi({
+    apiName: "ltr",
+    stack: new Error().stack!,
+    removalVersion: "0.215.0",
+    suggestion: "Use 'lessThan(version, rangeMin(range))' instead.",
+  });
+  return lessThan(version, rangeMin(range));
 }
