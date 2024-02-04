ac5deeb41bdc87b140d8da1603dd1589672a975f
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-02-05 09:28:57 +1300

    update main

--- artifact/main.ts
+++ artifact/main.ts
@@ -1,19 +1,3 @@
-import store from './kv.js'
-
 Deno.serve(async (_req) => {
-  //   return await test()
   return new Response('ok')
 })
-
-export const test = async () => {
-  const kv = await store.create()
-  console.log('request')
-  const promises = []
-  const count = 2000
-  for (let i = 0; i < count; i++) {
-    promises.push(kv.dispatch(i))
-  }
-  await Promise.all(promises)
-  console.log('done', count)
-  return new Response('ok')
-}
