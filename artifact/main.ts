665808644bd8ea91679393a78cdc4bbd09efa697
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-02-07 20:50:09 +1300

    test in deploy

--- artifact/main.ts
+++ artifact/main.ts
@@ -1,7 +1,31 @@
 // TODO on first boot, run some diagnostic tests and benchmarks
 // should be able to commit these benchmarks back to gh to publish them
+import { log } from './tst-helpers.js'
+const isolate = 'io.fixture'
+import { PID } from './constants.ts'
+import Artifact from '@/artifact/artifact.ts'
 
-const kv = await Deno.openKv()
-kv.listenQueue((msg) => {
-  console.log('queue', msg)
+const artifact = await Artifact.create()
+
+Deno.serve(async (req) => {
+  if (req.url === '/favicon.ico') {
+    return new Response('', { status: 404 })
+  }
+  log('start')
+
+  await artifact.pull('dreamcatcher-tech/HAL')
+  log('pull done')
+  const pid: PID = {
+    account: 'dreamcatcher-tech',
+    repository: 'HAL',
+    branches: ['main'],
+  }
+
+  const actions = await artifact.actions(isolate, pid)
+  log('actions done')
+  const result = await actions.local()
+  log('result', result)
+  const result2 = await actions.local()
+  log('result2', result2)
+  return new Response('Hello Artifact')
 })
