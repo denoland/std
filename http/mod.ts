5fd4fa1c51fc9b46df3fb81a46d6a8898bbeed3f
Halvard Mørstad <halvard@simplyundoable.com>
2024-02-12 14:09:34 +0100

    Added methods to http namespace

--- http/mod.ts
+++ http/mod.ts
@@ -71,3 +71,4 @@ export * from "./unstable_signed_cookie.ts";
 export * from "./server_sent_event_stream.ts";
 export * from "./user_agent.ts";
 export * from "./file_server.ts";
+export * from "./method.ts";
