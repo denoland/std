3a2305727d06fac4984e58b71f62826b63404696
Tim <tim.reichen@gmail.com>
2024-02-12 12:35:21 +0100

    initial commit

--- semver/_comparator_min.ts
+++ semver/_comparator_min.ts
@@ -1,21 +1,19 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-import type { Operator, SemVer } from "./types.ts";
+import type { Comparator, SemVer } from "./types.ts";
 import { ANY, MAX, MIN } from "./constants.ts";
 import { greaterThan } from "./greater_than.ts";
 import { increment } from "./increment.ts";
 
 /**
  * The minimum semantic version that could match this comparator
- * @param semver The semantic version of the comparator
+ * @param comparator The semantic version of the comparator
  * @param operator The operator of the comparator
  * @returns The minimum valid semantic version
  */
-export function comparatorMin(semver: SemVer, operator: Operator): SemVer {
-  if (semver === ANY) {
-    return MIN;
-  }
-
-  switch (operator) {
+export function comparatorMin(comparator: Comparator): SemVer {
+  const semver = comparator.semver ?? comparator;
+  if (semver === ANY) return MIN;
+  switch (comparator.operator) {
     case ">":
       return semver.prerelease && semver.prerelease.length > 0
         ? increment(semver, "pre")
