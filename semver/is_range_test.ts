cf915d6e1c47aa48a3e9c9217b95fc4fecc0a056
Tim <tim.reichen@gmail.com>
2024-02-26 11:41:11 +0100

    add tests

--- semver/is_range_test.ts
+++ semver/is_range_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assert } from "../assert/mod.ts";
-import { ALL } from "./constants.ts";
+import { ALL, INVALID, MIN } from "./constants.ts";
 import { isRange } from "./is_range.ts";
 
 Deno.test({
@@ -9,6 +9,18 @@ Deno.test({
     let i = 0;
     const ranges: unknown[] = [[
       [ALL],
+    ], [
+      [{
+        operator: ">=",
+        semver: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+        min: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+        max: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+      }, {
+        operator: "<",
+        semver: MIN,
+        min: INVALID,
+        max: INVALID,
+      }],
     ]];
     for (const r of ranges) {
       await t.step(`${(i++).toString().padStart(2, "0")}`, () => {
