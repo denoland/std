573842748dca1430a68ff2346af24b63c4138e1d
Tim <tim.reichen@gmail.com>
2024-02-28 00:00:58 +0100

    revert change

--- semver/_is_comparator_test.ts
+++ semver/_is_comparator_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assert } from "../assert/mod.ts";
-import { MIN } from "./constants.ts";
+import { INVALID, MIN } from "./constants.ts";
 import { isComparator } from "./_is_comparator.ts";
 import { formatComparator } from "./_format_comparator.ts";
 import { Comparator } from "./types.ts";
@@ -8,9 +8,18 @@ import { Comparator } from "./types.ts";
 Deno.test({
   name: "isComparator()",
   fn: async (t) => {
-    const comparators: Comparator[] = [
+    const comparators: unknown[] = [
       {
         operator: ">=",
+        semver: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+        min: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+        max: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+      },
+      {
+        operator: "<",
+        semver: MIN,
+        min: INVALID,
+        max: INVALID,
         major: 0,
         minor: 0,
         patch: 0,
