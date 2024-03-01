2c6d068e5ad9a939609943fcc80cf278607b701f
Tim Reichen <timreichen@users.noreply.github.com>
2024-03-01 07:43:28 +0100

    chore(semver): move comparator functions into range files (#4387)
    * initial commit
    
    * update
    
    * update
    
    * add tests
    
    * fix
    
    ---------
    
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- semver/is_range_test.ts
+++ semver/is_range_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assert } from "../assert/mod.ts";
-import { ALL } from "./constants.ts";
+import { ALL, MIN } from "./constants.ts";
 import { formatRange } from "./format_range.ts";
 import { isRange } from "./is_range.ts";
 import type { Range } from "./types.ts";
@@ -10,6 +10,20 @@ Deno.test({
   fn: async (t) => {
     const ranges: Range[] = [[
       [ALL],
+    ], [
+      [{
+        operator: ">=",
+        semver: { major: 0, minor: 0, patch: 0, prerelease: [], build: [] },
+        major: 0,
+        minor: 0,
+        patch: 0,
+        prerelease: [],
+        build: [],
+      }, {
+        operator: "<",
+        semver: MIN,
+        ...MIN,
+      }],
     ]];
     for (const r of ranges) {
       await t.step(`${formatRange(r)}`, () => {
