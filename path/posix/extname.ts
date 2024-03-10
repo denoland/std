623d7d0bf190f0801f43f7831a569466daff692b
Bruno Heridet <delapouite@gmail.com>
2024-03-10 13:40:17 +0100

    docs(path): add examples for posix basename, dirname and extname (#4458)
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>

--- path/posix/extname.ts
+++ path/posix/extname.ts
@@ -7,6 +7,15 @@ import { isPosixPathSeparator } from "./_util.ts";
 
 /**
  * Return the extension of the `path` with leading period.
+ *
+ * @example
+ * ```ts
+ * import { extname } from "https://deno.land/std@$STD_VERSION/path/extname.ts";
+ *
+ * console.log(extname("/home/user/Documents/")); // ""
+ * console.log(extname("/home/user/Documents/image.png")); // ".png"
+ * ```
+ *
  * @param path with extension
  * @returns extension (ex. for `file.ts` returns `.ts`)
  */
