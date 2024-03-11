b457162d6e21e742d656fa7f9b2a0d41191bb1ae
Asher Gomez <ashersaupingomez@gmail.com>
2024-03-11 17:25:30 +1100

    docs(bytes): module docs

--- bytes/mod.ts
+++ bytes/mod.ts
@@ -5,6 +5,14 @@
  * Provides helper functions to manipulate `Uint8Array` byte slices that are not
  * included on the `Uint8Array` prototype.
  *
+ * ```ts
+ * import { concat } from "https://deno.land/std@$STD_VERSION/bytes/concat.ts";
+ *
+ * const a = new Uint8Array([0, 1, 2]);
+ * const b = new Uint8Array([3, 4, 5]);
+ * concat([a, b]); // Uint8Array(6) [ 0, 1, 2, 3, 4, 5 ]
+ * ```
+ *
  * @module
  */
 
