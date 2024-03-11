4b082ac0df7ccf4e4b7ed294b6905390b7b9601e
Halvard Mørstad <halvard@simplyundoable.com>
2024-03-11 14:39:40 +0100

    Add assertThrowsAsync

--- assert/assert_throws.ts
+++ assert/assert_throws.ts
@@ -2,6 +2,67 @@
 import { assertIsError } from "./assert_is_error.ts";
 import { AssertionError } from "./assertion_error.ts";
 
+type ExpectedErrorO = {
+  // deno-lint-ignore no-explicit-any
+  ErrorClass: (new (...args: any[]) => Error) | undefined;
+  msgIncludes: string | undefined;
+  msgSuffix: string;
+  msg: string | undefined;
+};
+
+function parseAssertThrowsExpectedError<E extends Error>(
+  // deno-lint-ignore no-explicit-any
+  errorClassOrMsg?: (new (...args: any[]) => Error) | string,
+  msgIncludesOrMsg?: string,
+  msg?: string,
+): ExpectedErrorO {
+  // deno-lint-ignore no-explicit-any
+  let ErrorClass: (new (...args: any[]) => E) | undefined = undefined;
+  let msgIncludes: string | undefined = undefined;
+
+  if (typeof errorClassOrMsg !== "string") {
+    if (
+      errorClassOrMsg === undefined ||
+      errorClassOrMsg.prototype instanceof Error ||
+      errorClassOrMsg.prototype === Error.prototype
+    ) {
+      // deno-lint-ignore no-explicit-any
+      ErrorClass = errorClassOrMsg as new (...args: any[]) => E;
+      msgIncludes = msgIncludesOrMsg;
+    } else {
+      msg = msgIncludesOrMsg;
+    }
+  } else {
+    msg = errorClassOrMsg;
+  }
+  const msgSuffix = msg ? `: ${msg}` : ".";
+
+  return {
+    ErrorClass,
+    msgIncludes,
+    msgSuffix,
+    msg,
+  };
+}
+
+// deno-lint-ignore no-explicit-any
+function parseAssertThrowsError(expectedError: ExpectedErrorO, error: any) {
+  if (expectedError.ErrorClass) {
+    if (error instanceof Error === false) {
+      throw new AssertionError(
+        `A non-Error object was thrown${expectedError.msgSuffix}`,
+      );
+    }
+    assertIsError(
+      error,
+      expectedError.ErrorClass,
+      expectedError.msgIncludes,
+      expectedError.msg,
+    );
+  }
+  return error;
+}
+
 /**
  * Executes a function, expecting it to throw. If it does not, then it
  * throws.
@@ -47,48 +108,90 @@ export function assertThrows<E extends Error = Error>(
   msgIncludesOrMsg?: string,
   msg?: string,
 ): E | Error | unknown {
-  // deno-lint-ignore no-explicit-any
-  let ErrorClass: (new (...args: any[]) => E) | undefined = undefined;
-  let msgIncludes: string | undefined = undefined;
   let err;
-
-  if (typeof errorClassOrMsg !== "string") {
-    if (
-      errorClassOrMsg === undefined ||
-      errorClassOrMsg.prototype instanceof Error ||
-      errorClassOrMsg.prototype === Error.prototype
-    ) {
-      // deno-lint-ignore no-explicit-any
-      ErrorClass = errorClassOrMsg as new (...args: any[]) => E;
-      msgIncludes = msgIncludesOrMsg;
-    } else {
-      msg = msgIncludesOrMsg;
-    }
-  } else {
-    msg = errorClassOrMsg;
-  }
   let doesThrow = false;
-  const msgSuffix = msg ? `: ${msg}` : ".";
+  const parsedExpectedError = parseAssertThrowsExpectedError<E>(
+    errorClassOrMsg,
+    msgIncludesOrMsg,
+    msg,
+  );
+
   try {
     fn();
   } catch (error) {
-    if (ErrorClass) {
-      if (error instanceof Error === false) {
-        throw new AssertionError(`A non-Error object was thrown${msgSuffix}`);
-      }
-      assertIsError(
-        error,
-        ErrorClass,
-        msgIncludes,
-        msg,
-      );
-    }
-    err = error;
     doesThrow = true;
+    err = parseAssertThrowsError(parsedExpectedError, error);
   }
   if (!doesThrow) {
-    msg = `Expected function to throw${msgSuffix}`;
+    msg = `Expected function to throw${parsedExpectedError.msgSuffix}`;
     throw new AssertionError(msg);
   }
+  return err;
+}
+
+/**
+ * Executes an async function, expecting it to throw. If it does not, then it
+ * throws.
+ *
+ * @example
+ * ```ts
+ * import { assertThrowsAsync } from "https://deno.land/std@$STD_VERSION/assert/assert_throws.ts";
+ *
+ * assertThrowsAsync(() => { throw new TypeError("hello world!"); }); // Doesn't throw
+ * assertThrowsAsync(async () => await new Promise((_, reject) => { reject() })); // Throws
+ * ```
+ */
+export async function assertThrowsAsync(
+  fn: () => Promise<unknown>,
+  msg?: string,
+): Promise<unknown>;
+/**
+ * Executes a function, expecting it to throw. If it does not, then it
+ * throws. An error class and a string that should be included in the
+ * error message can also be asserted.
+ *
+ * @example
+ * ```ts
+ * import { assertThrowsAsync } from "https://deno.land/std@$STD_VERSION/assert/assert_throws.ts";
+ *
+ * assertThrowsAsync(() => { throw new TypeError("hello world!"); }, TypeError); // Doesn't throw
+ * assertThrowsAsync(() => { throw new TypeError("hello world!"); }, RangeError); // Throws
+ * ```
+ */
+export async function assertThrowsAsync<E extends Error = Error>(
+  fn: () => Promise<unknown>,
+  // deno-lint-ignore no-explicit-any
+  ErrorClass: new (...args: any[]) => E,
+  msgIncludes?: string,
+  msg?: string,
+): Promise<E>;
+export async function assertThrowsAsync<E extends Error = Error>(
+  fn: () => Promise<unknown>,
+  errorClassOrMsg?:
+    // deno-lint-ignore no-explicit-any
+    | (new (...args: any[]) => E)
+    | string,
+  msgIncludesOrMsg?: string,
+  msg?: string,
+): Promise<E | Error | unknown> {
+  let err;
+  let doesThrow = false;
+  const parsedExpectedError = parseAssertThrowsExpectedError<E>(
+    errorClassOrMsg,
+    msgIncludesOrMsg,
+    msg,
+  );
+
+  try {
+    await fn();
+  } catch (error) {
+    doesThrow = true;
+    err = parseAssertThrowsError(parsedExpectedError, error);
+  }
+  if (!doesThrow) {
+    msg = `Expected function to throw${parsedExpectedError.msgSuffix}`;
+    throw new AssertionError(msg);
+  }
+
   return err;
 }
--- assert/assert_throws_test.ts
+++ assert/assert_throws_test.ts
@@ -4,6 +4,7 @@ import {
   assertEquals,
   AssertionError,
   assertThrows,
+  assertThrowsAsync,
   fail,
 } from "./mod.ts";
 
@@ -120,6 +121,133 @@ Deno.test("assertThrows() matches and returns thrown non-error", () => {
 
 Deno.test("assertThrows() matches subclass of expected error", () => {
   assertThrows(
+    () => {
+      throw new AssertionError("Fail!");
+    },
+    Error,
+    "Fail!",
+  );
+});
+
+Deno.test("assertThrowsAsync() throws when thrown error class does not match expected", async () => {
+  await assertThrowsAsync(
+    async () => {
+      //This next assertThrowsAsync will throw an AssertionError due to the wrong
+      //expected error class
+      await assertThrowsAsync(
+        () => {
+          fail("foo");
+        },
+        TypeError,
+        "Failed assertion: foo",
+      );
+    },
+    AssertionError,
+    `Expected error to be instance of "TypeError", but was "AssertionError"`,
+  );
+});
+
+Deno.test("assertThrowsAsync() changes its return type by parameter", async () => {
+  await assertThrowsAsync(() => {
+    throw new Error();
+  });
+});
+
+Deno.test("assertThrowsAsync() throws on promise", async () => {
+  await assertThrowsAsync(() => {
+    return Promise.reject("something went wrong");
+  });
+});
+
+Deno.test("assertThrowsAsync() throws when error class is expected but non-error value is thrown", async () => {
+  await assertThrowsAsync(
+    async () => {
+      await assertThrowsAsync(
+        () => {
+          throw "Panic!";
+        },
+        Error,
+        "Panic!",
+      );
+    },
+    AssertionError,
+    "A non-Error object was thrown.",
+  );
+});
+
+Deno.test("assertThrowsAsync() matches thrown non-error value", async () => {
+  await assertThrowsAsync(
+    () => {
+      throw "Panic!";
+    },
+  );
+  await assertThrowsAsync(
+    () => {
+      throw null;
+    },
+  );
+  await assertThrowsAsync(
+    () => {
+      throw undefined;
+    },
+  );
+});
+
+Deno.test("assertThrowsAsync() matches thrown error with given error class", async () => {
+  await assertThrowsAsync(
+    () => {
+      throw new Error("foo");
+    },
+    Error,
+    "foo",
+  );
+});
+
+Deno.test("assertThrowsAsync() matches and returns thrown error value", async () => {
+  const error = await assertThrowsAsync(
+    () => {
+      throw new Error("foo");
+    },
+  );
+  assert(error instanceof Error);
+  assertEquals(error.message, "foo");
+});
+
+Deno.test("assertThrowsAsync() matches and returns thrown non-error", async () => {
+  const stringError = await assertThrowsAsync(
+    () => {
+      throw "Panic!";
+    },
+  );
+  assert(typeof stringError === "string");
+  assertEquals(stringError, "Panic!");
+
+  const numberError = await assertThrowsAsync(
+    () => {
+      throw 1;
+    },
+  );
+  assert(typeof numberError === "number");
+  assertEquals(numberError, 1);
+
+  const nullError = await assertThrowsAsync(
+    () => {
+      throw null;
+    },
+  );
+  assert(nullError === null);
+
+  const undefinedError = await assertThrowsAsync(
+    () => {
+      throw undefined;
+    },
+  );
+  assert(typeof undefinedError === "undefined");
+  assertEquals(undefinedError, undefined);
+});
+
+Deno.test("assertThrowsAsync() matches subclass of expected error", async () => {
+  await assertThrowsAsync(
     () => {
       throw new AssertionError("Fail!");
     },
