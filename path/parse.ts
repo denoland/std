3a5a9e0a811c5e7cb41f08040a6f05f90d728581
ooker <ganuongphap@gmail.com>
2024-03-15 15:28:14 +0700

    docs: add detailed info on the ParsedPath interface

--- path/parse.ts
+++ path/parse.ts
@@ -8,6 +8,18 @@ import { parse as windowsParse } from "./windows/parse.ts";
 
 /**
  * Return a `ParsedPath` object of the `path`. Use `format` to reverse the result.
+ * @example 
+ * ```ts
+ * const parsedPathObj = parse("c:\\path\\dir\\index.html")
+ * console.log(parsedPathObj)
+ * {
+ *   root: "c:\\",
+ *   dir: "c:\\path\\dir",
+ *   base: "index.html",
+ *   ext: ".html",
+ *   name: "index"
+ * }
+ * ```
  * @param path to process
  */
 export function parse(path: string): ParsedPath {
