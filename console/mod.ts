afd29994ded936097e82aee69ed0d76613c4afc5
Asher Gomez <ashersaupingomez@gmail.com>
2024-03-22 15:49:53 +1100

    docs(console): improve documentation

--- console/mod.ts
+++ console/mod.ts
@@ -1,7 +1,17 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 /**
- * Functions for console-related tasks such as TTY text layout
+ * Functions for console-related tasks such as TTY text layout.
+ *
+ * ## Unicode width
+ *
+ * Calculate the physical width of a string in a TTY-like environment.
+ *
+ * ```ts
+ * import { unicodeWidth } from "https://deno.land/std@$STD_VERSION/console/unicode_width.ts";
+ *
+ * unicodeWidth("天地玄黃宇宙洪荒"); // 16
+ * ```
  *
  * @module
  */
