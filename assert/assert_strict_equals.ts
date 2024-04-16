9ffe3282c72166f4240ad9fdb35194c2ede02cae
Asher Gomez <ashersaupingomez@gmail.com>
2024-04-16 14:05:49 +1000

    tweaks

--- assert/assert_strict_equals.ts
+++ assert/assert_strict_equals.ts
@@ -1,8 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
-import { format } from "../internal/format.ts";
+import { buildMessage, diff, diffstr, format } from "../internal/mod.ts";
 import { AssertionError } from "./assertion_error.ts";
-import { buildMessage, diff, diffstr } from "../internal/diff.ts";
 import { CAN_NOT_DISPLAY } from "./_constants.ts";
 import { red } from "../fmt/colors.ts";
 
--- internal/mod.ts
+++ internal/mod.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-
+// This module is browser compatible.
 /**
  * Internal utilities for the public API of the Deno Standard Library.
  *
@@ -7,3 +7,5 @@
  *
  * @module
  */
+export * from "./format.ts";
+export * from "./diff.ts";
