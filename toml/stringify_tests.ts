38fe46605dbbf35f1e8482f52d2e272d67f16f3f
Eliza Sorber <53704365+elizasorber@users.noreply.github.com>
2024-04-22 13:01:28 -0400

    Update toml/stringify_tests.ts
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- toml/stringify_tests.ts
+++ toml/stringify_tests.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assertEquals, assertThrows } from "../assert/mod.ts";
-import { stringify } from "./mod.ts";
+import { stringify } from "./stringify.ts";
 
 // https://github.com/denoland/deno_std/issues/1067#issuecomment-907740319
 Deno.test({
