d78321d9020f111a6ad10bf2e4b89eb9f8d39634
Eliza Sorber <53704365+elizasorber@users.noreply.github.com>
2024-04-23 20:06:11 -0400

    test(toml): split `toml/test.ts` into separate files (#4616)
    * Split toml/test.ts into separate files
    
    Handling issue #4593. Took tests from toml/test.ts and split into toml/parse_tests.ts and toml/stringify_tests.ts
    
    * Update toml/stringify_tests.ts
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>
    
    * Update toml/parse_test.ts
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>
    
    * Formatted files properly
    
    * Renamed file to stringify_test.ts
    
    * Update toml/stringify_test.ts
    
    * Fixed breaking tests.
    stringify() handles object value contains '='
    stringify() handles key alignment
    stringify() handles empty key
    stringify() handles empty object
    
    ---------
    
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

---
+++ toml/stringify_test.ts
@@ -0,0 +1,103 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+import { assertEquals, assertThrows } from "../assert/mod.ts";
+import { stringify } from "./stringify.ts";
+
+// https://github.com/denoland/deno_std/issues/1067#issuecomment-907740319
+Deno.test({
+  name: "stringify() handles object value contains '='",
+  fn() {
+    const src = {
+      "a": "a = 1",
+      "helloooooooo": 1,
+    };
+
+    const actual = stringify(src, { keyAlignment: true });
+    const expected = `a            = "a = 1"
+helloooooooo = 1
+`;
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "stringify() handles key alignment",
+  fn() {
+    const src = {
+      "a": 1,
+      "aa": 1,
+      "aaa": 1,
+      "aaaa": 1,
+      "aaaaa": 1,
+    };
+    const actual = stringify(src, { keyAlignment: true });
+    const expected = `a     = 1
+aa    = 1
+aaa   = 1
+aaaa  = 1
+aaaaa = 1
+`;
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "stringify() handles empty key",
+  fn() {
+    const src = {
+      "": "a",
+      "b": { "": "c" },
+    };
+    const actual = stringify(src);
+    const expected = `"" = "a"
+
+[b]
+"" = "c"
+`;
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "stringify() handles empty object",
+  fn() {
+    const src = {
+      "a": {},
+      "b": { "c": {} },
+    };
+    const actual = stringify(src);
+    const expected = `
+[a]
+
+[b.c]
+`;
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "stringify() handles special keys in inline object",
+  fn() {
+    const src = {
+      "a": [{ "/": "b" }, "c"],
+    };
+    const actual = stringify(src);
+    const expected = 'a = [{"/" = "b"},"c"]\n';
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "stringify() throws on invalid value",
+  fn() {
+    assertThrows(
+      () => stringify({ a: [[null]] }),
+      Error,
+      "should never reach",
+    );
+    assertThrows(
+      () => stringify({ a: [[undefined]] }),
+      Error,
+      "should never reach",
+    );
+  },
+});
