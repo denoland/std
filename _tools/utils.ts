77996a9eb15dca66ac42709dd7490cfd205b1dd1
Yoshiya Hinosawa <stibium121@gmail.com>
2024-04-30 08:37:05 +0900

    chore: update `check_deprecation.ts` (#4657)

---
+++ _tools/utils.ts
@@ -0,0 +1,36 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
+const workspaces = JSON.parse(await Deno.readTextFile("deno.json"))
+  .workspaces as string[];
+// deno-lint-ignore no-explicit-any
+const denoConfig = {} as Record<string, any>;
+for (const workspace of workspaces) {
+  const { default: config } = await import("../" + workspace + "/deno.json", {
+    with: { type: "json" },
+  });
+  denoConfig[config.name.replace("@std/", "")] = config;
+}
+
+export function resolveWorkspaceSpecifiers(
+  specifier: string,
+  referrer: string,
+) {
+  if (specifier.startsWith("../") || specifier.startsWith("./")) {
+    return new URL(specifier, referrer).href;
+  } else if (specifier.startsWith("@std/")) {
+    let [_std, pkg, exp] = specifier.split("/");
+    if (exp === undefined) {
+      exp = ".";
+    } else {
+      exp = "./" + exp;
+    }
+    const pkgPath = "../" + pkg!.replaceAll("-", "_") + "/";
+    const config = denoConfig[pkg!];
+    if (typeof config.exports === "string") {
+      return new URL(pkgPath + config.exports, import.meta.url).href;
+    }
+    return new URL(pkgPath + config.exports[exp], import.meta.url).href;
+  } else {
+    return new URL(specifier).href;
+  }
+}
