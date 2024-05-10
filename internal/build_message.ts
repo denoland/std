9c5f6f2f90c46fecdb9b5f1c8cd01e70d75c5c18
Asher Gomez <ashersaupingomez@gmail.com>
2024-05-10 11:08:18 +1000

    refactor(internal): cleanup `buildMessage()`

--- internal/build_message.ts
+++ internal/build_message.ts
@@ -6,22 +6,25 @@ import type { DiffResult, DiffType } from "./_types.ts";
 
 /**
  * Colors the output of assertion diffs
+ *
  * @param diffType Difference type, either added or removed
+ * @param background If true, colors the background instead of the text.
+ *
+ * @returns A function that colors the input string
+ *
+ * TODO(@littledivy): Remove this when we can detect true color terminals. See
+ * https://github.com/denoland/deno_std/issues/2575.
  */
 function createColor(
   diffType: DiffType,
-  { background = false } = {},
+  background = false,
 ): (s: string) => string {
-  // TODO(@littledivy): Remove this when we can detect
-  // true color terminals.
-  // https://github.com/denoland/deno_std/issues/2575
   background = false;
   switch (diffType) {
     case "added":
-      return (s: string): string =>
-        background ? bgGreen(white(s)) : green(bold(s));
+      return (s) => background ? bgGreen(white(s)) : green(bold(s));
     case "removed":
-      return (s: string): string => background ? bgRed(white(s)) : red(bold(s));
+      return (s) => background ? bgRed(white(s)) : red(bold(s));
     default:
       return white;
   }
@@ -42,32 +45,37 @@ function createSign(diffType: DiffType): string {
   }
 }
 
+/**
+ * Builds a message based on the provided diff result.
+ *
+ * @param diffResult The diff result array.
+ * @param options Optional parameters for customizing the message.
+ * @param options.stringDiff Whether to output the diff as a single string.
+ *
+ * @returns An array of strings representing the built message.
+ */
 export function buildMessage(
   diffResult: ReadonlyArray<DiffResult<string>>,
   { stringDiff = false } = {},
 ): string[] {
-  const messages: string[] = [];
-  const diffMessages: string[] = [];
-  messages.push("");
-  messages.push("");
-  messages.push(
+  const messages = [
+    "",
+    "",
     `    ${gray(bold("[Diff]"))} ${red(bold("Actual"))} / ${
       green(bold("Expected"))
     }`,
-  );
-  messages.push("");
-  messages.push("");
-  diffResult.forEach((result) => {
-    const c = createColor(result.type);
+    "",
+    "",
+  ];
+  const diffMessages = diffResult.map((result) => {
+    const color = createColor(result.type);
     const line = result.details?.map((detail) =>
       detail.type !== "common"
-        ? createColor(detail.type, { background: true })(detail.value)
+        ? createColor(detail.type, true)(detail.value)
         : detail.value
     ).join("") ?? result.value;
-    diffMessages.push(c(`${createSign(result.type)}${line}`));
+    return color(`${createSign(result.type)}${line}`);
   });
-  messages.push(...(stringDiff ? [diffMessages.join("")] : diffMessages));
-  messages.push("");
-
+  messages.push(...(stringDiff ? [diffMessages.join("")] : diffMessages), "");
   return messages;
 }
