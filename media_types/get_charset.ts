1271a7f392d8fc0839c65ffe9d80b36e1ecddb3f
Asher Gomez <ashersaupingomez@gmail.com>
2024-05-15 16:33:05 +1000

    docs(media-types): polish documentation (#4731)

--- media_types/get_charset.ts
+++ media_types/get_charset.ts
@@ -9,14 +9,20 @@ import { db, type KeyOfDb } from "./_db.ts";
  * Given a media type or header value, identify the encoding charset. If the
  * charset cannot be determined, the function returns `undefined`.
  *
- * @example
+ * @param type The media type or header value to get the charset for.
+ *
+ * @returns The charset for the given media type or header value, or `undefined`
+ * if the charset cannot be determined.
+ *
+ * @example Usage
  * ```ts
  * import { getCharset } from "@std/media-types/get-charset";
+ * import { assertEquals } from "@std/assert/assert-equals";
  *
- * getCharset("text/plain"); // "UTF-8"
- * getCharset("application/foo"); // undefined
- * getCharset("application/news-checkgroups"); // "US-ASCII"
- * getCharset("application/news-checkgroups; charset=UTF-8"); // "UTF-8"
+ * assertEquals(getCharset("text/plain"), "UTF-8");
+ * assertEquals(getCharset("application/foo"), undefined);
+ * assertEquals(getCharset("application/news-checkgroups"), "US-ASCII");
+ * assertEquals(getCharset("application/news-checkgroups; charset=UTF-8"), "UTF-8");
  * ```
  */
 export function getCharset(type: string): string | undefined {
