c9167d693a5b54763700ab622f071de77e3dac0f
Yoshiya Hinosawa <stibium121@gmail.com>
2024-05-19 23:02:09 +0900

    docs(collections): fix typos and improve example (#4763)
    Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

--- collections/drop_last_while.ts
+++ collections/drop_last_while.ts
@@ -18,11 +18,11 @@
  * import { dropLastWhile } from "@std/collections/drop-last-while";
  * import { assertEquals } from "@std/assert/assert-equals";
  *
- * const numbers = [20, 33, 44];
+ * const numbers = [11, 42, 55, 20, 33, 44];
  *
  * const notFortyFour = dropLastWhile(numbers, (number) => number > 30);
  *
- * assertEquals(notFortyFour, [20]);
+ * assertEquals(notFortyFour, [11, 42, 55, 20]);
  * ```
  */
 export function dropLastWhile<T>(
