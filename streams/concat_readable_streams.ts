6948d9fc548ec4d56a889de7b6d214ad8e1f735d
Asher Gomez <ashersaupingomez@gmail.com>
2024-05-21 08:18:48 +1000

    add Leo as co-author
    Co-authored-by: crowlKats <crowlkats@toaxl.com>

--- streams/concat_readable_streams.ts
+++ streams/concat_readable_streams.ts
@@ -21,7 +21,7 @@
  *
  * assertEquals(
  *   await Array.fromAsync(concatReadableStreams(stream1, stream2, stream3)),
- *   [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ *   [1, 2, 3, 4, 5, 6, 7, 8, 9],
  * );
  * ```
  */
