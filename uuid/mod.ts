cc24398cac784e6fdd1e3ef9a6dec9578d84743b
Marvin Hagemeister <marvin@deno.com>
2024-05-21 09:39:47 +0200

    docs(uuid): update module docs

--- uuid/mod.ts
+++ uuid/mod.ts
@@ -2,16 +2,25 @@
 // This module is browser compatible.
 
 /**
- * Generators and validators for UUIDs for versions v1, v3, v4 and v5.
+ * Generators and validators for [RFC4122](https://www.rfc-editor.org/rfc/rfc4122.html)
+ * UUIDs for versions v1, v3, v4 and v5.
  *
- * Use {@linkcode crypto.randomUUID} for v4 generating v4 UUIDs.
+ * Use the built-in [`crypto.randomUUID`](https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID)
+ * function instead of this package, if you only need to generate v4 UUIDs.
  *
- * Based on {@linkcode https://www.npmjs.com/package/uuid | npm:uuid}, which is
- * based on {@link https://www.rfc-editor.org/rfc/rfc4122.html | RFC 4122}.
- *
- * Support for RFC4122 version 1, 3, 4, and 5 UUIDs
+ * Based on {@linkcode https://www.npmjs.com/package/uuid | npm:uuid}.
  *
  * This module is browser compatible.
+ *
+ * @example
+ * ```js
+ * import { generate } from "@std/uuid/v5";
+ *
+ * const NAMESPACE_URL = "6ba7b811-9dad-11d1-80b4-00c04fd430c8";
+ *
+ * const uuid = await generate(NAMESPACE_URL, new TextEncoder().encode("python.org"));
+ * uuid === "7af94e2b-4dd9-50f0-9c9a-8a48519bdef0" // true
+ * ```
  *
  * @module
  */
