e913923c20f1b3891abf43132161910990293992
Nathan Whitaker <nathan@deno.com>
2024-05-21 12:51:06 -0700

    Add module docs

--- encoding/mod.ts
+++ encoding/mod.ts
@@ -1,5 +1,21 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+/**
+ * Utilities for encoding and decoding common formats like hex, base64, and varint.
+ *
+ * This module is browser compatible.
+ *
+ * ```ts
+ * import { encodeBase64, decodeBase64 } from "@std/encoding";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * assertEquals(encodeBase64("foobar"), "Zm9vYmFy");
+ * assertEquals(decodeBase64("Zm9vYmFy"), "foobar");
+ * ```
+ *
+ * @module
+ */
+
 export * from "./ascii85.ts";
 export * from "./base32.ts";
 export * from "./base58.ts";
