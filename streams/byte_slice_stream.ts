65ba402685df4b4ca0bba0bcecf2f934a77671bb
Yusuke Tanaka <wing0920@gmail.com>
2024-05-23 15:51:22 +0900

    improve streams/byte_slice_stream.ts

--- streams/byte_slice_stream.ts
+++ streams/byte_slice_stream.ts
@@ -7,13 +7,33 @@ import { assert } from "@std/assert/assert";
  * A transform stream that only transforms from the zero-indexed `start` and
  * `end` bytes (both inclusive).
  *
- * @example
+ * @example Basic usage
  * ```ts
  * import { ByteSliceStream } from "@std/streams/byte-slice-stream";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const stream = ReadableStream.from([
+ *   new Uint8Array([0, 1]),
+ *   new Uint8Array([2, 3, 4]),
+ * ]);
+ * const slicedStream = stream.pipeThrough(new ByteSliceStream(1, 3));
+ *
+ * assertEquals(
+ *   await Array.fromAsync(slicedStream),
+ *  [new Uint8Array([1]), new Uint8Array([2, 3])]
+ * );
+ * ```
+ *
+ * @example Get a range of bytes from a fetch response body
+ * ```ts
+ * import { ByteSliceStream } from "@std/streams/byte-slice-stream";
+ * import { assertEquals } from "@std/assert/assert-equals";
  *
  * const response = await fetch("https://example.com");
  * const rangedStream = response.body!
  *   .pipeThrough(new ByteSliceStream(3, 8));
+ * const collected = await Array.fromAsync(rangedStream);
+ * assertEquals(collected[0].length, 6);
  * ```
  */
 export class ByteSliceStream extends TransformStream<Uint8Array, Uint8Array> {
