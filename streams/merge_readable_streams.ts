7d0e56daea69dc889888374750172d29594dc8d6
Yusuke Tanaka <wing0920@gmail.com>
2024-05-24 01:19:50 +0900

    improve streams/merge_readable_streams.ts

--- streams/merge_readable_streams.ts
+++ streams/merge_readable_streams.ts
@@ -5,15 +5,31 @@
  * If a stream ends before other ones, the other will continue adding data,
  * and the finished one will not add any more data.
  *
- * @example
+ * @example Merge 2 streams
  * ```ts
  * import { mergeReadableStreams } from "@std/streams/merge-readable-streams";
+ * import { assertEquals } from "@std/assert/assert-equals";
  *
- * const stream1 = ReadableStream.from(["1", "2", "3"]);
- * const stream2 = ReadableStream.from(["a", "b", "c"]);
+ * const stream1 = ReadableStream.from([1, 2]);
+ * const stream2 = ReadableStream.from([3, 4, 5]);
  *
- * // ["2", "c", "a", "b", "3", "1"]
- * await Array.fromAsync(mergeReadableStreams(stream1, stream2));
+ * const mergedStream = mergeReadableStreams(stream1, stream2);
+ * const merged = await Array.fromAsync(mergedStream);
+ * assertEquals(merged.toSorted(), [1, 2, 3, 4, 5]);
+ * ```
+ *
+ * @example Merge 3 streams
+ * ```ts
+ * import { mergeReadableStreams } from "@std/streams/merge-readable-streams";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const stream1 = ReadableStream.from([1, 2]);
+ * const stream2 = ReadableStream.from([3, 4, 5]);
+ * const stream3 = ReadableStream.from([6]);
+ *
+ * const mergedStream = mergeReadableStreams(stream1, stream2, stream3);
+ * const merged = await Array.fromAsync(mergedStream);
+ * assertEquals(merged.toSorted(), [1, 2, 3, 4, 5, 6]);
  * ```
  */
 export function mergeReadableStreams<T>(
