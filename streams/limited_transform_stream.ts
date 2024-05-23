5ed8f71818b33e94f73c4fd09eb2f7f157d6c4c6
Yusuke Tanaka <wing0920@gmail.com>
2024-05-24 00:45:03 +0900

    improve streams/limited_transform_stream.ts

--- streams/limited_transform_stream.ts
+++ streams/limited_transform_stream.ts
@@ -6,13 +6,56 @@
  * chunks.
  *
  * If `options.error` is set, then instead of terminating the stream,
- * an error will be thrown.
+ * a {@linkcode RangeError} will be thrown when the total number of enqueued
+ * chunks is about to exceed the specified size.
  *
- * @example
+ * @example `size` is equal to the total number of chunks
  * ```ts
  * import { LimitedTransformStream } from "@std/streams/limited-transform-stream";
- * const res = await fetch("https://example.com");
- * const parts = res.body!.pipeThrough(new LimitedTransformStream(50));
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const stream = ReadableStream.from(["1234", "5678"]);
+ * const transformed = stream.pipeThrough(
+ *   new LimitedTransformStream(2),
+ * );
+ *
+ * // All chunks were read
+ * assertEquals(
+ *   await Array.fromAsync(transformed),
+ *   ["1234", "5678"],
+ * );
+ * ```
+ *
+ * @example `size` is less than the total number of chunks
+ * ```ts
+ * import { LimitedTransformStream } from "@std/streams/limited-transform-stream";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const stream = ReadableStream.from(["1234", "5678"]);
+ * const transformed = stream.pipeThrough(
+ *   new LimitedTransformStream(1),
+ * );
+ *
+ * // Only the first chunk was read
+ * assertEquals(
+ *   await Array.fromAsync(transformed),
+ *   ["1234"],
+ * );
+ * ```
+ *
+ * @example error: true
+ * ```ts
+ * import { LimitedTransformStream } from "@std/streams/limited-transform-stream";
+ * import { assertRejects } from "@std/assert/assert-rejects";
+ *
+ * const stream = ReadableStream.from(["1234", "5678"]);
+ * const transformed = stream.pipeThrough(
+ *   new LimitedTransformStream(1, { error: true }),
+ * );
+ *
+ * await assertRejects(async () => {
+ *   await Array.fromAsync(transformed);
+ * }, RangeError);
  * ```
  */
 export class LimitedTransformStream<T> extends TransformStream<T, T> {
