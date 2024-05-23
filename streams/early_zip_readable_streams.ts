0bc0ad1d2e6bfbec0b9f74ff72b1a7bbb8c25abd
Yusuke Tanaka <wing0920@gmail.com>
2024-05-24 01:00:00 +0900

    further improve streams/early_zip_readable_streams.ts

--- streams/early_zip_readable_streams.ts
+++ streams/early_zip_readable_streams.ts
@@ -2,9 +2,12 @@
 // This module is browser compatible.
 
 /**
- * Merge multiple streams into a single one, taking order into account, and each stream
- * will wait for a chunk to enqueue before the next stream can append another chunk.
- * If a stream ends before other ones, the others will be cancelled.
+ * Merge multiple streams into a single one, taking order into account, and each
+ * stream will wait for a chunk to enqueue before the next stream can append
+ * another chunk.
+ * If a stream ends before other ones, the others will be cancelled after the
+ * last chunk of said straem is read. See the examples below for more
+ * compresensible information.
  *
  * @example Zip 2 streams with the same length
  * ```ts
@@ -21,22 +24,43 @@
  * );
  * ```
  *
- * @example Zip 2 streams with differen length
+ * @example Zip 2 streams with different length (first one is shorter)
  * ```ts
  * import { earlyZipReadableStreams } from "@std/streams/early-zip-readable-streams";
  * import { assertEquals } from "@std/assert/assert-equals";
  *
  * const stream1 = ReadableStream.from(["1", "2"]);
- * const stream2 = ReadableStream.from(["a", "b", "c"]);
+ * const stream2 = ReadableStream.from(["a", "b", "c", "d"]);
  * const zippedStream = earlyZipReadableStreams(stream1, stream2);
  *
+ * // The first stream ends before the second one. When the first stream ends,
+ * // the second one is cancelled and no more data is read or added to the
+ * // zipped stream.
  * assertEquals(
  *   await Array.fromAsync(zippedStream),
  *   ["1", "a", "2", "b"],
  * );
  * ```
  *
- * @example Zip 3 streams with differen length
+ * @example Zip 2 streams with different length (first one is longer)
+ * ```ts
+ * import { earlyZipReadableStreams } from "@std/streams/early-zip-readable-streams";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const stream1 = ReadableStream.from(["1", "2", "3", "4"]);
+ * const stream2 = ReadableStream.from(["a", "b"]);
+ * const zippedStream = earlyZipReadableStreams(stream1, stream2);
+ *
+ * // The second stream ends before the first one. When the second stream ends,
+ * // the first one is cancelled, but the chunk of "3" is already read so it
+ * // is added to the zipped stream.
+ * assertEquals(
+ *   await Array.fromAsync(zippedStream),
+ *   ["1", "a", "2", "b", "3"],
+ * );
+ * ```
+ *
+ * @example Zip 3 streams
  * ```ts
  * import { earlyZipReadableStreams } from "@std/streams/early-zip-readable-streams";
  * import { assertEquals } from "@std/assert/assert-equals";
