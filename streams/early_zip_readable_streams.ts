2d93cedafc46f269dd7e4585e25ba4de177d381a
Yusuke Tanaka <wing0920@gmail.com>
2024-05-24 15:13:46 +0900

    improve streams/zip_readable_streams.ts

--- streams/early_zip_readable_streams.ts
+++ streams/early_zip_readable_streams.ts
@@ -5,9 +5,11 @@
  * Merge multiple streams into a single one, taking order into account, and each
  * stream will wait for a chunk to enqueue before the next stream can append
  * another chunk.
+ *
  * If a stream ends before other ones, the others will be cancelled after the
  * last chunk of said straem is read. See the examples below for more
- * compresensible information.
+ * compresensible information. If you want to continue reading the other streams
+ * even after one of them ends, use {@linkcode zipReadableStreams}.
  *
  * @example Zip 2 streams with the same length
  * ```ts
