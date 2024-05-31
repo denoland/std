e0fa3e0fd82ba8ec6e0ccff154289e39a74ca4d0
Yoshiya Hinosawa <stibium121@gmail.com>
2024-05-31 13:34:39 +0900

    add posix docs

--- path/posix/join.ts
+++ path/posix/join.ts
@@ -6,7 +6,18 @@ import { normalize } from "./normalize.ts";
 
 /**
  * Join all given a sequence of `paths`,then normalizes the resulting path.
- * @param paths to be joined and normalized
+ *
+ * @example Usage
+ * ```ts
+ * import { join } from "@std/path/posix/join";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const path = join("/foo", "bar", "baz/asdf", "quux", "..");
+ * assertEquals(path, "/foo/bar/baz/asdf");
+ * ```
+ *
+ * @param paths The paths to join.
+ * @returns The joined path.
  */
 export function join(...paths: string[]): string {
   if (paths.length === 0) return ".";
--- path/posix/join_globs.ts
+++ path/posix/join_globs.ts
@@ -8,7 +8,22 @@ import { normalizeGlob } from "./normalize_glob.ts";
 
 export type { GlobOptions };
 
-/** Like join(), but doesn't collapse "**\/.." when `globstar` is true. */
+/**
+ * Like join(), but doesn't collapse "**\/.." when `globstar` is true.
+ *
+ * @example Usage
+ * ```ts
+ * import { joinGlobs } from "@std/path/posix/join-globs";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const path = joinGlobs(["foo", "bar", "**"], { globstar: true });
+ * assertEquals(path, "foo/bar/**");
+ * ```
+ *
+ * @param globs The globs to join.
+ * @param options The options to use.
+ * @returns The joined path.
+ */
 export function joinGlobs(
   globs: string[],
   { extended = true, globstar = false }: GlobOptions = {},
