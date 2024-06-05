0146d96765a9b00234860dced5f014483bf384a9
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-06-04 20:58:06 -0400

    Refactored getIsWindows.

--- path/_os.ts
+++ path/_os.ts
@@ -1,21 +1,24 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export const isWindows = getIsWindows();
+export const isWindows: boolean = getIsWindows() === true;
 
-function getIsWindows(): boolean {
+function getIsWindows(): boolean | true {
+  return getIsWindowsOnDeno() ?? getIsWindowsOnBrowser() ?? false;
+}
+
+function getIsWindowsOnDeno(): boolean | undefined {
   // deno-lint-ignore no-explicit-any
   const { Deno } = globalThis as any;
   if (typeof Deno?.build?.os === "string") {
     return Deno.build.os === "windows";
   }
+}
 
+function getIsWindowsOnBrowser(): boolean | undefined {
   // deno-lint-ignore no-explicit-any
   const { navigator } = globalThis as any;
   if (navigator?.userAgent?.includes?.("Windows")) {
     return true;
   }
-
-  // default to false;
-  return false;
 }
