b8d94177a5b2c061e507f0b65dec01d460bccacd
Asher Gomez <ashersaupingomez@gmail.com>
2024-06-06 07:16:52 +1000

    docs(url): complete documentation (#4965)
    * docs(url): complete documentation
    
    * suggestions

--- url/basename.ts
+++ url/basename.ts
@@ -8,6 +8,8 @@ import { strip } from "./_strip.ts";
  * Returns the base name of a URL or URL string, optionally removing a suffix.
  *
  * Trailing `/`s are ignored. If no path is present, the host name is returned.
+ * If a suffix is provided, it will be removed from the base name. URL queries
+ * and hashes are ignored.
  *
  * @param url The URL from which to extract the base name.
  * @param suffix An optional suffix to remove from the base name.
@@ -16,31 +18,28 @@ import { strip } from "./_strip.ts";
  * @example Basic usage
  * ```ts
  * import { basename } from "@std/url/basename";
+ * import { assertEquals } from "@std/assert/assert-equals";
  *
- * basename("https://deno.land/std/assert/mod.ts"); // "mod.ts"
- *
- * basename(new URL("https://deno.land/std/assert/mod.ts")); // "mod.ts"
- *
- * basename("https://deno.land/std/assert/mod.ts?a=b"); // "mod.ts"
- *
- * basename("https://deno.land/std/assert/mod.ts#header"); // "mod.ts"
- *
- * basename("https://deno.land/"); // "deno.land"
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts"), "mod.ts");
+ * assertEquals(basename(new URL("https://deno.land/std/assert/mod.ts")), "mod.ts");
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts?a=b"), "mod.ts");
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts#header"), "mod.ts");
+ * assertEquals(basename("https://deno.land/"), "deno.land");
  * ```
  *
  * @example Removing a suffix
+ *
+ * Defining a suffix will remove it from the base name.
+ *
  * ```ts
  * import { basename } from "@std/url/basename";
+ * import { assertEquals } from "@std/assert/assert-equals";
  *
- * basename("https://deno.land/std/assert/mod.ts", ".ts"); // "mod"
- *
- * basename(new URL("https://deno.land/std/assert/mod.ts"), ".ts"); // "mod"
- *
- * basename("https://deno.land/std/assert/mod.ts?a=b", ".ts"); // "mod"
- *
- * basename("https://deno.land/std/assert/mod.ts#header", ".ts"); // "mod.ts"
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts", ".ts"), "mod");
+ * assertEquals(basename(new URL("https://deno.land/std/assert/mod.ts"), ".ts"), "mod");
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts?a=b", ".ts"), "mod");
+ * assertEquals(basename("https://deno.land/std/assert/mod.ts#header", ".ts"), "mod");
  * ```
- * Defining a suffix will remove it from the base name.
  */
 export function basename(url: string | URL, suffix?: string): string {
   url = new URL(url);
