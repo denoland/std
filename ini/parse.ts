ec73b76d91e6c7f21e940cc89f2b09da85125107
Yoshiya Hinosawa <stibium121@gmail.com>
2024-06-11 16:05:04 +0900

    docs(ini): improve ini docs (#5020)

--- ini/parse.ts
+++ ini/parse.ts
@@ -2,7 +2,59 @@
 // This module is browser compatible.
 
 import { IniMap, type ParseOptions } from "./ini_map.ts";
-/** Parse an INI config string into an object. Provide formatting options to override the default assignment operator. */
+/**
+ * Parse an INI config string into an object. Provide formatting options to override the default assignment operator.
+ *
+ * @example Usage
+ * ```ts
+ * import { parse } from "@std/ini/parse";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const parsed = parse(`
+ * key = value
+ *
+ * [section 1]
+ * foo = Hello
+ * baz = World
+ * `);
+ *
+ * assertEquals(parsed, { key: "value", "section 1": { foo: "Hello", baz: "World" } })
+ * ```
+ *
+ * @example Using custom reviver
+ * ```ts
+ * import { parse } from "@std/ini/parse";
+ * import { assertEquals } from "@std/assert/assert-equals";
+ *
+ * const parsed = parse(`
+ * [section Foo]
+ * date = 2012-10-10
+ * amount = 12345
+ * `, {
+ *   reviver(key, value, section) {
+ *     if (section === "section Foo") {
+ *       if (key === "date") {
+ *         return new Date(value);
+ *       } else if (key === "amount") {
+ *         return +value;
+ *       }
+ *     }
+ *     return value;
+ *   }
+ * });
+ *
+ * assertEquals(parsed, {
+ *   "section Foo": {
+ *     date: new Date("2012-10-10"),
+ *     amount: 12345,
+ *   }
+ * })
+ * ```
+ *
+ * @param text The text to parse
+ * @param options The options to use
+ * @return The parsed object
+ */
 export function parse(
   text: string,
   options?: ParseOptions,
