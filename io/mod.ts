5341f8a38fd5ac34d5069722739093e94470ee49
Yoshiya Hinosawa <stibium121@gmail.com>
2024-06-14 20:22:36 +0900

    BREAKING(io): remove `BufReader`, `BufWriter`, `copyN`, `LimitedReader`, `MultiReader`, `readDelim`, `readInt`, `readLines`, `readLong`, `readRange`, `readShort`, `readStringDelim`, `sliceLongToBytes`, `StringReader`, and `StringWriter`

--- io/mod.ts
+++ io/mod.ts
@@ -1,34 +1,22 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 /**
- * Utilities for working with Deno's readers, writers, and web streams.
+ * Utilities for working with Deno's {@linkcode Reader | readers} and {@linkcode Writer | writers}.
  *
- * `Reader` and `Writer` interfaces are deprecated in Deno, and so many of these
- * utilities are also deprecated. Consider using web streams instead.
+ * This package provides basic utilities for `Reader` and `Writer` interfaces
+ * mainly for the backward compatibility. `Reader` and `Writer` are not
+ * recommended anymore for performing I/O operations in Deno. Instead,
+ * you should consider using
+ * {@linkcode https://developer.mozilla.org/en-US/docs/Web/API/Streams_API | Web Streams API}.
  *
  * @module
  */
 
-export * from "./buf_reader.ts";
-export * from "./buf_writer.ts";
 export * from "./buffer.ts";
 export * from "./copy.ts";
-export * from "./copy_n.ts";
 export * from "./iterate_reader.ts";
-export * from "./limited_reader.ts";
-export * from "./multi_reader.ts";
 export * from "./read_all.ts";
-export * from "./read_delim.ts";
-export * from "./read_int.ts";
-export * from "./read_lines.ts";
-export * from "./read_long.ts";
-export * from "./read_range.ts";
-export * from "./read_short.ts";
-export * from "./read_string_delim.ts";
 export * from "./reader_from_stream_reader.ts";
-export * from "./slice_long_to_bytes.ts";
-export * from "./string_reader.ts";
-export * from "./string_writer.ts";
 export * from "./to_readable_stream.ts";
 export * from "./to_writable_stream.ts";
 export * from "./types.ts";
