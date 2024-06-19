8fb9eb0f4d9abc6962e27ea677aaa7fc07045fd3
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-06-19 16:50:26 +1000

    add(streams): test for cancelling stream

--- streams/early_zip_readable_streams_test.ts
+++ streams/early_zip_readable_streams_test.ts
@@ -1,62 +1,77 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-import { earlyZipReadableStreams } from "./early_zip_readable_streams.ts";
-import { assertEquals } from "@std/assert";
+import { earlyZipReadableStreams } from "./early_zip_readable_streams.ts"
+import { assertEquals } from "@std/assert"
 
 Deno.test("earlyZipReadableStreams() handles short first", async () => {
-  const textStream = ReadableStream.from(["1", "2", "3"]);
-  const textStream2 = ReadableStream.from(["a", "b", "c", "d", "e"]);
+	const textStream = ReadableStream.from(["1", "2", "3"])
+	const textStream2 = ReadableStream.from(["a", "b", "c", "d", "e"])
 
-  const buf = await Array.fromAsync(
-    earlyZipReadableStreams(textStream, textStream2),
-  );
+	const buf = await Array.fromAsync(
+		earlyZipReadableStreams(textStream, textStream2),
+	)
 
-  assertEquals(buf, [
-    "1",
-    "a",
-    "2",
-    "b",
-    "3",
-    "c",
-  ]);
-});
+	assertEquals(buf, [
+		"1",
+		"a",
+		"2",
+		"b",
+		"3",
+		"c",
+	])
+})
 
 Deno.test("earlyZipReadableStreams() handles long first", async () => {
-  const textStream = ReadableStream.from(["a", "b", "c", "d", "e"]);
-  const textStream2 = ReadableStream.from(["1", "2", "3"]);
+	const textStream = ReadableStream.from(["a", "b", "c", "d", "e"])
+	const textStream2 = ReadableStream.from(["1", "2", "3"])
 
-  const buf = await Array.fromAsync(
-    earlyZipReadableStreams(textStream, textStream2),
-  );
+	const buf = await Array.fromAsync(
+		earlyZipReadableStreams(textStream, textStream2),
+	)
 
-  assertEquals(buf, [
-    "a",
-    "1",
-    "b",
-    "2",
-    "c",
-    "3",
-    "d",
-  ]);
-});
+	assertEquals(buf, [
+		"a",
+		"1",
+		"b",
+		"2",
+		"c",
+		"3",
+		"d",
+	])
+})
 
 Deno.test("earlyZipReadableStreams() can zip three streams", async () => {
-  const textStream = ReadableStream.from(["a", "b", "c", "d", "e"]);
-  const textStream2 = ReadableStream.from(["1", "2", "3"]);
-  const textStream3 = ReadableStream.from(["x", "y"]);
+	const textStream = ReadableStream.from(["a", "b", "c", "d", "e"])
+	const textStream2 = ReadableStream.from(["1", "2", "3"])
+	const textStream3 = ReadableStream.from(["x", "y"])
 
-  const buf = await Array.fromAsync(
-    earlyZipReadableStreams(textStream, textStream2, textStream3),
-  );
+	const buf = await Array.fromAsync(
+		earlyZipReadableStreams(textStream, textStream2, textStream3),
+	)
 
-  assertEquals(buf, [
-    "a",
-    "1",
-    "x",
-    "b",
-    "2",
-    "y",
-    "c",
-    "3",
-  ]);
-});
+	assertEquals(buf, [
+		"a",
+		"1",
+		"x",
+		"b",
+		"2",
+		"y",
+		"c",
+		"3",
+	])
+})
+
+Deno.test("earlyZipReadableStreams() forwards cancel()", async () => {
+	const streams = new Array(10).fill(false).map(() => new ReadableStream(
+		{
+			pull(controller) {
+				controller.enqueue('chunk')
+			},
+			cancel(reason) {
+				assertEquals(reason, 'I was cancelled!')
+			}
+		}
+	))
+
+	await earlyZipReadableStreams(...streams).cancel('I was cancelled!')
+})
