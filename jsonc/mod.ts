65d465068aec64f1751bf406ee722211155d3ffd
Asher Gomez <ashersaupingomez@gmail.com>
2024-06-24 15:58:56 +1000

    refactor(jsonc): minor cleanups (#5114)
    * refactor(jsonc): minor cleanups
    
    * cleanups

--- jsonc/mod.ts
+++ jsonc/mod.ts
@@ -2,9 +2,12 @@
 // This module is browser compatible.
 
 /**
- * Provides tools for working with JSONC (JSON with comments). Currently, this
- * module only provides a means of parsing JSONC. JSONC serialization is not
- * yet supported.
+ * Provides tools for working with
+ * {@link https://code.visualstudio.com/docs/languages/json#_json-with-comments | JSONC}
+ * (JSON with comments).
+ *
+ * Currently, this module only provides a means of parsing JSONC. JSONC
+ * serialization is not yet supported.
  *
  * ```ts
  * import { parse } from "@std/jsonc";
