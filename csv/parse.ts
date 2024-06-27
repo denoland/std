230c6fc8f24f555e2b87dd8911b934bcfa862401
Asher Gomez <ashersaupingomez@gmail.com>
2024-06-27 17:19:27 +1000

    tweaks

--- csv/parse.ts
+++ csv/parse.ts
@@ -308,12 +308,13 @@ export interface ParseOptions {
    * Enabling checking number of expected fields for each row.
    *
    * If positive, each record is required to have the given number of fields.
-   * If === 0, it will be set to the number of fields in the first row, so that
+   * If 0, it will be set to the number of fields in the first row, so that
    * future rows must have the same field count.
    * If negative, no check is made and records may have a variable number of
    * fields.
    *
-   * If the wrong number of fields is in a row, a `ParseError` is thrown.
+   * If the wrong number of fields is in a row, a {@linkcode ParseError} is
+   * thrown.
    */
   fieldsPerRecord?: number;
   /**
