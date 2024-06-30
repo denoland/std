6f2d56b1c71235d32521d3722cdd51d18eb0e91a
Asher Gomez <ashersaupingomez@gmail.com>
2024-06-30 18:52:35 +1000

    fix

--- media_types/vendor/_update.ts
+++ media_types/vendor/_update.ts
@@ -7,7 +7,7 @@ const db = await response.text();
 const result =
   `// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
-// Copied and converted from https://raw.githubusercontent.com/jshttp/mime-db/master/db.json
+// deno-fmt-ignore-file
 
 export default ${db.slice(0, -1)} as const;`;
 await Deno.writeTextFile(new URL("./db.ts", import.meta.url), result);
