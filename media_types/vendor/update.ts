09070c1853d675a7a16dc087856cd2ec234336ae
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-01 07:53:23 +1000

    tweak

--- media_types/vendor/update.ts
+++ media_types/vendor/update.ts
@@ -1,4 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+// `deno fmt` must be run after running this script.
+
 const response = await fetch(
   "https://raw.githubusercontent.com/jshttp/mime-db/master/db.json",
 );
@@ -7,7 +9,6 @@ const db = await response.text();
 const result =
   `// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // This module is browser compatible.
-// deno-fmt-ignore-file
 
 export default ${db.slice(0, -1)} as const;`;
 await Deno.writeTextFile(new URL("./db.ts", import.meta.url), result);
