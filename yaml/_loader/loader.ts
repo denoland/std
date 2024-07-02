62c773a606df82fa1218b1b88c465aba0fe5c617
Tim <tim.reichen@gmail.com>
2024-07-02 11:12:05 +0200

    initial commit

--- yaml/_loader/loader.ts
+++ yaml/_loader/loader.ts
@@ -1656,12 +1656,8 @@ function readDocument(state: LoaderState) {
   }
 }
 
-export function loadDocuments(
-  input: string,
-  options?: LoaderStateOptions,
-): unknown[] {
+function sanitizeInput(input: string) {
   input = String(input);
-  options = options || {};
 
   if (input.length !== 0) {
     // Add tailing `\n` if not exists
@@ -1678,10 +1674,21 @@ export function loadDocuments(
     }
   }
 
-  const state = new LoaderState(input, options);
-
   // Use 0 as string terminator. That significantly simplifies bounds check.
-  state.input += "\0";
+  input += "\0";
+
+  return input;
+}
+
+export function loadDocuments(
+  input: string,
+  options?: LoaderStateOptions,
+): unknown[] {
+  input = sanitizeInput(input);
+
+  options = options || {};
+
+  const state = new LoaderState(input, options);
 
   while (state.input.charCodeAt(state.position) === 0x20 /* Space */) {
     state.lineIndent += 1;
