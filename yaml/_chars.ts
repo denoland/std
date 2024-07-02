7e235e62588889b78335bc9ed4b57d4738b215af
Tim <tim.reichen@gmail.com>
2024-07-02 11:06:06 +0200

    add copyright header

--- yaml/_chars.ts
+++ yaml/_chars.ts
@@ -1,3 +1,8 @@
+// Ported from js-yaml v3.13.1:
+// https://github.com/nodeca/js-yaml/commit/665aadda42349dcae869f12040d9b10ef18d12da
+// Copyright 2011-2015 by Vitaly Puzrin. All rights reserved. MIT license.
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
 export const TAB = 0x09; /* Tab */
 export const LINE_FEED = 0x0a; /* LF */
 export const CARRIAGE_RETURN = 0x0d; /* CR */
