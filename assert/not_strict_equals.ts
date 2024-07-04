29f5612eb131becddc74697fedd13a22e54ca833
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-04 15:40:05 +0900

    docs(assert): improve `assertNotStrictEquals` example (#5295)

--- assert/not_strict_equals.ts
+++ assert/not_strict_equals.ts
@@ -12,8 +12,11 @@ import { format } from "@std/internal/format";
  * ```ts no-eval
  * import { assertNotStrictEquals } from "@std/assert";
  *
- * assertNotStrictEquals(1, 1); // Doesn't throw
- * assertNotStrictEquals(1, 2); // Throws
+ * assertNotStrictEquals(1, 1); // Throws
+ * assertNotStrictEquals(1, 2); // Doesn't throw
+ *
+ * assertNotStrictEquals(0, 0); // Throws
+ * assertNotStrictEquals(0, -0); // Doesn't throw
  * ```
  *
  * @typeParam T The type of the values to compare.
