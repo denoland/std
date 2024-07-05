becbe909d97ec2e453f29048571c90fb883b16ec
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-05 16:37:53 +1000

    refactor(yaml): simplify `YamlError`

--- yaml/_error.ts
+++ yaml/_error.ts
@@ -6,15 +6,8 @@
 import type { Mark } from "./_mark.ts";
 
 export class YamlError extends Error {
-  constructor(
-    message = "(unknown reason)",
-    protected mark: Mark | string = "",
-  ) {
+  constructor(message: string, mark?: Mark) {
     super(`${message} ${mark}`);
     this.name = this.constructor.name;
-  }
-
-  override toString(): string {
-    return `${this.name}: ${this.message} ${this.mark}`;
   }
 }
