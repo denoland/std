c3f8b10bb19e216aeb3d971321c68f231777c594
Tim <tim.reichen@gmail.com>
2024-07-09 14:52:23 +0200

    initial commit

--- front_matter/_formats.ts
+++ front_matter/_formats.ts
@@ -1,52 +1,10 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-type Delimiter = string | [begin: string, end: string];
+import type { Format } from "./_types.ts";
 
-function getBeginToken(delimiter: Delimiter): string {
-  return Array.isArray(delimiter) ? delimiter[0] : delimiter;
-}
-
-function getEndToken(delimiter: Delimiter): string {
-  return Array.isArray(delimiter) ? delimiter[1] : delimiter;
-}
-
-function createRegExps(delimiters: Delimiter[]): [RegExp, RegExp] {
-  const beginPattern = "(" + delimiters.map(getBeginToken).join("|") + ")";
-  const pattern = "^(" +
-    "\\ufeff?" + // Maybe byte order mark
-    beginPattern +
-    "$([\\s\\S]+?)" +
-    "^(?:" + delimiters.map(getEndToken).join("|") + ")\\s*" +
-    "$" +
-    (globalThis?.Deno?.build?.os === "windows" ? "\\r?" : "") +
-    "(?:\\n)?)";
-
-  return [
-    new RegExp("^" + beginPattern + "$", "im"),
-    new RegExp(pattern, "im"),
-  ];
-}
-
-const [RECOGNIZE_YAML_REGEXP, EXTRACT_YAML_REGEXP] = createRegExps(
-  [
-    ["---yaml", "---"],
-    "= yaml =",
-    "---",
-  ],
-);
-const [RECOGNIZE_TOML_REGEXP, EXTRACT_TOML_REGEXP] = createRegExps(
-  [
-    ["---toml", "---"],
-    "\\+\\+\\+",
-    "= toml =",
-  ],
-);
-const [RECOGNIZE_JSON_REGEXP, EXTRACT_JSON_REGEXP] = createRegExps(
-  [
-    ["---json", "---"],
-    "= json =",
-  ],
-);
+const RECOGNIZE_YAML_REGEXP = /^---yaml|= yaml =|---$/im;
+const RECOGNIZE_TOML_REGEXP = /^---toml|\+\+\+|= toml =$/im;
+const RECOGNIZE_JSON_REGEXP = /^---json|= json =$/im;
 
 export const RECOGNIZE_REGEXP_MAP = {
   yaml: RECOGNIZE_YAML_REGEXP,
@@ -54,7 +12,14 @@ export const RECOGNIZE_REGEXP_MAP = {
   json: RECOGNIZE_JSON_REGEXP,
 } as const;
 
-export const EXTRACT_REGEXP_MAP = {
+const EXTRACT_YAML_REGEXP =
+  /^\ufeff?(?:---yaml|= yaml =|---)$([\s\S]+?)^(?:---|= yaml =)\s*$\r?\n?/im;
+const EXTRACT_TOML_REGEXP =
+  /^\ufeff?(?:---toml|\+\+\+|= toml =)$([\s\S]+?)^(?:---|\+\+\+|= toml =)\s*$\r?\n?/im;
+const EXTRACT_JSON_REGEXP =
+  /^\ufeff?(?:---json|= json =)$([\s\S]+?)^(?:---|= json =)\s*$\r?\n?/im;
+
+export const EXTRACT_REGEXP_MAP: Record<Format, RegExp> = {
   yaml: EXTRACT_YAML_REGEXP,
   toml: EXTRACT_TOML_REGEXP,
   json: EXTRACT_JSON_REGEXP,
