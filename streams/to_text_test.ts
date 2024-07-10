fd9286e10e863e05a5b3a1ccc81c0a0836ca8c89
liang <836792+leonco@users.noreply.github.com>
2024-07-10 18:52:35 +0800

    fix(streams): toText() incorrect with multibyte strings in different chunks (#5381)

--- streams/to_text_test.ts
+++ streams/to_text_test.ts
@@ -12,4 +12,14 @@ Deno.test("toText()", async () => {
   const stringStream = ReadableStream.from(["hello", " deno ", "world"]);
 
   assertEquals(await toText(stringStream), "hello deno world");
+
+  const utf8ByteStream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(new Uint8Array([228, 184, 173, 230, 150]));
+      controller.enqueue(new Uint8Array([135]));
+      controller.close();
+    },
+  });
+
+  assertEquals(await toText(utf8ByteStream), "中文");
 });
