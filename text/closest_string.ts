0775450cf15577a888e2fbf9725262f73151fead
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-16 17:54:21 +1000

    feat(text): add ability for user to control word comparison function

--- text/closest_string.ts
+++ text/closest_string.ts
@@ -2,16 +2,30 @@
 // This module is browser compatible.
 import { levenshteinDistance } from "./levenshtein_distance.ts";
 
-// NOTE: this metric may change in future versions (e.g. better than levenshteinDistance)
-const getWordDistance = levenshteinDistance;
+/** Options for {@linkcode closestString}. */
+export interface ClosestStringOptions {
+  /**
+   * Whether the distance should include case.
+   *
+   * @default {false}
+   */
+  caseSensitive?: boolean;
+  /**
+   * A custom comparison function to use for comparing strings.
+   *
+   * @param a The first string for comparison.
+   * @param b The second string for comparison.
+   * @returns The distance between the two strings.
+   * @default {levenshteinDistance}
+   */
+  compareFn?: (a: string, b: string) => number;
+}
 
 /**
  * Finds the most similar string from an array of strings.
  *
- * Note: the ordering of words may change with version-updates
- * E.g. word-distance metric may change (improve)
- * use a named-distance (e.g. levenshteinDistance) to
- * guarantee a particular ordering.
+ * By default, calculates the distance between words using the
+ * {@link https://en.wikipedia.org/wiki/Levenshtein_distance | Levenshtein distance}.
  *
  * @example Usage
  * ```ts
@@ -26,23 +40,20 @@ const getWordDistance = levenshteinDistance;
  *
  * @param givenWord The string to measure distance against
  * @param possibleWords The string-array to pick the closest string from
- * @param options An options bag containing a `caseSensitive` flag indicating
- * whether the distance should include case. Default is false.
+ * @param options The options for the comparison.
  * @returns The closest string
  */
 export function closestString(
   givenWord: string,
   possibleWords: ReadonlyArray<string>,
-  options?: {
-    caseSensitive?: boolean;
-  },
+  options?: ClosestStringOptions,
 ): string {
   if (possibleWords.length === 0) {
     throw new TypeError(
       "When using closestString(), the possibleWords array must contain at least one word",
     );
   }
-  const { caseSensitive } = { ...options };
+  const { caseSensitive, compareFn = levenshteinDistance } = { ...options };
 
   if (!caseSensitive) {
     givenWord = givenWord.toLowerCase();
@@ -52,13 +63,12 @@ export function closestString(
   let closestStringDistance = Infinity;
   for (const each of possibleWords) {
     const distance = caseSensitive
-      ? getWordDistance(givenWord, each)
-      : getWordDistance(givenWord, each.toLowerCase());
+      ? compareFn(givenWord, each)
+      : compareFn(givenWord, each.toLowerCase());
     if (distance < closestStringDistance) {
       nearestWord = each;
       closestStringDistance = distance;
     }
   }
-  // this distance metric could be swapped/improved in the future
   return nearestWord;
 }
