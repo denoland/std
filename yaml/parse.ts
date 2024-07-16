997139d092a7aa900c65f47a87cb3e48b5af5576
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-16 20:12:33 +1000

    BREAKING(yaml): replace `YamlError` with `SyntaxError` in `parse()` (#5446)

--- yaml/parse.ts
+++ yaml/parse.ts
@@ -22,7 +22,7 @@ export interface ParseOptions {
   schema?: "core" | "default" | "failsafe" | "json" | "extended";
   /**
    * If `true`, duplicate keys will overwrite previous values. Otherwise,
-   * duplicate keys will throw a {@linkcode YamlError}.
+   * duplicate keys will throw a {@linkcode SyntaxError}.
    *
    * @default {false}
    */
@@ -52,7 +52,7 @@ export interface ParseOptions {
  * assertEquals(data, { id: 1, name: "Alice" });
  * ```
  *
- * @throws {YamlError} Throws error on invalid YAML.
+ * @throws {SyntaxError} Throws error on invalid YAML.
  * @param content YAML string to parse.
  * @param options Parsing options.
  * @returns Parsed document.
