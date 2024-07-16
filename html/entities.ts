1a263a8659f1195e2c6c7b2fe6c9be6230848b74
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2024-07-16 10:30:20 -0300

    docs: include a copy paste of the HTML spec for custom elements

--- html/entities.ts
+++ html/entities.ts
@@ -130,8 +130,26 @@ export function unescape(
 }
 
 /**
- * Validates if a element is a valid custom element name according to the HTML
- * spec: https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+ * A valid custom element name is a sequence of characters name that meets all of the following requirements:
+ *
+ * name must match the PotentialCustomElementName production:
+ *
+ * PotentialCustomElementName ::= [a-z] (PCENChar)* '-' (PCENChar)*
+ *
+ * PCENChar ::= "-" | "." | [0-9] | "_" | [a-z] | #xB7 | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x203F-#x2040] | [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
+ *
+ * This uses the EBNF notation from the XML specification. [XML]
+ *
+ * name must not be any of the following:
+ *
+ * annotation-xml
+ * color-profile
+ * font-face
+ * font-face-src
+ * font-face-uri
+ * font-face-format
+ * font-face-name
+ * missing-glyph
  *
  * @example Basic usage
  *
