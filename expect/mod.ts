9e71e1e8e430ef2758b580dcf1edd3e58f525790
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-22 09:31:26 +1000

    docs(expect): minor doc tweaks

--- expect/mod.ts
+++ expect/mod.ts
@@ -5,73 +5,6 @@
 /**
  * This module provides Jest compatible expect assertion functionality.
  *
- * Currently this module supports the following functions:
- * - Common matchers:
- *   - `toBe`
- *   - `toEqual`
- *   - `toStrictEqual`
- *   - `toMatch`
- *   - `toMatchObject`
- *   - `toBeDefined`
- *   - `toBeUndefined`
- *   - `toBeNull`
- *   - `toBeNaN`
- *   - `toBeTruthy`
- *   - `toBeFalsy`
- *   - `toContain`
- *   - `toContainEqual`
- *   - `toHaveLength`
- *   - `toBeGreaterThan`
- *   - `toBeGreaterThanOrEqual`
- *   - `toBeLessThan`
- *   - `toBeLessThanOrEqual`
- *   - `toBeCloseTo`
- *   - `toBeInstanceOf`
- *   - `toThrow`
- *   - `toHaveProperty`
- *   - `toHaveLength`
- * - Mock related matchers:
- *   - `toHaveBeenCalled`
- *   - `toHaveBeenCalledTimes`
- *   - `toHaveBeenCalledWith`
- *   - `toHaveBeenLastCalledWith`
- *   - `toHaveBeenNthCalledWith`
- *   - `toHaveReturned`
- *   - `toHaveReturnedTimes`
- *   - `toHaveReturnedWith`
- *   - `toHaveLastReturnedWith`
- *   - `toHaveNthReturnedWith`
- * - Asymmetric matchers:
- *   - `expect.anything`
- *   - `expect.any`
- *   - `expect.arrayContaining`
- *   - `expect.not.arrayContaining`
- *   - `expect.closeTo`
- *   - `expect.stringContaining`
- *   - `expect.not.stringContaining`
- *   - `expect.stringMatching`
- *   - `expect.not.stringMatching`
- * - Utilities:
- *   - `expect.addEqualityTester`
- *   - `expect.extend`
- *
- * Only these functions are still not available:
- * - Matchers:
- *   - `toMatchSnapShot`
- *   - `toMatchInlineSnapShot`
- *   - `toThrowErrorMatchingSnapShot`
- *   - `toThrowErrorMatchingInlineSnapShot`
- * - Asymmetric matchers:
- *   - `expect.objectContaining`
- *   - `expect.not.objectContaining`
- * - Utilities:
- *   - `expect.assertions`
- *   - `expect.hasAssertions`
- *   - `expect.addSnapshotSerializer`
- *
- * This module is largely inspired by
- * {@link https://github.com/allain/expect | x/expect} module by Allain Lalonde.
- *
  * ```ts no-assert
  * import { expect } from "@std/expect";
  *
@@ -82,8 +15,16 @@
  * await expect(Promise.resolve(x)).resolves.toEqual(42);
  * ```
  *
+ * See {@linkcode Expected} for the list of supported matchers.
+ *
+ * The tracking issue to add support for unsupported parts of the API is
+ * {@link https://github.com/denoland/std/issues/3964}.
+ *
+ * This module is largely inspired by
+ * {@link https://github.com/allain/expect | x/expect} module by
+ * {@link https://github.com/allain | Allain Lalonde}.
+ *
  * @module
  */
-export { expect } from "./expect.ts";
-export type { AnyConstructor, Async, Expected } from "./expect.ts";
+export * from "./expect.ts";
 export { fn } from "./fn.ts";
