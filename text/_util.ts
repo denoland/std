5c3d8e8f7d711aec026277fc041aee2f973e2388
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-22 11:33:11 +0900

    address review feedbacks

--- text/_util.ts
+++ text/_util.ts
@@ -1,16 +1,16 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-const RE_ACRONYM = /\p{Lu}{2,}/gu; // e.g. ID, URL
-const RE_CAPITALIZED_WORD = /\p{Lu}\p{Ll}+/gu; // e.g. Apple
-const RE_LOWERCASED_WORD = /\P{Lu}+/gu; // e.g. apple, this will also match a sequence of unicode letters in languages without a concept of upper/lower case
-const RE_DIGITS = /\p{N}+/gu; // e.g. 123
-const RE_MATCH_WORD_BY_CASE = new RegExp(
-  `${RE_ACRONYM.source}|${RE_CAPITALIZED_WORD.source}|${RE_DIGITS.source}|${RE_LOWERCASED_WORD.source}`,
+const ACRONYM_REGEXP = /\p{Lu}{2,}/u; // e.g. ID, URL
+const CAPITALIZED_WORD_REGEXP = /\p{Lu}\p{Ll}+/u; // e.g. Apple
+const LOWERCAASED_WORD_REGEXP = /\P{Lu}+/u; // e.g. apple, this will also match a sequence of unicode letters in languages without a concept of upper/lower case
+const DIGITS_REGEXP = /\p{N}+/u; // e.g. 123
+const MATCH_WORD_BASE_CASE_REGEXP = new RegExp(
+  `${ACRONYM_REGEXP.source}|${CAPITALIZED_WORD_REGEXP.source}|${DIGITS_REGEXP.source}|${LOWERCAASED_WORD_REGEXP.source}`,
   "gu",
 );
 
 function splitByCase(input: string): string[] {
-  const matches = input.match(RE_MATCH_WORD_BY_CASE);
+  const matches = input.match(MATCH_WORD_BASE_CASE_REGEXP);
 
   return matches || [input];
 }
