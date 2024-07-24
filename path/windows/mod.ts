2b64f3784cfd6fb9e6dc101552688bc99af4f918
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-24 11:13:10 +1000

    docs(path): cleanup `@std/path/posix` and `@std/path/windows` module documentation

--- path/windows/mod.ts
+++ path/windows/mod.ts
@@ -4,11 +4,7 @@
 // This module is browser compatible.
 
 /**
- * Utilities for working with OS-specific file paths.
- *
- * Codes in the examples uses POSIX path but it automatically use Windows path
- * on Windows. Use methods under `posix` or `win32` object instead to handle non
- * platform specific path like:
+ * Utilities for working with Windows-specific paths.
  *
  * ```ts
  * import { fromFileUrl } from "@std/path/windows";
