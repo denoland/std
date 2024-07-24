6ccc7ad346c8b2b2a122830e0913fa08c29a9f76
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-24 16:58:07 +1000

    docs(path): cleanup `@std/path/posix` and `@std/path/windows` module documentation (#5529)
    * docs(path): cleanup `@std/path/posix` and `@std/path/windows` module documentation
    
    * tweak

--- path/posix/mod.ts
+++ path/posix/mod.ts
@@ -4,17 +4,10 @@
 // This module is browser compatible.
 
 /**
- * Utilities for working with OS-specific file paths.
- *
- * This module also provides some functions that help when working with URLs.
- * See the documentation for examples.
- *
- * Codes in the examples uses POSIX path but it automatically use Windows path
- * on Windows. Use methods under `posix` or `win32` object instead to handle non
- * platform specific path like:
+ * Utilities for working with POSIX-formatted paths.
  *
  * ```ts
- * import { fromFileUrl } from "@std/path/posix";
+ * import { fromFileUrl } from "@std/path/posix/from-file-url";
  * import { assertEquals } from "@std/assert";
  *
  * assertEquals(fromFileUrl("file:///home/foo"), "/home/foo");
