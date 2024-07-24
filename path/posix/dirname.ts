eec5304a633bc8bfc2014eeb5f0131f21af39d41
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-24 11:06:53 +1000

    work

--- path/posix/dirname.ts
+++ path/posix/dirname.ts
@@ -18,6 +18,17 @@ import { isPosixPathSeparator } from "./_util.ts";
  * assertEquals(dirname("https://deno.land/std/path/mod.ts"), "https://deno.land/std/path");
  * ```
  *
+ * @example Working with URLs
+ *
+ * ```ts
+ * import { dirname } from "@std/path/posix/dirname";
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts"), "https://deno.land/std/path");
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts?a=b"), "https://deno.land/std/path");
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts#header"), "https://deno.land/std/path");
+ * ```
+ *
  * @param path The path to get the directory from.
  * @returns The directory path.
  */
