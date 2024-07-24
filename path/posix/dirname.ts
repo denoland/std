d8592d18920c1fb0ca7d178915175b9ce8bab382
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-24 16:14:56 +1000

    docs(path): add examples to `@std/path/posix` examples (#5371)
    * docs(path): add examples to `@std/path/posix` examples
    
    * work

--- path/posix/dirname.ts
+++ path/posix/dirname.ts
@@ -15,6 +15,18 @@ import { isPosixPathSeparator } from "./_util.ts";
  *
  * assertEquals(dirname("/home/user/Documents/"), "/home/user");
  * assertEquals(dirname("/home/user/Documents/image.png"), "/home/user/Documents");
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts"), "https://deno.land/std/path");
+ * ```
+ *
+ * @example Working with URLs
+ *
+ * ```ts
+ * import { dirname } from "@std/path/posix/dirname";
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts"), "https://deno.land/std/path");
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts?a=b"), "https://deno.land/std/path");
+ * assertEquals(dirname("https://deno.land/std/path/mod.ts#header"), "https://deno.land/std/path");
  * ```
  *
  * @param path The path to get the directory from.
