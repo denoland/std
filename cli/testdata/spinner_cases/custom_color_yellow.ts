6db0aad9783210cf072e852e941fae115af62f0b
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-26 13:16:08 +0900

    test(cli): stop spinner without error

--- cli/testdata/spinner_cases/custom_color_yellow.ts
+++ cli/testdata/spinner_cases/custom_color_yellow.ts
@@ -4,4 +4,4 @@ const spinner = new Spinner({ color: "yellow" });
 
 spinner.start();
 
-setTimeout(spinner.stop, 100);
+setTimeout(() => spinner.stop(), 100);
--- cli/testdata/spinner_cases/custom_message.ts
+++ cli/testdata/spinner_cases/custom_message.ts
@@ -4,4 +4,4 @@ const spinner = new Spinner({ message: "Spinning with Deno 🦕" });
 
 spinner.start();
 
-setTimeout(spinner.stop, 100);
+setTimeout(() => spinner.stop(), 100);
