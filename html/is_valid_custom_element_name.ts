33a5f7e72e35bd19c389a62c15698aa4c6c84ac7
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-01 12:22:29 +0900

    refactor: clean up regexp

--- html/is_valid_custom_element_name.ts
+++ html/is_valid_custom_element_name.ts
@@ -12,8 +12,8 @@ const FORBIDDEN_CUSTOM_ELEMENT_NAMES: string[] = [
   "missing-glyph",
 ] as const;
 
-const POTENTIAL_CUSTOM_ELEMENT_NAME_CHARS =
-  /^[a-z](?:[-.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*(-?(?:[-.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*)*$/;
+const CUSTOM_ELEMENT_NAME_CHARS =
+  /^[a-z](?:[-.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*-(?:[-.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*$/;
 
 /**
  * Returns whether the given string is a valid custom element name, as per the
@@ -53,6 +53,5 @@ const POTENTIAL_CUSTOM_ELEMENT_NAME_CHARS =
  */
 export function isValidCustomElementName(elementName: string): boolean {
   return !FORBIDDEN_CUSTOM_ELEMENT_NAMES.includes(elementName) &&
-    POTENTIAL_CUSTOM_ELEMENT_NAME_CHARS.test(elementName) &&
-    elementName.includes("-");
+    CUSTOM_ELEMENT_NAME_CHARS.test(elementName);
 }
