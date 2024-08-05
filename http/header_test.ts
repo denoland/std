51586ed3eed1dd4001ce33e807bb36b9b9b56d6c
Halvard Mørstad <halvard@simplyundoable.com>
2024-08-05 18:32:02 +0200

    Cleaned up and added jsr support

---
+++ http/header_test.ts
@@ -0,0 +1,15 @@
+import { HEADER } from "./header.ts";
+import { assertEquals } from "@std/assert";
+
+Deno.test({
+  name: "HEADER",
+  fn() {
+    // just spot check a few common codes
+    assertEquals(HEADER.Accept, "Accept");
+    assertEquals(HEADER.AIm, "A-IM");
+    assertEquals(HEADER.ClientCertChain, "Client-Cert-Chain");
+    assertEquals(HEADER.Connection, "Connection");
+    assertEquals(HEADER.Origin, "Origin");
+    assertEquals(HEADER.Referer, "Referer");
+  },
+});
