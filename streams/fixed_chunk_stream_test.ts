fac3b59200997d633cb4b659d1c44883445123f8
Asher Gomez <ashersaupingomez@gmail.com>
2024-08-06 14:15:36 +0200

    tweak test

--- streams/fixed_chunk_stream_test.ts
+++ streams/fixed_chunk_stream_test.ts
@@ -1,20 +1,18 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-import { assertEquals } from "@std/assert";
+import { assert, assertLessOrEqual } from "@std/assert";
 import { FixedChunkStream } from "./fixed_chunk_stream.ts";
 
 Deno.test("FixedChunkStream", async () => {
+  const size = 512;
+
   const readable = ReadableStream.from(function* () {
     for (let i = 0; i < 100; ++i) {
-      yield new Uint8Array(Math.floor(Math.random() * 1000));
+      yield new Uint8Array(Math.random() * 1000);
     }
-  }())
-    .pipeThrough(new FixedChunkStream(512));
+  }()).pipeThrough(new FixedChunkStream(size));
+  const result = await Array.fromAsync(readable);
 
-  assertEquals(
-    (await Array.fromAsync(readable))
-      .slice(0, -1)
-      .every((chunk) => chunk.length === 512),
-    true,
-  );
+  assert(result.slice(0, -1).every((chunk) => chunk.length === size));
+  assertLessOrEqual(result.at(-1)!.length, size);
 });
