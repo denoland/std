e8aa4dfa4c41c1402cbcb8e1c00bb468e965bd32
Asher Gomez <ashersaupingomez@gmail.com>
2024-08-07 14:35:13 +0200

    feat(http/unstable): `route` module (#5644)
    * feat(http): `http/router`
    
    * work
    
    * work
    
    * fix
    
    * work
    
    * fix
    
    * clean
    
    * add unstable notices
    
    * fix
    
    * work
    
    * work
    
    * work
    
    * work
    
    * fix
    
    * fix
    
    * fix
    
    * work

---
+++ http/route_test.ts
@@ -0,0 +1,57 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
+import { type Route, route } from "./route.ts";
+import { assertEquals } from "../assert/equals.ts";
+
+const routes: Route[] = [
+  {
+    pattern: new URLPattern({ pathname: "/about" }),
+    handler: (request: Request) => new Response(new URL(request.url).pathname),
+  },
+  {
+    pattern: new URLPattern({ pathname: "/users/:id" }),
+    method: "POST",
+    handler: (_request, _info, params) =>
+      new Response(params?.pathname.groups.id),
+  },
+];
+
+function defaultHandler(request: Request) {
+  return new Response(new URL(request.url).pathname, { status: 404 });
+}
+
+const info: Deno.ServeHandlerInfo = {
+  remoteAddr: {
+    transport: "tcp",
+    hostname: "example.com",
+    port: 80,
+  },
+  completed: Promise.resolve(),
+};
+
+Deno.test("route()", async (t) => {
+  const handler = route(routes, defaultHandler);
+
+  await t.step("handles static routes", async () => {
+    const request = new Request("http://example.com/about");
+    const response = await handler(request, info);
+    assertEquals(response?.status, 200);
+    assertEquals(await response?.text(), "/about");
+  });
+
+  await t.step("handles dynamic routes", async () => {
+    const request = new Request("http://example.com/users/123", {
+      method: "POST",
+    });
+    const response = await handler(request, info);
+    assertEquals(await response?.text(), "123");
+    assertEquals(response?.status, 200);
+  });
+
+  await t.step("handles default handler", async () => {
+    const request = new Request("http://example.com/not-found");
+    const response = await handler(request, info);
+    assertEquals(response?.status, 404);
+    assertEquals(await response?.text(), "/not-found");
+  });
+});
