a48a313412262e7611cc5b51600e1848ce2bd9d0
Tim <tim.reichen@gmail.com>
2024-08-11 23:45:22 +0200

    remove char map replacement

--- text/slugify_test.ts
+++ text/slugify_test.ts
@@ -16,16 +16,7 @@ Deno.test("slugify() handles whitespaces", () => {
   assertEquals(slugify("Hello\r\nWorld"), "hello-world");
 });
 
-Deno.test("slugify() handles special characters", () => {
-  assertEquals(slugify("Hello, World!"), "hello-world");
-  assertEquals(slugify("I ♥ Dogs"), "i-love-dogs");
-  assertEquals(slugify("Copyright © by deno"), "copyright-c-by-deno");
-  assertEquals(slugify("Café & Restaurant"), "cafe-and-restaurant");
-  assertEquals(slugify("100% Real"), "100percent-real");
-});
-
 Deno.test("slugify() replaces diacritic characters", () => {
-  assertEquals(slugify("я люблю единорогов"), "ya-lyublyu-edinorogov");
   assertEquals(slugify("déjà vu"), "deja-vu");
   assertEquals(slugify("Cliché"), "cliche");
   assertEquals(slugify("façade"), "facade");
@@ -50,7 +41,7 @@ Deno.test("slugify() removes unknown special characters", () => {
   );
   assertEquals(
     slugify("Fintech startups raised $34B in 2019"),
-    "fintech-startups-raised-dollar34b-in-2019",
+    "fintech-startups-raised-34b-in-2019",
   );
   assertEquals(
     slugify("Shopify joins Facebook’s cryptocurrency Libra Association"),
@@ -62,6 +53,6 @@ Deno.test("slugify() removes unknown special characters", () => {
   );
   assertEquals(
     slugify("Bitcoin soars past $33,000, its highest ever"),
-    "bitcoin-soars-past-dollar33000-its-highest-ever",
+    "bitcoin-soars-past-33000-its-highest-ever",
   );
 });
