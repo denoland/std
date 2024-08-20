340a2521cb8947c9f5330716e27ed2f4860d479b
Michael Ekstrand <md@ekstrandom.net>
2024-08-20 09:31:39 -0400

    fix lint error on type import

--- front_matter/yaml.ts
+++ front_matter/yaml.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 import { extractAndParse, type Parser } from "./_shared.ts";
-import { parse, ParseOptions } from "@std/yaml/parse";
+import { parse, type ParseOptions } from "@std/yaml/parse";
 import type { Extract } from "./types.ts";
 import { EXTRACT_YAML_REGEXP } from "./_formats.ts";
 
