ebf632baed10b9d6ea37f2b08fc8c0317aa7f791
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-21 14:36:35 +0900

    add unstable overload, add example

--- front_matter/yaml.ts
+++ front_matter/yaml.ts
@@ -31,9 +31,39 @@ export type { Extract };
  *
  * @typeParam T The type of the parsed front matter.
  * @param text The text to extract YAML front matter from.
+ * @returns The extracted YAML front matter and body content.
+ */
+export function extract<T>(text: string): Extract<T>;
+/**
+ * Extracts and parses {@link https://yaml.org | YAML} from the metadata of
+ * front matter content.
+ *
+ * @experimental **UNSTABLE**: New API, yet to be vetted.
+ *
+ * @example Extract YAML front matter
+ * ```ts
+ * import { extract } from "@std/front-matter/yaml";
+ * import { assertEquals } from "@std/assert";
+ *
+ * const output = `---yaml
+ * date: 2022-01-01
+ * ---
+ * Hello, world!`;
+ * const result = extract(output, { schema: "json" });
+ *
+ * assertEquals(result, {
+ *   frontMatter: "date: 2022-01-01",
+ *   body: "Hello, world!",
+ *   attrs: { date: "2022-01-01" },
+ * });
+ * ```
+ *
+ * @typeParam T The type of the parsed front matter.
+ * @param text The text to extract YAML front matter from.
  * @param options The options to pass to `@std/yaml/parse`.
  * @returns The extracted YAML front matter and body content.
  */
+export function extract<T>(text: string, options?: ParseOptions): Extract<T>;
 export function extract<T>(text: string, options?: ParseOptions): Extract<T> {
   return extractAndParse(
     text,
