0332f0ada801c6c7ccff73c4203b364dced0069b
Tim Reichen <timreichen@users.noreply.github.com>
2024-08-21 23:58:27 +0200

    refactor(yaml): remove `ArrayObject` (#5761)

--- yaml/_dumper_state.ts
+++ yaml/_dumper_state.ts
@@ -29,7 +29,7 @@ import {
 } from "./_chars.ts";
 import { DEFAULT_SCHEMA, type Schema } from "./_schema.ts";
 import type { KindType, StyleVariant, Type } from "./_type.ts";
-import { type ArrayObject, getObjectTypeString, isObject } from "./_utils.ts";
+import { getObjectTypeString, isObject } from "./_utils.ts";
 
 const STYLE_PLAIN = 1;
 const STYLE_SINGLE = 2;
@@ -90,11 +90,11 @@ function charCodeToHexString(charCode: number): string {
 }
 
 function compileStyleMap(
-  map?: ArrayObject<StyleVariant> | null,
-): ArrayObject<StyleVariant> {
+  map?: Record<string, StyleVariant> | null,
+): Record<string, StyleVariant> {
   if (typeof map === "undefined" || map === null) return {};
 
-  const result: ArrayObject<StyleVariant> = {};
+  const result: Record<string, StyleVariant> = {};
   for (let tag of Object.keys(map)) {
     const style = String(map[tag]) as StyleVariant;
     if (tag.slice(0, 2) === "!!") {
@@ -430,7 +430,7 @@ export interface DumperStateOptions {
    */
   flowLevel?: number;
   /** Each tag may have own set of styles.	- "tag" => "style" map. */
-  styles?: ArrayObject<StyleVariant> | null;
+  styles?: Record<string, StyleVariant> | null;
   /** specifies a schema to use. */
   schema?: Schema;
   /**
@@ -478,7 +478,7 @@ export class DumperState {
   explicitTypes: Type<KindType>[];
   duplicates: unknown[] = [];
   usedDuplicates: Set<unknown> = new Set();
-  styleMap: ArrayObject<StyleVariant>;
+  styleMap: Record<string, StyleVariant>;
 
   constructor({
     schema = DEFAULT_SCHEMA,
