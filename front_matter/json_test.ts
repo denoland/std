55fecde77c535aaf4b2c8a1209bc8713febb7cf9
Tim Reichen <timreichen@users.noreply.github.com>
2024-08-24 16:51:15 +0200

    test(front-matter): remove duplicate tests (#5808)

--- front_matter/json_test.ts
+++ front_matter/json_test.ts
@@ -1,22 +1,11 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-import { test } from "./test.ts";
 import { extract } from "./json.ts";
 import {
   runExtractJsonTests,
   runExtractTypeErrorTests,
-  runTestInvalidInputTests,
-  runTestValidInputTests,
 } from "./_test_utils.ts";
 import { assertEquals } from "@std/assert/equals";
-
-Deno.test("test() handles valid input", () => {
-  runTestValidInputTests("json", test);
-});
-
-Deno.test("test() handles invalid input", () => {
-  runTestInvalidInputTests("json", test);
-});
 
 Deno.test("json() extracts type error on invalid input", () => {
   runExtractTypeErrorTests("json", extract);
