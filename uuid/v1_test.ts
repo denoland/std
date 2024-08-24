000be22d683cad4910aee2345a16450a19f0a88d
Ian Bull <irbull@eclipsesource.com>
2024-08-23 19:56:06 -0700

    refactor(uuid): align additional error messages
    Aligns the error messages in the `uuid` folder to match the style guide.
    
    https://github.com/denoland/std/issues/5574

--- uuid/v1_test.ts
+++ uuid/v1_test.ts
@@ -49,7 +49,7 @@ Deno.test("generate() throws when node is passed with less than 6 numbers", () =
       generate({ node: [0x01, 0x23, 0x45, 0x67, 0x89] });
     },
     Error,
-    "Cannot create UUID. The node option must be an array of 6 bytes",
+    "Cannot create UUID: the node option must be an array of 6 bytes",
   );
 });
 
@@ -59,7 +59,7 @@ Deno.test("generate() throws when node is passed with more than 6 numbers", () =
       generate({ node: [0x01, 0x23, 0x45, 0x67, 0x89, 0x89, 0x89] });
     },
     Error,
-    "Cannot create UUID. The node option must be an array of 6 bytes",
+    "Cannot create UUID: the node option must be an array of 6 bytes",
   );
 });
 
