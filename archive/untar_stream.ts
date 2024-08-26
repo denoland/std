32577cfcc008c334f5147a8ac83ad7148403970b
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-08-26 13:00:45 +1000

    adjust(archive): linkname, uname, and gname to follow the same decoding rules as name and prefix

--- archive/untar_stream.ts
+++ archive/untar_stream.ts
@@ -242,10 +242,7 @@ export class UnTarStream
         size: parseInt(decoder.decode(value.subarray(124, 136)).trimEnd(), 8),
         mtime: parseInt(decoder.decode(value.subarray(136, 148 - 1)), 8),
         typeflag: decoder.decode(value.subarray(156, 157)),
-        linkname: decoder.decode(value.subarray(157, 257)).replaceAll(
-          "\0",
-          "",
-        ),
+        linkname: decoder.decode(value.subarray(157, 257)).split("\0")[0]!,
       };
       if (header.typeflag === "\0") header.typeflag = "0";
       // "ustar\u000000"
@@ -258,8 +255,8 @@ export class UnTarStream
           ...header,
           magic: decoder.decode(value.subarray(257, 263)),
           version: decoder.decode(value.subarray(263, 265)),
-          uname: decoder.decode(value.subarray(265, 297)).replaceAll("\0", ""),
-          gname: decoder.decode(value.subarray(297, 329)).replaceAll("\0", ""),
+          uname: decoder.decode(value.subarray(265, 297)).split("\0")[0]!,
+          gname: decoder.decode(value.subarray(297, 329)).split("\0")[0]!,
           devmajor: decoder.decode(value.subarray(329, 337)).replaceAll(
             "\0",
             "",
