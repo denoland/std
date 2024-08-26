6248c08d3d1624730b7e17eee98883afd8f64cde
Tim <tim.reichen@gmail.com>
2024-08-26 10:47:35 +0200

    initial commit

--- yaml/stringify_test.ts
+++ yaml/stringify_test.ts
@@ -698,3 +698,11 @@ Deno.test("stringify() returns emtpy array on invalid entries", () => {
     "[]\n",
   );
 });
+
+Deno.test("stringify() handles duplicate array references", () => {
+  const a = ["foo"];
+  assertEquals(
+    stringify([a, a]),
+    "- &ref_0\n  - foo\n- *ref_0\n",
+  );
+});
