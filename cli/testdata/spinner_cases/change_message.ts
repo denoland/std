c2c5a9a7a509fd778de9f59111b51033047ef821
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-26 14:57:40 +0900

    delay the 2nd update

--- cli/testdata/spinner_cases/change_message.ts
+++ cli/testdata/spinner_cases/change_message.ts
@@ -5,6 +5,6 @@ const spinner = new Spinner();
 spinner.start();
 
 setTimeout(() => (spinner.message = "One dino 🦕"), 10);
-setTimeout(() => (spinner.message = "Two dinos 🦕🦕"), 200);
+setTimeout(() => (spinner.message = "Two dinos 🦕🦕"), 250);
 
 setTimeout(() => spinner.stop(), 500);
