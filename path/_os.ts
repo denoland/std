aabaca9ed580e5df47542c9316447ea202687899
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-08-29 07:04:03 -0400

    Added support for node.

--- path/_os.ts
+++ path/_os.ts
@@ -5,4 +5,6 @@
 // Check Deno, then the remaining runtimes (e.g. Node, Bun and the browser)
 export const isWindows: boolean =
   (globalThis as any).Deno?.build.os === "windows" ||
-  (globalThis as any).navigator.platform?.startsWith("Win") || false;
+  (globalThis as any).navigator?.platform?.startsWith("Win") ||
+  (globalThis as any).process?.platform?.startsWith("win") ||
+  false;
