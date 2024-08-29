505e01f9c544b948512626ba155c86ca8ab01005
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-29 16:33:23 +0900

    feat(path/unstable): support URL in first arg of join

--- path/windows/join.ts
+++ path/windows/join.ts
@@ -4,6 +4,7 @@
 import { assertPath } from "../_common/assert_path.ts";
 import { isPathSeparator } from "./_util.ts";
 import { normalize } from "./normalize.ts";
+import { fromFileUrl } from "./from_file_url.ts";
 
 /**
  * Join all given a sequence of `paths`,then normalizes the resulting path.
@@ -20,7 +21,27 @@ import { normalize } from "./normalize.ts";
  * @param paths The paths to join.
  * @returns The joined path.
  */
-export function join(...paths: string[]): string {
+export function join(...paths: string[]): string;
+/**
+ * Join all given a sequence of `paths`,then normalizes the resulting path.
+ *
+ * @example Usage
+ * ```ts
+ * import { join } from "@std/path/windows/join";
+ * import { assertEquals } from "@std/assert";
+ *
+ * const joined = join(new URL("file:///C:/foo"), "bar", "baz\\..");
+ * assertEquals(joined, "C:\\foo\\bar");
+ * ```
+ *
+ * @param path The path to join. This can be string or file URL.
+ * @param paths The paths to join.
+ * @returns The joined path.
+ */
+export function join(path?: URL | string, ...paths: string[]): string;
+export function join(path?: URL | string, ...paths: string[]): string {
+  path = path instanceof URL ? fromFileUrl(path) : path;
+  paths = path ? [path, ...paths] : paths;
   paths.forEach((path) => assertPath(path));
   paths = paths.filter((path) => path.length > 0);
   if (paths.length === 0) return ".";
