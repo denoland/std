5d6d1e43dc67d87d2cb628f8eea6a4f74284d482
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2024-09-10 03:12:01 +0900

    refactor(collections): unuse `flatMap` in `pick`

--- collections/pick.ts
+++ collections/pick.ts
@@ -28,7 +28,7 @@ export function pick<T extends object, K extends keyof T>(
   obj: Readonly<T>,
   keys: readonly K[],
 ): Pick<T, K> {
-  return Object.fromEntries(
-    keys.flatMap((k) => (k in obj ? [[k, obj[k]]] : [])),
-  ) as Pick<T, K>;
+  const result = {} as Pick<T, K>;
+  for (const key of keys) if (key in obj) result[key] = obj[key];
+  return result;
 }
