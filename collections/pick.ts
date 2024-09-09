153b4fbd70b5b03648e5b17dcd8c5211e82b7eb1
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2024-09-09 22:49:31 +0900

    fix(collections): ensure `pick` doesn't generate missing properties as `undefined`

--- collections/pick.ts
+++ collections/pick.ts
@@ -28,5 +28,7 @@ export function pick<T extends object, K extends keyof T>(
   obj: Readonly<T>,
   keys: readonly K[],
 ): Pick<T, K> {
-  return Object.fromEntries(keys.map((k) => [k, obj[k]])) as Pick<T, K>;
+  return Object.fromEntries(
+    keys.flatMap((k) => (Object.hasOwn(obj, k) ? [[k, obj[k]]] : []))
+  ) as Pick<T, K>;
 }
--- collections/pick_test.ts
+++ collections/pick_test.ts
@@ -15,8 +15,18 @@ Deno.test({
 });
 
 Deno.test({
-  name:
-    "pick() returns a new object from the provided object with the provided keys",
+  name: "pick() returns a new object without properties missing in the original object",
+  fn() {
+    const obj = JSON.parse('{ "a": 5, "b": 6, "c": 7, "d": 8 }');
+    const picked: { a: 5; x?: 5; y?: 5 } = pick(obj, ["a", "x", "y"]);
+
+    assertEquals(picked, { a: 5 });
+    assertNotStrictEquals(picked, obj);
+  },
+});
+
+Deno.test({
+  name: "pick() returns a new object from the provided object with the provided keys",
   fn() {
     const obj = { a: 5, b: 6, c: 7, d: 8 };
     const picked = pick(obj, ["a", "c"]);
