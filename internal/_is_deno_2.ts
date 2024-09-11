005808f4da87cf6384cc0cf2ebb5d822690d2d1b
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-11 18:57:36 +1000

    copyright headers

--- internal/_is_deno_2.ts
+++ internal/_is_deno_2.ts
@@ -1,3 +1,4 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // TODO(iuioiua): Remove this file when Deno 2 is released.
 // deno-lint-ignore no-explicit-any
 export const IS_DENO_2 = (globalThis as any).window === undefined;
