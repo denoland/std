2b12e382af78ced3413da4a692bca3801b0b070e
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-12 13:45:04 +1000

    chore: cleanup `IS_DENO_2` constant (#5945)

--- internal/_is_deno_2.ts
+++ internal/_is_deno_2.ts
@@ -1,4 +1,3 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // TODO(iuioiua): Remove this file when Deno 2 is released.
-// deno-lint-ignore no-explicit-any
-export const IS_DENO_2 = (globalThis as any).window === undefined;
+export const IS_DENO_2 = Deno.version.deno.startsWith("2.");
