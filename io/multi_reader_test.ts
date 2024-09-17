242cb4da564ddbe4118e163b76bd3c9d5c5e007e
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-17 14:20:18 +1000

    BREAKING(io/unstable): remove `copyN()`

--- io/multi_reader_test.ts
+++ io/multi_reader_test.ts
@@ -3,16 +3,12 @@
 import { assertEquals } from "@std/assert";
 import { MultiReader } from "./multi_reader.ts";
 import { StringWriter } from "./string_writer.ts";
-import { copyN } from "./copy_n.ts";
 import { copy } from "./copy.ts";
 import { StringReader } from "./string_reader.ts";
 
 Deno.test("ioMultiReader", async function () {
   const r = new MultiReader([new StringReader("abc"), new StringReader("def")]);
   const w = new StringWriter();
-  const n = await copyN(r, w, 4);
-  assertEquals(n, 4);
-  assertEquals(w.toString(), "abcd");
   await copy(r, w);
   assertEquals(w.toString(), "abcdef");
 });
