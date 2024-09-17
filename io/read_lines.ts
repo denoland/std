223eb2a2b2806f14abc07eed72f9a0358c49c0d1
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-17 14:51:15 +1000

    update

--- io/read_lines.ts
+++ io/read_lines.ts
@@ -26,7 +26,7 @@ import { concat } from "@std/bytes/concat";
  *
  * @deprecated Use
  * {@linkcode https://jsr.io/@std/streams/doc/unstable-to-lines/~/toLines | toLines}
- * instead. This will be removed in the future.
+ * on the readable stream instead. This will be removed in the future.
  */
 export async function* readLines(
   reader: Reader,
