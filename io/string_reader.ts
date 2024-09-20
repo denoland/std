2ddb723e8e00396945d0550599a2917fdd35fae8
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-20 10:01:50 +1000

    deprecation(io/unstable): deprecate `StringReader`

--- io/string_reader.ts
+++ io/string_reader.ts
@@ -21,7 +21,8 @@ import { Buffer } from "./buffer.ts";
  * assertEquals(new TextDecoder().decode(data), "abcdef");
  * ```
  *
- * @deprecated This will be removed in 1.0.0. Use the {@link https://developer.mozilla.org/en-US/docs/Web/API/Streams_API | Web Streams API} instead.
+ * @deprecated Pass an encoded string, using {@linkcode TextEncoder.encode}, to
+ * a new {@linkcode Buffer} instance instead. This will be removed in 0.226.0.
  */
 export class StringReader extends Buffer {
   /**
