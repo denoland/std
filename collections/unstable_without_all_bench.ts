c7fffb2c153bceef1c038cc82def832c2b28b298
liam-tait <liamr60@gmail.com>
2024-09-22 21:14:00 +1200

    fmt

--- collections/unstable_without_all_bench.ts
+++ collections/unstable_without_all_bench.ts
@@ -7,16 +7,16 @@ const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const b = [2, 4, 6, 8, 10];
 
 Deno.bench({
-    name: "withoutAll",
-    baseline: true,
-    fn: () => {
-        withoutAll(a, b);
-    },
+  name: "withoutAll",
+  baseline: true,
+  fn: () => {
+    withoutAll(a, b);
+  },
 });
 
 Deno.bench({
-    name: "(unstable) withoutAll",
-    fn: () => {
-        unstableWithoutAll(a, b);
-    },
+  name: "(unstable) withoutAll",
+  fn: () => {
+    unstableWithoutAll(a, b);
+  },
 });
