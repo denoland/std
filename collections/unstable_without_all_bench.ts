51efac928b58436ff1e30bd22ec6e49dc92142cc
liam-tait <liamr60@gmail.com>
2024-09-22 21:07:23 +1200

    feat(collections/unstable): support Iterable argument in withoutAll()

---
+++ collections/unstable_without_all_bench.ts
@@ -0,0 +1,22 @@
+import { withoutAll } from "./without_all.ts";
+import { withoutAll as unstableWithoutAll } from "./unstable_without_all.ts";
+
+// TODO remove this benchmark before merging
+// Edit these arrays to test different scenarios
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const b = [2, 4, 6, 8, 10];
+
+Deno.bench({
+    name: "withoutAll",
+    baseline: true,
+    fn: () => {
+        withoutAll(a, b);
+    },
+});
+
+Deno.bench({
+    name: "(unstable) withoutAll",
+    fn: () => {
+        unstableWithoutAll(a, b);
+    },
+});
