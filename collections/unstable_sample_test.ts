2e4f44be4ac938b0c89580bdc62b0ebc496f6f1e
liam-tait <liamr60@gmail.com>
2024-09-24 01:13:05 +1200

    add more tests

--- collections/unstable_sample_test.ts
+++ collections/unstable_sample_test.ts
@@ -54,5 +54,74 @@ Deno.test("(unstable) sample() handles a generator", () => {
     yield 3;
   }
   const actual = sample(gen());
-  assert(actual && [1, 2, 3].includes(actual));
+  assert(actual !== undefined);
+  assert(Array.from(gen()).includes(actual));
+});
+
+Deno.test("(unstable) sample() handles an array with empty slots", () => {
+  const input = Array(10);
+  const actual = sample(input);
+  assertEquals(actual, undefined);
+});
+
+Deno.test("(unstable) sample() handles a Set", () => {
+  const input = new Set([1, 2, 3]);
+  const actual = sample(input);
+  assert(actual !== undefined);
+  assert(input.has(actual));
+});
+
+Deno.test("(unstable) sample() handles a Map", () => {
+  const input = new Map([
+    ["a", 1],
+    ["b", 2],
+    ["c", 3],
+  ]);
+  const actual = sample(input);
+  assert(actual !== undefined);
+  assert(Array.isArray(actual));
+  assert(actual.length === 2);
+  assert(input.has(actual[0]));
+  assert(input.get(actual[0]) === actual[1]);
+});
+
+Deno.test("(unstable) sample() handles TypedArrays", () => {
+  const typedArrays = [
+    Uint8Array,
+    Uint8ClampedArray,
+    Uint16Array,
+    Uint32Array,
+    Int8Array,
+    Int16Array,
+    Int32Array,
+    Float32Array,
+    Float64Array,
+  ];
+  for (const TypedArray of typedArrays) {
+    const input = new TypedArray([1, 2, 3]);
+    const actual = sample(input);
+    assert(actual !== undefined);
+    assert(input.includes(actual));
+  }
+});
+
+Deno.test("(unstable) sample() handles a string", () => {
+  const input = "abc";
+  const actual = sample("abc");
+  assert(actual !== undefined);
+  assert(actual.length === 1);
+  assert(input.includes(actual));
+});
+
+Deno.test("(unstable) sample() handles user-defined iterable", () => {
+  const iterable = {
+    *[Symbol.iterator]() {
+      yield "a";
+      yield "b";
+      yield "c";
+    },
+  };
+  const actual = sample(iterable);
+  assert(actual !== undefined);
+  assert(["a", "b", "c"].includes(actual));
 });
