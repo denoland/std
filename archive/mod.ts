4f831f925dadd105ec673d59ebf000ca3f4879b0
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-24 16:14:16 +1000

    tweak

--- archive/mod.ts
+++ archive/mod.ts
@@ -63,7 +63,7 @@
  * ```
  *
  * @deprecated Use {@linkcode https://jsr.io/@std/tar | @std/tar} instead.
- * `@std/archive` will be removed, with 0.225.4 being the last published version.
+ * `@std/archive` will be removed in the future.
  *
  * @experimental **UNSTABLE**: New API, yet to be vetted.
  *
