4ec394e268d0573a6d51244cf80e220e75803880
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-09-24 18:02:46 +1000

    refactor(cbor): `upgradeStreamFromGen()`

--- cbor/_common.ts
+++ cbor/_common.ts
@@ -50,34 +50,31 @@ export function upgradeStreamFromGen(
   return new ReadableStream({
     type: "bytes",
     async pull(controller) {
-      while (true) {
-        const { done, value } = await gen.next();
-        if (done) {
-          try {
-            controller.byobRequest?.respond(0);
-            return controller.close();
-          } catch {
-            controller.close();
-            return controller.byobRequest?.respond(0);
-          }
+      const value = await async function () {
+        while (true) {
+          const { done, value } = await gen.next();
+          if (done) return undefined;
+          if (value.length) return value;
         }
-        if (controller.byobRequest?.view) {
-          const buffer = new Uint8Array(controller.byobRequest.view.buffer);
-          const size = buffer.length;
-          if (value.length > size) {
-            buffer.set(value.slice(0, size));
-            controller.byobRequest.respond(size);
-            controller.enqueue(value.slice(size));
-          } else {
-            buffer.set(value);
-            controller.byobRequest.respond(value.length);
-          }
-          break;
-        } else if (value.length) {
-          controller.enqueue(value);
-          break;
-        }
+      }();
+
+      if (value == undefined) {
+        controller.close();
+        return controller.byobRequest?.respond(0);
       }
+
+      if (controller.byobRequest?.view) {
+        const buffer = new Uint8Array(controller.byobRequest.view.buffer);
+        const size = buffer.length;
+        if (value.length > size) {
+          buffer.set(value.slice(0, size));
+          controller.byobRequest.respond(size);
+          controller.enqueue(value.slice(size));
+        } else {
+          buffer.set(value);
+          controller.byobRequest.respond(value.length);
+        }
+      } else controller.enqueue(value);
     },
   });
 }
