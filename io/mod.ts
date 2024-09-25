eb064eba09f56ca92623f4dc7655cb084c5c4db4
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-25 13:58:16 +1000

    BREAKING(io/unstable): remove `sliceLongToBytes()` (#6005)

--- io/mod.ts
+++ io/mod.ts
@@ -31,7 +31,6 @@ export * from "./read_range.ts";
 export * from "./read_short.ts";
 export * from "./read_string_delim.ts";
 export * from "./reader_from_stream_reader.ts";
-export * from "./slice_long_to_bytes.ts";
 export * from "./string_reader.ts";
 export * from "./string_writer.ts";
 export * from "./to_readable_stream.ts";
