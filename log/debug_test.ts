97428edc9fb3b62f8521ba37895a0cceadf98e88
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-26 15:21:24 +1000

    update

--- log/debug_test.ts
+++ log/debug_test.ts
@@ -1,18 +1,10 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assertEquals } from "@std/assert";
-import { assertSpyCall, spy } from "@std/testing/mock";
 import { debug } from "./debug.ts";
 
 Deno.test("debug()", () => {
-  using consoleInfoSpy = spy(console, "log");
   const debugData: string = debug("foo");
   const debugResolver: string | undefined = debug(() => "foo");
   assertEquals(debugData, "foo");
   assertEquals(debugResolver, undefined);
-  assertSpyCall(consoleInfoSpy, 0, {
-    args: ["DEBUG foo"],
-  });
-  assertSpyCall(consoleInfoSpy, 1, {
-    args: ["DEBUG foo"],
-  });
 });
