2557b7c80f507bbf433b2a0f0828b8ead2519b3b
liam-tait <liamr60@gmail.com>
2024-09-28 21:05:38 +1200

    feat(collections/unstable): support `Iterable` argument in `dropLastWhile()`

---
+++ collections/drop_last_while_bench.ts
@@ -0,0 +1,22 @@
+import { dropLastWhile } from "./drop_last_while.ts";
+import {
+    dropLastWhile as unstableDropLastWhile,
+} from "./unstable_drop_last_while.ts";
+
+const arr = Array.from({ length: 1_000 }, (_, i) => i);
+const predicate = (number: number) => number > 500;
+
+Deno.bench({
+    name: "dropLastWhile",
+    baseline: true,
+    fn: () => {
+        dropLastWhile(arr, predicate);
+    },
+});
+
+Deno.bench({
+    name: "(unstable) dropLastWhile",
+    fn: () => {
+        unstableDropLastWhile(arr, predicate);
+    },
+});
