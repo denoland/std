5e507e46ef85106cf43986b1eea6ed1dbd0463ca
Liam Tait <liam@runn.io>
2024-10-08 01:22:38 +1300

    tests

--- collections/unstable_sliding_windows_test.ts
+++ collections/unstable_sliding_windows_test.ts
@@ -295,3 +295,41 @@ Deno.test("slidingWindows() handles a generator", () => {
 
   slidingWindowsTest([emptyGen(), 3], []);
 });
+
+
+Deno.test("slidingWindows() handles a string", () => {
+  const str = "12345";
+  slidingWindowsTest([str, 5], [["1", "2", "3", "4", "5"]]);
+  slidingWindowsTest([str, 3], [["1", "2", "3"], ["2", "3", "4"], ["3", "4", "5"]]);
+  slidingWindowsTest([str, 1], [["1"], ["2"], ["3"], ["4"], ["5"]]);
+})
+
+Deno.test("slidingWindows() handles a Set", () => {
+  const set = new Set([1, 2, 3, 4, 5]);
+  slidingWindowsTest([set, 5], [[1, 2, 3, 4, 5]]);
+  slidingWindowsTest([set, 3], [[1, 2, 3], [2, 3, 4], [3, 4, 5]]);
+  slidingWindowsTest([set, 1], [[1], [2], [3], [4], [5]]);
+}
+)
+
+Deno.test("slidingWindows() handles a Map", () => {
+  const map = new Map([
+    ["a", 1],
+    ["b", 2],
+    ["c", 3],
+    ["d", 4],
+    ["e", 5],
+  ]);
+  slidingWindowsTest([map, 3], [
+    [["a", 1], ["b", 2], ["c", 3]],
+    [["b", 2], ["c", 3], ["d", 4]],
+    [["c", 3], ["d", 4], ["e", 5]],
+  ]);
+  slidingWindowsTest([map, 1], [
+    [["a", 1]],
+    [["b", 2]],
+    [["c", 3]],
+    [["d", 4]],
+    [["e", 5]]
+  ]);
+})
