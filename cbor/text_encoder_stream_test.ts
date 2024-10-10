aa6e8bb696d15d6a0399b8a8b9662b2aafb0fb46
Yoshiya Hinosawa <stibium121@gmail.com>
2024-10-10 11:24:44 +0100

    disable CborTextEncoderStream() testing

--- cbor/text_encoder_stream_test.ts
+++ cbor/text_encoder_stream_test.ts
@@ -6,46 +6,54 @@ import { random } from "./_common_test.ts";
 import { encodeCbor } from "./encode_cbor.ts";
 import { CborTextEncoderStream } from "./text_encoder_stream.ts";
 
-Deno.test("CborTextEncoderStream() correctly encoding", async () => {
-  const strings = [
-    "a".repeat(random(0, 24)),
-    "a".repeat(random(24, 2 ** 8)),
-    "a".repeat(random(2 ** 8, 2 ** 10)),
-  ];
+Deno.test(
+  "CborTextEncoderStream() correctly encoding",
+  { ignore: true },
+  async () => {
+    const strings = [
+      "a".repeat(random(0, 24)),
+      "a".repeat(random(24, 2 ** 8)),
+      "a".repeat(random(2 ** 8, 2 ** 10)),
+    ];
 
-  const expectedOutput = concat([
-    new Uint8Array([0b011_11111]),
-    ...strings.filter((x) => x).map((x) => encodeCbor(x)),
-    new Uint8Array([0b111_11111]),
-  ]);
+    const expectedOutput = concat([
+      new Uint8Array([0b011_11111]),
+      ...strings.filter((x) => x).map((x) => encodeCbor(x)),
+      new Uint8Array([0b111_11111]),
+    ]);
 
-  const actualOutput = concat(
-    await Array.fromAsync(
-      ReadableStream.from(strings).pipeThrough(new CborTextEncoderStream()),
-    ),
-  );
+    const actualOutput = concat(
+      await Array.fromAsync(
+        ReadableStream.from(strings).pipeThrough(new CborTextEncoderStream()),
+      ),
+    );
 
-  assertEquals(actualOutput, expectedOutput);
-});
+    assertEquals(actualOutput, expectedOutput);
+  },
+);
 
-Deno.test("CborTextEncoderStream.from() correctly encoding", async () => {
-  const strings = [
-    "a".repeat(random(0, 24)),
-    "a".repeat(random(24, 2 ** 8)),
-    "a".repeat(random(2 ** 8, 2 ** 10)),
-  ];
+Deno.test(
+  "CborTextEncoderStream.from() correctly encoding",
+  { ignore: true },
+  async () => {
+    const strings = [
+      "a".repeat(random(0, 24)),
+      "a".repeat(random(24, 2 ** 8)),
+      "a".repeat(random(2 ** 8, 2 ** 10)),
+    ];
 
-  const expectedOutput = concat([
-    new Uint8Array([0b011_11111]),
-    ...strings.filter((x) => x).map((x) => encodeCbor(x)),
-    new Uint8Array([0b111_11111]),
-  ]);
+    const expectedOutput = concat([
+      new Uint8Array([0b011_11111]),
+      ...strings.filter((x) => x).map((x) => encodeCbor(x)),
+      new Uint8Array([0b111_11111]),
+    ]);
 
-  const actualOutput = concat(
-    await Array.fromAsync(
-      CborTextEncoderStream.from(strings).readable,
-    ),
-  );
+    const actualOutput = concat(
+      await Array.fromAsync(
+        CborTextEncoderStream.from(strings).readable,
+      ),
+    );
 
-  assertEquals(actualOutput, expectedOutput);
-});
+    assertEquals(actualOutput, expectedOutput);
+  },
+);
