1e6c9d875170cb774b9eb77d4b208ea7ef3c1dfd
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-17 17:22:58 +0900

    remove misleading example

--- expect/_assert_is_error.ts
+++ expect/_assert_is_error.ts
@@ -9,17 +9,6 @@ import { stripAnsiCode } from "@std/internal/styles";
  * An error class and a string that should be included in the
  * error message can also be asserted.
  *
- * @example Usage
- * ```ts ignore
- * import { assertIsError } from "@std/assert";
- *
- * assertIsError(null); // Throws
- * assertIsError(new RangeError("Out of range")); // Doesn't throw
- * assertIsError(new RangeError("Out of range"), SyntaxError); // Throws
- * assertIsError(new RangeError("Out of range"), SyntaxError, "Out of range"); // Doesn't throw
- * assertIsError(new RangeError("Out of range"), SyntaxError, "Within range"); // Throws
- * ```
- *
  * @typeParam E The type of the error to assert.
  * @param error The error to assert.
  * @param ErrorClass The optional error class to assert.
