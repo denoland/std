3c1b8e9c7202905414019d6040657ebd131f4305
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-01-13 04:31:19 -0500

    test(csv): unstable stringify tests (#6337)

--- csv/unstable_stringify_test.ts
+++ csv/unstable_stringify_test.ts
@@ -2,6 +2,7 @@
 
 import { assertEquals } from "@std/assert/equals";
 import { stringify } from "./unstable_stringify.ts";
+import { assertThrows } from "../assert/throws.ts";
 
 const CRLF = "\r\n";
 
@@ -25,6 +26,66 @@ Deno.test("(unstable) stringify", async (t) => {
         const columns = ["a"];
         const output = `a${CRLF}1${CRLF}2${CRLF}${CRLF}`;
         assertEquals(stringify(data, { columns }), output);
+      },
+    },
+  );
+
+  await t.step(
+    {
+      name: "options.columns is a bool",
+      fn() {
+        const options = { columns: true };
+        const data = [{ a: 1 }];
+        assertThrows(
+          // @ts-ignore: for test
+          () => stringify(data, options),
+          "Cannot stringify data as the columns option is invalid: columns must be an array or undefined",
+        );
+      },
+    },
+  );
+
+  await t.step(
+    {
+      name: "options.columns is an object",
+      fn() {
+        const options = { columns: { v: true } };
+        const data = [{ a: 1 }];
+        assertThrows(
+          // @ts-ignore: for test
+          () => stringify(data, options),
+          "Cannot stringify data as the columns option is invalid: columns must be an array or undefined",
+        );
+      },
+    },
+  );
+
+  await t.step(
+    {
+      name: "options.columns is a number",
+      fn() {
+        const options = { columns: 127 };
+        const data = [{ a: 1 }];
+        assertThrows(
+          // @ts-ignore: for test
+          () => stringify(data, options),
+          "Cannot stringify data as the columns option is invalid: columns must be an array or undefined",
+        );
+      },
+    },
+  );
+
+  await t.step(
+    {
+      name: "options.columns is a string",
+      fn() {
+        const options = { columns: "i am a string" };
+        const data = [{ a: 1 }];
+        assertThrows(
+          // @ts-ignore: for test
+          () => stringify(data, options),
+          "Cannot stringify data as the columns option is invalid: columns must be an array or undefined",
+        );
       },
     },
   );
