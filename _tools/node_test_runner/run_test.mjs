a2a6305725b297b39bd41d96516f039d71d624af
James Bronder <36022278+jbronder@users.noreply.github.com>
2025-01-20 20:03:46 -0800

    feat(fs/unstable): add symlink and symlinkSync (#6352)

--- _tools/node_test_runner/run_test.mjs
+++ _tools/node_test_runner/run_test.mjs
@@ -51,6 +51,7 @@ import "../../collections/without_all_test.ts";
 import "../../collections/zip_test.ts";
 import "../../fs/unstable_read_dir_test.ts";
 import "../../fs/unstable_stat_test.ts";
+import "../../fs/unstable_symlink_test.ts";
 import "../../fs/unstable_lstat_test.ts";
 import "../../fs/unstable_chmod_test.ts";
 
