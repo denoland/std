3c3e28e896f7ce1e5346c09f0f318b001240953c
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2025-02-03 21:23:15 +1100

    docs(cli): added for progress bar

--- cli/unstable_progress_bar.ts
+++ cli/unstable_progress_bar.ts
@@ -1,18 +1,101 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
+/**
+ * The options that are provided to a {@link createProgressBar} or
+ * {@link ProgressBarStream}.
+ */
 export interface ProgressBarOptions {
+  /**
+   * The offset size of the input if progress is resuming part way through.
+   * @default {0}
+   */
   currentSize?: number;
+  /**
+   * The total size expected to receive.
+   */
   totalSize: number;
+  /**
+   * The length that the progress bar should be, in characters.
+   * @default {50}
+   */
   barLength?: number;
+  /**
+   * The character to fill the progress bar up with as it makes progress.
+   * @default {'#'}
+   */
   fullChar?: string;
+  /**
+   * The character the progress bar starts out with.
+   * @default {'-'}
+   */
   emptyChar?: string;
+  /**
+   * Whether the progress bar should be removed after completion.
+   * @default {false}
+   */
   clearBar?: boolean;
+  /**
+   * Whether or not a timer `[mm:ss]` should be displayed.
+   * @default {true}
+   */
   includeTime?: boolean;
+  /**
+   * Whether or not a percentage `[0.00%]` should be displayed.
+   * @default {false}
+   */
   includePercent?: boolean;
+  /**
+   * Whether or not the amount `[0.00/97.04 KiB]` passed through should be
+   * displayed.
+   * @default {true}
+   */
   includeAmount?: boolean;
+  /**
+   * Whether or not the {@link WritableStream} argument should be closed upon
+   * completion.
+   * @default {true}
+   */
   preventClose?: boolean;
 }
 
+/**
+ * `createProgressBar` is a customisable function that reports updates to a
+ * {@link WritableStream} on a 1s interval. Progress is communicated by calling
+ * the function returned from `createProgressBar`.
+ *
+ * @param writable The {@link WritableStream} that will receive the progress bar
+ * reports.
+ * @param options The options to configure various settings of the progress bar.
+ * @returns A function to update the amount of progress made.
+ *
+ * @experimental **UNSTABLE**: New API, yet to be vetted.
+ *
+ * @example Basic Usage
+ * ```ts
+ * import { createProgressBar } from "@std/cli/unstable-progress-bar";
+ *
+ * const gen = async function* (): AsyncGenerator<Uint8Array> {
+ *     for (let i = 0; i < 100; ++i) {
+ *       yield new Uint8Array(1000).fill(97);
+ *       await new Promise((a) => setTimeout(a, (Math.random() * 200) | 0));
+ *     }
+ *   }();
+ * const file = await Deno.create("./_tmp/output.txt");
+ * const writer = file.writable.getWriter();
+ *
+ * const addProgress = createProgressBar(
+ *   Deno.stdout.writable,
+ *   { totalSize: 100_000 },
+ * );
+ *
+ * for await (const buffer of gen) {
+ *   await addProgress(buffer.length);
+ *   await writer.write(buffer);
+ * }
+ * await addProgress(0, true);
+ * file.close();
+ * ```
+ */
 export function createProgressBar(
   writable: WritableStream<Uint8Array>,
   options: ProgressBarOptions,
@@ -43,6 +126,10 @@ export function createProgressBar(
   const startTime = performance.now();
   const id = setInterval(print, 1_000);
 
+  /**
+   * @param addSize The amount of bytes of progressed made since last call.
+   * @param done Whether or not you're done reporting progress.
+   */
   return async function addProgress(
     addSize: number,
     done = false,
@@ -87,6 +174,7 @@ export function createProgressBar(
     if (!options.includePercent) return "";
     return "[" +
       (options.currentSize! / options.totalSize * 100).toFixed(2) +
+      "%] ";
   }
   function amount(): string {
     if (!options.includeAmount) return "";
--- cli/unstable_progress_bar_stream.ts
+++ cli/unstable_progress_bar_stream.ts
@@ -5,7 +5,37 @@ import {
   type ProgressBarOptions,
 } from "./unstable_progress_bar.ts";
 
+/**
+ * `ProgressBarStream` is a {@link TransformStream} class that reports updates
+ * to a separate {@link WritableStream} on a 1s interval.
+ *
+ * @experimental **UNSTABLE**: New API, yet to be vetted.
+ *
+ * @example Basic Usage
+ * ```ts
+ * import { ProgressBarStream } from "@std/cli/unstable-progress-bar-stream";
+ *
+ * const response = await fetch("https://example.com/");
+ * const totalSize = Number(response.headers.get("content-length"));
+ * let readable = response.body
+ * if (totalSize) {
+ *   readable = readable
+ *     ?.pipeThrough(new ProgressBarStream(
+ *       Deno.stdout.writable,
+ *       { totalSize },
+ *     ));
+ * }
+ * await readable?.pipeTo((await Deno.create("./_tmp/example.com.html")).writable);
+ * ```
+ */
 export class ProgressBarStream extends TransformStream<Uint8Array, Uint8Array> {
+  /**
+   * Constructs a new instance.
+   *
+   * @param writable The {@link WritableStream} that will receive the progress bar
+   * reports.
+   * @param options The options to configure various settings of the progress bar.
+   */
   constructor(
     writable: WritableStream<Uint8Array>,
     options: ProgressBarOptions,
