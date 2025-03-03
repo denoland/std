01cb2393d87c46eb29aa1c275b2f238ff87f8f4f
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2025-03-03 11:44:50 +1100

    revert(encoding): changes for base64 stream

--- encoding/unstable_base64url_stream_test.ts
+++ encoding/unstable_base64url_stream_test.ts
@@ -1,33 +1,33 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
 import { assertEquals } from "@std/assert";
-import { encodeBase64 } from "./base64.ts";
+import { encodeBase64Url } from "./base64url.ts";
 import {
-  Base64DecoderStream,
-  Base64EncoderStream,
-} from "./unstable_base64_stream.ts";
+  Base64UrlDecoderStream,
+  Base64UrlEncoderStream,
+} from "./unstable_base64url_stream.ts";
 import { RandomSliceStream } from "./_random_slice_stream.ts";
 import { toText } from "@std/streams/to-text";
 import { concat } from "@std/bytes/concat";
 
-Deno.test("Base64EncoderStream() encodes stream", async () => {
+Deno.test("Base64UrlEncoderStream() encodes stream", async () => {
   const stream = (await Deno.open("./deno.lock"))
     .readable
     .pipeThrough(new RandomSliceStream())
-    .pipeThrough(new Base64EncoderStream());
+    .pipeThrough(new Base64UrlEncoderStream());
 
   assertEquals(
     await toText(stream),
-    encodeBase64(await Deno.readFile("./deno.lock")),
+    encodeBase64Url(await Deno.readFile("./deno.lock")),
   );
 });
 
-Deno.test("Base64DecoderStream() decodes stream", async () => {
+Deno.test("Base64UrlDecoderStream() decodes stream", async () => {
   const stream = (await Deno.open("./deno.lock"))
     .readable
-    .pipeThrough(new Base64EncoderStream())
+    .pipeThrough(new Base64UrlEncoderStream())
     .pipeThrough(new RandomSliceStream())
-    .pipeThrough(new Base64DecoderStream());
+    .pipeThrough(new Base64UrlDecoderStream());
 
   assertEquals(
     concat(await Array.fromAsync(stream)),
