aeb2f8c4636e69a418259ef64f1f9f0361e9bd64
Tim <tim.reichen@gmail.com>
2025-03-07 00:33:37 +0100

    initial commit

--- front_matter/any.ts
+++ front_matter/any.ts
@@ -4,28 +4,9 @@ import { extract as extractToml } from "./toml.ts";
 import { extract as extractYaml } from "./yaml.ts";
 import { extract as extractJson } from "./json.ts";
 import type { Extract } from "./types.ts";
-import type { Format } from "./test.ts";
-import { EXTRACT_REGEXP_MAP, RECOGNIZE_REGEXP_MAP } from "./_formats.ts";
+import { RECOGNIZE_REGEXP_MAP } from "./_formats.ts";
 
 export type { Extract };
-
-/**
- * Recognizes the format of the front matter in a string.
- * Supports {@link https://yaml.org | YAML}, {@link https://toml.io | TOML} and
- * {@link https://www.json.org/ | JSON}.
- *
- * @param str String to recognize.
- * @param formats A list of formats to recognize. Defaults to all supported formats.
- */
-function recognize(
-  str: string,
-  formats: Format[],
-): Format {
-  for (const format of formats) {
-    if (RECOGNIZE_REGEXP_MAP.get(format)?.test(str)) return format;
-  }
-  throw new TypeError("Unsupported front matter format");
-}
 
 /**
  * Extracts and parses {@link https://yaml.org | YAML}, {@link https://toml.io |
@@ -53,9 +34,11 @@ function recognize(
  * @param text The text to extract front matter from.
  * @returns The extracted front matter and body content.
  */
-export function extract<T>(text: string): Extract<T> {
-  const formats = [...EXTRACT_REGEXP_MAP.keys()] as Format[];
-  const format = recognize(text, formats);
+export function extract<T>(
+  text: string,
+): Extract<T> {
+  const format = RECOGNIZE_REGEXP_MAP.entries()
+    .find(([_, regexp]) => regexp.test(text))?.[0];
   switch (format) {
     case "yaml":
       return extractYaml<T>(text);
@@ -63,5 +46,7 @@ export function extract<T>(text: string): Extract<T> {
       return extractToml<T>(text);
     case "json":
       return extractJson<T>(text);
+    default:
+      throw new TypeError("Unsupported front matter format");
   }
 }
