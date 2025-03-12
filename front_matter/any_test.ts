2cd22ba63bbd7e5573245fd24c965684cc240432
Tim <tim.reichen@gmail.com>
2025-03-12 22:11:00 +0100

    update

---
+++ front_matter/any_test.ts
@@ -0,0 +1,67 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
+
+import { extract } from "@std/front-matter/any";
+import { assertEquals } from "../assert/equals.ts";
+import { assertThrows } from "../assert/throws.ts";
+
+Deno.test("extract() handles yaml data", () => {
+  const input = `---
+  foo: bar
+---
+Hello, world!`;
+  const actual = extract(input);
+  const expected = {
+    attrs: {
+      foo: "bar",
+    },
+    body: "Hello, world!",
+    frontMatter: "foo: bar",
+  };
+  assertEquals(actual, expected);
+});
+
+Deno.test("extract() handles toml data", () => {
+  const input = `+++
+  foo = "bar"
++++
+Hello, world!`;
+  const actual = extract(input);
+  const expected = {
+    attrs: {
+      foo: "bar",
+    },
+    body: "Hello, world!",
+    frontMatter: 'foo = "bar"',
+  };
+  assertEquals(actual, expected);
+});
+
+Deno.test("extract() handles json data", () => {
+  const input = `---json
+{
+  "foo": "bar"
+}
+---
+Hello, world!`;
+
+  const actual = extract(input);
+  const expected = {
+    attrs: { foo: "bar" },
+    body: "Hello, world!",
+    frontMatter: '{\n  "foo": "bar"\n}',
+  };
+  assertEquals(actual, expected);
+});
+
+Deno.test("extract() throws on unsupported format", () => {
+  const input = `---unsupported
+  foo: bar
+---
+Hello, world!`;
+
+  assertThrows(
+    () => extract(input),
+    TypeError,
+    "Unsupported front matter format",
+  );
+});
