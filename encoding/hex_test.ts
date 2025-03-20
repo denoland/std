8d5c90594ee7fd8614a73a7d993d78c80b280881
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2025-03-20 16:08:30 +1100

    perf(encoding): hex

--- encoding/hex_test.ts
+++ encoding/hex_test.ts
@@ -21,14 +21,14 @@ const testCases = [
 
 const errCases: [string, ErrorConstructor, string][] = [
   // encoded(hex) / error / msg
-  ["0", RangeError, ""],
-  ["zd4aa", TypeError, "'z'"],
-  ["d4aaz", TypeError, "'z'"],
-  ["30313", RangeError, ""],
-  ["0g", TypeError, "'g'"],
-  ["00gg", TypeError, "'g'"],
-  ["0\x01", TypeError, "'\x01'"],
-  ["ffeed", RangeError, ""],
+  ["0", TypeError, "Invalid Character (0)"],
+  ["zd4aa", TypeError, "Invalid Character (a)"],
+  ["d4aaz", TypeError, "Invalid Character (z)"],
+  ["30313", TypeError, "Invalid Character (3)"],
+  ["0g", TypeError, "Invalid Character (g)"],
+  ["00gg", TypeError, "Invalid Character (g)"],
+  ["0\x01", TypeError, "Invalid Character (\x01)"],
+  ["ffeed", TypeError, "Invalid Character (d)"],
 ];
 
 Deno.test("encodeHex() handles string", () => {
