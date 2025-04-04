76ad618ea3b1ebbdd28e08197188758d9849304d
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-04 19:28:52 +1100

    chore(_tools): Deno Style Guide lint plugin

---
+++ _tools/lint_plugin.ts
@@ -0,0 +1,39 @@
+/**
+ * Lint plugin that enforces the
+ * {@link https://docs.deno.com/runtime/contributing/style_guide/ | Deno Style Guide}
+ */
+
+export default {
+  name: "deno-style-guide",
+  rules: {
+    // https://docs.deno.com/runtime/contributing/style_guide/#prefer-%23-over-private-keyword
+    "prefer-private-field": {
+      create(context) {
+        return {
+          MethodDefinition(node) {
+            if (node.accessibility === "private") {
+              // TODO(iuioiua): add fix
+              context.report({
+                node,
+                range: node.range,
+                message: "Use private field instead",
+                hint: "Use #foo() instead of private foo()",
+              });
+            }
+          },
+          PropertyDefinition(node) {
+            if (node.accessibility === "private") {
+              // TODO(iuioiua): add fix
+              context.report({
+                node,
+                range: node.range,
+                message: "Use private field instead",
+                hint: "Use #foo instead of private foo",
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+} satisfies Deno.lint.Plugin;
---
+++ _tools/lint_plugin_test.ts
@@ -0,0 +1,50 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
+
+import { assertEquals } from "@std/assert/equals";
+import lintPlugin from "./lint_plugin.ts";
+
+function assertLintPluginDiagnostics(
+  source: string,
+  // Note: if empty, means no lint errors
+  expectedDiagnostics: Deno.lint.Diagnostic[],
+) {
+  const actualDiagnostics = Deno.lint.runPlugin(
+    lintPlugin,
+    "main.ts", // Dummy filename, file doesn't need to exist.
+    source,
+  );
+  assertEquals(actualDiagnostics, expectedDiagnostics);
+}
+
+Deno.test("deno-style-guide/prefer-private-field", () => {
+  // Good
+  assertLintPluginDiagnostics(
+    `
+class MyClass {
+  #foo = 1;
+  #bar() {}
+}
+    `,
+    [],
+  );
+  assertLintPluginDiagnostics(
+    `
+class MyClass {
+  private foo = 1;
+  private bar() {}
+}`,
+    [{
+      id: "deno-style-guide/prefer-private-field",
+      fix: [],
+      range: [18, 34],
+      message: "Use private field instead",
+      hint: "Use #foo instead of private foo",
+    }, {
+      id: "deno-style-guide/prefer-private-field",
+      range: [37, 53],
+      fix: [],
+      message: "Use private field instead",
+      hint: "Use #foo() instead of private foo()",
+    }],
+  );
+});
