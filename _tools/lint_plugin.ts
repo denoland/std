5ad1a81d781440cdccfc123cbc5137c2926f9aca
Tim <tim.reichen@gmail.com>
2025-04-08 16:48:25 +0200

    initial commit

--- _tools/lint_plugin.ts
+++ _tools/lint_plugin.ts
@@ -1,10 +1,26 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
-// @ts-nocheck Deno.lint namespace does not pass type checking in Deno 1.x
 
 /**
  * Lint plugin that enforces the
  * {@link https://docs.deno.com/runtime/contributing/style_guide/ | Deno Style Guide}
  */
+
+import { toCamelCase, toPascalCase } from "@std/text";
+
+const PASCAL_CASE_REGEXP = /^[_A-Z][a-z0-9]*(?:[A-Z][a-z0-9]+)*$/;
+function isPascalCase(string: string): boolean {
+  return PASCAL_CASE_REGEXP.test(string);
+}
+
+const CAMEL_CASE_REGEXP = /^[_a-z][a-z0-9]*(?:[A-Z][a-z0-9]*)*$/;
+function isCamelCase(string: string): boolean {
+  return CAMEL_CASE_REGEXP.test(string);
+}
+
+const CONSTANT_CASE_REGEXP = /^[A-Z]+(?:_[A-Z0-9]+)*$/;
+function isConstantCase(string: string): boolean {
+  return CONSTANT_CASE_REGEXP.test(string);
+}
 
 export default {
   name: "deno-style-guide",
@@ -34,6 +50,142 @@ export default {
               hint:
                 "Use private field (`#`) instead of the `private` keyword. E.g. Use `#foo` instead of `private foo`.",
             });
+          },
+        };
+      },
+    },
+    // https://docs.deno.com/runtime/contributing/style_guide/#naming-convention/
+    "variable-name": {
+      create(context) {
+        return {
+          TSTypeAliasDeclaration(node) {
+            const name = node.id.name;
+            if (!name) return;
+            if (!isPascalCase(name)) {
+              context.report({
+                node: node.id,
+                message: `Type name '${name}' is not PascalCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(node.id, toPascalCase(name));
+                },
+              });
+            }
+          },
+          TSInterfaceDeclaration(node) {
+            const name = node.id.name;
+            if (!name) return;
+            if (!isPascalCase(name)) {
+              context.report({
+                node: node.id,
+                message: `Interface name '${name}' is not PascalCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(node.id, toPascalCase(name));
+                },
+              });
+            }
+          },
+          TSEnumDeclaration(node) {
+            const name = node.id.name;
+            if (!name) return;
+            if (!isPascalCase(name)) {
+              context.report({
+                node: node.id,
+                message: `Enum name '${name}' is not PascalCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(node.id, toPascalCase(name));
+                },
+              });
+            }
+          },
+          FunctionDeclaration(node) {
+            const id = node.id;
+            if (!id) return;
+            const name = id.name;
+            if (!name) return;
+            if (!isCamelCase(name)) {
+              context.report({
+                node: id,
+                message: `Function name '${name}' is not camelCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(id, toCamelCase(name));
+                },
+              });
+            }
+          },
+          ClassDeclaration(node) {
+            const id = node.id;
+            if (!id) return;
+            const name = id.name;
+            if (!name) return;
+            if (!isPascalCase(name)) {
+              context.report({
+                node: id,
+                message: `Class name '${name}' is not PascalCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(id, toPascalCase(name));
+                },
+              });
+            }
+          },
+          MethodDefinition(node) {
+            const key = node.key;
+            if (key.type !== "Identifier") return;
+            const name = key.name;
+            if (!name) return;
+            if (!isCamelCase(name)) {
+              context.report({
+                node: key,
+                message: `Method name '${name}' is not camelCase.`,
+                fix(fixer) {
+                  return fixer.replaceText(key, toCamelCase(name));
+                },
+              });
+            }
+          },
+          PropertyDefinition(node) {
+            const key = node.key;
+            switch (key.type) {
+              case "Identifier":
+              case "PrivateIdentifier": {
+                const name = key.name;
+                if (!name) return;
+                if (!isCamelCase(name)) {
+                  context.report({
+                    node: key,
+                    message: `Property name '${name}' is not camelCase.`,
+                    fix(fixer) {
+                      return fixer.replaceText(key, toCamelCase(name));
+                    },
+                  });
+                }
+                break;
+              }
+              default:
+                break;
+            }
+          },
+          VariableDeclaration(node) {
+            for (const declaration of node.declarations) {
+              const id = declaration.id;
+              if (id.type !== "Identifier") return;
+              const name = id.name;
+              if (!name) return;
+              if (
+                node.kind === "const" &&
+                isConstantCase(name) &&
+                context.sourceCode.getAncestors(node).length === 1
+              ) return;
+
+              if (!isCamelCase(name)) {
+                context.report({
+                  node: id,
+                  message: `Property name '${name}' is not camelCase.`,
+                  fix(fixer) {
+                    return fixer.replaceText(id, toCamelCase(name));
+                  },
+                });
+              }
+            }
           },
         };
       },
--- _tools/lint_plugin_test.ts
+++ _tools/lint_plugin_test.ts
@@ -55,3 +55,159 @@ class MyClass {
     }],
   );
 });
+
+Deno.test("deno-style-guide/variable-name", {
+  ignore: !Deno.version.deno.startsWith("2"),
+}, () => {
+  // Good
+  assertLintPluginDiagnostics(
+    `
+const CONSTANT_NAME = "foo";
+const constName = "foo";
+let letName = "foo";
+var varName = "foo";
+
+const objectName = {
+  methodName() {
+  },
+  get getName() {
+  },
+  set setName(value) {
+  }
+};
+function functionName() {
+}
+class ClassName {}
+
+type TypeName = unknown;
+interface InterfaceName {};
+enum EnumName {
+  foo = "bar",
+}
+
+    `,
+    [],
+  );
+
+  // Bad
+  assertLintPluginDiagnostics(
+    `
+const CONSTANT_name = "foo";
+const ConstName = "foo";
+let LetName = "foo";
+let LET_NAME = "foo";
+var VarName = "foo";
+let VAR_NAME = "foo";
+
+const ObjectName = {
+  MethodName() {
+  },
+  get GetName() {
+  },
+  set SetName(value: unknown) {
+  },
+};
+function FunctionName() {
+}
+
+function fn() {
+  const NESTED_CONSTANT_CASE = "foo";
+}
+
+class className {}
+
+type typeName = unknown;
+interface interfaceName {};
+enum enumName {
+  foo = "bar",
+}
+
+`,
+    [
+      {
+        fix: [{ range: [7, 20], text: "constantName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'CONSTANT_name' is not camelCase.",
+        range: [7, 20],
+      },
+      {
+        fix: [{ range: [36, 45], text: "constName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'ConstName' is not camelCase.",
+        range: [36, 45],
+      },
+      {
+        fix: [{ range: [59, 66], text: "letName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'LetName' is not camelCase.",
+        range: [59, 66],
+      },
+      {
+        fix: [{ range: [80, 88], text: "letName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'LET_NAME' is not camelCase.",
+        range: [80, 88],
+      },
+      {
+        fix: [{ range: [102, 109], text: "varName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'VarName' is not camelCase.",
+        range: [102, 109],
+      },
+      {
+        fix: [{ range: [123, 131], text: "varName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'VAR_NAME' is not camelCase.",
+        range: [123, 131],
+      },
+      {
+        fix: [{ range: [148, 158], text: "objectName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'ObjectName' is not camelCase.",
+        range: [148, 158],
+      },
+      {
+        fix: [{ range: [257, 269], text: "functionName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Function name 'FunctionName' is not camelCase.",
+        range: [257, 269],
+      },
+      {
+        fix: [{ range: [301, 321], text: "nestedConstantCase" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Property name 'NESTED_CONSTANT_CASE' is not camelCase.",
+        range: [301, 321],
+      },
+      {
+        fix: [{ range: [340, 349], text: "ClassName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Class name 'className' is not PascalCase.",
+        range: [340, 349],
+      },
+      {
+        fix: [{ range: [359, 367], text: "TypeName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Type name 'typeName' is not PascalCase.",
+        range: [359, 367],
+      },
+      {
+        fix: [{ range: [389, 402], text: "InterfaceName" }],
+        hint: undefined,
+        id: "deno-style-guide/variable-name",
+        message: "Interface name 'interfaceName' is not PascalCase.",
+        range: [389, 402],
+      },
+    ],
+  );
+});
