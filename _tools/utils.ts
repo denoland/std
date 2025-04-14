e7d55faa519f83e15fae46c5e511b7f3f7c02033
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-14 22:19:22 +1000

    chore: add `getEntrypoints()` for internal tools

--- _tools/utils.ts
+++ _tools/utils.ts
@@ -1,6 +1,12 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
-
+import ROOT_DENO_JSON from "../deno.json" with { type: "json" };
 import ts from "npm:typescript";
+
+export interface DenoJson {
+  name: string;
+  version: string;
+  exports: Record<string, string>;
+}
 
 const workspaces = JSON.parse(await Deno.readTextFile("deno.json"))
   .workspace as string[];
@@ -35,6 +41,19 @@ export function resolveWorkspaceSpecifiers(
   } else {
     return new URL(specifier).href;
   }
+}
+
+export async function getEntrypoints(): Promise<string[]> {
+  const { workspace } = ROOT_DENO_JSON;
+  const packagesDenoJsons = await Promise.all(workspace.map(async (path) => {
+    return (await import(`../${path}/deno.json`, { with: { type: "json" } }))
+      .default as DenoJson;
+  }));
+  return packagesDenoJsons.flatMap((denoJson) =>
+    Object.keys(denoJson.exports).map((mod) =>
+      mod === "." ? denoJson.name : `${denoJson.name}/${mod}`
+    )
+  );
 }
 
 /**
