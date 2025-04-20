b8392ef0c6a0f442ab7bbc8e1a3f9867488fc30d
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2025-04-20 13:53:55 +1000

    align(encoding): unstable base64 modules with stage 3 proposal

--- encoding/unstable_base64_stream_test.ts
+++ encoding/unstable_base64_stream_test.ts
@@ -11,49 +11,49 @@ import {
 } from "./unstable_base64_stream.ts";
 
 Deno.test("Base64EncoderStream() with normal format", async () => {
-  for (const format of ["Base64", "Base64Url"] as const) {
+  for (const alphabet of ["base64", "base64url"] as const) {
     const readable = (await Deno.open("./deno.lock"))
       .readable
       .pipeThrough(new FixedChunkStream(1021))
-      .pipeThrough(new Base64EncoderStream({ format, output: "string" }));
+      .pipeThrough(new Base64EncoderStream({ alphabet, output: "string" }));
 
     assertEquals(
       await toText(readable),
-      encodeBase64(await Deno.readFile("./deno.lock"), format),
-      format,
+      encodeBase64(await Deno.readFile("./deno.lock"), { alphabet }),
+      alphabet,
     );
   }
 });
 
 Deno.test("Base64EncoderStream() with raw format", async () => {
-  for (const format of ["Base64", "Base64Url"] as const) {
+  for (const alphabet of ["base64", "base64url"] as const) {
     const readable = (await Deno.open("./deno.lock"))
       .readable
       .pipeThrough(new FixedChunkStream(1021))
-      .pipeThrough(new Base64EncoderStream({ format, output: "bytes" }));
+      .pipeThrough(new Base64EncoderStream({ alphabet, output: "bytes" }));
 
     assertEquals(
       await toBytes(readable),
       new TextEncoder().encode(
         encodeBase64(
           await Deno.readFile("./deno.lock"),
-          format,
+          { alphabet },
         ),
       ),
-      format,
+      alphabet,
     );
   }
 });
 
 Deno.test("Base64DecoderStream() with normal format", async () => {
-  for (const format of ["Base64", "Base64Url"] as const) {
+  for (const alphabet of ["base64", "base64url"] as const) {
     const readable = (await Deno.open("./deno.lock"))
       .readable
-      .pipeThrough(new Base64EncoderStream({ format, output: "string" }))
+      .pipeThrough(new Base64EncoderStream({ alphabet, output: "string" }))
       .pipeThrough(new TextEncoderStream())
       .pipeThrough(new FixedChunkStream(1021))
       .pipeThrough(new TextDecoderStream())
-      .pipeThrough(new Base64DecoderStream({ format, input: "string" }));
+      .pipeThrough(new Base64DecoderStream({ alphabet, input: "string" }));
 
     assertEquals(
       await toBytes(readable),
@@ -63,12 +63,12 @@ Deno.test("Base64DecoderStream() with normal format", async () => {
 });
 
 Deno.test("Base64DecoderStream() with raw format", async () => {
-  for (const format of ["Base64", "Base64Url"] as const) {
+  for (const alphabet of ["base64", "base64url"] as const) {
     const readable = (await Deno.open("./deno.lock"))
       .readable
-      .pipeThrough(new Base64EncoderStream({ format, output: "bytes" }))
+      .pipeThrough(new Base64EncoderStream({ alphabet, output: "bytes" }))
       .pipeThrough(new FixedChunkStream(1021))
-      .pipeThrough(new Base64DecoderStream({ format, input: "bytes" }));
+      .pipeThrough(new Base64DecoderStream({ alphabet, input: "bytes" }));
 
     assertEquals(
       await toBytes(readable),
