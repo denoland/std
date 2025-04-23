02a24256c0baadf9149009d02e6121364e1ffdc5
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-23 14:37:19 +1000

    chore: simplify and move `isTestFile()`

--- _tools/utils.ts
+++ _tools/utils.ts
@@ -1,7 +1,5 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
-import ts from "npm:typescript";
-
 export function resolve(
   specifier: string,
   referrer: string,
@@ -9,38 +7,4 @@ export function resolve(
   return (specifier.startsWith("./") || specifier.startsWith("../"))
     ? new URL(specifier, referrer).href
     : import.meta.resolve(specifier);
-}
-
-/**
- * Checks whether a file is named `test.ts` and has no exports.
- * @param filePath
- * @returns true if file is named `test.ts` and has no exports, false otherwise
- */
-export function isTestFile(filePath: string): boolean {
-  if (!filePath.endsWith("test.ts")) return false;
-  const source = Deno.readTextFileSync(filePath);
-  const sourceFile = ts.createSourceFile(
-    filePath,
-    source,
-    ts.ScriptTarget.Latest,
-  );
-
-  let result = true;
-
-  function visitNode(node: ts.Node) {
-    if (!result) return;
-    if (
-      ts.isExportSpecifier(node) ||
-      ts.isExportAssignment(node) ||
-      ts.isExportDeclaration(node) ||
-      node.kind === ts.SyntaxKind.ExportKeyword
-    ) {
-      result = false;
-    } else {
-      ts.forEachChild(node, visitNode);
-    }
-  }
-
-  visitNode(sourceFile);
-  return result;
 }
