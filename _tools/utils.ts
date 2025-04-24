2a059d96434aa7e7cf7f0d9064c417a98798b78c
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-24 16:48:54 +1000

    chore: add new internal utilities and simplify `lint:docs` task script (#6579)

--- _tools/utils.ts
+++ _tools/utils.ts
@@ -1,4 +1,30 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
+export interface DenoJson {
+  name: string;
+  version: string;
+  exports: Record<string, string>;
+  workspace?: string[];
+}
+
+async function importJson(path: string): Promise<DenoJson> {
+  return (await import(path, { with: { type: "json" } })).default;
+}
+
+export async function getPackagesDenoJsons(): Promise<DenoJson[]> {
+  const { workspace } = await importJson("../deno.json");
+  return Promise.all(
+    workspace!.map((path) => importJson(`../${path}/deno.json`)),
+  );
+}
+
+export async function getEntrypoints(): Promise<string[]> {
+  return (await getPackagesDenoJsons())
+    .flatMap(({ name, exports }) =>
+      Object.keys(exports).map((mod) =>
+        mod === "." ? name : name + mod.slice(1)
+      )
+    );
+}
 
 export function resolve(
   specifier: string,
