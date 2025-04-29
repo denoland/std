aa39536e71348779d4cf3b7822ead67419eab927
easrng <me@easrng.net>
2025-04-28 21:37:40 -0400

    don't run util.inspect tests in deno 1.x

--- internal/format_test.ts
+++ internal/format_test.ts
@@ -105,37 +105,39 @@ if (typeof Deno.inspect === "function") {
   });
 }
 
-Deno.test("format() has fallback to util.inspect if Deno.inspect is not available", async (t) => {
-  // Simulates the environment where Deno.inspect is not available
-  const inspect = Deno.inspect;
-  // deno-lint-ignore no-explicit-any
-  delete (Deno as any).inspect;
-  try {
-    assertEquals(format([..."abcd"]), `[\n  'a',\n  'b',\n  'c',\n  'd'\n]`);
-    assertEquals(format({ a: 1, b: 2 }), `{\n  a: 1,\n  b: 2\n}`);
-    await t.step("format() sorts properties", () =>
-      assertEquals(
-        format({ b: 2, a: 1 }),
-        format({ a: 1, b: 2 }),
-      ));
+// deno-lint-ignore no-explicit-any
+if (typeof (globalThis as any).process?.getBuiltinModule === "function") {
+  Deno.test("format() has fallback to util.inspect if Deno.inspect is not available", async (t) => {
+    // Simulates the environment where Deno.inspect is not available
+    const inspect = Deno.inspect;
+    // deno-lint-ignore no-explicit-any
+    delete (Deno as any).inspect;
+    try {
+      assertEquals(format([..."abcd"]), `[\n  'a',\n  'b',\n  'c',\n  'd'\n]`);
+      assertEquals(format({ a: 1, b: 2 }), `{\n  a: 1,\n  b: 2\n}`);
+      await t.step("format() sorts properties", () =>
+        assertEquals(
+          format({ b: 2, a: 1 }),
+          format({ a: 1, b: 2 }),
+        ));
 
-    await t.step("format() wraps Object with getters", () =>
-      assertEquals(
-        format(Object.defineProperty({}, "a", {
-          enumerable: true,
-          get() {
-            return 1;
-          },
-        })),
-        `{
+      await t.step("format() wraps Object with getters", () =>
+        assertEquals(
+          format(Object.defineProperty({}, "a", {
+            enumerable: true,
+            get() {
+              return 1;
+            },
+          })),
+          `{
   a: [Getter: 1]
 }`,
-      ));
+        ));
 
-    await t.step("format() wraps nested small objects", () =>
-      assertEquals(
-        stripAnsiCode(format([{ x: { a: 1, b: 2 }, y: ["a", "b"] }])),
-        `[
+      await t.step("format() wraps nested small objects", () =>
+        assertEquals(
+          stripAnsiCode(format([{ x: { a: 1, b: 2 }, y: ["a", "b"] }])),
+          `[
   {
     x: {
       a: 1,
@@ -147,13 +149,13 @@ Deno.test("format() has fallback to util.inspect if Deno.inspect is not availabl
     ]
   }
 ]`,
-      ));
+        ));
 
-    // Grouping is disabled.
-    await t.step("format() disables grouping", () =>
-      assertEquals(
-        stripAnsiCode(format(["i", "i", "i", "i", "i", "i", "i"])),
-        `[
+      // Grouping is disabled.
+      await t.step("format() disables grouping", () =>
+        assertEquals(
+          stripAnsiCode(format(["i", "i", "i", "i", "i", "i", "i"])),
+          `[
   'i',
   'i',
   'i',
@@ -162,23 +164,24 @@ Deno.test("format() has fallback to util.inspect if Deno.inspect is not availabl
   'i',
   'i'
 ]`,
-      ));
-    await t.step("format() doesn't truncate long strings in object", () => {
-      const str = format({
-        foo:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      });
-      assertEquals(
-        str,
-        `{
+        ));
+      await t.step("format() doesn't truncate long strings in object", () => {
+        const str = format({
+          foo:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        });
+        assertEquals(
+          str,
+          `{
   foo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }`,
-      );
-    });
-  } finally {
-    Deno.inspect = inspect;
-  }
-});
+        );
+      });
+    } finally {
+      Deno.inspect = inspect;
+    }
+  });
+}
 
 Deno.test("format() has fallback to String if util.inspect and Deno.inspect are not available", () => {
   // Simulates the environment where Deno.inspect and util.inspect are not available
@@ -187,13 +190,13 @@ Deno.test("format() has fallback to String if util.inspect and Deno.inspect are 
   delete (Deno as any).inspect;
   // deno-lint-ignore no-explicit-any
   const { process } = globalThis as any;
-  const getBuiltinModule = process.getBuiltinModule;
-  delete process.getBuiltinModule;
+  const getBuiltinModule = process?.getBuiltinModule;
+  delete process?.getBuiltinModule;
   try {
     assertEquals(format([..."abcd"]), `"a,b,c,d"`);
     assertEquals(format({ a: 1, b: 2 }), `"[object Object]"`);
   } finally {
     Deno.inspect = inspect;
-    process.getBuiltinModule = getBuiltinModule;
+    if (process) process.getBuiltinModule = getBuiltinModule;
   }
 });
