78a200c064fa024cb5561de14968ffc34b4955d5
scarf <greenscarf005@gmail.com>
2025-05-23 22:28:18 +0900

    refactor: extend DumperStateOptions

--- yaml/_unstable_dumper_state.ts
+++ yaml/_unstable_dumper_state.ts
@@ -30,6 +30,7 @@ import {
 import { DEFAULT_SCHEMA, type Schema } from "./_schema.ts";
 import type { KindType, StyleVariant, Type } from "./_type.ts";
 import { isObject } from "./_utils.ts";
+import type { DumperStateOptions as StableDumperStateOptions } from "./_dumper_state.ts";
 
 const STYLE_PLAIN = 1;
 const STYLE_SINGLE = 2;
@@ -400,53 +401,7 @@ function inspectNode(
   }
 }
 
-export interface DumperStateOptions {
-  /** indentation width to use (in spaces). */
-  indent?: number;
-  /** when true, adds an indentation level to array elements */
-  arrayIndent?: boolean;
-  /**
-   * do not throw on invalid types (like function in the safe schema)
-   * and skip pairs and single values with such types.
-   */
-  skipInvalid?: boolean;
-  /**
-   * specifies level of nesting, when to switch from
-   * block to flow style for collections. -1 means block style everywhere
-   */
-  flowLevel?: number;
-  /** Each tag may have own set of styles.	- "tag" => "style" map. */
-  styles?: Record<string, StyleVariant>;
-  /** specifies a schema to use. */
-  schema?: Schema;
-  /**
-   * If true, sort keys when dumping YAML in ascending, ASCII character order.
-   * If a function, use the function to sort the keys. (default: false)
-   * If a function is specified, the function must return a negative value
-   * if first argument is less than second argument, zero if they're equal
-   * and a positive value otherwise.
-   */
-  sortKeys?: boolean | ((a: string, b: string) => number);
-  /** set max line width. (default: 80) */
-  lineWidth?: number;
-  /**
-   * if false, don't convert duplicate objects
-   * into references (default: true)
-   */
-  useAnchors?: boolean;
-  /**
-   * if false don't try to be compatible with older yaml versions.
-   * Currently: don't quote "yes", "no" and so on,
-   * as required for YAML 1.1 (default: true)
-   */
-  compatMode?: boolean;
-  /**
-   * if true flow sequences will be condensed, omitting the
-   * space between `key: value` or `a, b`. Eg. `'[a,b]'` or `{a:{b:c}}`.
-   * Can be useful when using yaml for pretty URL query params
-   * as spaces are %-encoded. (default: false).
-   */
-  condenseFlow?: boolean;
+export interface DumperStateOptions extends StableDumperStateOptions {
   /**
    * Strings will be quoted using this quoting style.
    * If you specify single quotes, double quotes will still be used
