f6dd449356e958b6d70fbf6c2d7e3a61aae0a15b
Tim <tim.reichen@gmail.com>
2025-05-24 11:04:00 +0200

    initial commit

--- cli/unstable_progress_bar.ts
+++ cli/unstable_progress_bar.ts
@@ -243,10 +243,12 @@ export class ProgressBar {
       .pipeTo(writable, { preventClose: this.#keepOpen })
       .catch(() => clearInterval(this.#id));
     this.#writer = stream.writable.getWriter();
-    this.#id = setInterval(() => this.#print(), 1000);
     this.#startTime = performance.now();
     this.#lastTime = this.#startTime;
     this.#lastValue = this.value;
+
+    this.#id = setInterval(() => this.#print(), 1000);
+    this.#print();
   }
 
   async #print(): Promise<void> {
@@ -293,8 +295,7 @@ export class ProgressBar {
    */
   async stop(): Promise<void> {
     clearInterval(this.#id);
-    await this.#print()
-      .then(() => this.#writer.write(this.#clear ? "\r\u001b[K" : "\n"))
+    await this.#writer.write(this.#clear ? "\r\u001b[K" : "\n")
       .then(() => this.#writer.close())
       .then(() => this.#pipePromise)
       .catch(() => {});
