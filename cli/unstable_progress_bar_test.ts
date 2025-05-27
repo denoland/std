8282b65d40c7d7eed703a4bcd6451e4b1c05dcb4
Tim <tim.reichen@gmail.com>
2025-05-27 09:30:00 +0200

    initial commit

--- cli/unstable_progress_bar_test.ts
+++ cli/unstable_progress_bar_test.ts
@@ -3,23 +3,12 @@
 import { assertEquals } from "@std/assert";
 import { ProgressBar } from "./unstable_progress_bar.ts";
 
-async function* getData(
-  loops: number,
-  bufferSize: number,
-): AsyncGenerator<Uint8Array> {
-  for (let i = 0; i < loops; ++i) {
-    yield new Uint8Array(bufferSize);
-    await new Promise((a) => setTimeout(a, Math.random() * 100));
-  }
-}
-
 const decoder = new TextDecoder();
 
 Deno.test("ProgressBar() outputs default result", async () => {
   const { readable, writable } = new TransformStream();
   const bar = new ProgressBar({ writable, max: 10 * 1000 });
-
-  for await (const a of getData(10, 1000)) bar.value += a.length;
+  bar.value += 10 * 1000;
   bar.stop().then(() => writable.close());
 
   const expected = [
@@ -35,21 +24,37 @@ Deno.test("ProgressBar() outputs default result", async () => {
   assertEquals(actual, expected);
 });
 
+Deno.test("ProgressBar() prints every second", async () => {
+  const { readable, writable } = new TransformStream();
+  const bar = new ProgressBar({ writable, max: 10 * 1000 });
+  await new Promise((a) => setTimeout(a, 3000));
+  bar.stop().then(() => writable.close());
+
+  const expected = [
+    "\r\x1b[K[00:00] [--------------------------------------------------] [0.00/9.77 KiB]",
+    "\r\x1b[K[00:01] [--------------------------------------------------] [0.00/9.77 KiB]",
+    "\r\x1b[K[00:02] [--------------------------------------------------] [0.00/9.77 KiB]",
+    "\r\x1b[K[00:03] [--------------------------------------------------] [0.00/9.77 KiB]",
+    "\n",
+  ];
+
+  const actual: string[] = [];
+  for await (const buffer of readable) {
+    actual.push(decoder.decode(buffer));
+  }
+  assertEquals(actual, expected);
+});
+
 Deno.test("ProgressBar() can handle a readable.cancel() correctly", async () => {
   const { readable, writable } = new TransformStream();
   const bar = new ProgressBar({ writable, max: 10 * 1000 });
-
-  for await (const a of getData(10, 1000)) bar.value += a.length;
   bar.stop();
-
   await readable.cancel();
 });
 
 Deno.test("ProgressBar() can remove itself when finished", async () => {
   const { readable, writable } = new TransformStream();
   const bar = new ProgressBar({ writable, max: 10 * 1000, clear: true });
-
-  for await (const a of getData(10, 1000)) bar.value += a.length;
   bar.stop().then(() => writable.close());
 
   const expected = [
@@ -68,11 +73,13 @@ Deno.test("ProgressBar() passes correct values to formatter", async () => {
   const { readable, writable } = new TransformStream();
   let lastTime: undefined | number = undefined;
   let lastValue: undefined | number = undefined;
+  let called = false;
   const bar = new ProgressBar({
     writable,
     max: 10 * 1000,
     keepOpen: false,
     fmt(x) {
+      called = true;
       if (lastTime != undefined) assertEquals(x.previousTime, lastTime);
       if (lastValue != undefined) assertEquals(x.previousValue, lastValue);
       lastTime = x.time;
@@ -81,9 +88,9 @@ Deno.test("ProgressBar() passes correct values to formatter", async () => {
     },
   });
 
-  for await (const a of getData(10, 1000)) bar.value += a.length;
+  bar.value += 1000;
   bar.stop();
-
+  assertEquals(called, true);
   await new Response(readable).bytes();
 });
 
