1b23eb5d5dd0e9a5419b19518ac8fce08eba75dc
lionel-rowe <lionel.rowe@gmail.com>
2025-06-29 21:20:26 +0800

    Add error cases for stubProperty

--- internal/_testing_test.ts
+++ internal/_testing_test.ts
@@ -1,70 +1,106 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
+// deno-lint-ignore-file deno-style-guide/naming-convention
 import { assertEquals } from "@std/assert/equals";
 import { stubProperty } from "./_testing.ts";
+import { assertThrows } from "@std/assert/throws";
+
+const PROP = "foo";
+const ORIGINAL_VALUE = "bar";
+const STUBBED_VALUE = "baz";
+
+type Obj = { [PROP]?: string };
 
 Deno.test("stubProperty() stubs properties and returns them to their original values", () => {
-  const prop = "foo";
-  const originalValue = "bar";
-  const stubbedValue = "baz";
-
   const descriptors: (TypedPropertyDescriptor<string> | undefined)[] = [
     undefined,
     {
-      get: () => originalValue,
+      get: () => ORIGINAL_VALUE,
       configurable: true,
     },
     {
       set: () => {},
       configurable: true,
     },
-    { value: originalValue, writable: true, configurable: true },
-    { value: originalValue, writable: true, configurable: false },
-    { value: originalValue, writable: false, configurable: true },
+    { value: ORIGINAL_VALUE, writable: true, configurable: true },
+    { value: ORIGINAL_VALUE, writable: true, configurable: false },
+    { value: ORIGINAL_VALUE, writable: false, configurable: true },
   ];
 
   const Empty = () => class Empty {};
   const Val = () =>
     class Val {
-      [prop] = originalValue;
+      [PROP] = ORIGINAL_VALUE;
     };
   const Get = () =>
     class Get {
-      get [prop]() {
-        return originalValue;
+      get [PROP]() {
+        return ORIGINAL_VALUE;
       }
     };
 
-  const objCreators: (() => { [prop]?: string })[] = [
+  const objCreators: (() => Obj)[] = [
     () => ({}),
-    () => ({ [prop]: originalValue }),
+    () => ({ [PROP]: ORIGINAL_VALUE }),
+    () => {
+      const o: Obj = {};
+      o[PROP] = ORIGINAL_VALUE;
+      return o;
+    },
     () => new (Val())(),
     () => new (Get())(),
     () => new (Empty())(),
     () => Val().prototype,
     () => Get().prototype,
     () => Empty().prototype,
-    () => Object.create({ [prop]: originalValue }),
+    () => Object.create({ [PROP]: ORIGINAL_VALUE }),
   ];
 
   for (const getObj of objCreators) {
     for (const descriptor of descriptors) {
       const obj = getObj();
-      if (descriptor != null) Object.defineProperty(obj, prop, descriptor);
+      if (descriptor != null) Object.defineProperty(obj, PROP, descriptor);
 
-      const initialDescriptor = Object.getOwnPropertyDescriptor(obj, prop);
-      const initialValue = obj[prop];
+      const initialDescriptor = Object.getOwnPropertyDescriptor(obj, PROP);
+      const initialValue = obj[PROP];
 
       {
-        using _ = stubProperty(obj, prop, stubbedValue);
-
-        assertEquals(obj[prop], stubbedValue);
+        using _ = stubProperty(obj, PROP, STUBBED_VALUE);
+        assertEquals(obj[PROP], STUBBED_VALUE);
       }
 
-      assertEquals(obj[prop], initialValue);
+      assertEquals(obj[PROP], initialValue);
       assertEquals(
-        Object.getOwnPropertyDescriptor(obj, prop),
+        Object.getOwnPropertyDescriptor(obj, PROP),
         initialDescriptor,
       );
     }
   }
+});
+
+Deno.test("stubProperty() throws if property is not configurable or writable", () => {
+  assertThrows(
+    () =>
+      stubProperty(
+        Object.defineProperty(
+          {} as Obj,
+          PROP,
+          { value: ORIGINAL_VALUE },
+        ),
+        PROP,
+        STUBBED_VALUE,
+      ),
+    TypeError,
+    'Cannot stub property "foo" because it is not configurable or writable.',
+  );
+
+  assertThrows(
+    () =>
+      stubProperty(
+        Object.freeze({ [PROP]: ORIGINAL_VALUE } as Obj),
+        PROP,
+        STUBBED_VALUE,
+      ),
+    TypeError,
+    'Cannot stub property "foo" because it is not configurable or writable.',
+  );
 });
