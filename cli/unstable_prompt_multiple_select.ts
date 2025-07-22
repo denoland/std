894fe2c7c4a97c22921a9a243982d356a25bbbc0
Leo Kettmeir <crowlkats@toaxl.com>
2025-07-22 10:47:10 +0200

    refactor(cli/unstable): dedupe logic between promptSelect and promptMultipleSelect (#6769)
    ---------
    
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>

--- cli/unstable_prompt_multiple_select.ts
+++ cli/unstable_prompt_multiple_select.ts
@@ -1,5 +1,7 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
+import { handlePromptSelect } from "./_prompt_select.ts";
+
 /** Options for {@linkcode promptMultipleSelect}. */
 export interface PromptMultipleSelectOptions {
   /** Clear the lines after the user's input. */
@@ -39,20 +41,11 @@ const ARROW_DOWN = "\u001B[B";
 const CR = "\r";
 
 const DELETE = "\u007F";
-const MORE_CONTENT_BEFORE_INDICATOR = "...";
-const MORE_CONTENT_AFTER_INDICATOR = "...";
 
 const CHECKED = "◉";
 const UNCHECKED = "◯";
 
 const input = Deno.stdin;
-const output = Deno.stdout;
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
-
-const CLEAR_ALL = encoder.encode("\x1b[J"); // Clear all lines after cursor
-const HIDE_CURSOR = encoder.encode("\x1b[?25l");
-const SHOW_CURSOR = encoder.encode("\x1b[?25h");
 
 /**
  * Shows the given message and waits for the user's input. Returns the user's selected value as string.
@@ -114,150 +107,57 @@ export function promptMultipleSelect<V = undefined>(
 ): PromptEntry<V>[] | null {
   if (!input.isTerminal()) return null;
 
-  const SAFE_PADDING = 4;
-  let {
-    // Deno.consoleSize().rows - 3 because we need to output the message, the up arrow, the terminal line and the down arrow
-    visibleLines = Math.min(
-      Deno.consoleSize().rows - SAFE_PADDING,
-      values.length,
-    ),
-    indicator = "❯",
-  } = options;
-  const PADDING = " ".repeat(indicator.length);
-  const ARROW_PADDING = " ".repeat(indicator.length + 1);
-
-  const indexedValues = values.map((value, absoluteIndex) => ({
-    value,
-    absoluteIndex,
-  }));
-  let activeIndex = 0;
   const selectedAbsoluteIndexes = new Set<number>();
-  let offset = 0;
-  let clearLength = values.length + 1;
 
-  input.setRaw(true);
-  output.writeSync(HIDE_CURSOR);
-
-  const buffer = new Uint8Array(4);
-
-  let searchBuffer = "";
-
-  loop:
-  while (true) {
-    output.writeSync(
-      encoder.encode(
-        `${message + (searchBuffer ? ` (filter: ${searchBuffer})` : "")}\r\n`,
-      ),
-    );
-    const filteredChunks = indexedValues.filter((item) => {
-      if (searchBuffer === "") {
-        return true;
-      } else {
-        return (typeof item.value === "string" ? item.value : item.value.label)
-          .toLowerCase().includes(searchBuffer.toLowerCase());
-      }
-    });
-    const visibleChunks = filteredChunks.slice(offset, visibleLines + offset);
-    const length = visibleChunks.length;
-
-    const hasUpArrow = offset !== 0;
-    const hasDownArrow = (length + offset) < filteredChunks.length;
-
-    if (hasUpArrow) {
-      output.writeSync(
-        encoder.encode(`${ARROW_PADDING}${MORE_CONTENT_BEFORE_INDICATOR}\r\n`),
-      );
-    }
-
-    for (const [index, { absoluteIndex, value }] of visibleChunks.entries()) {
-      const start = index === (activeIndex - offset) ? indicator : PADDING;
+  handlePromptSelect(
+    message,
+    options.indicator ?? "❯",
+    values,
+    options.clear,
+    options.visibleLines,
+    (_active, absoluteIndex) => {
       const checked = selectedAbsoluteIndexes.has(absoluteIndex);
-      const state = checked ? CHECKED : UNCHECKED;
-      output.writeSync(
-        encoder.encode(
-          `${start} ${state} ${
-            typeof value === "string" ? value : value.label
-          }\r\n`,
-        ),
-      );
-    }
-
-    if (hasDownArrow) {
-      output.writeSync(
-        encoder.encode(`${ARROW_PADDING}${MORE_CONTENT_AFTER_INDICATOR}\r\n`),
-      );
-    }
-    const n = input.readSync(buffer);
-    if (n === null || n === 0) break;
-    const string = decoder.decode(buffer.slice(0, n));
-
-    switch (string) {
-      case ETX:
-        output.writeSync(SHOW_CURSOR);
-        return Deno.exit(0);
-      case ARROW_UP:
-        if (activeIndex === 0) {
-          activeIndex = filteredChunks.length - 1;
-          offset = Math.max(filteredChunks.length - visibleLines, 0);
-        } else {
-          activeIndex--;
-          offset = Math.max(offset - 1, 0);
-        }
-        break;
-      case ARROW_DOWN:
-        if (activeIndex === (filteredChunks.length - 1)) {
-          activeIndex = 0;
-          offset = 0;
-        } else {
-          activeIndex++;
-
-          if (activeIndex >= visibleLines) {
-            offset++;
+      return checked ? CHECKED : UNCHECKED;
+    },
+    (str, absoluteIndex, {
+      etx,
+      up,
+      down,
+      remove,
+      inputStr,
+    }) => {
+      switch (str) {
+        case ETX:
+          return etx();
+        case ARROW_UP:
+          up();
+          break;
+        case ARROW_DOWN:
+          down();
+          break;
+        case CR:
+          return true;
+        case " ": {
+          if (absoluteIndex !== undefined) {
+            if (selectedAbsoluteIndexes.has(absoluteIndex)) {
+              selectedAbsoluteIndexes.delete(absoluteIndex);
+            } else {
+              selectedAbsoluteIndexes.add(absoluteIndex);
+            }
           }
+          break;
         }
-        break;
-      case CR:
-        break loop;
-      case " ": {
-        const absoluteIndex = filteredChunks[activeIndex]!.absoluteIndex;
-        if (selectedAbsoluteIndexes.has(absoluteIndex)) {
-          selectedAbsoluteIndexes.delete(absoluteIndex);
-        } else {
-          selectedAbsoluteIndexes.add(absoluteIndex);
-        }
-        break;
+        case DELETE:
+          remove();
+          break;
+        default:
+          inputStr();
+          break;
       }
-      case DELETE:
-        activeIndex = 0;
-        searchBuffer = searchBuffer.slice(0, -1);
-        break;
-      default:
-        activeIndex = 0;
-        searchBuffer += string;
-        break;
-    }
 
-    visibleLines = Math.min(
-      Deno.consoleSize().rows - SAFE_PADDING,
-      visibleLines,
-    );
-
-    clearLength = 1 + // message
-      (hasUpArrow ? 1 : 0) +
-      length +
-      (hasDownArrow ? 1 : 0);
-
-    output.writeSync(encoder.encode(`\x1b[${clearLength}A`));
-    output.writeSync(CLEAR_ALL);
-  }
-
-  if (options.clear) {
-    output.writeSync(encoder.encode(`\x1b[${clearLength}A`));
-    output.writeSync(CLEAR_ALL);
-  }
-
-  output.writeSync(SHOW_CURSOR);
-  input.setRaw(false);
+      return false;
+    },
+  );
 
   return [...selectedAbsoluteIndexes].map((it) => values[it]!);
 }
--- cli/unstable_prompt_select.ts
+++ cli/unstable_prompt_select.ts
@@ -1,5 +1,7 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
+import { handlePromptSelect } from "./_prompt_select.ts";
+
 /** Options for {@linkcode promptSelect}. */
 export interface PromptSelectOptions {
   /** Clear the lines after the user's input. */
@@ -39,17 +41,7 @@ const ARROW_DOWN = "\u001B[B";
 const CR = "\r";
 const DELETE = "\u007F";
 
-const MORE_CONTENT_BEFORE_INDICATOR = "...";
-const MORE_CONTENT_AFTER_INDICATOR = "...";
-
 const input = Deno.stdin;
-const output = Deno.stdout;
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
-
-const CLEAR_ALL = encoder.encode("\x1b[J"); // Clear all lines after cursor
-const HIDE_CURSOR = encoder.encode("\x1b[?25l");
-const SHOW_CURSOR = encoder.encode("\x1b[?25h");
 
 /**
  * Shows the given message and waits for the user's input. Returns the user's selected value as string.
@@ -114,142 +106,49 @@ export function promptSelect<V = undefined>(
 ): PromptEntry<V> | null {
   if (!input.isTerminal()) return null;
 
-  const SAFE_PADDING = 4;
-  let {
-    // Deno.consoleSize().rows - 3 because we need to output the message, the up arrow, the terminal line and the down arrow
-    visibleLines = Math.min(
-      Deno.consoleSize().rows - SAFE_PADDING,
-      values.length,
-    ),
-    indicator = "❯",
-  } = options;
-  const PADDING = " ".repeat(indicator.length);
-  const ARROW_PADDING = " ".repeat(indicator.length + 1);
-
-  const indexedValues = values.map((value, absoluteIndex) => ({
-    value,
-    absoluteIndex,
-  }));
-  let activeIndex = 0;
   let selectedIndex = 0;
-  let offset = 0;
-  let clearLength = values.length + 1;
 
-  input.setRaw(true);
-  output.writeSync(HIDE_CURSOR);
-
-  const buffer = new Uint8Array(4);
-
-  let searchBuffer = "";
-
-  loop:
-  while (true) {
-    output.writeSync(
-      encoder.encode(
-        `${message + (searchBuffer ? ` (filter: ${searchBuffer})` : "")}\r\n`,
-      ),
-    );
-    const filteredChunks = indexedValues.filter((item) => {
-      if (searchBuffer === "") {
-        return true;
-      } else {
-        return (typeof item.value === "string" ? item.value : item.value.label)
-          .toLowerCase().includes(searchBuffer.toLowerCase());
-      }
-    });
-    const visibleChunks = filteredChunks.slice(offset, visibleLines + offset);
-    const length = visibleChunks.length;
-
-    const hasUpArrow = offset !== 0;
-    const hasDownArrow = (length + offset) < filteredChunks.length;
-
-    if (hasUpArrow) {
-      output.writeSync(
-        encoder.encode(`${ARROW_PADDING}${MORE_CONTENT_BEFORE_INDICATOR}\r\n`),
-      );
-    }
-
-    for (const [index, { absoluteIndex, value }] of visibleChunks.entries()) {
-      const active = index === (activeIndex - offset);
-      const start = active ? indicator : PADDING;
+  handlePromptSelect(
+    message,
+    options.indicator ?? "❯",
+    values,
+    options.clear,
+    options.visibleLines,
+    (active, absoluteIndex) => {
       if (active) {
         selectedIndex = absoluteIndex;
       }
-      output.writeSync(
-        encoder.encode(
-          `${start} ${typeof value === "string" ? value : value.label}\r\n`,
-        ),
-      );
-    }
+    },
+    (str, _absoluteIndex, {
+      etx,
+      up,
+      down,
+      remove,
+      inputStr,
+    }) => {
+      switch (str) {
+        case ETX:
+          return etx();
+        case ARROW_UP:
+          up();
+          break;
+        case ARROW_DOWN:
+          down();
+          break;
+        case CR:
+        case " ":
+          return true;
+        case DELETE:
+          remove();
+          break;
+        default:
+          inputStr();
+          break;
+      }
 
-    if (hasDownArrow) {
-      output.writeSync(
-        encoder.encode(`${ARROW_PADDING}${MORE_CONTENT_AFTER_INDICATOR}\r\n`),
-      );
-    }
-    const n = input.readSync(buffer);
-    if (n === null || n === 0) break;
-    const string = decoder.decode(buffer.slice(0, n));
-
-    switch (string) {
-      case ETX:
-        output.writeSync(SHOW_CURSOR);
-        return Deno.exit(0);
-      case ARROW_UP:
-        if (activeIndex === 0) {
-          activeIndex = filteredChunks.length - 1;
-          offset = Math.max(filteredChunks.length - visibleLines, 0);
-        } else {
-          activeIndex--;
-          offset = Math.max(offset - 1, 0);
-        }
-        break;
-      case ARROW_DOWN:
-        if (activeIndex === (filteredChunks.length - 1)) {
-          activeIndex = 0;
-          offset = 0;
-        } else {
-          activeIndex++;
-
-          if (activeIndex >= visibleLines) {
-            offset++;
-          }
-        }
-        break;
-      case CR:
-      case " ":
-        break loop;
-      case DELETE:
-        activeIndex = 0;
-        searchBuffer = searchBuffer.slice(0, -1);
-        break;
-      default:
-        activeIndex = 0;
-        searchBuffer += string;
-        break;
-    }
-
-    visibleLines = Math.min(
-      Deno.consoleSize().rows - SAFE_PADDING,
-      visibleLines,
-    );
-
-    clearLength = 1 + // message
-      (hasUpArrow ? 1 : 0) +
-      length +
-      (hasDownArrow ? 1 : 0);
-
-    output.writeSync(encoder.encode(`\x1b[${clearLength}A`));
-    output.writeSync(CLEAR_ALL);
-  }
-
-  if (options.clear) {
-    output.writeSync(encoder.encode(`\x1b[${clearLength}A`));
-    output.writeSync(CLEAR_ALL);
-  }
-
-  output.writeSync(SHOW_CURSOR);
-  input.setRaw(false);
+      return false;
+    },
+  );
 
   return values[selectedIndex] ?? null;
 }
