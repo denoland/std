3da0c4e0b7d0d63a4b904aa093954d7264a1d9e1
Yoshiya Hinosawa <stibium121@gmail.com>
2025-08-12 14:40:15 +0900

    modify test name

--- path/is_glob_test.ts
+++ path/is_glob_test.ts
@@ -110,7 +110,7 @@ Deno.test({
 });
 
 // ref https://github.com/denoland/std/pull/6764
-Deno.test("isGlob works the input with large number of open brackets", async () => {
+Deno.test("isGlob works with the input what includes large number of open brackets", async () => {
   const { promise, resolve, reject } = Promise.withResolvers<void>();
   const timer = setTimeout(() => {
     reject(new Error("isGlob() did not finish in time"));
