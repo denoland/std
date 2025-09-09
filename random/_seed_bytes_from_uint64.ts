7f7c8f8455923530b5b2eaac823901d3f0127272
Yoshiya Hinosawa <stibium121@gmail.com>
2025-09-09 15:10:25 +0900

    fix browser type checking

--- random/_seed_bytes_from_uint64.ts
+++ random/_seed_bytes_from_uint64.ts
@@ -1,5 +1,6 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 import { Pcg32 } from "./_pcg32.ts";
+import type { Uint8Array_ } from "./_types.ts";
 
 /** Initial increment for the PCG32 algorithm. Only used during seeding. */
 const INITIAL_INCREMENT = 11634580027462260723n;
@@ -10,8 +11,8 @@ const INITIAL_INCREMENT = 11634580027462260723n;
  */
 export function seedBytesFromUint64(
   u64: bigint,
-  bytes: Uint8Array,
-): Uint8Array {
+  bytes: Uint8Array_,
+): Uint8Array_ {
   return new Pcg32({ state: u64, increment: INITIAL_INCREMENT })
     // We advance the state first (to get away from the input value,
     // in case it has low Hamming Weight).
