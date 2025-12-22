7bb3552f66090bf967500411a7f7818f321ae770
Doctor <44320105+BlackAsLight@users.noreply.github.com>
2025-12-22 11:40:25 +1100

    feat(streams/unstable): new CappedDelimiterStream() (#6890)

---
+++ streams/unstable_capped_delimiter_stream_test.ts
@@ -0,0 +1,52 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
+
+import { assertEquals } from "@std/assert";
+import { CappedDelimiterStream } from "./unstable_capped_delimiter_stream.ts";
+
+Deno.test("CappedDelimiterStream matching", async function () {
+  const input = Uint8Array.from(
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 5, 5, 5, 5, 5, 2, 3, 3, 4, 6, 0, 0, 2, 3, 3],
+  );
+  const expected = [
+    { match: true, value: Uint8Array.from([]) },
+    { match: true, value: Uint8Array.from([1, 1, 1]) },
+    { match: true, value: Uint8Array.from([]) },
+    { match: false, value: Uint8Array.from([5, 5, 5, 5, 5]) },
+    { match: true, value: Uint8Array.from([2, 3, 3, 4, 6]) },
+    { match: false, value: Uint8Array.from([2, 3, 3]) },
+  ];
+
+  const actual = await Array.fromAsync(
+    ReadableStream
+      .from([input])
+      .pipeThrough(
+        new CappedDelimiterStream({ delimiter: new Uint8Array(2), limit: 5 }),
+      ),
+  );
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("CappedDelimiterStream splitting", async function () {
+  const input = Uint8Array.from(
+    [1, 1, 1, 0, 0, 0, 0, 5, 5, 5, 5, 5, 2, 3, 3, 4, 6, 0, 0, 2, 0, 0, 0, 0],
+  );
+  const expected = [
+    { match: true, value: Uint8Array.from([1, 1, 1]) },
+    { match: true, value: Uint8Array.from([]) },
+    { match: false, value: Uint8Array.from([5, 5, 5, 5, 5]) },
+    { match: true, value: Uint8Array.from([2, 3, 3, 4, 6]) },
+    { match: true, value: Uint8Array.from([2]) },
+    { match: true, value: Uint8Array.from([]) },
+  ];
+
+  const actual = await Array.fromAsync(
+    ReadableStream
+      .from([input])
+      .pipeThrough(
+        new CappedDelimiterStream({ delimiter: new Uint8Array(2), limit: 5 }),
+      ),
+  );
+
+  assertEquals(actual, expected);
+});
