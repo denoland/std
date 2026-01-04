8a88a98b81729c8b2d34362f4f8620284b920f83
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2026-01-04 12:34:04 +0100

    fix(collections/partition): correct overload ordering for type-guard

--- collections/partition.ts
+++ collections/partition.ts
@@ -6,36 +6,6 @@
  * the given array that match the given predicate and the second one containing
  * all that do not.
  *
- * @typeParam T The type of the elements in the array.
- *
- * @param array The array to partition.
- * @param predicate The predicate function to determine which array an element
- * belongs to.
- *
- * @returns A tuple of two arrays. The first array contains all elements that
- * match the predicate, the second contains all elements that do not.
- *
- * @example Basic usage
- * ```ts
- * import { partition } from "@std/collections/partition";
- * import { assertEquals } from "@std/assert";
- *
- * const numbers = [5, 6, 7, 8, 9];
- * const [even, odd] = partition(numbers, (it) => it % 2 === 0);
- *
- * assertEquals(even, [6, 8]);
- * assertEquals(odd, [5, 7, 9]);
- * ```
- */
-export function partition<T>(
-  array: Iterable<T>,
-  predicate: (el: T) => boolean,
-): [T[], T[]];
-/**
- * Returns a tuple of two arrays with the first one containing all elements in
- * the given array that match the given predicate and the second one containing
- * all that do not.
- *
  * This version of the function is a type-guard version of the function. It
  * allows you to specify a type-guard predicate function that narrows the type
  * of the elements in the array.
@@ -66,6 +36,36 @@ export function partition<T, U extends T>(
   array: Iterable<T>,
   predicate: (el: T) => el is U,
 ): [U[], Exclude<T, U>[]];
+/**
+ * Returns a tuple of two arrays with the first one containing all elements in
+ * the given array that match the given predicate and the second one containing
+ * all that do not.
+ *
+ * @typeParam T The type of the elements in the array.
+ *
+ * @param array The array to partition.
+ * @param predicate The predicate function to determine which array an element
+ * belongs to.
+ *
+ * @returns A tuple of two arrays. The first array contains all elements that
+ * match the predicate, the second contains all elements that do not.
+ *
+ * @example Basic usage
+ * ```ts
+ * import { partition } from "@std/collections/partition";
+ * import { assertEquals } from "@std/assert";
+ *
+ * const numbers = [5, 6, 7, 8, 9];
+ * const [even, odd] = partition(numbers, (it) => it % 2 === 0);
+ *
+ * assertEquals(even, [6, 8]);
+ * assertEquals(odd, [5, 7, 9]);
+ * ```
+ */
+export function partition<T>(
+  array: Iterable<T>,
+  predicate: (el: T) => boolean,
+): [T[], T[]];
 export function partition(
   array: Iterable<unknown>,
   predicate: (el: unknown) => boolean,
