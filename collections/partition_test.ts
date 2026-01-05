4b413cfbb888b9fef53aefe91b8433510857d21e
Tomas Zijdemans <113360400+tomas-zijdemans@users.noreply.github.com>
2026-01-05 08:44:59 +0100

    fix(collections): correct overload ordering (#6930)

--- collections/partition_test.ts
+++ collections/partition_test.ts
@@ -73,3 +73,42 @@ Deno.test({
     );
   },
 });
+
+Deno.test({
+  name: "partition() handles type guard predicates",
+  fn() {
+    const mixed: (string | number)[] = [1, "a", 2, "b"];
+    const isString = (x: string | number): x is string => typeof x === "string";
+    const [strings, numbers] = partition(mixed, isString);
+
+    assertEquals(strings, ["a", "b"]);
+    assertEquals(numbers, [1, 2]);
+  },
+});
+
+Deno.test({
+  name: "partition() handles generators",
+  fn() {
+    function* gen() {
+      yield 1;
+      yield 2;
+      yield 3;
+      yield 4;
+    }
+    const [even, odd] = partition(gen(), (x) => x % 2 === 0);
+
+    assertEquals(even, [2, 4]);
+    assertEquals(odd, [1, 3]);
+  },
+});
+
+Deno.test({
+  name: "partition() handles Sets",
+  fn() {
+    const set = new Set([1, 2, 3, 4, 5]);
+    const [even, odd] = partition(set, (x) => x % 2 === 0);
+
+    assertEquals(even, [2, 4]);
+    assertEquals(odd, [1, 3, 5]);
+  },
+});
