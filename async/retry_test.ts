50aae314cf5d09d2d23e14872b91be5fad7cdcbc
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2026-01-09 09:43:58 +0100

    feat(async): stabilize isRetriable option for retry

--- async/retry_test.ts
+++ async/retry_test.ts
@@ -336,3 +336,46 @@ Deno.test("retry() caps backoff at maxTimeout", async () => {
 
   await assertRejects(() => promise, RetryError);
 });
+
+Deno.test("retry() only retries errors that are retriable with `isRetriable` option", async () => {
+  class HttpError extends Error {
+    status: number;
+    constructor(status: number) {
+      super();
+      this.status = status;
+    }
+  }
+
+  const isRetriable = (err: unknown) =>
+    err instanceof HttpError && (err.status === 429 || err.status >= 500);
+
+  const options = {
+    minTimeout: 1,
+    isRetriable,
+  };
+
+  let numCalls: number;
+
+  numCalls = 0;
+  await assertRejects(() =>
+    retry(() => {
+      numCalls++;
+      throw new HttpError(400);
+    }, options), HttpError);
+  assertEquals(numCalls, 1);
+
+  numCalls = 0;
+  await assertRejects(() =>
+    retry(() => {
+      numCalls++;
+      throw new HttpError(500);
+    }, options), RetryError);
+  assertEquals(numCalls, 5);
+
+  numCalls = 0;
+  await assertRejects(() =>
+    retry(() => {
+      throw new HttpError(++numCalls === 3 ? 400 : 500);
+    }, options), HttpError);
+  assertEquals(numCalls, 3);
+});
