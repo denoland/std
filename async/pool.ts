c2f77fe136c9d796a98154522fe43758523b8e03
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2026-02-22 13:38:40 +0100

    doc

--- async/pool.ts
+++ async/pool.ts
@@ -32,9 +32,11 @@ const ERROR_WHILE_MAPPING_MESSAGE =
  * @typeParam T the input type.
  * @typeParam R the output type.
  * @param poolLimit The maximum count of items being processed concurrently.
+ * Must be a positive integer.
  * @param array The input array for mapping.
  * @param iteratorFn The function to call for every item of the array.
  * @returns The async iterator with the transformed values.
+ * @throws {RangeError} If `poolLimit` is not a positive integer.
  */
 export function pooledMap<T, R>(
   poolLimit: number,
