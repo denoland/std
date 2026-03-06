b9f522dfa8d550c174334bd2c389ede1ebee3935
Yoshiya Hinosawa <stibium121@gmail.com>
2026-03-06 14:12:31 +0900

    copyright

--- cli/unstable_get_cursor.ts
+++ cli/unstable_get_cursor.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2025 the Deno authors. MIT license.
+// Copyright 2018-2026 the Deno authors. MIT license.
 
 /**
  * This function asks the terminal for the cursor's current position.
