b1ad71afe535177a35184b323fe42c391521861a
uynilo9 <uynilo9@outlook.com>
2026-03-25 23:20:58 +0800

    fix: remove duplicate function export

--- front_matter/unstable_json.ts
+++ front_matter/unstable_json.ts
@@ -1,44 +1,7 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 // This module is browser compatible.
 
-import { extractFrontMatter } from "./_shared.ts";
-import { EXTRACT_JSON_REGEXP } from "./_formats.ts";
-import type { Extract } from "./types.ts";
 import { test as _test } from "./test.ts";
-
-export type { Extract };
-
-/**
- * Extracts and parses {@link https://www.json.org/ | JSON } from the metadata
- * of front matter content.
- *
- * @example Extract JSON front matter
- * ```ts
- * import { extract } from "@std/front-matter/json";
- * import { assertEquals } from "@std/assert";
- *
- * const output = `---json
- * { "title": "Three dashes marks the spot" }
- * ---
- * Hello, world!`;
- * const result = extract(output);
- *
- * assertEquals(result, {
- *   frontMatter: '{ "title": "Three dashes marks the spot" }',
- *   body: "Hello, world!",
- *   attrs: { title: "Three dashes marks the spot" },
- * });
- * ```
- *
- * @typeParam T The type of the parsed front matter.
- * @param text The text to extract JSON front matter from.
- * @returns The extracted JSON front matter and body content.
- */
-export function extract<T>(text: string): Extract<T> {
-  const { frontMatter, body } = extractFrontMatter(text, EXTRACT_JSON_REGEXP);
-  const attrs = (frontMatter ? JSON.parse(frontMatter) : {}) as T;
-  return { frontMatter, body, attrs };
-}
 
 /**
  * Tests if a string has valid front matter.
--- front_matter/unstable_toml.ts
+++ front_matter/unstable_toml.ts
@@ -1,46 +1,7 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 // This module is browser compatible.
 
-import { extractFrontMatter } from "./_shared.ts";
-import { parse } from "@std/toml/parse";
-import type { Extract } from "./types.ts";
-import { EXTRACT_TOML_REGEXP } from "./_formats.ts";
 import { test as _test } from "./test.ts";
-
-export type { Extract };
-
-/**
- * Extracts and parses {@link https://toml.io | TOML} from the metadata of
- * front matter content.
- *
- * @example Extract TOML front matter
- * ```ts
- * import { extract } from "@std/front-matter/toml";
- * import { assertEquals } from "@std/assert";
- *
- * const output = `---toml
- * title = "Three dashes marks the spot"
- * ---
- * Hello, world!`;
- * const result = extract(output);
- *
- * assertEquals(result, {
- *   frontMatter: 'title = "Three dashes marks the spot"',
- *   body: "Hello, world!",
- *   attrs: { title: "Three dashes marks the spot" },
- * });
- * ```
- *
- * @typeParam T The type of the parsed front matter.
- * @param text The text to extract TOML front matter from.
- * @returns The extracted TOML front matter and body content.
- */
-export function extract<T>(text: string): Extract<T> {
-  const { frontMatter, body } = extractFrontMatter(text, EXTRACT_TOML_REGEXP);
-
-  const attrs = (frontMatter ? parse(frontMatter) : {}) as T;
-  return { frontMatter, body, attrs };
-}
 
 /**
  * Tests if a string has valid TOML front matter.
