c74d4244956a1f5edeb26f53f6c085f8ff3871f2
Tomas Zijdemans <113360400+tomas-zijdemans@users.noreply.github.com>
2026-03-25 15:39:26 +0100

    feat(streams): stabilize `toBytes` (#7043)

--- encoding/unstable_base64_stream_test.ts
+++ encoding/unstable_base64_stream_test.ts
@@ -2,7 +2,7 @@
 
 import { assertEquals } from "@std/assert";
 import { toText } from "@std/streams";
-import { toBytes } from "@std/streams/unstable-to-bytes";
+import { toBytes } from "@std/streams/to-bytes";
 import { FixedChunkStream } from "@std/streams/unstable-fixed-chunk-stream";
 import { encodeBase64 } from "./unstable_base64.ts";
 import {
